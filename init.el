834eeb6ba1ea37a5c1d34db9dfe3ea68ad85d188
stardiviner <numbchild@gmail.com>
2014-03-31 04:11:32 +0800

    add el-get: use el-get to manage emacs packages

---
+++ init.el
@@ -0,0 +1,30 @@
+;;; el-get
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+(setq el-get-verbose t)
+
+;;; extra sources which not in el-get recipes.
+(setq el-get-extra-sources
+      '(
+	;; (:name pretty-symbols
+        ;;        :description "Emacs minor mode for drawing multi-character tokens as Unicode glyphs (lambda -> λ). A configurable replacement for pretty and the like."
+        ;;        :type git
+        ;;        :url "https://github.com/drothlis/pretty-symbols")
+	))
+
+;;; my packages which will be installed.
+(setq my-el-get-packages
+      (append
+        '(
+           )
+        (mapcar 'el-get-source-name el-get-extra-sources)))
+
+(el-get 'sync my-el-get-packages)
