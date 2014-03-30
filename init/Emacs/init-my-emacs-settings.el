0e1fc4f8a143790a1415517a1c6ae1727d2da3f9
stardiviner <numbchild@gmail.com>
2014-03-31 05:41:51 +0800

    append some basic Emacs settings

--- init/Emacs/init-my-emacs-settings.el
+++ init/Emacs/init-my-emacs-settings.el
@@ -9,6 +9,22 @@
 ;; TODO uncomment this when you are very familiar with Emacs.
 ;; (setq disabled-command-function nil)
 
+
+(fset 'yes-or-no-p 'y-or-n-p) ; treat 'y' as yes, 'n' as no.
+
+(setq inhibit-startup-message t) ; no splash screen
+
+
+;;; [ Time ]
+(setq system-time-locale "C") ; make timestamps in org-mode appear in English.
+;;; time-stamp
+;; (add-hook 'before-save-hook 'time-stamp)
+;; (setq time-stamp-pattern nil)
+
+
+;; [ Bell ]
+(setq visible-bell t)
+
 
 
 
