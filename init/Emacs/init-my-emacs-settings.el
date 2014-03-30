48b1c5c71b7ab0e9fcf40e54085cce5aa770502a
stardiviner <numbchild@gmail.com>
2014-03-31 05:45:00 +0800

    let Emacs use auto-save and saveplace to apply session

--- init/Emacs/init-my-emacs-settings.el
+++ init/Emacs/init-my-emacs-settings.el
@@ -35,6 +35,20 @@
 ;;; [ mode ]
 (setq default-major-mode 'org-mode) ; use org-mode for any unspecified mode.
 
+
+;;; [ session ]
+
+;;; [ auto save ]
+;; (auto-save-mode t)
+(setq auto-save-default t               ; create #autosave# files
+      auto-save-list-file-prefix "~/.emacs.d/auto-save-list/.saves-"
+      auto-save-interval 200)
+
+;; places
+(require 'saveplace)
+(setq save-place t                      ; save point place
+      save-place-file "~/.emacs.d/.emacs-places")
+
 
 
 
