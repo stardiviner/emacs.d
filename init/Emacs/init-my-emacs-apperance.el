c035ac2dca196df28a16cb826ed7609634064dca
stardiviner <numbchild@gmail.com>
2014-04-01 07:26:21 +0800

    add init Emacs apperance files

---
+++ init/Emacs/init-my-emacs-apperance.el
@@ -0,0 +1,320 @@
+;;; init-my-emacs-apperance.el --- my Emacs apperance init
+
+;;; Commentary:
+
+
+;;; Code:
+
+;;; [ Title ]
+(setq frame-title-format "Emacs λ %b")
+
+
+;;; [ Tool Bar ]
+(tool-bar-mode -1)
+
+
+;;; [ Menu Bar ]
+;; the menu bar is mostly useless as well
+;; but removing it under OS X doesn't make much sense
+(if (eq system-type 'darwin)
+    ;; (string-match "apple-darwin" system-configuration)
+    (with-selected-frame 'frame
+      (if (display-graphic-p)
+          (modify-frame-parameters 'frame '((menu-bar-lines . 1)))
+        (modify-frame-parameters 'frame '((menu-bar-lines . 0)))))
+  (menu-bar-mode -1))
+
+(setq-default imenu-auto-rescan t)
+
+
+;;; [ Scroll Bar ]
+(scroll-bar-mode -1)
+
+;; smooth scroll
+(setq scroll-margin 10
+      scroll-conservatively 100000
+      scroll-preserve-screen-position 1)
+
+
+;;; [ modeline ]
+
+(require 'init-my-emacs-modeline)
+
+;; (require 'init-my-emacs-powerline)
+
+
+;;; [ echo area ]
+(setq echo-keystrokes 0.1)
+
+
+;;; [ Frame ]
+
+;;; full screen
+;; - [F11] -- fullscreen.
+;; - [M-F10] -- max window.
+
+
+;;; [ Layout ]
+
+;; - (info "(elisp) Layout Parameters")
+
+
+;;; [ Window ]
+(setq window-min-height 4
+      window-min-width 10
+      )
+
+
+;;; [ line space(spacing) / line height ]
+;; - (info "(elisp) Line Height")
+;; - (info "(elisp) Layout Parameters")
+;; The total height of each display line consists of the height of the
+;; contents of the line, plus optional additional vertical line spacing
+;; above or below the display line.
+
+(setq-default line-spacing 0.1) ; additional space to put between lines.
+
+
+;;; [ line number ]
+(line-number-mode 1)
+(column-number-mode 1)
+
+;; display line numbers in margin
+;; (global-linum-mode 1) ; disable linum-mode because I display line number in mode line.
+;; Linum: separating line numbers from text
+;; (setq linum-format "%d ") ; 'dynamic
+(setq linum-format "%4d \u2502") ; a solid line separator
+;; TODO combine 'dynamic result with \u2502
+;; (setq linum-format '(combine 'dynamic "\u2502"))
+
+
+;;; [ current line & column ]
+;; highlight current line
+(global-hl-line-mode 1) ; highlight current line
+;; disable soft wrap lines for windows which has smaller width than 80.
+(global-visual-line-mode -1) ; soft wrap lines at word boundary
+(set-face-attribute 'hl-line nil
+                    ;; :foreground nil
+                    :background "#004A5D"
+                    ;; :box '(:color "cyan" :line-width 1 :style nil) :underline nil
+                    ;; :underline "cyan" :box nil
+                    )
+(setq hl-line-face 'hl-line)
+
+
+;;; [ point & cursor ]
+(setq mouse-avoidance-mode 'animate) ;; auto move mouse away when cursor is at mouse position
+(blink-cursor-mode 1)
+(set-cursor-color "cyan")
+(setq-default cursor-type '(bar . 3) ; '(hbar. 3), '(bar . 3), '(box . 2). '(hollow . 2)
+              cursor-in-non-selected-windows t)
+(setq mouse-yank-at-point t) ; yank at point position instead of mouse position
+
+
+;;; change cursor color dynamically
+
+
+
+;;; [ Selection ]
+;; transpose (for region mark)
+(setq transient-mark-mode t)
+;; (transient-mark-mode 1) ; highlight text selection
+;; (delete-selection-mode 1) ; delete selected text when typing
+
+
+;;; [ wrap & fill ]
+;; default column length (80)
+(setq-default fill-column 80)
+
+
+;;; [ auto-fill-mode ] -- auto fill paragraphs like hitting [M-q].
+;; (auto-fill-mode t)
+;;; enable only for text writing modes.
+(dolist (hook
+         '(text-mode-hook
+           org-mode-hook))
+  (add-hook hook 'turn-on-auto-fill))
+;;; auto fill comments but not code in programming modes:
+(dolist (hook
+         '(prog-mode-hook))
+  (add-hook hook (lambda ()
+                   ;; fill-mode default implement
+                   (setq comment-auto-fill-only-comments t)
+                   ;; -- manually --
+                   ;; (auto-fill-mode 1)
+                   ;; (set (make-local-variable 'fill-nobreak-predicate)
+                   ;;      (lambda ()
+                   ;;        (not (eq (get-text-property (point) 'face)
+                   ;;               'font-lock-comment-face))))
+                   )))
+
+
+;;; { refill mode }
+;; This mode does automatic refilling, all the time.
+;; Basically, it hits M-q automatically after changes to the buffer that might
+;; normally trigger auto-filling. This prevents you from ever writing anything
+;; other than perfectly filled paragraphs in your buffers. If you need to write
+;; a table or anything else non-filled, you need to disable Refill mode
+;; temporarily.
+;; (global-set-key (kbd "C-c q") 'refill-mode)
+;; types of fill
+;; run those commands on paragraphs, buffer, region etc.
+;; - [M-x set-justification-full]
+;; - [M-x set-justification-left]
+;; - [M-x set-justification-center]
+;; - [M-x set-justification-right]
+;; - [M-x set-justification-none]
+
+
+;;; [ Color Theme ]
+;; Usage:
+;; - [M-x customize-face] -- to custom current point face color/style.
+;; - [C-u M-x list-faces-display RET org] -- overview of all the faces in org-mode.
+;; - [M-x customize-group org-font-lock] -- custom org-faces and other aspects of org-apperance.
+;; - [C-u C-x =] -- verbose information about the properties of the text under the point.
+;; - [M-x load-theme RET (theme)] -- load a color-theme.
+
+;;; initialize color-theme
+(require 'color-theme)
+(eval-after-load "color-theme"
+  '(progn
+     (color-theme-initialize)))
+(setq color-theme-is-global t)
+
+;; load theme way
+(add-to-list 'custom-theme-load-path "~/.emacs.d/color-themes/")
+;; (load-theme 'color-theme-midnight)
+
+
+;;; color-theme-solarized
+
+(require 'color-theme-solarized)
+
+(color-theme-solarized-dark)
+(setq solarized-termcolors 256
+      ;; solarized-degrade t
+      solarized-bold t
+      solarized-underline t
+      solarized-italic t
+      solarized-contrast 'normal ; 'normal, 'hight, 'low
+      solarized-visibility 'high ; 'normal, 'high, 'low
+      ;; solarized-broken-srgb nil    ; nil, t
+      )
+
+
+;;; [ Faces ]
+;; italic & bold
+(set-face-attribute 'italic nil
+                    :slant 'italic
+                    :underline nil
+                    :foreground "white")
+(set-face-attribute 'bold nil
+                    :weight 'bold
+                    :underline nil
+                    :foreground "white")
+;; region
+(set-face-attribute 'region nil
+                    :inverse-video nil
+                    :foreground nil
+                    :background "#222222"
+                    )
+;; search
+(set-face-attribute 'isearch nil
+                    :background "gray"
+                    :foreground "black"
+                    :weight 'bold
+                    :box '(:color "yellow" :line-width 1 :style nil)
+                    )
+(set-face-attribute 'lazy-highlight nil
+                    :background "yellow"
+                    :foreground "black"
+                    :weight 'bold
+                    )
+(set-face-attribute 'match nil
+                    :background "yellow"
+                    :foreground "black"
+                    )
+(set-face-attribute 'isearch-fail nil
+                    :background "red"
+                    :foreground "black"
+                    :weight 'bold
+                    )
+;; replace
+(set-face-attribute 'query-replace nil
+                    :background "cornsilk"
+                    :foreground "black"
+                    :weight 'bold
+                    :box '(:color "deep pink" :line-width 1 :style nil))
+
+;; comment
+;; family: DejaVu Serif,  Droid Serif, Gabriola, Gentium, GFS Didot, Latin Modern Mono, Segoe Print,
+(set-face-attribute 'font-lock-comment-face nil
+                    :slant 'italic
+                    )
+;; built-in function.
+(set-face-attribute 'font-lock-builtin-face nil
+                    :slant 'italic)
+
+
+;;; highlight search
+(setq search-highlight t
+      query-replace-highlight t)
+
+
+;;; [ highlight ]
+(global-font-lock-mode t)
+
+
+;;; [ show-paren-mode ]
+
+(show-paren-mode 1) ; highlight matched parentheses
+(setq show-paren-style 'mixed) ; 'parenthesis, 'expression, 'mixed
+
+
+;;; [ pretty-mode ] -- Redisplay parts of the Emacs buffer as pretty symbols.
+
+;; https://github.com/akatov/pretty-mode
+
+(require 'pretty-mode)
+
+;;; 1. if you want to set it globally
+(global-pretty-mode t)
+;;; 2. if you want to set it only for a specific mode
+;; (dolist (hook '(prog-mode-hook
+;;                 lisp-mode-hook emacs-lisp-mode-hook scheme-mode-hook
+;;                 ruby-mode-hook))
+;;   (add-hook hook 'turn-on-pretty-mode))
+
+
+
+;;; [ pretty-symbols ]
+
+;; (require 'pretty-symbols)
+;;
+;; (setq pretty-symbol-categories '(lambda relational logical)
+;;       pretty-symbol-patterns '())
+
+
+;;; [ page-break-lines-mode ] --- page breaks (^L characters) are displayed as a horizontal line of a character.
+
+;;; In Page Break mode, page breaks (^L characters) are displayed as a horizontal line of `page-break-string-char' characters.
+
+(require 'page-break-lines)
+
+(global-page-break-lines-mode t)
+(diminish 'page-break-lines-mode)
+
+(setq page-break-lines-char ?─)
+
+;; 
+;; ;;; [ Fold ]
+
+;; ;;; hs-minor-mode --
+
+;; (add-hook 'prog-mode-hook 'hs-minor-mode)
+
+
+
+(provide 'init-my-emacs-apperance)
+
+;;; init-my-emacs-apperance.el ends here
---
+++ init/Emacs/init-my-emacs-modeline.el
@@ -0,0 +1,306 @@
+;; collapse modeline
+;; variable: `mode-line-format'
+;; use `setq-default` to set it for /all/ modes
+;;
+;; [C-u C-\ greek RET] then [C-\] to write Greek characters.
+;; - [C-h v major-mode] for current buffer major mode.
+;; - [C-h v minor-mode-alist] for current buffer minor modes list.
+
+;; load necessary package which will be used later.
+(require 'window-number)
+(require 'vc)
+(require 'vc-git)
+
+;; (default-value mode-line-format)
+;; setq-default
+(setq-default mode-line-format
+              (quote
+               (
+                (:propertize "%e"
+                             face (:foreground "red" :inverse-video nil))
+
+                ;; (:propertize " 暗月:> "
+                ;;              face (:foreground "#444444" :background "black")
+                ;;              help-echo "九州 ❯ 羽传说 ❯ 向异翅")
+
+                ;; window-number
+                (:propertize (:eval (window-number-string))
+                             face (:foreground "red"))
+
+                (:propertize " ")
+                
+                ;; mule info
+                (:propertize mode-line-mule-info) ; U:
+
+                ;; Buffer status
+                ;; insert vs overwrite mode, input-method in a tool-tip ("Ovr", "Ins")
+                (:propertize "[")
+                (:propertize (:eval (if overwrite-mode "♜, " ""))
+                             face (:foreground "dark green")
+                             help-echo (concat "Buffer is in "
+                                                 (if overwrite-mode "overwrite" "insert")))
+                ;; was this buffer modified since the last save? ("☡" "Mod")
+                (:propertize (:eval (if (buffer-modified-p) "☡"))
+                             face (:foreground "deep pink")
+                             help-echo "Buffer has been modified")
+                ;; is this buffer read-only? ("RO")
+                (:propertize (:eval (when buffer-read-only ", ⚑"))
+                             face (:foreground "red")
+                             help-echo "Buffer is read-only!!!")
+                (:propertize "] ")
+
+                ;; VCS - Git, SVN, CVS,
+                ;; vc indicator
+                (:propertize (vc-mode vc-mode)
+                             face (:foreground "cyan")
+                             help-echo (vc-mode))
+
+                ;; (vc-mode (:eval (propertize vc-mode
+                ;;                             'face (pcase (vc-state buffer-file-truename)
+                ;;                                     (`up-to-date '(:foreground "green"))
+                ;;                                     (`edited nil)
+                ;;                                     (_ '(:foreground "red"))))))
+
+                ;; (vc-mode (:eval (propertize vc-mode 'face
+                ;;   (pcase (vc-state buffer-file-truename)
+                ;;     (`up-to-date 'mode-line-important)
+                ;;     (`edited                      nil)
+                ;;     (_             'mode-line-warning)))))
+
+                ;; the buffer name; the filename as a tool tip
+                ;; mode-line-client
+                ;; mode-line-remote
+                ;; mode-line-frame-identification
+                ;; mode-line-buffer-identification
+                (:propertize " [")
+                (:propertize " %b "
+                             face (:foreground "green")
+                             help-echo  (buffer-file-name))
+                (:propertize "] ")
+
+                ;; line and column number, relative position
+                ;; mode-line-position
+                ;; '%02' to set to 2 chars at least; prevents flicking
+                (:propertize "(%02l,%02c)"
+                             face (:foreground "gray")
+                             ;; (:eval (if (>= (current-column) 80)
+                             ;;            face (:foreground "red" :weight 'bold)))
+                             )
+
+                ;; relative position, size of file
+                ;; (:propertize " [%p,%I] ")
+
+                ;; process
+                ;; mode-line-process
+
+                ;; notifications
+                ;; ERC
+                ;; TODO [#e,i]
+
+                ;; the major mode of the current buffer.
+                (:propertize " ("
+                             face (:foreground "orange red"))
+                (:propertize "{%m}"
+                             face (:foreground "cyan")
+                             help-echo buffer-file-coding-system)
+                (:propertize "|")
+                ;; list of minor modes
+                (:propertize minor-mode-alist
+                             face (:foreground "dim gray"))
+                (:propertize ") "
+                             face (:foreground "orange red"))
+
+                ;; add the time, with the date and the emacs uptime in the tool-tip
+                (:propertize (:eval (format-time-string "%H:%M"))
+                             face (:foreground "white")
+                             help-echo (concat (format-time-string "%c; ")
+                                               (emacs-uptime "Uptime: %D, %z%2h:%.2m")))
+
+                ;; --------------------------- right align ----------------------------------
+
+                ;; Email
+
+                ;; newsticker RSS new feeds.
+                ;; (:propertize (:eval (let ((unread (or (newsticker--stat-num-items-total 'new) 0)))
+                ;;                       (when (> unread 0)
+                ;;                         (format "RSS: %d" unread)
+                ;;                         )))
+                ;;              face (:foreground "green")
+                ;;              help-echo (format "You have %d unread RSS items! [C-c r]" unread)
+                ;;              mouse-face 'mode-line-highlight)
+
+                ;; fill with ' '.
+                ;; (:propertize "% ")
+
+                (:propertize mode-line-end-spaces)
+                )))
+
+;;; mode-line right align (which replace `mode-line-end-spaces' to implement the right alignment.)
+;;
+;; (display-time)
+;; (setq global-mode-string (remove 'display-time-string global-mode-string))
+;; (setq mode-line-end-spaces
+;;       (list (propertize " " 'display '(space :align-to (- right 10)))
+;;             ;; you can custom here (add right aligned things here)
+;;             ;; '(:propertize " /铁甲依然在 !/ "
+;;             ;;               face (:foreground "cyan")
+;;             ;;               help-echo "我不要这样生，也不要那样死！\n\n 人的一生是如此的的短暂，而你还有很多的事情,很多的梦想 需要曲实现。\n我不想那样生存，也不想就 这样死去，所以，我要拼命去实现我的想法。")
+;;             'display-time-string
+;;             ))
+
+(defvar mode-line-cleaner-alist
+  '((auto-complete-mode . " ac") ; AC (auto-complete)
+    (yas-minor-mode . " yas") ; yas (yasnippet)
+    (helm-mode . "") ; Helm (helm),
+    (paredit-mode . "") ; Paredit (paredit)
+    (paredit-everywhere-mode . "") ; Par-
+    (eldoc-mode . "") ; ElDoc (ElDoc)
+    ;; minor modes
+    (autopair-mode . "") ; pair (auto-pair)
+    (auto-indent-mode . "")
+    (auto-indent-minor-mode . "") ; AI (auto-indent)
+    (highlight-indentation-mode . "") ; ||, ▸
+    (highlight-indentation-current-column-mode . "") ; |
+    (visual-line-mode . "") ; vl (visual-line)
+    (global-visual-line-mode . "") ; vl (visual-line)
+    (hs-minor-mode . "") ; hs (hideshow)
+    (abbrev-mode . "") ; (abbrev)
+    (defining-kbd-macro . " Def:macro") ; Def
+    (undo-tree-mode . "") ; Undo-tree (undo-tree)
+    (outline-minor-mode . "") ; Outl (outline)
+    (global-whitespace-mode . "") ; WS (global-whitespace-mode)
+    (whitespace-mode . "") ; ws (whitespace-mode)
+    (global-whitespace-newline-mode . "") ; NL (global-whitespace-newline-mode)
+    (whitespace-newline-mode . "") ; nl (whitespace-newline-mode)
+    (whitespace-cleanup-mode . "") ; WSC
+    (auto-fill-mode . "") ; Fill (auto-fill-mode), A/Fill
+    (auto-fill-function . "") ; Fill (auto-fill-function), A/Fill
+    (rainbow-mode . "") ; Rbow (rainbow-mode)
+    (rainbow-delimiters-mode . "") ; (rainbow-delimiters-mode)
+    (auto-capitalize . "") ; Cap, (auto-capitalize), A/Cap
+    (ispell-minor-mode . "") ; Spell (ispell-minor-mode)
+    (flyspell-mode . "") ; Fly (flyspell)
+    (flycheck-mode . flycheck-mode-line) ; FlyC
+    (flymake-mode . 'flymake-mode-line) ; FlyM
+    (hi-lock-mode . "") ; syntax highlight
+    (compilation-minor-mode . " Compilation") ; Compilation
+    (compilation-shell-minor-mode . " Shell-Compile") ; Shell-Compile
+    (compilation-in-progress . " Compiling") ; Compiling
+    (next-error-follow-minor-mode . "") ; Fol
+    (visible-mode . "") ; Vis (visible-mode)
+    (window-number-mode . "") ; window-number
+    (workgroups-mode . "") ; wg
+    (fic-mode . "") ; FIC (fic-mode)
+    ;; Project
+    (projectile-mode . projectile-mode-line)        ; " Projectile" (projectile)
+    ;; major modes
+    ;; Tools
+    ;; Org
+    (org-mode . "Org") ; Org (org-mode)
+    (org-indent-mode . "") ; Ind (org-indent-mode)
+    (org-capture-mode . "Org/Capture") ; Rem;
+    (org-cdlatex-mode . "Org/OCDL") ; OCDL;
+    (orgstruct-mode . "Org/Struct") ; OrgStruct;
+    (org-agenda-mode . "*Org Agenda*")
+    ;; VCS
+    (magit-auto-revert-mode . "") ; MRev
+    ;; Programming Languages modes
+    (lisp-interaction-mode . "λ:>")
+    (lisp-mode . "λ") ; Lisp mode
+    (emacs-lisp-mode . "Emacs Lisp") ; Emacs-Lisp (emacs-lisp)
+    (elisp-slime-nav-mode . "") ; SliNav
+    (sh-mode . "#!") ; shell-script (Bash)
+    (ruby-mode . "Ruby")
+    (enh-ruby-mode . "EnRuby") ; EnhRuby (Enhanced-Ruby-Mode)
+    (ruby-electric-mode . " Ruby/Elec")
+    (ruby-block-mode . " Ruby/Block")
+    (ruby-test-mode . " Ruby/Test")
+    (rspec-mode . " RSpec")
+    (robe-mode . "") ; robe
+    (rinari-minor-mode . " Rinari") ; Rin
+    (ruby-capistrano-minor-mode . " Capstrano") ; capstrano
+    (ruby-compilation-minor-mode . " Ruby:comp") ; ruby:comp
+    (inf-ruby-minor-mode . "")
+    (python-mode . "Python")
+    (c-mode . "C")
+    (c++-mode . "C++")
+    (go-mode . "Go")
+    (lua-mode . "Lua")
+    (rust-mode . "Rust")
+    (clojure-mode . "Clojure")
+    (ocaml-mode . "OCaml")
+    (scala-mode . "Scala")
+    (ess-transcript-minor-mode . "") ; ESStr
+    (ess-listing-minor-mode . "") ; ESSlst
+    (noweb-mode . "Noweb") ; Noweb
+    (noweb-font-lock-mode . "") ; NWFL
+    (html-mode . "HTML")
+    (zencoding-mode . "Zen") ; " Zen" (zencoding-mode)
+    (css-mode . "CSS")
+    (js-mode . "JavaScript")
+    (js2-mode . "JavaScript(2)")
+    (js2-minor-mode . "JavaScript2")
+    (js3-mode . "JavaScript(3)")
+    (inferior-js-keys-mode . "") ; InfJS
+    ;; TODO (haml-mode . "Haml")
+    ))
+
+(defun clean-mode-line ()
+  (interactive)
+  (loop for cleaner in mode-line-cleaner-alist
+        do (let* ((mode (car cleaner))
+                  (mode-str (cdr cleaner))
+                  (old-mode-str (cdr (assq mode minor-mode-alist))))
+             (when old-mode-str
+               (setcar old-mode-str mode-str))
+             ;; major mode
+             (when (eq mode major-mode)
+               (setq mode-name mode-str)))))
+
+(add-hook 'after-change-major-mode-hook 'clean-mode-line)
+
+;; (setq mode-line-in-non-selected-windows t)
+
+;; modeline colors
+(set-face-attribute 'mode-line nil
+                    :inverse-video nil
+                    :foreground "#444444"
+                    :background "black"
+                    :box '(:color "#444444" :line-width 1 :style nil)
+                    :family "DejaVu Sans Mono"
+                    )
+(set-face-attribute 'mode-line-inactive nil
+                    :inverse-video nil
+                    :foreground "dim gray"
+                    :background "#002F3B"
+                    :box '(:color "dark green" :line-width 1 :style nil)
+                    :family "DejaVu Sans Mono"
+                    )
+
+
+;;; [ display-time ]
+
+;; (display-time-mode 1)
+
+;; (setq display-time-interval 60)
+;; (setq display-time-24hr-format nil)
+;; (setq display-time-format nil)
+;; (setq display-time-day-and-date nil)
+;; ;;; event
+;; (display-time-event-handler)
+;; ;;; Mail
+;; (setq display-time-use-mail-icon t)
+;; (setq display-time-mail-function nil)
+;; (setq display-time-mail-file nil)
+;; (setq display-time-mail-directory "~/Mails/INBOX/new/")
+;; (display-time-mail-check-directory)
+;; ;; (display-time-mail-check-directory)
+;; ;;; load-average
+;; (setq display-time-default-load-average 0)
+;; (setq display-time-load-average-threshold 0.5)
+
+
+
+
+
+(provide 'init-my-emacs-modeline)
