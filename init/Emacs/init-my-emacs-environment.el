63ae34fc623448e1e3f42dffc4f07a76e74e2891
stardiviner <numbchild@gmail.com>
2014-04-07 02:39:18 +0800

    update my utf-8 environment settings.

--- init/Emacs/init-my-emacs-environment.el
+++ init/Emacs/init-my-emacs-environment.el
@@ -16,17 +16,16 @@
 
 
 ;; format (Unix, DOS) & encoding
-(prefer-coding-system 'utf-8-unix)
 (set-default-coding-systems 'utf-8)
+(setq locale-coding-system 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
 (set-language-environment 'utf-8)
+(prefer-coding-system 'utf-8-unix)
 
-(setq-default buffer-file-coding-system 'utf-8-unix)
-(setq-default default-buffer-file-coding-system 'utf-8-unix)
-
-
-
+;; (setq-default buffer-file-coding-system 'utf-8-unix)
+;; (setq-default default-buffer-file-coding-system 'utf-8-unix)
 
 
 
