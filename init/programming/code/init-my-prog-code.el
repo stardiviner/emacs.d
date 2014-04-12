becaf933be3085edd10cafa6f2b0b7eb39950a57
stardiviner <numbchild@gmail.com>
2014-04-13 02:40:19 +0800

    init Programming write code settings.

---
+++ init/programming/code/init-my-prog-code.el
@@ -0,0 +1,25 @@
+;;; init-my-prog-code.el --- init Code writting for Emacs.
+;;; -*- coding: utf-8 -*-
+
+;;; Commentary:
+
+
+
+;;; Code:
+
+;;; [ glasses-mode ] -- make CamelCase identifiers easy look.
+
+;;; insert a virtual underscore separator between the conjoined words, so
+;;; `fooBarBaz' will look like `foo_Bar_Baz'.
+;;;
+;;; When glasses-mode is enabled, you should continue following the CamelCase
+;;; style as Emacs will automagically insert the virtual separator, as needed,
+;;; when you type a capitalized character.
+
+;; (require 'glasses-mode)
+
+
+
+(provide 'init-my-prog-code)
+
+;;; init-my-prog-code.el ends here
