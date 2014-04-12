0f9895a35e5cc0bbd6caa64b5aa68e7e3744f426
stardiviner <numbchild@gmail.com>
2014-04-13 02:06:53 +0800

    init Guile.

---
+++ init/programming/languages/Guile/init-my-prog-lang-guile.el
@@ -0,0 +1,16 @@
+;;; init-my-prog-lang-guile.el --- init for Guile.
+;;; -*- coding: utf-8 -*-
+
+;;; Commentary:
+
+
+
+;;; Code:
+
+
+
+
+
+(provide 'init-my-prog-lang-guile)
+
+;;; init-my-prog-lang-guile.el ends here
