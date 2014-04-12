5bed0c09572dd8976301971a370a56e888b98bf5
stardiviner <numbchild@gmail.com>
2014-04-12 23:31:11 +0800

    add jedi for Python completion.

--- init/programming/languages/Python/init-my-prog-lang-python.el
+++ init/programming/languages/Python/init-my-prog-lang-python.el
@@ -24,6 +24,33 @@
                   )))
 
 
+
+;;; [ jedi ] --- a python auto-completion library.
+
+(unless (package-installed-p 'jedi)
+  (package-install 'jedi))
+(require 'jedi)
+
+;; (add-hook 'python-mode-hook 'jedi:setup)
+;; (setq jedi:complete-on-dot t)           ; optional, but you need `jedi:setup' instead of `jedi:ac-setup'.
+;;
+;; use auto-complete and use jedi as ac source.
+(add-hook 'python-mode-hook 'jedi:ac-setup)
+
+
+;; variable (jedi:server-command '("~/.emacs.d/.python-environments/default/bin/jediepcserver.py"))
+;;    Command used to run Jedi server.
+;;    Note
+;;    If you used jedi:install-server (recommended) to install Python server jediepcserver.py, you don’t need to mess around with jediepcserver.py. Jedi.el handles everything automatically.
+;;
+;; install python jedi server (jediepcserver.py) by running:
+;; [M-x jedi:install-server] in Emacs.
+
+(setq jedi:tooltip-method '(pos-tip popup))
+
+;; (setq jedi:environment-root "jedi"
+;;       jedi:environment-virtualenv '("--python" "/PATH/TO/python3")
+;;       )
 
 
 
