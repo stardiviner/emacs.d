bdb52f55d110dd31404110fbae84fe639810d650
stardiviner <numbchild@gmail.com>
2014-04-12 22:45:49 +0800

    init Calc calculator for Emacs.

---
+++ init/tools/init-my-tool-calculator.el
@@ -0,0 +1,24 @@
+;;; init-my-tool-calculator.el --- init Tool Calculator
+;;; -*- coding: utf-8 -*-
+
+;;; Commentary:
+
+
+
+;;; Code:
+
+;;; [ Calc ]
+
+;;; Usage:
+;;
+;; - get help -> [C-h i g (calc)]
+;; - [C-x *] :: prefix
+;; - [C-x * *] ::
+;; ELISP> (calc-eval "1+2")
+
+
+
+
+(provide 'init-my-tool-calculator)
+
+;;; init-my-tool-calculator.el ends here
