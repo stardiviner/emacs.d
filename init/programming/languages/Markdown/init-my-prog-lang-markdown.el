109144b4e09bd946c64762a200ed120beb3f0b93
stardiviner <numbchild@gmail.com>
2014-04-12 23:36:12 +0800

    init Markdown for Emacs.

---
+++ init/programming/languages/Markdown/init-my-prog-lang-markdown.el
@@ -0,0 +1,19 @@
+;;; init-my-prog-lang-markdown.el --- init Emacs for Markdown
+;;; -*- coding: utf-8 -*-
+
+;;; Commentary:
+
+
+
+;;; Code:
+
+;;; [ markdown-mode ]
+
+(require 'markdown-mode)
+
+
+
+
+(provide 'init-my-prog-lang-markdown)
+
+;;; init-my-prog-lang-markdown.el ends here
