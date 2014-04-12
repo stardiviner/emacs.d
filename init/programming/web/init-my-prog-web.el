fb50b05ec735aaee33a2c0f2e3f7e4b78a8d06c4
stardiviner <numbchild@gmail.com>
2014-04-12 22:46:47 +0800

    add web-mode and config it.

---
+++ init/programming/web/init-my-prog-web.el
@@ -0,0 +1,45 @@
+;;; init-my-prog-web.el --- init Web settings for Emacs
+;;; -*- coding: utf-8 -*-
+
+;;; Commentary:
+
+
+
+;;; Code:
+
+;;; [ web-mode ]
+
+(require 'web-mode)
+
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.blade\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist
+'("/\\(views\\|html\\|theme\\|templates\\)/.*\\.php\\'" . web-mode))
+
+
+
+(eval-after-load 'web-mode
+  '(progn
+     (defun my-web-mode-defaults ()
+       ;; Customizations
+       (setq web-mode-markup-indent-offset 4
+             web-mode-css-indent-offset 2
+             web-mode-code-indent-offset 4
+             web-mode-disable-autocompletion t)
+       (local-set-key (kbd "RET") 'newline-and-indent))
+     (setq my-web-mode-hook 'my-web-mode-defaults)
+
+     (add-hook 'web-mode-hook
+               (lambda ()
+                 (run-hooks 'my-web-mode-hook)))))
+
+
+
+(provide 'init-my-prog-web)
+
+;;; init-my-prog-web.el ends here
