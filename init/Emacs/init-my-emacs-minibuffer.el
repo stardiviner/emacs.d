962bff2a31459a766ed09e4b5d539009dd51b2f2
stardiviner <numbchild@gmail.com>
2014-04-12 21:59:35 +0800

    add eldoc mode for Emacs and Emacs Lisp.

--- init/Emacs/init-my-emacs-minibuffer.el
+++ init/Emacs/init-my-emacs-minibuffer.el
@@ -112,7 +112,40 @@
             (define-key ido-completion-map (kbd "M-n") 'ido-next-match)
             (define-key ido-completion-map (kbd "M-p") 'ido-prev-match)))
 
-(provide 'init-my-emacs-minibuffer)
+
+;; eldoc-eval --- Enable eldoc support when minibuffer is in use. [M-:]
 
+;; Eldoc info is shown by default in mode-line, but you can have eldoc info
+;; somewhere else by setting eldoc-in-minibuffer-show-fn to another function
+;; (e.g tooltip-show).
+(setq eldoc-in-minibuffer-show-fn 'tooltip-show) ; 'eldoc-show-in-mode-line.
+
+;; It also provides a convenient macro to enable eldoc support in your own
+;; functions using minibuffer or in your defadvices, that is
+;; with-eldoc-in-minibuffer, e.g:
+;;
+;; (defadvice edebug-eval-expression (around with-eldoc activate)
+;;   "This advice enable eldoc support."
+;;   (interactive (list (with-eldoc-in-minibuffer
+;;                        (read-from-minibuffer
+;;                         "Eval: " nil read-expression-map t
+;;                         'read-expression-history))))
+;;   ad-do-it)
+
+;; Users of own minibuffer frame will have to set
+;; `eldoc-in-minibuffer-own-frame-p' to non-nil.
+
+;; You can turn On/Off eldoc support in minibuffer at any time with
+;; `eldoc-in-minibuffer-mode'.
+
+;; TODO: this eldoc-eval does not looks great.
+;; (autoload 'eldoc-in-minibuffer-mode "eldoc-eval")
+;; (eldoc-in-minibuffer-mode 1)
+
+
+
+
+
+(provide 'init-my-emacs-minibuffer)
 
 ;;; init-my-emacs-minibuffer.el ends here
