1695049b717d733475ea3dcd816cbd6a1b7163e1
stardiviner <numbchild@gmail.com>
2014-04-13 02:05:18 +0800

    init TeX.

---
+++ init/programming/languages/TeX/init-my-prog-lang-tex.el
@@ -0,0 +1,16 @@
+;;; init-my-prog-lang-tex.el --- init TeX/LaTeX for Emacs.
+;;; -*- coding: utf-8 -*-
+
+;;; Commentary:
+
+
+
+;;; Code:
+
+
+
+
+
+(provide 'init-my-prog-lang-tex)
+
+;;; init-my-prog-lang-tex.el ends here
