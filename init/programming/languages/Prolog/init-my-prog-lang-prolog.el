8cfbb7bf019204afcb58204230ab1c12c0c56b3a
stardiviner <numbchild@gmail.com>
2014-04-13 02:06:12 +0800

    init Prolog.

---
+++ init/programming/languages/Prolog/init-my-prog-lang-prolog.el
@@ -0,0 +1,24 @@
+;;; init-my-prog-lang-prolog.el --- init Prolog programming languages for Emacs.
+;;; -*- coding: utf-8 -*-
+
+;;; Commentary:
+
+
+
+;;; Code:
+
+;;; [ Prolog mode ]
+
+(require 'prolog)
+
+(add-to-list 'auto-mode-alist '("\\.prolog$" . prolog-mode))
+
+
+;;; [ ediprolog ]
+
+
+
+
+(provide 'init-my-prog-lang-prolog)
+
+;;; init-my-prog-lang-prolog.el ends here
