a9ccb3b87b97b45c36bd5c536cd9f970155e8f8d
stardiviner <numbchild@gmail.com>
2014-04-12 21:36:45 +0800

    add many keybindings for paste tool Gist.

--- init/tools/init-my-tool-paste.el
+++ init/tools/init-my-tool-paste.el
@@ -21,6 +21,24 @@
 (require 'gist)
 
 (setq gist-view-gist t) ; view your Gist using `browse-url` after it is created.
+
+;;; TODO: enable gist-mode in Org-mode.
+;; FIXME: gist-mode enable will lead to Org-mode [C-x C-s] error.
+
+;;; --------------------------------------------------
+(define-prefix-command 'my-tools-prefix-map)
+(global-set-key (kbd "C-c t") 'my-tools-prefix-map)
+
+(define-key my-tools-prefix-map (kbd "p p") 'gist-region-or-buffer)
+(define-key my-tools-prefix-map (kbd "p v") 'gist-region-or-buffer-private)
+(define-key my-tools-prefix-map (kbd "p r") 'gist-region)
+(define-key my-tools-prefix-map (kbd "p b") 'gist-buffer)
+(define-key my-tools-prefix-map (kbd "p l") 'gist-list)
+;;; --------------------------------------------------
+
+
+
+
 
 
 (provide 'init-my-tool-paste)
