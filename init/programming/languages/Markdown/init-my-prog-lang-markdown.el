db65e7cc40bc73c713f5ed06c63e120a182b64b6
stardiviner <numbchild@gmail.com>
2014-04-12 23:36:55 +0800

    add a config for Firefox addon "It's All Text" to edit post in sites like:
    Stack Overflow, GitHub, etc.

--- init/programming/languages/Markdown/init-my-prog-lang-markdown.el
+++ init/programming/languages/Markdown/init-my-prog-lang-markdown.el
@@ -13,6 +13,16 @@
 
 
 
+;;; for Stack Overflow {It's All Text} (Firefox Addon)
+;; Integrate Emacs with Stack Exchange http://stackoverflow.com/a/10386560/789593
+(add-to-list 'auto-mode-alist '("stack\\(exchange\\|overflow\\)\\.com\\.[a-z0-9]+\\.txt" . markdown-mode))
+;; Alternatively, if `as-external-alist' is defined—if M-x describe-variable RET
+;; as-external-alist doesn't fail—it will probably override your
+;; auto-mode-alist. It has a slightly different format (it's a list of pairs
+;; instead of a list of cons cells) so this will work:
+;; (add-to-list 'as-external-alist '("stack\\(exchange\\|overflow\\)\\.com\\.[a-z0-9]+\\.txt" markdown-mode))
+
+
 
 (provide 'init-my-prog-lang-markdown)
 
