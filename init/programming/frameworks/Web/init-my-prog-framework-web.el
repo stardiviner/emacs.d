d53d0cf884e78130fed75cffebdc94b0510f404d
stardiviner <numbchild@gmail.com>
2014-04-13 02:04:50 +0800

    init web-mode.

--- init/programming/frameworks/Web/init-my-prog-framework-web.el
+++ init/programming/frameworks/Web/init-my-prog-framework-web.el
@@ -1,4 +1,4 @@
-;;; init-my-prog-framework-web.el --- 
+;;; init-my-prog-framework-web.el --- init Web framework settings for Emacs.
 ;;; -*- coding: utf-8 -*-
 
 ;;; Commentary:
@@ -9,6 +9,13 @@
 
 ;;; [ web-mode ]
 
+;;; web-mode.el is an autonomous emacs major mode for editing web templates aka
+;;; HTML files embedding client parts (CSS/JavaScript) and server blocks.
+
+;;; web-mode.el is compatible with many template engines: PHP, JSP, ASP, Django,
+;;; Twig, Jinja(2), ERB, FreeMarker, Velocity, Cheetah, Smarty, CTemplate,
+;;; Mustache, Blade, ErlyDTL, Go Template, Dust.js, Google Closure (soy), etc.
+
 (require 'web-mode)
 
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
@@ -17,12 +24,43 @@
 (add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+;; Using web-mode for editing plain HTML files can be done this way
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+
 (add-to-list 'auto-mode-alist
-'("/\\(views\\|html\\|theme\\|templates\\)/.*\\.php\\'" . web-mode))
+             '("/\\(views\\|html\\|theme\\|templates\\)/.*\\.php\\'" . web-mode))
 
+;;; ----------------------------------------------------------
+;;; Associate an engine
 
+;;; Using this association list is required as soon as the file extension is
+;;; unknown (by web-mode) or is too general (e.g. *.html).
+;;;
+;;; The recognized file extensions are listed in the Engine families paragraph.
+;;; In summary, you may have to set both auto-mode-alist and web-mode-engines-alist.
 
+(setq web-mode-engines-alist
+      '(("php" . "\\.phtml\\'")
+        ("blade" . "\\.blade\\.")
+        ))
+
+
+;;; Engine families
+
+;;; Never forget to update the auto-mode-alist.
+
+
+;;; auto-pairs
+
+;;; add auto-pair
+;; (setq web-mode-extra-auto-pairs
+;;       '(("erb" . (("open" "close")))
+;;         ("php" . (("open" "close") ("open" "close")))
+;;         ))
+
+
 (eval-after-load 'web-mode
   '(progn
      (defun my-web-mode-defaults ()
@@ -38,6 +76,148 @@
                (lambda ()
                  (run-hooks 'my-web-mode-hook)))))
 
+
+;;; snippets
+
+;;; add a snippet
+;; (setq web-mode-extra-snippets
+;;       '(("erb" . (("name" . ("beg" . "end"))))
+;;         ("php" . (("name" . ("beg" . "end"))
+;;                   ("name" . ("beg" . "end"))))
+;;         ))
+
+
+
+;;; Faces
+
+(setq web-mode-enable-block-face t
+      web-mode-enable-part-face t
+      web-mode-enable-comment-keywords t
+      web-mode-enable-whitespaces t
+      web-mode-enable-heredoc-fontification t
+      web-mode-enable-current-element-highlight t)
+
+;; ;; effects
+;; (set-face-attribute 'web-mode-folded-face nil
+;;                     :foreground "white" :background "#004A5D"
+;;                     :box '(:color "cyan" :line-width 1 :style nil))
+;; (set-face-attribute 'web-mode-warning-face nil
+;;                     :foreground "red" :background "black")
+;; (set-face-attribute 'web-mode-whitespace-face nil
+;;                     :foreground "red")
+;; (set-face-attribute 'web-mode-comment-keyword-face nil
+;;                     :foreground "white" :background "#004A5D"
+;;                     :box '(:color "cyan" :line-width 1 :style nil))
+;; ;; html
+;; (set-face-attribute 'web-mode-doctype-face nil
+;;                     :foreground "white" :background "#004A5D"
+;;                     :box '(:color "cyan" :line-width 1 :style nil))
+;; (set-face-attribute 'web-mode-html-tag-face nil
+;;                     :foreground "white" :background "#004A5D"
+;;                     :box '(:color "cyan" :line-width 1 :style nil))
+;; (set-face-attribute 'web-mode-html-attr-name-face nil
+;;                     :foreground "white" :background "#004A5D"
+;;                     :box '(:color "cyan" :line-width 1 :style nil))
+;; (set-face-attribute 'web-mode-html-attr-value-face nil
+;;                     :foreground "white" :background "#004A5D"
+;;                     :box '(:color "cyan" :line-width 1 :style nil))
+;; ;; json
+;; (set-face-attribute 'web-mode-json-key-face nil
+;;                     :foreground "white" :background "#004A5D"
+;;                     :box '(:color "cyan" :line-width 1 :style nil))
+;; (set-face-attribute 'web-mode-json-context-face nil
+;;                     :foreground "white" :background "#004A5D"
+;;                     :box '(:color "cyan" :line-width 1 :style nil))
+;; (set-face-attribute 'web-mode-json-string-face nil
+;;                     :foreground "white" :background "#004A5D"
+;;                     :box '(:color "cyan" :line-width 1 :style nil))
+;; ;; css
+;; (set-face-attribute 'web-mode-css-at-rule-face nil
+;;                     :foreground "white" :background "#004A5D"
+;;                     :box '(:color "cyan" :line-width 1 :style nil))
+;; (set-face-attribute 'web-mode-css-property-name-face nil
+;;                     :foreground "white" :background "#004A5D"
+;;                     :box '(:color "cyan" :line-width 1 :style nil))
+;; (set-face-attribute 'web-mode-css-function-face nil
+;;                     :foreground "white" :background "#004A5D"
+;;                     :box '(:color "cyan" :line-width 1 :style nil))
+;; (set-face-attribute 'web-mode-css-priority-face nil
+;;                     :foreground "white" :background "#004A5D"
+;;                     :box '(:color "cyan" :line-width 1 :style nil))
+;; (set-face-attribute 'web-mode-css-pseudo-class-face nil
+;;                     :foreground "white" :background "#004A5D"
+;;                     :box '(:color "cyan" :line-width 1 :style nil))
+;; (set-face-attribute 'web-mode-css-selector-face nil
+;;                     :foreground "white" :background "#004A5D"
+;;                     :box '(:color "cyan" :line-width 1 :style nil))
+;; (set-face-attribute 'web-mode-css-css-string-face nil
+;;                     :foreground "white" :background "#004A5D"
+;;                     :box '(:color "cyan" :line-width 1 :style nil))
+;; ;; code
+;; (set-face-attribute 'web-mode-string-face nil
+;;                     :foreground "white" :background "#004A5D"
+;;                     :box '(:color "cyan" :line-width 1 :style nil))
+;; (set-face-attribute 'web-mode-comment-face nil
+;;                     :foreground "white" :background "#004A5D"
+;;                     :box '(:color "cyan" :line-width 1 :style nil))
+;; (set-face-attribute 'web-mode-preprocessor-face nil
+;;                     :foreground "white" :background "#004A5D"
+;;                     :box '(:color "cyan" :line-width 1 :style nil))
+;; (set-face-attribute 'web-mode-variable-name-face nil
+;;                     :foreground "white" :background "#004A5D"
+;;                     :box '(:color "cyan" :line-width 1 :style nil))
+;; (set-face-attribute 'web-mode-function-name-face nil
+;;                     :foreground "white" :background "#004A5D"
+;;                     :box '(:color "cyan" :line-width 1 :style nil))
+;; (set-face-attribute 'web-mode-constant-face nil
+;;                     :foreground "white" :background "#004A5D"
+;;                     :box '(:color "cyan" :line-width 1 :style nil))
+;; (set-face-attribute 'web-mode-type-face nil
+;;                     :foreground "white" :background "#004A5D"
+;;                     :box '(:color "cyan" :line-width 1 :style nil))
+;; (set-face-attribute 'web-mode-keyword-face nil
+;;                     :foreground "white" :background "#004A5D"
+;;                     :box '(:color "cyan" :line-width 1 :style nil))
+;; (set-face-attribute 'web-mode-symbol-face nil
+;;                     :foreground "white" :background "#004A5D"
+;;                     :box '(:color "cyan" :line-width 1 :style nil))
+;; (set-face-attribute 'web-mode-builtin-face nil
+;;                     :foreground "white" :background "#004A5D"
+;;                     :box '(:color "cyan" :line-width 1 :style nil))
+;; ;; block
+;; (set-face-attribute 'web-mode-block-control-face nil
+;;                     :foreground "white" :background "#004A5D"
+;;                     :box '(:color "cyan" :line-width 1 :style nil))
+;; (set-face-attribute 'web-mode-block-face nil
+;;                     :foreground "white" :background "#004A5D"
+;;                     :box '(:color "cyan" :line-width 1 :style nil))
+;; (set-face-attribute 'web-mode-block-string-face nil
+;;                     :foreground "white" :background "#004A5D"
+;;                     :box '(:color "cyan" :line-width 1 :style nil))
+;; (set-face-attribute 'web-mode-block-comment-face nil
+;;                     :foreground "white" :background "#004A5D"
+;;                     :box '(:color "cyan" :line-width 1 :style nil))
+;; ;; part
+;; (set-face-attribute 'web-mode-part-face nil
+;;                     :foreground "white" :background "#004A5D"
+;;                     :box '(:color "cyan" :line-width 1 :style nil))
+;; (set-face-attribute 'web-mode-part-string-face nil
+;;                     :foreground "white" :background "#004A5D"
+;;                     :box '(:color "cyan" :line-width 1 :style nil))
+;; (set-face-attribute 'web-mode-part-comment-face nil
+;;                     :foreground "white" :background "#004A5D"
+;;                     :box '(:color "cyan" :line-width 1 :style nil))
+;; (set-face-attribute 'web-mode-javascript-string-face nil
+;;                     :foreground "white" :background "#004A5D"
+;;                     :box '(:color "cyan" :line-width 1 :style nil))
+
+
+
+;;; [ multi-web-mode ]
+
+
+
+;;; [ web-beautify ] ---
 
 
 
