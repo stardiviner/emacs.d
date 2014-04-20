a84a2164f887965fa971fffe8d2329d2c942aac3
stardiviner <numbchild@gmail.com>
2014-04-20 23:13:31 +0800

    modify my custom abbrev_def file.

--- init/Emacs/init-my-emacs-abbrev.el
+++ init/Emacs/init-my-emacs-abbrev.el
@@ -16,22 +16,42 @@
 ;;;   [M-x abbrev-mode]
 
 ;;; Code:
+
+;;; [ abbrev-mode ]
+
+;;; Usage:
+;; -
 
 (require 'abbrev)
 
-(setq abbrev-file-name "~/.emacs.d/my-init/abbrevs/abbrev_defs")
-(setq save-abbrevs t)   ; save abbrevs when files are saved, nil: stop asking.
+(setq abbrev-file-name "~/.emacs.d/init/abbrevs/abbrev_defs")
 (if (file-exists-p abbrev-file-name)
     (quietly-read-abbrev-file) ; reads the abbreviations file on startup quietly.
-    )
+  )
+
+(define-abbrev-table 'global-abbrev-table '(
+                                            ("mynick" "stardiviner")
+                                            ("myemail" "numbchild@gmail.com")
+                                            ("mygmail" "numbchild@gmail.com")
+                                            ("mygmailformat" "numbchild[@]{gmail}.com")
+                                            ;; TODO: ("myhomepage" "")
+                                            ("mytwitter" "@numbchild")
+                                            ))
+
+
+(setq save-abbrevs t)   ; save abbrevs when files are saved, nil: stop asking.
 
 (setq-default abbrev-mode t)            ; turn on abbrev mode globally.
 
+
 ;;; [ dabbrev ]
 ;; Usage:
 ;; - [M-/] -- dabbrev-expand
 (require 'dabbrev)
 
+
+
+
 (provide 'init-my-emacs-abbrev)
 
 ;;; init-my-emacs-abbrev.el ends here
