59a2616c0ef2334ca150c084ff950ccab58d2847
stardiviner <numbchild@gmail.com>
2014-04-25 01:16:17 +0800

    don't init company-mode if user use auto-complete.

--- init/Emacs/init-company-mode.el
+++ init/Emacs/init-company-mode.el
@@ -53,8 +53,12 @@
                          ))
 
 ;; To use company-mode in all buffers, add the following line to your init file:
-(add-hook 'after-init-hook 'global-company-mode)
-(diminish company-mode)
+(unless (featurep 'auto-complete)
+  (message "auto-complete isn't enabled, active company-mode instead.")
+  (add-hook 'after-init-hook 'global-company-mode)
+  (after 'global-company-mode
+    (diminish company-mode))
+  )
 
 ;; keybindings
 ;; TODO:
