af31713d4c621d47d1434b245e73a5cb51d607e2
stardiviner <numbchild@gmail.com>
2014-04-26 05:59:54 +0800

    add Erlang init file.

---
+++ init/programming/languages/Erlang/init-my-prog-lang-erlang.el
@@ -0,0 +1,18 @@
+;;; init-my-prog-lang-erlang.el --- init for Erlang
+;;; -*- coding: utf-8 -*-
+
+;;; Commentary:
+
+
+
+;;; Code:
+
+
+
+;; (run-erlang)
+
+
+
+(provide 'init-my-prog-lang-erlang)
+
+;;; init-my-prog-lang-erlang.el ends here
