5210c5d51949615b4d5872a5d3904865e631d4c7
stardiviner <numbchild@gmail.com>
2014-04-26 15:25:47 +0800

    add a separate Scheme init file under Lisp directory.

---
+++ init/programming/languages/Lisp/init-my-prog-lang-scheme.el
@@ -0,0 +1,22 @@
+;;; init-my-prog-lang-scheme.el --- init for Scheme
+;;; -*- coding: utf-8 -*-
+
+;;; Commentary:
+
+
+
+;;; Code:
+
+;;; [ Scheme Mode ]
+
+
+
+;;; [ Inferior Scheme ]
+
+;; (run-scheme)
+
+
+
+(provide 'init-my-prog-lang-scheme)
+
+;;; init-my-prog-lang-scheme.el ends here
