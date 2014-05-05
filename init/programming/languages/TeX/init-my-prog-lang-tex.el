347c65721da286868c96dcc62cb71c724774838b
stardiviner <numbchild@gmail.com>
2014-05-05 11:59:09 +0800

    setup TeX/LaTeX init.

--- init/programming/languages/TeX/init-my-prog-lang-tex.el
+++ init/programming/languages/TeX/init-my-prog-lang-tex.el
@@ -7,11 +7,29 @@
 
 ;;; Code:
 
+;;; [ TeX ]
+
+(require 'tex)
+
+
+;;; [ LaTeX ]
+
+(require 'latex)
+
+
 ;;; [ AUCTeX ] -- Integrated environment for TeX.
 
+;; LaTeX/P mode defined in `tex-site.el':
+;; Major mode in AUCTeX for editing LaTeX files.
+;; See info under AUCTeX for full documentation.
+(require 'tex-site)
+
+;;; [C-c C-p] :: preview prefix.
 
 
 ;;; [ auto-complete-auctex ]
+
+(require 'auto-complete-auctex)
 
 
 ;;; [ auto-complete-latex ]
