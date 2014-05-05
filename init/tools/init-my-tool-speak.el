f22bd882373179cb0d1df8dbadd0b2c13b78cde7
stardiviner <numbchild@gmail.com>
2014-05-05 11:56:22 +0800

    add a speak init file.

---
+++ init/tools/init-my-tool-speak.el
@@ -0,0 +1,46 @@
+;;; init-my-tool-speak.el --- init Speak
+;;; -*- coding: utf-8 -*-
+
+;;; Commentary:
+
+
+;;; Code:
+
+;;; [ Emacspeak ]
+
+;; http://www.emacswiki.org/emacs/EmacSpeak
+
+
+;;; [ Festival ]
+
+(require 'festival)
+
+(run-festival)
+(festival-start-process)
+
+(require 'thingatpt)
+
+;; FIXME: this function seems does not work.
+(defun festival-read ()
+  "Read current word that at point by Festival."
+  (interactive)
+  (if (use-region-p)
+      (let ((region (cons (region-beginning) (region-end)))
+            (festival-say-region region)
+            (message "Festival reading (region) ...")))
+    (let ((word (thing-at-point 'word)))
+      (festival-say-string word)
+      (message "Festival reading (word): %s" word))
+    )
+  )
+
+(unless (boundp 'speak-map)
+  (define-prefix-command 'speak-map))
+(define-key my-tools-prefix-map (kbd "s") 'speak-map)
+
+(define-key speak-map (kbd "s") 'festival-read)
+
+
+(provide 'init-my-tool-speak)
+
+;;; init-my-tool-speak.el ends here
