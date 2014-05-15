248592774ac59df24386bd77fcdfbf1c27617715
stardiviner <numbchild@gmail.com>
2014-05-15 15:50:06 +0800

    add Nginx init file.

---
+++ init/programming/frameworks/Web/init-my-prog-nginx.el
@@ -0,0 +1,29 @@
+;;; init-my-prog-nginx.el --- init Nginx mode
+;;; -*- coding: utf-8 -*-
+
+;;; Commentary:
+
+
+
+;;; Code:
+
+;;; [ nginx-mode ]
+
+;; https://github.com/ajc/nginx-mode
+;; https://github.com/zev/nginx-mode
+
+(unless (package-installed-p 'nginx-mode)
+  (package-install 'nginx-mode))
+
+(autoload 'nginx-mode "nginx-mode")
+;; (require 'nginx-mode)
+
+(add-to-list 'auto-mode-alist
+             '("nginx\\.conf$" . nginx-mode)
+             '("/etc/nginx/.*" . nginx-mode))
+
+
+
+(provide 'init-my-prog-nginx)
+
+;;; init-my-prog-nginx.el ends here
