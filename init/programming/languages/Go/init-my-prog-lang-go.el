44ed136f381cb05eb00b98040cec9475cfdd1b77
stardiviner <numbchild@gmail.com>
2014-05-15 15:43:08 +0800

    update go-mode and go-fmt etc init settings.

--- init/programming/languages/Go/init-my-prog-lang-go.el
+++ init/programming/languages/Go/init-my-prog-lang-go.el
@@ -7,7 +7,30 @@
 
 ;;; [ go-mode ]
 
+;;; Usage:
+;; - `gofmt'
+;; - `godoc'
+;; - [C-c C-a] -- `go-import-add'
+;; - `go-remove-unused-imports'
+;; - `go-goto-imports'
+;; - `go-play-buffer' and `go-play-region'
+;; - `go-download-play'
+;; - [C-c C-d] -- `godef-describe' and [C-c C-j] -- `godef-jump'
+;; - `go-coverage'
+
+
 (require 'go-mode)
+
+;; godoc -- `go doc [QUERY]`
+(add-hook 'go-mode-hook (lambda ()
+                          (local-set-key (kbd "C-h d") 'godoc)))
+
+;; gofmt
+(add-hook 'before-save-hook #'gofmt-before-save)
+
+;; use `godef-jump' instead of etags etc tags jumping.
+(add-hook 'go-mode-hook (lambda ()
+                          (local-set-key (kbd "M-.") #'godef-jump)))
 
 
 
@@ -16,9 +39,16 @@
 (require 'go-eldoc) ; Don't need to require, if you install by package.el
 (add-hook 'go-mode-hook 'go-eldoc-setup)
 
+
+;;; [ gocode ]
+
+
 
 
 ;;; [ go-autocomplete ]
+
+
+;;; [ go-company ]
 
 
 ;;; [ go-play ]
