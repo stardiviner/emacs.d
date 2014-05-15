d06c23a7fe8c6469aa5bc698eda08221dfa56bfc
stardiviner <numbchild@gmail.com>
2014-05-15 15:50:30 +0800

    add MongoDB init file.

---
+++ init/programming/languages/Databases/init-my-prog-database-mongo.el
@@ -0,0 +1,18 @@
+;;; init-my-prog-database-mongo.el --- init MongoDB
+
+;;; Commentary:
+
+;;;; Code:
+
+;;; [ mongo ]
+
+
+;;; [ inf-mongo ]
+
+
+;;; [ mongo-elnode ]
+
+
+(provide 'init-my-prog-database-mongo)
+
+;;; init-my-prog-database-mongo.el ends here
