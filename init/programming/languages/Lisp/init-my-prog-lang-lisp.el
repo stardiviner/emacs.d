4274a218a19f73841136ee0db365e2dcf1115eff
stardiviner <numbchild@gmail.com>
2014-05-21 21:54:32 +0800

    enable ac-geiser.

--- init/programming/languages/Lisp/init-my-prog-lang-lisp.el
+++ init/programming/languages/Lisp/init-my-prog-lang-lisp.el
@@ -134,6 +134,14 @@
 (require 'ac-geiser)
 
 
+(dolist (hook '(lisp-mode-hook
+                lisp-interaction-mode-hook
+                scheme-mode-hook
+                ))
+  (add-hook hook (lambda ()
+                   (ac-geiser-setup)
+                   (add-to-list 'ac-sources 'ac-source-geiser))))
+
 
 ;;; [ Quack ] -- enhanced Emacs Support for Editing and Running Scheme Code
 
