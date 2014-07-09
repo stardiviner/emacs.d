7606a04f69fc8089ceff744f20101efa252ba449
stardiviner <numbchild@gmail.com>
2014-07-09 10:37:25 +0800

    add abbrevs table for SQL mode to auto uppercase sql keywords.

--- init/programming/languages/Databases/init-my-prog-database.el
+++ init/programming/languages/Databases/init-my-prog-database.el
@@ -11,6 +11,12 @@
 
 
 
+;;; Auto Uppercase SQL Keywords
+(setq sql-abbrev-file-name "~/.emacs.d/init/abbrevs/sql.abbrev")
+(if (file-exists-p sql-abbrev-file-name)
+    (load sql-abbrev-file-name))
+
+
 (provide 'init-my-prog-database)
 
 ;;; init-my-prog-database.el ends here
