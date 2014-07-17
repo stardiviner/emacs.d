64aca6b8a77458d6a48eb709dcc303402de128e3
stardiviner <numbchild@gmail.com>
2014-07-17 21:34:46 +0800

    add SLIME into my-inferior-prefix-map keybinding.

--- init/programming/init-my-prog-programming.el
+++ init/programming/init-my-prog-programming.el
@@ -13,21 +13,22 @@
 (define-key inferior-map (kbd "e") 'my-ielm-start-or-switch)
 ;; Lisp dialects
 (define-key inferior-map (kbd "l l") 'run-geiser) ; Common Lisp
-(define-key inferior-map (kbd "l c") 'run-lisp) ; Clojure cider.
+(define-key inferior-map (kbd "l c") 'run-lisp)   ; Clojure cider.
 (define-key inferior-map (kbd "l s") 'run-scheme) ; Scheme
-(define-key inferior-map (kbd "l g") 'run-guile) ; Guile
+(define-key inferior-map (kbd "l g") 'run-guile)  ; Guile
+(define-key inferior-map (kbd "l m") 'slime)      ; SLIME
 ;; Ruby
-(define-key inferior-map (kbd "r") 'run-ruby) ; Ruby
+(define-key inferior-map (kbd "r") 'run-ruby)     ; Ruby
 ;; Python
-(define-key inferior-map (kbd "p") 'run-python) ; Python
+(define-key inferior-map (kbd "p") 'run-python)   ; Python
 ;; Prolog
-(define-key inferior-map (kbd "g") 'run-prolog) ; Prolog
+(define-key inferior-map (kbd "g") 'run-prolog)   ; Prolog
 ;; Octave
-(define-key inferior-map (kbd "o") 'run-octave) ; Octave
+(define-key inferior-map (kbd "o") 'run-octave)   ; Octave
 ;; Haskell
-(define-key inferior-map (kbd "h") 'run-haskell) ; Haskell
+(define-key inferior-map (kbd "h") 'run-haskell)  ; Haskell
 ;; Erlang
-(define-key inferior-map (kbd "e") 'run-erlang) ; Erlang
+(define-key inferior-map (kbd "e") 'run-erlang)   ; Erlang
 ;; Festival
 (define-key inferior-map (kbd "f") 'run-festival) ; Festival
 
