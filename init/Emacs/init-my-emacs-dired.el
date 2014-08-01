e4dd1a74cb724b5ddc1556c53fccc6f441483699
stardiviner <numbchild@gmail.com>
2014-08-01 20:04:21 +0800

    disable diredx.

--- init/Emacs/init-my-emacs-dired.el
+++ init/Emacs/init-my-emacs-dired.el
@@ -17,18 +17,18 @@
 
 ;;; [ direx ] --- direx.el is a simple directory explorer. It also works as a generic tree explore library.
 
-(require 'direx)
-
-;; (global-set-key (kbd "C-c C-j") 'direx:jump-to-directory)
-
-(push '(direx:direx-mode :position left :width 35 :dedicated t)
-      popwin:special-display-config)
-(global-set-key (kbd "C-c C-j") 'direx:jump-to-directory-other-window)
-
-;;; [ direx-project ] --- (bundled with direx.el) -- project tree explorer.
-(require 'direx-project)
-
-;; (global-set-key (kbd "C-c C-p") 'direx-project:jump-to-project-root)
+;; (require 'direx)
+;;
+;; ;; (global-set-key (kbd "C-c C-j") 'direx:jump-to-directory)
+;;
+;; (push '(direx:direx-mode :position left :width 35 :dedicated t)
+;;       popwin:special-display-config)
+;; (global-set-key (kbd "C-c C-j") 'direx:jump-to-directory-other-window)
+;;
+;; ;;; [ direx-project ] --- (bundled with direx.el) -- project tree explorer.
+;; (require 'direx-project)
+;;
+;; ;; (global-set-key (kbd "C-c C-p") 'direx-project:jump-to-project-root)
 
 
 ;;; [ emacs-dired-k ] -- highlights dired buffer like "k".
