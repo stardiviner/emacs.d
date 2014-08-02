b7611876bb3d82a53af386a107f90136248cb8a6
stardiviner <numbchild@gmail.com>
2014-08-02 08:22:20 +0800

    add D-Bus init file for Emacs.

---
+++ init/Emacs/init-my-emacs-dbus.el
@@ -0,0 +1,19 @@
+;;; init-my-emacs-dbus.el --- 
+;;; -*- coding: utf-8 -*-
+
+;;; Commentary:
+
+
+
+;;;_ Code:
+
+;;;_* D-Bus
+
+;;;_ + Usage
+;; - (dbus-init-bus :system) / (dbus-init-bus :session)
+
+
+
+(provide 'init-my-emacs-dbus)
+
+;;; init-my-emacs-dbus.el ends here
