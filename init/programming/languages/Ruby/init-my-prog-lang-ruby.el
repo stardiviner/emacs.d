e3275dfcad35e8a8d1b85f0dc0ba7240f656c97f
stardiviner <numbchild@gmail.com>
2014-08-03 20:40:18 +0800

    add ruby-lint and rubocop for flycheck style define.

--- init/programming/languages/Ruby/init-my-prog-lang-ruby.el
+++ init/programming/languages/Ruby/init-my-prog-lang-ruby.el
@@ -560,12 +560,46 @@
 ;;; [ yard-mode ] --- Minor mode for Ruby YARD comments
 
 
+;;; [ ruby-lint ]
+
+;;; $ gem install ruby-lint
+
+;; (flycheck-define-checker ruby-rubylint
+;;   "A Ruby syntax and style checker using the rubylint tool."
+;;   :command ("ruby-lint" source)
+;;   :error-patterns
+;;   ((warning line-start
+;;             (file-name) ":" line ":" column ": " (or "C" "W") ": " (message)
+;;             line-end)
+;;    (error line-start
+;;           (file-name) ":" line ":" column ": " (or "E" "F") ": " (message)
+;;           line-end))
+;;   :modes (ruby-mode enh-ruby-mode))
 
 
+
+;;; [ rubocop ]
 
+;;; $ gem install rubocop
 
+;; (require 'rubocop)
+;; (add-hook 'ruby-mode-hook 'rubocop-mode)
 
-
+;; (flycheck-define-checker ruby-rubocop
+;;   "A Ruby syntax and style checker using the RuboCop tool."
+;;   :command ("rubocop" "--format" "emacs" "--silent"
+;;             (config-file "--config" flycheck-rubocoprc)
+;;             source)
+;;   :error-patterns
+;;   ((warning line-start
+;;             (file-name) ":" line ":" column ": " (or "C" "W") ": " (message)
+;;             line-end)
+;;    (error line-start
+;;           (file-name) ":" line ":" column ": " (or "E" "F") ": " (message)
+;;           line-end))
+;;   :modes (ruby-mode
+;;           ;; enh-ruby-mode motion-mode
+;;           ))
 
 
 ;;; [ motion-mode ] -- RubyMotion
