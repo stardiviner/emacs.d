b1b0771812ca50f041c84212fe19fd53ce7c8f77
stardiviner <numbchild@gmail.com>
2014-08-15 21:25:16 +0800

    use helm for projectile completing system.

--- init/programming/project/init-my-prog-project.el
+++ init/programming/project/init-my-prog-project.el
@@ -59,7 +59,7 @@
 ;; If you want Projectile to be usable in every directory (even without the presence of project file):
 (setq projectile-require-project-root nil)
 ;; Completion Options
-(setq projectile-completion-system 'default ; 'ido, 'grizzl, 'default
+(setq projectile-completion-system 'helm ; 'helm, 'ido, 'grizzl, 'default
       projectile-use-git-grep nil)
 
 ;; (setq projectile-tags-command "ctags -Re %s")
