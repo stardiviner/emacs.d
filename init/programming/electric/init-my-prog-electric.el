13ae762f04536bc44b73687712c2bc8aef9b548c
stardiviner <numbchild@gmail.com>
2014-09-26 23:17:51 +0800

    migrate rainbow series packages into a separate init file.

--- init/programming/electric/init-my-prog-electric.el
+++ init/programming/electric/init-my-prog-electric.el
@@ -7,36 +7,12 @@
 
 ;;; Code:
 
-;;; Rainbow Mode (rainbow-mode.el -- colorize color names in buffers)
-
-(require 'rainbow-mode)
-(eval-after-load 'rainbow-mode
-  '(diminish 'rainbow-mode))
-
-(dolist (hook
-         '(emacs-lisp-mode-hook
-           css-mode-hook
-           html-mode-hook))
-  (add-hook hook (lambda () (rainbow-mode 1))))
-
-
-;;; Rainbow Delimiters
-
-(when (require 'rainbow-delimiters nil 'noerror)
-  (rainbow-delimiters-mode t)
-  ;; (global-rainbow-delimiters-mode) ; global
-  ;; enable in all programming-related modes
-  (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
-  ;; enable in specific modes
-  ;; (add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
-  )
-
-
 
 (require 'init-my-paredit)
 (require 'init-my-smartparens)
 ;; (require 'init-my-autopair)
 
+(require 'init-my-rainbow)
 
 
 (provide 'init-my-prog-electric)
---
+++ init/programming/electric/init-my-rainbow.el
@@ -0,0 +1,52 @@
+;;; init-my-rainbow.el --- 
+;;; -*- coding: utf-8 -*-
+
+;;; Commentary:
+
+
+
+;;; Code:
+
+;;; rainbow-mode -- colorize color names in buffers
+
+;; (require 'rainbow-mode)
+
+;; (eval-after-load 'rainbow-mode
+;;   '(diminish 'rainbow-mode))
+
+;; (dolist (hook
+;;          '(emacs-lisp-mode-hook
+;;            css-mode-hook
+;;            html-mode-hook))
+;;   (add-hook hook (lambda () (rainbow-mode 1))))
+
+
+;;; rainbow-delimiters
+
+;; (when (require 'rainbow-delimiters nil 'noerror)
+;;   (rainbow-delimiters-mode t)
+;;   ;; (global-rainbow-delimiters-mode) ; global
+;;   ;; enable in all programming-related modes
+;;   (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+;;   ;; enable in specific modes
+;;   ;; (add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
+;;   )
+
+
+;;; rainbow-identifiers
+
+;; (require 'rainbow-identifiers)
+
+;; (add-hook 'prog-mode-hook 'rainbow-identifiers-mode)
+
+
+;;; rainbow-block
+
+;; (require 'rainbow-blocks)
+
+;; (add-hook 'clojure-mode-hook 'rainbow-blocks-mode)
+
+
+(provide 'init-my-rainbow)
+
+;;; init-my-rainbow.el ends here
