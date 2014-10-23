22bf0fa16ba6b82bd91c74058e691ecc3c7550da
stardiviner <numbchild@gmail.com>
2014-10-23 19:52:31 +0800

    comment out tramp init.

--- init/Emacs/init-my-emacs-tramp.el
+++ init/Emacs/init-my-emacs-tramp.el
@@ -7,23 +7,23 @@
 
 ;;; Code:
 
-
 ;;; [ Tramp ]
+
 ;; Usage:
 ;; - [C-x C-f /sudo::/path/to/file]
 ;; - [C-x C-f /su::/path/to/file] -- edit stuff as root.
 ;; - [ssh]
 ;; - [telnet]
-;; (require 'tramp)
-(autoload 'tramp "Tramp" t)
 
-(setq tramp-default-method "ssh"        ; default value is "scpc" (ssh and scp)
-      ;; tramp-methods
-      tramp-chunksize 500
-      ;;; for debugging
-      tramp-debug-buffer-name t
-      tramp-verbose 10
-      )
+;; (require 'tramp)
+
+;; (setq tramp-default-method "ssh"        ; default value is "scpc" (ssh and scp)
+;;       ;; tramp-methods
+;;       tramp-chunksize 500
+;;       ;;; for debugging
+;;       tramp-debug-buffer-name t
+;;       tramp-verbose 10
+;;       )
 
 ;; change SHELL environment variable to solve Tramp hangs issue.
 ;; (eval-after-load 'tramp '(setenv "SHELL" "/bin/bash"))
