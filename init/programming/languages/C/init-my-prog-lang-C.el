86558b083f430fb517977aeeb4d67ff7f13a9a7f
stardiviner <numbchild@gmail.com>
2014-11-25 17:08:25 +0800

    configure company-irony.

--- init/programming/languages/C/init-my-prog-lang-C.el
+++ init/programming/languages/C/init-my-prog-lang-C.el
@@ -81,6 +81,18 @@
 (add-hook 'irony-mode-hook 'my-irony-mode-hook)
 
 
+;;; [ company-irony ]
+
+(eval-after-load 'company
+  '(add-to-list 'company-backends 'company-irony))
+
+;; (optional) adds CC special commands to `company-begin-commands' in order to
+;; trigger completion at interesting places, such as after scope operator
+;;     std::|
+(add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
+
+
+
 ;;; [ ac-irony ]
 
 ;; (defun my-ac-irony-setup ()
