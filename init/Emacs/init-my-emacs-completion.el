5be1261d34ae11886bcc28846909aa620f6d96ca
stardiviner <numbchild@gmail.com>
2014-11-26 12:30:29 +0800

    remove auto-complete and company-mode from capf completion-at-point-functions.

--- init/Emacs/init-my-emacs-completion.el
+++ init/Emacs/init-my-emacs-completion.el
@@ -10,11 +10,10 @@
 ;; (setq tab-always-indent 'complete)
 ;; (add-to-list 'completion-styles 'initials t)
 ;; (setq completion-cycle-threshold 5)
-(setq completion-at-point-functions '(company-complete
-                                      ;; auto-complete
-                                      ;; semantic-completion-at-point-function ; NOTE: require `semantic'.
-                                      elisp-completion-at-point)
-      )
+;; (setq completion-at-point-functions '(;; semantic-completion-at-point-function ; NOTE: require `semantic'.
+;;                                       elisp-completion-at-point)
+;;       ;; completion-styles
+;;       )
 
 
 ;;; [ pcomplete ] --- Programmable, Context-Sensitive Completion Library
