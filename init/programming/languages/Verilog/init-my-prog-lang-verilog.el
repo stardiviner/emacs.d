f99a98e8e4e80323ffc66b1612e4e2787205bce1
stardiviner <numbchild@gmail.com>
2014-11-26 12:38:28 +0800

    add verilog init file.

---
+++ init/programming/languages/Verilog/init-my-prog-lang-verilog.el
@@ -0,0 +1,17 @@
+;;; init-my-prog-lang-verilog.el --- init for Verilog.
+;;; -*- coding: utf-8 -*-
+
+;;; Commentary:
+
+
+
+;;; Code:
+
+
+
+
+
+
+(provide 'init-my-prog-lang-verilog)
+
+;;; init-my-prog-lang-verilog.el ends here
