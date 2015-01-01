ef5dbaef06a1c834e52df76713e6ab2f53db5dd9
stardiviner <numbchild@gmail.com>
2015-01-01 17:15:07 +0800

    disable custom function to use cursor-chg to indicate snippet available.

--- init/programming/snippet/init-my-prog-snippet.el
+++ init/programming/snippet/init-my-prog-snippet.el
@@ -307,26 +307,29 @@ $0"
 
 ;; It will test whether it can expand, if yes, cursor color -> green.
 
-(defun yasnippet-can-fire-p (&optional field)
-  (interactive)
-  (setq yas--condition-cache-timestamp (current-time))
-  (let (templates-and-pos)
-    (unless (and yas-expand-only-for-last-commands
-                 (not (member last-command yas-expand-only-for-last-commands)))
-      (setq templates-and-pos (if field
-                                  (save-restriction
-                                    (narrow-to-region (yas--field-start field)
-                                                      (yas--field-end field))
-                                    (yas--templates-for-key-at-point))
-                                (yas--templates-for-key-at-point))))
+;; (require 'cursor-chg)
 
-    (set-cursor-color (if (and templates-and-pos (first templates-and-pos)) 
-                          "yellow" "cyan"))
-    (curchg-set-cursor-type (if (and templates-and-pos (first templates-and-pos))
-                                "bar" "hbar"))))
+;; (defun yasnippet-can-fire-p (&optional field)
+;;   (interactive)
+;;   (setq yas--condition-cache-timestamp (current-time))
+;;   (let (templates-and-pos)
+;;     (unless (and yas-expand-only-for-last-commands
+;;                  (not (member last-command yas-expand-only-for-last-commands)))
+;;       (setq templates-and-pos (if field
+;;                                   (save-restriction
+;;                                     (narrow-to-region (yas--field-start field)
+;;                                                       (yas--field-end field))
+;;                                     (yas--templates-for-key-at-point))
+;;                                 (yas--templates-for-key-at-point))))
 
-;; As pointed out by Dmitri, this will make sure it will update color when needed.
-(add-hook 'post-command-hook 'yasnippet-can-fire-p)
+;;     (set-cursor-color (if (and templates-and-pos (first templates-and-pos)) 
+;;                           "yellow" "cyan"))
+;;     (curchg-set-cursor-type (if (and templates-and-pos (first templates-and-pos))
+;;                                 "bar" "hbar"))))
+
+;; ;; As pointed out by Dmitri, this will make sure it will update color when needed.
+;; (add-hook 'post-command-hook 'yasnippet-can-fire-p)
+
 (diminish 'yas-minor-mode)
 
 
