d2ef62e53fc97ac95285627653eb39a09cf8b7c6
stardiviner <numbchild@gmail.com>
2015-01-20 14:59:59 +0800

    disable many latex packages.

--- init/programming/languages/TeX/init-my-prog-lang-tex.el
+++ init/programming/languages/TeX/init-my-prog-lang-tex.el
@@ -33,21 +33,29 @@
 
 ;;;_ [ auto-complete-latex ]
 
-(require 'auto-complete-latex)
-
-(setq ac-l-dict-directory "~/.emacs.d/el-get/auto-complete-latex/ac-l-dict/")
-
-(add-to-list 'ac-modes 'latex-mode)
-
-(dolist (hook '(latex-mode-hook
-                LaTeX-mode-hook
-                ))
-  (add-hook hook 'ac-l-setup))
+;; (require 'auto-complete-latex)
+;;
+;; (setq ac-l-dict-directory "~/.emacs.d/el-get/auto-complete-latex/ac-l-dict/")
+;;
+;; (add-to-list 'ac-modes 'latex-mode)
+;;
+;; (dolist (hook '(latex-mode-hook
+;;                 LaTeX-mode-hook
+;;                 ))
+;;   (add-hook hook 'ac-l-setup))
 
 
-;;;_ [ ac-math ]
+;;; [ ac-math ]
 
 ;; (require 'ac-math)
+
+;; TODO: https://github.com/vspinu/ac-math
+;; https://github.com/vspinu/math-symbol-lists
+
+
+;;; [ company-math ]
+
+;; TODO: https://github.com/vspinu/company-math
 
 
 ;;;_ [ reftex ]
