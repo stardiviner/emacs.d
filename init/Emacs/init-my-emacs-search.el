2628d107ecd0312ca2548e6081b527fa0bc11af9
stardiviner <numbchild@gmail.com>
2015-01-26 23:28:59 +0800

    add all rectangle commands under my custom rectangle map.

--- init/Emacs/init-my-emacs-search.el
+++ init/Emacs/init-my-emacs-search.el
@@ -38,7 +38,39 @@
 ;; - [C-M-%] – Start a regular-expression query replace using the current search
 ;;   string.
 
+
+(setq query-replace-show-replacement t
+      ;; query-replace-from-to-separator "->"
+      )
+
 ;; (setq isearch-lazy-highlight-??)
+
+(unless (boundp 'my-rectangle-map)
+  (define-prefix-command 'my-rectangle-map))
+(global-set-key (kbd "C-x r r") 'my-rectangle-map)
+
+(global-unset-key (kbd "C-x r N"))
+(global-unset-key (kbd "C-x r t"))
+(global-unset-key (kbd "C-x r c"))
+(global-unset-key (kbd "C-x r i"))
+(global-unset-key (kbd "C-x r n"))
+(global-unset-key (kbd "C-x r o"))
+(global-unset-key (kbd "C-x r y"))
+(global-unset-key (kbd "C-x r k"))
+(global-unset-key (kbd "C-x r d"))
+(global-unset-key (kbd "C-x r M-w"))
+
+(define-key my-rectangle-map (kbd "r") 'rectangle-mark-mode)
+(define-key my-rectangle-map (kbd "m") 'rectangle-mark-mode)
+(define-key my-rectangle-map (kbd "c") 'copy-rectangle-to-register)
+(define-key my-rectangle-map (kbd "M-w") 'copy-rectangle-as-kill)
+(define-key my-rectangle-map (kbd "y") 'yank-rectangle)
+(define-key my-rectangle-map (kbd "x") 'clear-rectangle)
+(define-key my-rectangle-map (kbd "d") 'delete-rectangle)
+(define-key my-rectangle-map (kbd "k") 'kill-rectangle)
+(define-key my-rectangle-map (kbd "o") 'open-rectangle)
+(define-key my-rectangle-map (kbd "t") 'string-rectangle)
+(define-key my-rectangle-map (kbd "N") 'rectangle-number-lines)
 
 ;;; swap isearch with isearch-regexp.
 ;; replace [C-s] default (isearch-forward), press [M-r] to toggle isearch-regexp.
