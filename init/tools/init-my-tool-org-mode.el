76133496bf74b18b3d33a66bc6b957ccf780d6c7
stardiviner <numbchild@gmail.com>
2015-02-15 09:08:47 +0800

    add 'org-todo-list into my custom org prefix map.

--- init/tools/init-my-tool-org-mode.el
+++ init/tools/init-my-tool-org-mode.el
@@ -2528,6 +2528,8 @@ This function will promote all items in a subtree."
 ;;;_* custom keybindings
 
 (define-key my-org-prefix-map (kbd "a") 'org-agenda)
+;; [C-u C-c o t] -- prompt for a keyword for todo list.
+(define-key my-org-prefix-map (kbd "t") 'org-todo-list)
 (define-key my-org-prefix-map (kbd "c") 'org-capture)
 (define-key my-org-prefix-map (kbd "l") 'org-insert-link-global)
 (define-key my-org-prefix-map (kbd "u") 'org-open-at-point-global)
