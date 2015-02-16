f36c56f5e81d3c4ec74c7f3aa5315964c11cfeb1
stardiviner <numbchild@gmail.com>
2015-02-16 09:33:57 +0800

    adjust SLIME config.

--- init/programming/languages/Lisp/init-my-prog-lang-lisp.el
+++ init/programming/languages/Lisp/init-my-prog-lang-lisp.el
@@ -20,28 +20,7 @@
 ;;; Usage:
 ;;; - [M-x slime] ::
 
-;; (load (expand-file-name "~/quicklisp/slime-helper.el"))
-;; (require 'slime)
-
-;; NOTE: currently using slime from el-get installation.
-
-;;; -----------------------------------
-(require 'slime-autoloads)
-
 (setq inferior-lisp-program "sbcl")
-;; (setq slime-contribs '(slime-fancy))
-;;; -----------------------------------
-
-;; a list of alternative Common Lisp implementations that can be
-;; used with SLIME. Note that their presence render
-;; inferior-lisp-program useless. This variable holds a list of
-;; programs and if you invoke SLIME with a negative prefix
-;; argument, M-- M-x slime, you can select a program from that list.
-(setq slime-lisp-implementations
-      '((ccl ("ccl"))
-        (clisp ("clisp" "-q"))
-        (cmucl ("cmucl" "-quiet"))
-        (sbcl ("sbcl" "--noinform") :coding-system utf-8-unix)))
 
 ;; select the default value from slime-lisp-implementations
 (if (eq system-type 'darwin)
@@ -50,24 +29,18 @@
   ;; default to SBCL on Linux and Windows
   (setq slime-default-lisp 'sbcl))
 
-(defun my-start-slime ()
-  "Start SLIME unless it's already running."
-  (unless (slime-connected-p)
-    (save-excursion (slime))))
+;; a list of alternative Common Lisp implementations that can be
+;; used with SLIME. Note that their presence render
+;; inferior-lisp-program useless. This variable holds a list of
+;; programs and if you invoke SLIME with a negative prefix
+;; argument, M-- M-x slime, you can select a program from that list.
+;;
+;; (setq slime-lisp-implementations
+;;       '((ccl ("ccl"))
+;;         (clisp ("clisp" "-q"))
+;;         (cmucl ("cmucl" "-quiet"))
+;;         (sbcl ("sbcl" "--noinform") :coding-system utf-8-unix)))
 
-;; start slime automatically when we open a lisp file
-(add-hook 'slime-mode-hook 'my-start-slime)
-
-(eval-after-load 'slime
-  '(progn
-     (setq slime-complete-symbol-function 'slime-fuzzy-complete-symbol ; 'slime-simple-complete-symbol.
-           slime-fuzzy-completion-in-place t
-           slime-enable-evaluate-in-emacs t
-           slime-autodoc-use-multiline-p t)
-     
-     (define-key slime-mode-map (kbd "TAB") 'slime-indent-and-complete-symbol)
-     (define-key slime-mode-map (kbd "C-c i") 'slime-inspect)
-     (define-key slime-mode-map (kbd "C-c C-s") 'slime-selector)))
 
 
 ;;; [ slime-company ]
