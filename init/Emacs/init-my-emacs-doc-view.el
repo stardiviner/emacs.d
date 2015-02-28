0ac0475e19f3aea7aa86856849e77c3715f2183b
stardiviner <numbchild@gmail.com>
2015-02-28 16:25:06 +0800

    load doc-view, and configure it.

--- init/Emacs/init-my-emacs-doc-view.el
+++ init/Emacs/init-my-emacs-doc-view.el
@@ -9,7 +9,19 @@
 
 ;;; [ doc-view ]
 
+;;; Usage:
+;;
+;; - [] ::
 
+(require 'doc-view)
+
+(setq doc-view-continuous t
+      ;; doc-view--image-type ; 'png, 'tiff
+      ;; doc-view-doc-type ; Can be `dvi', `pdf', or `ps'.
+      ;; doc-view-cache-directory "/tmp/docview1000"
+      doc-view-scale-internally t
+      doc-view-image-width 850
+      )
 
 
 
