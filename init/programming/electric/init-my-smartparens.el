c9f549d7db7fbafc74844312e6b9341db973e183
stardiviner <numbchild@gmail.com>
2015-03-07 13:01:51 +0800

    improve smartparens-mode config.

--- init/programming/electric/init-my-smartparens.el
+++ init/programming/electric/init-my-smartparens.el
@@ -42,8 +42,15 @@
 ;;;_* require
 (require 'smartparens-config)
 
+;;;_* enable smartparens-mode
+(smartparens-global-mode t)
+(show-smartparens-global-mode t)
+
+
 ;;;_* options
 (setq sp-highlight-wrap-overlay t
+      sp-highlight-pair-overlay t
+      sp-highlight-wrap-tag-overlay t
       ;; sp-undo-pairs-separately
       sp-navigate-consider-sgml-tags '(html-erb-mode
                                        jinja2-mode
@@ -63,6 +70,9 @@
       sp-autoinsert-pair t
       sp-autoinsert-if-followed-by-word t
       sp-show-pair-delay 0.125
+      ;; sp-show-enclosing-pair-commands '(sp-show-enclosing-pair
+      ;;                                   sp-forward-slurp-sexp sp-backward-slurp-sexp
+      ;;                                   sp-forward-barf-sexp sp-backward-barf-sexp)
       sp-navigate-close-if-unbalanced nil
       ;; sp-paredit-bindings '(("C-M-f" . sp-forward-sexp)
       ;;                       ("C-M-b" . sp-backward-sexp)
@@ -124,14 +134,9 @@
       sp-autodelete-closing-pair t
       sp-autodelete-pair t
       sp-message-width 'frame
-      sp-highlight-pair-overlay t
       ;; sp-show-pair-overlays t
       ;; sp-show-pair-enc-overlays
       )
-
-;;;_* enable smartparens-mode
-(smartparens-global-mode t)
-(show-smartparens-global-mode t)
 
 ;;;_* set smartparens faces.
 (set-face-attribute 'sp-pair-overlay-face nil
@@ -166,7 +171,64 @@
   (add-hook hook 'turn-on-smartparens-strict-mode))
 
 ;;;_ + TODO: add paredit like keybindings with smartparens-mode.
+;; add paredit like keybindings with smartparens-mode.
 ;; (define-key smartparens-strict-mode-map (kbd "C-M-b") 'sp-backward-sexp)
+
+;;;;;;;;;;;;;;;;;;;;;;;;
+;; keybinding management
+;;
+;; (define-key sp-keymap (kbd "C-M-f") 'sp-forward-sexp)
+;; (define-key sp-keymap (kbd "C-M-b") 'sp-backward-sexp)
+;;
+;; (define-key sp-keymap (kbd "C-M-d") 'sp-down-sexp)
+;; (define-key sp-keymap (kbd "C-M-a") 'sp-backward-down-sexp)
+;; (define-key sp-keymap (kbd "C-S-a") 'sp-beginning-of-sexp)
+;; (define-key sp-keymap (kbd "C-S-d") 'sp-end-of-sexp)
+;;
+;; (define-key sp-keymap (kbd "C-M-e") 'sp-up-sexp)
+;; (define-key emacs-lisp-mode-map (kbd ")") 'sp-up-sexp)
+;; (define-key sp-keymap (kbd "C-M-u") 'sp-backward-up-sexp)
+;; (define-key sp-keymap (kbd "C-M-t") 'sp-transpose-sexp)
+;;
+;; (define-key sp-keymap (kbd "C-M-n") 'sp-next-sexp)
+;; (define-key sp-keymap (kbd "C-M-p") 'sp-previous-sexp)
+;;
+;; (define-key sp-keymap (kbd "C-M-k") 'sp-kill-sexp)
+;; (define-key sp-keymap (kbd "C-M-w") 'sp-copy-sexp)
+;;
+;; (define-key sp-keymap (kbd "M-<delete>") 'sp-unwrap-sexp)
+;; (define-key sp-keymap (kbd "M-<backspace>") 'sp-backward-unwrap-sexp)
+;;
+;; (define-key sp-keymap (kbd "C-<right>") 'sp-forward-slurp-sexp)
+;; (define-key sp-keymap (kbd "C-<left>") 'sp-forward-barf-sexp)
+;; (define-key sp-keymap (kbd "C-M-<left>") 'sp-backward-slurp-sexp)
+;; (define-key sp-keymap (kbd "C-M-<right>") 'sp-backward-barf-sexp)
+;;
+;; (define-key sp-keymap (kbd "M-D") 'sp-splice-sexp)
+;; (define-key sp-keymap (kbd "C-M-<delete>") 'sp-splice-sexp-killing-forward)
+;; (define-key sp-keymap (kbd "C-M-<backspace>") 'sp-splice-sexp-killing-backward)
+;; (define-key sp-keymap (kbd "C-S-<backspace>") 'sp-splice-sexp-killing-around)
+;;
+;; (define-key sp-keymap (kbd "C-]") 'sp-select-next-thing-exchange)
+;; (define-key sp-keymap (kbd "C-<left_bracket>") 'sp-select-previous-thing)
+;; (define-key sp-keymap (kbd "C-M-]") 'sp-select-next-thing)
+;;
+;; (define-key sp-keymap (kbd "M-F") 'sp-forward-symbol)
+;; (define-key sp-keymap (kbd "M-B") 'sp-backward-symbol)
+;;
+;; (define-key sp-keymap (kbd "H-t") 'sp-prefix-tag-object)
+;; (define-key sp-keymap (kbd "H-p") 'sp-prefix-pair-object)
+;; (define-key sp-keymap (kbd "H-s c") 'sp-convolute-sexp)
+;; (define-key sp-keymap (kbd "H-s a") 'sp-absorb-sexp)
+;; (define-key sp-keymap (kbd "H-s e") 'sp-emit-sexp)
+;; (define-key sp-keymap (kbd "H-s p") 'sp-add-to-previous-sexp)
+;; (define-key sp-keymap (kbd "H-s n") 'sp-add-to-next-sexp)
+;; (define-key sp-keymap (kbd "H-s j") 'sp-join-sexp)
+;; (define-key sp-keymap (kbd "H-s s") 'sp-split-sexp)
+
+
+
+(sp-local-pair 'minibuffer-inactive-mode "'" nil :actions nil)
 
 ;;;_* smartparens for other modes.
 (require 'smartparens-ruby)
@@ -176,8 +238,17 @@
   ;; (sp-local-pair "<" ">")
   (sp-local-pair "<%" "%>"))
 
+;;; html-mode
+;; (sp-with-modes '(html-mode sgml-mode)
+;;   (sp-local-pair "<" ">"))
 (require 'smartparens-html)
 
+;;; markdown-mode
+(sp-with-modes '(markdown-mode gfm-mode rst-mode)
+  (sp-local-pair "*" "*" :bind "C-*")
+  (sp-local-tag "2" "**" "**")
+  (sp-local-tag "s" "```scheme" "```")
+  (sp-local-tag "<"  "<_>" "</_>" :transform 'sp-match-sgml-tags))
 
 
 ;;;_ provide init
