ae58c267d73517678ebc5cdff677c178de0b5b0e
stardiviner <numbchild@gmail.com>
2015-03-29 17:22:30 +0800

    delete duplicate elements in ac-modes when setting.

--- init/Emacs/init-auto-complete.el
+++ init/Emacs/init-auto-complete.el
@@ -72,18 +72,20 @@
 ;; (if (null global-auto-complete-mode)
 ;;     )
 (setq ac-modes
-      (append ac-modes
-              '(prog-mode               ; programming modes
-                web-mode
-                text-mode markdown-mode
-                change-log-mode
-                org-mode ; speed up org-mode typing by disabling auto-complete.
-                mail-mode mu4e-compose-mode
-                ;; objc-mode
-                sql-mode js3-mode
-                makefile-mode makefile-gmake-mode makefile-automake-mode
-                autoconf-mode
-                snippet-mode)))
+      (delete-dups
+       (append ac-modes
+               '(prog-mode               ; programming modes
+                 web-mode
+                 text-mode markdown-mode
+                 change-log-mode
+                 org-mode ; speed up org-mode typing by disabling auto-complete.
+                 mail-mode mu4e-compose-mode
+                 ;; objc-mode
+                 sql-mode js3-mode
+                 makefile-mode makefile-gmake-mode makefile-automake-mode
+                 autoconf-mode
+                 snippet-mode)))
+      )
 
 
 
