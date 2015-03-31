f3d97353911600ad2825301a5720a188e60c5003
stardiviner <numbchild@gmail.com>
2015-03-31 21:27:21 +0800

    add two highlight packages: highlight-quoted, highlight-numbers.

--- init/Emacs/init-my-emacs-highlight.el
+++ init/Emacs/init-my-emacs-highlight.el
@@ -7,8 +7,34 @@
 
 ;;; Code:
 
+;;; highlight-quoted
 
+(load-file (expand-file-name "init/extensions/highlight-quoted.el" user-emacs-directory))
 
+(add-hook 'prog-mode-hook 'highlight-quoted-mode)
+
+;; (setq highlight-quoted-highlight-symbols t)
+
+(eval-after-load 'highlight-quoted
+  (progn
+    (set-face-attribute 'highlight-quoted-quote nil
+                        :inherit 'font-lock-keyword-face)
+    (set-face-attribute 'highlight-quoted-symbol nil
+                        :inherit 'font-lock-constant-face))
+  )
+
+
+;;; highlight-numbers
+
+(load-file (expand-file-name "init/extensions/highlight-numbers.el" user-emacs-directory))
+
+(add-hook 'prog-mode-hook 'highlight-numbers-mode)
+
+;; (setq highlight-numbers-modelist)
+
+(eval-after-load 'highlight-numbers
+  (set-face-attribute 'highlight-numbers-number nil
+                      :inherit 'font-lock-constant-face))
 
 
 
