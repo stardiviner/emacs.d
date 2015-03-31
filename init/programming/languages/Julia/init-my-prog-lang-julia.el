9bec7a5392a265dc80da2e78cd3d52de47f40418
stardiviner <numbchild@gmail.com>
2015-03-31 21:30:20 +0800

    add Julia init file.

---
+++ init/programming/languages/Julia/init-my-prog-lang-julia.el
@@ -0,0 +1,17 @@
+;;; init-my-prog-lang-julia.el --- init for Julia
+;;; -*- coding: utf-8 -*-
+
+;;; Commentary:
+
+
+
+;;; Code:
+
+
+
+
+
+
+(provide 'init-my-prog-lang-julia)
+
+;;; init-my-prog-lang-julia.el ends here
