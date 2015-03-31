29050b31137575b1fe90087e0e08119673eeb0e6
stardiviner <numbchild@gmail.com>
2015-03-31 21:31:44 +0800

    set [C-r] to swiper too. and disable ivy customization.

--- init/Emacs/init-my-emacs-search.el
+++ init/Emacs/init-my-emacs-search.el
@@ -181,25 +181,27 @@
 ;;   (setq swiper-completion-method 'ivy)
 ;;   )
 
-(eval-after-load 'ivy
-  (lambda ()
-    (setq ivy-height 10)
-    ;; (defcustom ivy-height 10
-    ;;   "Number of lines for the minibuffer window."
-    ;;   :type 'integer)
-
-    (set-face-attribute 'ivy-current-match nil
-                        :foreground nil
-                        :background (color-darken-name (face-background 'default) 5)
-                        )
-    ))
+;; (eval-after-load 'ivy
+;;   (setq ivy-height 10)
+;;   ;; (defcustom ivy-height 10
+;;   ;;   "Number of lines for the minibuffer window."
+;;   ;;   :type 'integer)
+;;
+;;   (set-face-attribute 'ivy-current-match nil
+;;                       :foreground nil
+;;                       :background (color-darken-name (face-background 'default) 5)
+;;                       )
+;;   )
 
 
 (define-key my-search-prefix-map (kbd "C-s") 'swiper)
+(define-key my-search-prefix-map (kbd "C-r") 'swiper)
 
 ;; if swiper is available, then replace `vr/isearch' with `swiper'.
 (if (functionp 'swiper)
-    (global-set-key (kbd "C-s") 'swiper))
+    (progn
+      (global-set-key (kbd "C-s") 'swiper)
+      (global-set-key (kbd "C-r") 'swiper)))
 
 
 ;;; [ Lazy Search ]
