41e326967c96402a51966cdd88e65dfc49d4d2a6
stardiviner <numbchild@gmail.com>
2015-04-24 12:28:51 +0800

    add EWW keybindings.

--- init/tools/init-my-tool-browser.el
+++ init/tools/init-my-tool-browser.el
@@ -35,14 +35,42 @@
       eww-use-external-browser-for-content-type "\\`\\(video/\\|audio/\\|application/ogg\\)"
       )
 
-(set-face-attribute 'eww-form-checkbox nil
-                    :box '(:color "cyan" :line-width 2 :style 'released-button)
-                    :foreground "gray" :background "black"
-                    )
+(eval-after-load "eww"
+  (lambda ()
+    (set-face-attribute 'eww-form-checkbox nil
+                        :box '(:color "cyan" :line-width 2 :style 'released-button)
+                        :foreground "gray" :background "black"
+                        )
 
-(define-key eww-mode-map (kbd "o") 'eww) ; prompt for a URL.
-(define-key eww-mode-map (kbd "b") 'eww-list-bookmarks)
-(define-key eww-mode-map (kbd "B") 'eww-add-bookmark)
+    (define-key eww-mode-map (kbd "o") 'eww) ; prompt for a URL.
+
+    (define-key eww-mode-map (kbd "f") 'eww-follow-link)
+    (define-key eww-mode-map (kbd "d") 'eww-download)
+    
+    (define-key eww-mode-map (kbd "C-i") 'eww-back-url)
+    (define-key eww-mode-map (kbd "C-o") 'eww-forward-url)
+    (define-key eww-mode-map (kbd "r") 'eww-reload)
+
+    (define-key eww-mode-map (kbd "<C-tab>") 'eww-buffer-select) ; 'eww-buffer-show, 'eww-list-buffers
+    ;; (define-key eww-mode-map (kbd "<C-tab>") 'eww-buffer-show-next)
+    ;; (define-key eww-mode-map (kbd "<C-S-iso-lefttab>") 'eww-buffer-show-previous)
+    
+    (define-key eww-mode-map (kbd "h") 'eww-history-browse) ; 'eww-list-histories
+    
+    (define-key eww-mode-map (kbd "b") 'eww-list-bookmarks)
+    (define-key eww-mode-map (kbd "B") 'eww-add-bookmark)
+    (define-key eww-mode-map (kbd "M-n") nil)
+    (define-key eww-mode-map (kbd "M-p") nil)
+
+    (define-key eww-mode-map (kbd "&") 'eww-browse-with-external-browser)
+
+    (define-key eww-mode-map (kbd "v") 'eww-view-source)
+    (define-key eww-mode-map (kbd "H") 'eww-parse-headers)
+    (define-key eww-mode-map (kbd "R") 'eww-readable)
+
+    (define-key eww-mode-map (kbd "<enter>") 'eww-submit)
+    )
+  )
 
 (define-key my-tools-prefix-map (kbd "b") 'eww)
 
