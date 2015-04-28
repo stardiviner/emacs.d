32fa285c7b9e99f49b34892b310c0f662c26c07f
stardiviner <numbchild@gmail.com>
2015-04-28 21:08:36 +0800

    migrate highlight-symbol.

--- init/Emacs/init-my-emacs-apperance.el
+++ init/Emacs/init-my-emacs-apperance.el
@@ -567,47 +567,6 @@
 ;;       pretty-symbol-patterns '())
 
 
-;;; [ highlight-symbol ] -- automatic and manual symbol highlighting for Emacs
-
-;;; Usage:
-;;
-;; Use highlight-symbol-at-point to toggle highlighting of the symbol at point
-;; throughout the current buffer. Use highlight-symbol-mode to keep the symbol
-;; at point highlighted.
-;;
-;; The functions highlight-symbol-next, highlight-symbol-prev,
-;; highlight-symbol-next-in-defun and highlight-symbol-prev-in-defun allow for
-;; cycling through the locations of any symbol at point. Use
-;; highlight-symbol-nav-mode to enable key bindings (M-p and M-p) for
-;; navigation. When highlight-symbol-on-navigation-p is set, highlighting is
-;; triggered regardless of highlight-symbol-idle-delay.
-;;
-;; highlight-symbol-query-replace can be used to replace the symbol.
-
-(require 'highlight-symbol)
-
-;; (global-set-key [(control f3)] 'highlight-symbol-at-point)
-;; (global-set-key [f3] 'highlight-symbol-next)
-;; (global-set-key [(shift f3)] 'highlight-symbol-prev)
-;; (global-set-key [(meta f3)] 'highlight-symbol-query-replace)
-
-(setq highlight-symbol-idle-delay 1.5
-      highlight-symbol-border-pattern '("\\_<" . "\\_>")
-      highlight-symbol-colors '("yellow" "DeepPink" "cyan"
-                                "MediumPurple1" "SpringGreen1" "DarkOrange" "HotPink1"
-                                "RoyalBlue1" "OliveDrab")
-      ;; highlight-symbol-foreground-color
-      )
-
-(set-face-attribute 'highlight-symbol-face nil
-                    :foreground nil :background "midnight blue"
-                    :slant 'italic)
-
-(add-hook 'prog-mode-hook 'highlight-symbol-mode)
-
-(define-key my-search-prefix-map (kbd "h") 'highlight-symbol-at-point)
-
-
 ;;; [ page-break-lines-mode ] --- page breaks (^L characters) are displayed as a horizontal line of a character.
 
 ;;; In Page Break mode, page breaks (^L characters) are displayed as a horizontal line of `page-break-string-char' characters.
--- init/Emacs/init-my-emacs-highlight.el
+++ init/Emacs/init-my-emacs-highlight.el
@@ -7,6 +7,45 @@
 
 ;;; Code:
 
+;;; [ highlight-symbol ] -- automatic and manual symbol highlighting for Emacs
+
+;;; Usage:
+;;
+;; Use highlight-symbol-at-point to toggle highlighting of the symbol at point
+;; throughout the current buffer. Use highlight-symbol-mode to keep the symbol
+;; at point highlighted.
+;;
+;; The functions highlight-symbol-next, highlight-symbol-prev,
+;; highlight-symbol-next-in-defun and highlight-symbol-prev-in-defun allow for
+;; cycling through the locations of any symbol at point. Use
+;; highlight-symbol-nav-mode to enable key bindings (M-p and M-p) for
+;; navigation. When highlight-symbol-on-navigation-p is set, highlighting is
+;; triggered regardless of highlight-symbol-idle-delay.
+;;
+;; highlight-symbol-query-replace can be used to replace the symbol.
+
+(require 'highlight-symbol)
+
+;; (global-set-key [(control f3)] 'highlight-symbol-at-point)
+;; (global-set-key [f3] 'highlight-symbol-next)
+;; (global-set-key [(shift f3)] 'highlight-symbol-prev)
+;; (global-set-key [(meta f3)] 'highlight-symbol-query-replace)
+
+(setq highlight-symbol-idle-delay 1.5
+      highlight-symbol-border-pattern '("\\_<" . "\\_>")
+      highlight-symbol-colors '("yellow" "DeepPink" "cyan"
+                                "MediumPurple1" "SpringGreen1" "DarkOrange" "HotPink1"
+                                "RoyalBlue1" "OliveDrab")
+      ;; highlight-symbol-foreground-color
+      )
+
+(set-face-attribute 'highlight-symbol-face nil
+                    :foreground nil :background "midnight blue"
+                    :slant 'italic)
+
+(add-hook 'prog-mode-hook 'highlight-symbol-mode)
+
+(define-key my-search-prefix-map (kbd "h") 'highlight-symbol-at-point)
 ;;; highlight-quoted
 
 (load-file (expand-file-name "init/extensions/highlight-quoted.el" user-emacs-directory))
