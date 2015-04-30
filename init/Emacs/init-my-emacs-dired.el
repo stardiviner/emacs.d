016f6e44b590fcf3d8bbe85709af59f283241ed8
stardiviner <numbchild@gmail.com>
2015-04-30 18:14:54 +0800

    don't keep opened Dired buffers.

--- init/Emacs/init-my-emacs-dired.el
+++ init/Emacs/init-my-emacs-dired.el
@@ -28,6 +28,21 @@
         ("\\.ods\\'\\|\\.xlsx?\\'\\|\\.docx?\\'\\|\\.csv\\'" "libreoffice") ; Office
         ))
 
+;; Another thing that did annoy me was the fact that when I traverse the
+;; directory hierarchy, I leave a trail of open Dired buffers with all the
+;; directories I go through. This is also easy to change: just enable the
+;; `dired-find-alternate-file' function (bound to a in Dired) and use it to
+;; visit a file or directory in place (IOW, open it instead of the current Dired
+;; buffer – this also works for files I want to visit!).
+;;
+(put 'dired-find-alternate-file 'disabled nil) ; key [a] in Dired.
+
+
+
+;; How to make dired use the same buffer for viewing directory?
+(define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file) ; was dired-advertised-find-file
+(define-key dired-mode-map (kbd "^") (lambda () (interactive) (find-alternate-file "..")))  ; was dired-up-directory
+
 
 ;;; [ direx ] --- direx.el is a simple directory explorer. It also works as a generic tree explore library.
 
