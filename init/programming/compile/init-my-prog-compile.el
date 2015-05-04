4b1040ff4df23b621935a5455001b53f67e1c41d
stardiviner <numbchild@gmail.com>
2015-05-04 13:03:25 +0800

    custom compilation-mode faces.

--- init/programming/compile/init-my-prog-compile.el
+++ init/programming/compile/init-my-prog-compile.el
@@ -8,6 +8,43 @@
 
 (setq compilation-scroll-output t)
 
+(set-face-attribute 'compilation-info nil
+                    :background (color-darken-name (face-background 'default) 5)
+                    :underline nil
+                    :weight 'normal
+                    )
+(set-face-attribute 'compilation-line-number nil
+                    :background (color-darken-name (face-background 'default) 10)
+                    :foreground "cyan"
+                    :underline nil
+                    :weight 'bold
+                    :box '(:color "black" :line-width -1))
+(set-face-attribute 'compilation-column-number nil
+                    :background (color-darken-name (face-background 'default) 10)
+                    :foreground "orange"
+                    :underline nil
+                    :weight 'bold :slant 'normal
+                    :box '(:color "black" :line-width -1))
+(set-face-attribute 'compilation-info nil
+                    :background (color-darken-name (face-background 'default) 5)
+                    :underline nil
+                    :weight 'normal
+                    )
+(set-face-attribute 'compilation-warning nil
+                    :foreground "orange")
+(set-face-attribute 'compilation-error nil
+                    :background "dark red"
+                    ;; :family nil
+                    )
+(set-face-attribute 'compilation-mode-line-exit nil
+                    :inherit 'compilation-info
+                    :foreground "forest green")
+(set-face-attribute 'compilation-mode-line-fail nil
+                    :foreground "black" :background "dark red")
+(set-face-attribute 'compilation-mode-line-run nil
+                    :foreground "black" :background nil)
+
+
 
 ;;; [ smart-compile ]
 
