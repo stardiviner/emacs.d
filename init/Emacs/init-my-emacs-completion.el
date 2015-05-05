ed2c9720c2d4db1e4dda3ef7da0e241875ae8a92
stardiviner <numbchild@gmail.com>
2015-05-05 10:40:16 +0800

    configure icomplete-mode.

--- init/Emacs/init-my-emacs-completion.el
+++ init/Emacs/init-my-emacs-completion.el
@@ -23,9 +23,30 @@
 
 ;;; [ Icomplete (icomplete/ido/iswitchb) ] -- enhance the default minibuffer completion.
 
-;; (require 'icomplete)
+;;; Usage:
+;;
+;; - [M-x icomplete-mode] :: toggle `icomplete-mode'.
+;; - See `icomplete-completions' docstring for a description of the icomplete
+;;   display format.
 
-;; (icomplete-mode 1)
+
+(require 'icomplete)
+
+;; (add-hook 'icomplete-minibuffer-setup-hook
+;;           ())
+
+(icomplete-mode 1)
+
+(setq icomplete-compute-delay 0.3
+      icomplete-max-delay-chars 2
+      icomplete-in-buffer nil ; also use Icomplete when completing in non-mini buffers.
+      icomplete-separator " | "
+      icomplete-hide-common-prefix t
+      icomplete-show-matches-on-no-input nil
+      )
+
+(set-face-attribute 'icomplete-first-match nil
+                    :weight 'bold)
 
 
 ;;; [ Hippie ] --- hippie complete.
