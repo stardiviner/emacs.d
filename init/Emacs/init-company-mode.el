7794868a72b49628d6cbe2f24011b04c0d99a247
stardiviner <numbchild@gmail.com>
2015-05-14 12:23:26 +0800

    update company-mode configs.

--- init/Emacs/init-company-mode.el
+++ init/Emacs/init-company-mode.el
@@ -22,24 +22,24 @@
 
 (setq-default company-minimum-prefix-length 2   ; minimum prefix character number for auto complete.
               company-idle-delay 0.3
+              company-auto-complete t ; 'company-explicit-action-p, t, nil, 'function
+              ;; company-auto-complete-chars
+              company-require-match 'company-explicit-action-p ; 'company-explicit-action-p, t, nil, 'function
               company-echo-delay 0
               ;; company-show-numbers t
               company-tooltip-align-annotations t ; align annotations to the right tooltip border.
+              company-tooltip-flip-when-above t
               company-tooltip-limit 10          ; tooltip candidates max limit.
-              company-tooltip-minimum 6         ; minimum candidates limit.
+              company-tooltip-minimum 6         ; minimum candidates height limit.
               company-tooltip-minimum-width 0   ; The minimum width of the tooltip's inner area.
                                         ; This doesn't include the margins and the scroll bar.
-              company-tooltip-margin 1          ; width of margin columns to show around the tooltip
+              company-tooltip-margin 2          ; width of margin columns to show around the tooltip
               company-tooltip-offset-display 'scrollbar ; 'lines - how to show tooltip unshown candidates number.
               company-show-numbers nil ; t: show quick-access numbers for the first ten candidates.
               company-selection-wrap-around t ; loop over candidates
               ;; company-async-wait 0.03
               ;; company-async-timeout 2
               )
-
-;; (setq company-auto-complete t
-;;       company-auto-complete-chars
-;;       )
 
 ;; (setq company-begin-commands '(self-insert-command
 ;;                                org-self-insert-command orgtbl-self-insert-command
@@ -176,19 +176,10 @@
 
 
 ;; navigation
-(defun company-new-line ()
-  "insert a literal return new line."
-  (interactive)
-  (company-abort)
-  (newline-and-indent)
-  )
-
 (define-key company-active-map "\t" nil)
 (define-key company-active-map [tab] nil)
 (define-key company-active-map (kbd "<tab>") nil)
 (define-key company-active-map (kbd "<S-tab>") nil)
-(define-key company-active-map [return] 'company-new-line)
-(define-key company-active-map "\r" 'company-new-line)
 (define-key company-active-map (kbd "C-n") nil)
 (define-key company-active-map (kbd "C-p") nil)
 (define-key company-active-map (kbd "C-j") nil)
@@ -221,17 +212,22 @@
 (define-key company-search-map (kbd "M-o") 'company-search-kill-others)
 (define-key company-search-map (kbd "M-j") 'company-complete-selection)
 
+(defun company-new-line ()
+  "insert a literal return new line."
+  (interactive)
+  ;; (company-abort)
+  (company-cancel 'abort)
+  (newline-and-indent)
+  )
+
+(define-key company-active-map [return] 'company-new-line)
+(define-key company-active-map "\r" 'company-new-line)
 
 (define-key company-active-map (kbd "SPC")
   '(lambda ()
      (interactive)
      (company-abort)
      (insert " ")))
-
-(define-key company-active-map (kbd "C-g")
-  '(lambda ()
-     (interactive)
-     (company-abort)))
 
 
 ;; faces
@@ -369,14 +365,6 @@
 
 
 ;; (setq company-dabbrev-downcase 'case-replace) ; disable the down-case feature of the dabbrev back-end?
-
-(setq company-tooltip-align-annotations t
-      company-tooltip-flip-when-above t
-      company-tooltip-limit 10
-      company-tooltip-margin 2
-      company-tooltip-minimum 6
-      company-tooltip-minimum-width 0
-      company-tooltip-offset-display 'scrollbar)
 
 
 ;;; [ company-statistics ] -- Sort completion candidates by previous completion choices.
