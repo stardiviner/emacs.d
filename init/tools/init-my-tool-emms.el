0b8c661c5df9395d80902c68996d04cb6fc89691
stardiviner <numbchild@gmail.com>
2015-05-24 15:54:32 +0800

    improve emms-player-mpd config.

--- init/tools/init-my-tool-emms.el
+++ init/tools/init-my-tool-emms.el
@@ -67,6 +67,12 @@
 (setq emms-player-mpd-server-name "127.0.0.1"
       emms-player-mpd-server-port "6600")
 
+;; (setq emms-player-mpd-music-directory)
+(setq emms-player-mpd-sync-playlist t)
+
+(add-to-list 'emms-info-functions 'emms-info-mpd)
+(add-to-list 'emms-player-list 'emms-player-mpd)
+
 ;; TODO capture the key code of [Fn + <F10>] to apply. also include next [F11] etc.
 ;; (global-set-key (kbd "Fn + <F10>") 'emms-player-mpd-pause)
 
