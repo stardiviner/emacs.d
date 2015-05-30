e21c0fdc97addc176099661d86c5e25ff9c72bce
stardiviner <numbchild@gmail.com>
2015-05-30 11:33:23 +0800

    adjust cursor & point settings.

--- init/Emacs/init-my-emacs-apperance.el
+++ init/Emacs/init-my-emacs-apperance.el
@@ -225,15 +225,18 @@
 
 
 ;;; [ point & cursor ]
-(setq mouse-avoidance-mode 'animate) ;; auto move mouse away when cursor is at mouse position
-(blink-cursor-mode 1)
-(set-cursor-color "cyan")
-(setq-default cursor-type '(hbar . 3) ; '(hbar. 3), '(bar . 3), '(box . 2). '(hollow . 2)
-              cursor-in-non-selected-windows t)
-(setq mouse-yank-at-point t) ; yank at point position instead of mouse position
 
-(setq cursor-in-echo-area nil
-      cursor-in-non-selected-windows t)
+(blink-cursor-mode 1)
+
+(set-cursor-color "cyan")
+
+(setq mouse-avoidance-mode 'animate ; auto move mouse away when cursor is at mouse position
+      cursor-type '(hbar . 3) ; '(hbar. 3), '(bar . 3), '(box . 2). '(hollow . 2)
+      cursor-in-echo-area nil
+      cursor-in-non-selected-windows t
+      mouse-yank-at-point t ; yank at point position instead of mouse position
+      blink-cursor-blinks 10 ; how many times to blink before using a solid cursor on NS, X, and MS-Windows.
+      )
 
 
 ;;; [ cursor-chg ] -- change cursor color dynamically
