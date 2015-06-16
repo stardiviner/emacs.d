e34d68a742b719d7f47ec74329a3820c9b27612e
stardiviner <numbchild@gmail.com>
2015-06-17 06:59:42 +0800

    migrate diff faces customization into diff init file.

--- init/programming/vcs/init-my-prog-vcs.el
+++ init/programming/vcs/init-my-prog-vcs.el
@@ -15,42 +15,6 @@
 
 
 
-;;; [ diff ]
-
-(set-face-attribute 'diff-file-header nil
-                    :foreground "orange"
-                    :weight 'bold
-                    )
-(set-face-attribute 'diff-hunk-header nil
-                    :foreground "sky blue"
-                    )
-(set-face-attribute 'diff-context nil
-                    :foreground "dark gray"
-                    )
-(set-face-attribute 'diff-removed nil
-                    :background (color-darken-name (face-background 'default) 5)
-                    :foreground "red3"
-                    :weight 'normal)
-(set-face-attribute 'diff-added nil
-                    :background (color-darken-name (face-background 'default) 5)
-                    :foreground "forest green"
-                    :weight 'normal)
-(set-face-attribute 'diff-refine-removed nil
-                    :inherit 'diff-removed
-                    :background "black"
-                    :foreground "red"
-                    :inverse-video nil
-                    :weight 'bold
-                    )
-(set-face-attribute 'diff-refine-added nil
-                    :inherit 'diff-added
-                    :background "black"
-                    :foreground "green"
-                    :inverse-video nil
-                    :weight 'bold
-                    )
-
-
 
 (require 'init-my-prog-vcs-git)
 (require 'init-my-prog-vcs-diff)
