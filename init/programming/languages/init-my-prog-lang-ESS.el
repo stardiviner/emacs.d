e559f5001ee77be7fba76fd0dd4af38b168ea39f
stardiviner <numbchild@gmail.com>
2015-07-02 09:07:15 +0800

    add [C-c C-s] `ess-switch-process` to usage comments.

--- init/programming/languages/init-my-prog-lang-ESS.el
+++ init/programming/languages/init-my-prog-lang-ESS.el
@@ -21,6 +21,7 @@
 ;; - Start an ESS process.
 ;;   - [M-x ESS RET] :: start ESS session.
 ;;   - [M-x S RET] :: start S session.
+;; - [C-c C-s] :: `ess-switch-process'
 
 ;; (add-to-list 'load-path "~/.emacs.d/el-get/ess/lisp/")
 ;; (load "ess-site")
