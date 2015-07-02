432b92429403418f0c9d07097a18c85fd45fdd8e
stardiviner <numbchild@gmail.com>
2015-07-02 08:44:53 +0800

    disable motion-mode.

--- init/programming/languages/Ruby/init-my-prog-lang-ruby.el
+++ init/programming/languages/Ruby/init-my-prog-lang-ruby.el
@@ -926,22 +926,22 @@
 
 ;;; [ motion-mode ] -- RubyMotion
 
-(require 'motion-mode)
-
-;; following add-hook is very important.
-(add-hook 'ruby-mode-hook 'motion-recognize-project)
-(if (featurep 'auto-complete)
-    (progn
-      (add-to-list 'ac-modes 'motion-mode)
-      (add-to-list 'ac-sources 'ac-source-dictionary)))
-;; set key-binds as you like
-(define-key motion-mode-map (kbd "C-c C-c") 'motion-execute-rake)
-(define-key motion-mode-map (kbd "C-c C-d") 'motion-dash-at-point)
-
+;; (require 'motion-mode)
+;;
+;; ;; following add-hook is very important.
+;; (add-hook 'ruby-mode-hook 'motion-recognize-project)
+;; (if (featurep 'auto-complete)
+;;     (progn
+;;       (add-to-list 'ac-modes 'motion-mode)
+;;       (add-to-list 'ac-sources 'ac-source-dictionary)))
+;; ;; set key-binds as you like
 ;; (define-key motion-mode-map (kbd "C-c C-c") 'motion-execute-rake)
-;; (define-key motion-mode-map (kbd "C-c C-d") (lambda () (interactive) (motion-execute-rake-command "device")))
-;; (define-key motion-mode-map (kbd "C-c C-o") 'motion-dash-at-point)
-;; (define-key motion-mode-map (kbd "C-c C-p") 'motion-convert-code-region)
+;; (define-key motion-mode-map (kbd "C-c C-d") 'motion-dash-at-point)
+;;
+;; ;; (define-key motion-mode-map (kbd "C-c C-c") 'motion-execute-rake)
+;; ;; (define-key motion-mode-map (kbd "C-c C-d") (lambda () (interactive) (motion-execute-rake-command "device")))
+;; ;; (define-key motion-mode-map (kbd "C-c C-o") 'motion-dash-at-point)
+;; ;; (define-key motion-mode-map (kbd "C-c C-p") 'motion-convert-code-region)
 
 
 ;;; [ Cucumber ]
