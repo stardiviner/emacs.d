b4cb48fa31f7a1796785b9fa969117c80c7252d8
stardiviner <numbchild@gmail.com>
2015-07-13 10:07:11 +0800

    add Elixir init file.

---
+++ init/programming/languages/Elixir/init-my-prog-lang-elixir.el
@@ -0,0 +1,14 @@
+;;; init-my-prog-lang-elixir.el --- init for Elixir
+;;; -*- coding: utf-8 -*-
+
+;;; Commentary:
+
+
+
+;;; Code:
+
+
+
+(provide 'init-my-prog-lang-elixir)
+
+;;; init-my-prog-lang-elixir.el ends here
