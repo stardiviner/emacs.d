604cf264bc3b2acd3649850ab8d4a630db8eb15a
stardiviner <numbchild@gmail.com>
2015-07-22 19:01:35 +0800

    separate cursor settings out.

--- init/Emacs/init-my-emacs-apperance.el
+++ init/Emacs/init-my-emacs-apperance.el
@@ -223,15 +223,22 @@
 
 (blink-cursor-mode 1)
 
-(set-cursor-color "red")
-
 (setq-default mouse-avoidance-mode 'animate ; auto move mouse away when cursor is at mouse position
-              cursor-type '(hbar . 3) ; '(hbar. 3), '(bar . 3), '(box . 2). '(hollow . 2)
               cursor-in-echo-area nil
               cursor-in-non-selected-windows t
               mouse-yank-at-point t ; yank at point position instead of mouse position
               blink-cursor-blinks 10 ; how many times to blink before using a solid cursor on NS, X, and MS-Windows.
               )
+
+;;; red horizontal bar
+(setq-default cursor-type '(hbar . 3))
+(set-cursor-color "red")
+
+;;; transparent hollow
+;; (setq-default cursor-type 'hollow)
+;; (set-cursor-color "green")
+;; (set-face-attribute 'cursor nil
+;;                     :foreground "red" :background "green")
 
 
 ;;; [ cursor-chg ] -- change cursor color dynamically
