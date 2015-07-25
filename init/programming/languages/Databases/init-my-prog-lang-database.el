022e15e2bd63fd375da4094db4a8ac9dd1d25c2e
stardiviner <numbchild@gmail.com>
2015-07-25 09:47:00 +0800

    add database related keymap prefix.

--- init/programming/languages/Databases/init-my-prog-lang-database.el
+++ init/programming/languages/Databases/init-my-prog-lang-database.el
@@ -6,6 +6,20 @@
 
 ;;; Code:
 
+
+(unless (boundp 'my-inferior-db-map)
+  (define-prefix-command 'my-inferior-db-map))
+(define-key my-prog-inferior-map (kbd "d") 'my-inferior-db-map)
+
+(unless (boundp 'my-inferior-db-sql-map)
+  (define-prefix-command 'my-inferior-db-sql-map))
+(define-key my-inferior-db-map (kbd "s") 'my-inferior-db-sql-map)
+
+(unless (boundp 'my-inferior-db-nosql-map)
+  (define-prefix-command 'my-inferior-db-nosql-map))
+(define-key my-inferior-db-map (kbd "n") 'my-inferior-db-nosql-map)
+
+
 (require 'init-my-prog-lang-database-sql)
 (require 'init-my-prog-lang-database-sqlite)
 (require 'init-my-prog-lang-database-mysql)
