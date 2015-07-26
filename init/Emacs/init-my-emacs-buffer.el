6315e73457d363bc1e252a132b4662fb2ef9fd0e
stardiviner <numbchild@gmail.com>
2015-07-26 10:13:09 +0800

    update recentf configs.

--- init/Emacs/init-my-emacs-buffer.el
+++ init/Emacs/init-my-emacs-buffer.el
@@ -65,15 +65,13 @@
 
 (recentf-mode)
 
-(setq recentf-max-menu-items 25
-        recentf-max-saved-items 1000
-        recentf-exclude '("/tmp/" "/ssh:")
-        ;; recentf-keep
-        recentf-save-file (expand-file-name ".temp/recentf" user-emacs-directory)
-        recentf-auto-cleanup 'never ; 'mode
-        recentf-max-saved-items 100
-        recentf-case-fold-search t
-        )
+(setq recentf-save-file (expand-file-name ".temp/recentf" user-emacs-directory)
+      recentf-exclude '("/tmp/" "/ssh:")
+      recentf-max-menu-items 25
+      recentf-max-saved-items 100
+      recentf-auto-cleanup 'mode ; 'mode, 'never.
+      recentf-case-fold-search t
+      )
 
 
 
