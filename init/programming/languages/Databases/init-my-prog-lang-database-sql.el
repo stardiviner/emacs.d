d3cde115f8708a57c746a73aee74d247eba21de1
stardiviner <numbchild@gmail.com>
2015-07-30 11:15:26 +0800

    update edbi-sqlite usage comments.

--- init/programming/languages/Databases/init-my-prog-lang-database-sql.el
+++ init/programming/languages/Databases/init-my-prog-lang-database-sql.el
@@ -134,9 +134,8 @@
 
 ;;; Usage:
 ;;
-;; Run `edbi-sqlite' interactive command. Select appropriate sqlite database
-;; file. Type `RET' and you'll be connected to specified database with edbi
-;; tool.
+;; 1. $ sqlite3
+;; 2. `edbi-sqlite'
 
 (require 'edbi-sqlite)
 
