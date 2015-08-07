6edd9fca4f5bc35e9c3cd20a52c1e42c0a3b0633
stardiviner <numbchild@gmail.com>
2015-08-07 14:48:54 +0800

    update minimal init file.

--- minimal-init.el
+++ minimal-init.el
@@ -5,11 +5,22 @@
 
 ;;; Code:
 
+;;; [ Debug ]
+
+(setq debug-on-error t
+      debug-on-signal nil
+      debug-on-quit t)
+
+
+;;; some settings for easy usage
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
 ;;; [ package manager ]
 
 ;;; el-get
 
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+(add-to-list 'load-path "~/.emacs.d/el-get")
 
 
 ;;; [ package.el ]
@@ -26,10 +37,10 @@
 (let ((default-directory "~/.emacs.d/init/"))
   (setq load-path
         (append
-          (let ((load-path (copy-sequence load-path))) ; shadow
-            (append
-              (copy-sequence (normal-top-level-add-to-load-path '(".")))
-              (normal-top-level-add-subdirs-to-load-path)))
-          load-path)))
+         (let ((load-path (copy-sequence load-path))) ; shadow
+           (append
+            (copy-sequence (normal-top-level-add-to-load-path '(".")))
+            (normal-top-level-add-subdirs-to-load-path)))
+         load-path)))
 
 
