bf1d6841350279ccbd0b2d514a3237f35d27bf35
stardiviner <numbchild@gmail.com>
2015-08-17 20:42:41 +0800

    add a extra extension "sql-indent.el".

--- init/programming/languages/Databases/init-my-prog-lang-database-sql.el
+++ init/programming/languages/Databases/init-my-prog-lang-database-sql.el
@@ -28,6 +28,8 @@
 (eval-after-load "sql"
   '(load-library "sql-indent"))
 
+(load "~/.emacs.d/init/extensions/sql-indent.el")
+
 (dolist (hook '(sql-mode-hook
                 sql-interactive-mode-hook
                 edbi:sql-mode-hook
