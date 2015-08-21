34fb3ae9603a6b52f92fb1d3d6503de068eee9d0
stardiviner <numbchild@gmail.com>
2015-08-21 21:27:30 +0800

    enable package db-pg for PostgreSQL.

--- init/programming/languages/Databases/init-my-prog-lang-database-nosql.el
+++ init/programming/languages/Databases/init-my-prog-lang-database-nosql.el
@@ -45,23 +45,6 @@
 (require 'db)
 
 
-;;; [ db-pg ] -- KeyValue Database on PostgreSQL.
-
-;; Here's a small example that shows, at least, how to specify the connection details:
-;;
-;; ##! emacs-lisp
-;; (let ((db 
-;;        (db-make 
-;;         '(db-pg
-;;           :db "my-db" :username "nic"
-;;           :table "t1" :column "c1" :key "a"))))
-;;   (db-get "10" db))
-;;
-;; => '(("a" . 10)("b" . "20"))
-
-(require 'db-pg)
-
-
 (provide 'init-my-prog-lang-database-nosql)
 
 ;;; init-my-prog-lang-database-nosql.el ends here
