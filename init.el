5ee437ae8429fc65e0e6157de3a6bfbeb2c8bdc5
stardiviner <numbchild@gmail.com>
2015-08-22 18:33:25 +0800

    remove el-get + packages.el bootstrapping stratege.

--- init.el
+++ init.el
@@ -123,37 +123,6 @@
 (load "~/.emacs.d/init/init-my-pm-package.el")
 (require 'init-my-pm-package)
 
-
-
-
-;;; Bootstrapping el-get + packages
-
-;; (setq el-get-sources
-;; '((:name package-name)))
-
-;; (defun sync-packages ()
-;; "Synchronize packages"
-;; (interactive)
-;; (el-get 'sync '(el-get package))
-;; (add-to-list 'package-archives '("tromey" . "http://tromey.com/elpa/"))
-;; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-;; (setq my-packages (mapcar 'el-get-source-name el-get-sources))
-;; (el-get 'sync my-packages))
-
-;; (if (require 'el-get nil t)
-;; (sync-packages)
-;; (url-retrieve
-;; "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
-;; (lambda (s)
-;; (let (el-get-master-branch)
-;; (end-of-buffer)
-;; (eval-print-last-sexp)
-;; (setq el-get-verbose t)
-;; (sync-packages)))))
-
-
-;; (require 'init-my-pm-paradox)
-
 
 ;;; namespace
 
