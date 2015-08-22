fa7d84f7be9d3b3ecf982a968008605162ea5937
stardiviner <numbchild@gmail.com>
2015-08-22 08:34:47 +0800

    format company-mode options.

--- init/Emacs/init-company-mode.el
+++ init/Emacs/init-company-mode.el
@@ -22,27 +22,39 @@
 
 (require 'company)
 
-(setq-default company-minimum-prefix-length 2   ; minimum prefix character number for auto complete.
-              company-idle-delay 0.3
-              ;; determines when to auto-complete.
-              company-auto-complete 'company-explicit-action-p ; 'company-explicit-action-p, t, nil, 'function
-              ;; company-auto-complete-chars
-              company-require-match 'company-explicit-action-p ; 'company-explicit-action-p, t, nil, 'function
-              company-echo-delay 0
-              ;; company-show-numbers t
-              company-tooltip-align-annotations t ; align annotations to the right tooltip border.
-              company-tooltip-flip-when-above t
-              company-tooltip-limit 10          ; tooltip candidates max limit.
-              company-tooltip-minimum 6         ; minimum candidates height limit.
-              company-tooltip-minimum-width 0   ; The minimum width of the tooltip's inner area.
-                                        ; This doesn't include the margins and the scroll bar.
-              company-tooltip-margin 1          ; width of margin columns to show around the tooltip
-              company-tooltip-offset-display 'scrollbar ; 'lines - how to show tooltip unshown candidates number.
-              company-show-numbers nil ; t: show quick-access numbers for the first ten candidates.
-              company-selection-wrap-around t ; loop over candidates
-              ;; company-async-wait 0.03
-              ;; company-async-timeout 2
-              )
+(setq-default
+ ;; minimum prefix character number for auto complete.
+ company-minimum-prefix-length 2
+ company-idle-delay 0.3
+ ;; determines when to auto-complete.
+ ;; 'company-explicit-action-p, t, nil, 'function
+ company-auto-complete 'company-explicit-action-p
+ ;; company-auto-complete-chars
+ ;; 'company-explicit-action-p, t, nil, 'function
+ company-require-match 'company-explicit-action-p
+ company-echo-delay 0
+ ;; company-show-numbers t
+ ;; align annotations to the right tooltip border.
+ company-tooltip-align-annotations t
+ company-tooltip-flip-when-above t
+ ;; tooltip candidates max limit.
+ company-tooltip-limit 10
+ ;; minimum candidates height limit.
+ company-tooltip-minimum 6
+ ;; The minimum width of the tooltip's inner area.
+ company-tooltip-minimum-width 0
+ ;; This doesn't include the margins and the scroll bar.
+ ;; width of margin columns to show around the tooltip
+ company-tooltip-margin 1
+ ;; 'lines - how to show tooltip unshown candidates number.
+ company-tooltip-offset-display 'scrollbar
+ ;; t: show quick-access numbers for the first ten candidates.
+ company-show-numbers nil
+ ;; loop over candidates
+ company-selection-wrap-around t
+ ;; company-async-wait 0.03
+ ;; company-async-timeout 2
+ )
 
 ;; (setq company-begin-commands '(self-insert-command
 ;;                                org-self-insert-command orgtbl-self-insert-command
