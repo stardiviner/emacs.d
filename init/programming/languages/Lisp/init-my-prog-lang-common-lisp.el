ea8cc5ccc5effda0ced485bb2ee6c4aaf1e0730a
stardiviner <numbchild@gmail.com>
2015-08-23 18:01:08 +0800

    migrate some lisp dialects language init files.

--- init/programming/languages/Lisp/init-my-prog-lang-common-lisp.el
+++ init/programming/languages/Lisp/init-my-prog-lang-common-lisp.el
@@ -11,8 +11,28 @@
 
 ;; Open files with .cl extension in lisp-mode
 (add-to-list 'auto-mode-alist '("\\.cl\\'" . lisp-mode))
+(add-to-list 'auto-mode-alist '("\\.lisp\\'" . common-lisp-mode))
 
+;;; re-define upstream default function 'lispdoc key binding.
+(define-key lisp-mode-map (kbd "C-h d") 'lispdoc)
 
+(setq cl-lookup-categories
+      '(:hyperspec-index           ; e.g. "", "spec" "CLHS"
+        :hyperspec-chapters        ; e.g. [index], [syntax]
+        :format-control-characters ; e.g. "~C: Character", "~%: Newline"
+        :reader-macro-characters   ; e.g. "(", "#'", "#b", "#+"
+        :loop                      ; e.g. loop:with, loop:collect
+        :arguments                 ; e.g. :test, :key, :eof-error-p
+        :concepts                  ; e.g. "lambda lists:", "character names:"
+        "cl-lookup-glossary"       ; e.g. {absolute}, {binding}
+        "cl-lookup-mop"            ; e.g. add-dependent, ensure-class
+
+        ;; implementation specific categories
+        ;; "cl-lookup-clisp"          ; e.g. ext:cd
+
+        ;; library categories
+        "cl-lookup-ppcre"          ; e.g. cl-ppcre:parse-tree-synonym
+        ))
 
 
 ;;; [ SBCL ]
@@ -29,6 +49,25 @@
 ;; (if (file-exists-p (expand-file-name "~/quicklisp/slime-helper.el"))
 ;;     (load (expand-file-name "~/quicklisp/slime-helper.el"))
 ;;   (message "%s" "SLIME is not installed. Use Quicklisp to install it."))
+
+
+;;; [ sly ] --- Common Lisp IDE
+
+;;; Usage:
+;;
+;; - `sly-connect' ::
+
+(require 'sly-autoloads)
+
+;; (setq inferior-lisp-program "/usr/bin/sbcl")
+
+
+;;; [ sly-company ]
+
+
+
+
+;;; [ ac-sly ]
 
 
 
