72a26f8d9df78c5a7b52c69f794c211fedfeb6a9
stardiviner <numbchild@gmail.com>
2015-08-23 18:02:59 +0800

    config some things before init load.

--- init.el
+++ init.el
@@ -16,6 +16,19 @@
 ;; (profiler-start 'cpu+mem)
 
 
+;;; [ things before load ]
+
+;; Turn off mouse interface early in startup to avoid momentary display
+(when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;; No splash screen please... jeez
+(setq inhibit-startup-screen t)
+
+(toggle-frame-maximized)
+
+
 ;;; [ version check ]
 
 (when (version< emacs-version "24")
