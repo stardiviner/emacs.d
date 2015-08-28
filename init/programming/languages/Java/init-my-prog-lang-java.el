95a3839d2c1731854e7779b2ea8bff91cfa1d7d2
stardiviner <numbchild@gmail.com>
2015-08-28 08:07:22 +0800

    improve Java init file.

--- init/programming/languages/Java/init-my-prog-lang-java.el
+++ init/programming/languages/Java/init-my-prog-lang-java.el
@@ -7,6 +7,59 @@
 
 ;;; Code:
 
+;;; [ jdee ] -- Java Development Environment for Emacs
+
+(require 'jdee)
+
+;; (autoload 'jde-mode "jde" "JDE mode" t)
+;; (setq auto-mode-alist
+;;       (append '(("\\.java\\'" . jde-mode)) auto-mode-alist))
+
+
+;;; [ jde-maven ] -- 
+
+;; (require 'jde-maven)
+
+
+;;; [ eclim ]
+
+;; (require 'eclim)
+(autoload 'eclim "eclim" nil t)
+
+;; ;; If you want to control eclimd from emacs, also add:
+;; (require 'eclimd)
+
+
+;; (setq eclim-eclipse-dirs '("/Applications/eclipse"
+;;                            "/usr/lib/eclipse"
+;;                            "/usr/local/lib/eclipse"
+;;                            "/usr/share/eclipse")
+;;       ;; eclim-executable "~/Libraries/Emacs/eclim"
+;;       )
+
+;; for company-mode
+(require 'company-emacs-eclim)
+
+;; (company-emacs-eclim-setup)
+(add-hook 'java-mode-hook
+          (lambda ()
+            (eclim-mode)
+            
+            ;; (setq-local company-backends
+            ;;             (cons 'company-emacs-eclim
+            ;;                   (remove-if (lambda (b) (find b '(company-nxml company-eclim)))
+            ;;                              company-backends)))
+
+            (add-to-list (make-local-variable 'company-backends)
+                         'company-emacs-eclim)
+            ))
+
+;; (global-eclim-mode t)
+
+
+;;; [ java-complete ]
+
+
 ;;; [ javadoc-lookup ]
 
 ;;; Usage:
@@ -18,18 +71,24 @@
 ;; `sort-java-imports'. The former integrates with the javadoc-lookup index to
 ;; provide completions.
 
-(add-hook 'java-mode-hook
-          (lambda ()
-            (local-set-key (kbd "C-h d") 'my-prog-help-document-map)
-            (define-key my-prog-help-document-map (kbd "d") 'javadoc-lookup)
-            ))
-
-(javadoc-add-roots "/usr/share/doc/openjdk-7-doc/api"
-                   ;; "~/src/project/doc"
-                   )
+;; (add-hook 'java-mode-hook
+;;           (lambda ()
+;;             (local-set-key (kbd "C-h d") 'my-prog-help-document-map)
+;;             (define-key my-prog-help-document-map (kbd "d") 'javadoc-lookup)
+;;             ))
+;;
+;; (javadoc-add-roots "/usr/share/doc/openjdk-7-doc/api"
+;;                    ;; "~/src/project/doc"
+;;                    )
 
 
 ;;; [ javadoc-help ]
+
+
+
+;;; [ malabar-mode ] -- EMCS JVM Integration for Java and other JVM based languages.
+
+;; (require 'malabar-mode)
 
 
 ;;; [ groovy-mode ] -- Groovy mode derived mode.
@@ -40,87 +99,6 @@
 ;;; [ inf-groovy ] -- minor-mode that adds some Grails project management to a grails project.
 
 ;; (require 'inf-groovy)
-
-
-;;; [ jdee ] -- Java Development Environment for Emacs
-
-;; (require 'jde-autoload)
-
-;; (autoload 'jde-mode "jde" "JDE mode" t)
-;; (setq auto-mode-alist
-;;       (append '(("\\.java\\'" . jde-mode)) auto-mode-alist))
-
-
-;;; [ jde-maven ]
-
-;; (require 'jde-maven)
-
-
-;;; [ malabar-mode ] -- EMCS JVM Integration for Java and other JVM based languages.
-
-;; (require 'malabar-mode)
-
-
-;;; [ eclim ]
-
-;; Eclim is an Eclipse plugin which exposes Eclipse features through a server
-;; interface. When this server is started, the command line utility eclim can be
-;; used to issue requests to that server.
-;;
-;; Emacs-eclim uses the eclim server to integrate eclipse with emacs. This
-;; project wants to bring some of the invaluable features from eclipse to
-;; emacs. Please note, emacs-eclim is limited to mostly java support at this
-;; time.
-;;
-;; It is also possible to start and stop the eclim daemon from emacs using the
-;; eclimd package.
-
-;; (require 'eclim)
-;; ;; If you want to control eclimd from emacs, also add:
-;; (require 'eclimd)
-(autoload 'eclim "eclim" nil t)
-
-;; (setq eclim-eclipse-dirs '("/Applications/eclipse"
-;;                            "/usr/lib/eclipse"
-;;                            "/usr/local/lib/eclipse"
-;;                            "/usr/share/eclipse")
-;;       ;; eclim-executable "~/Libraries/Emacs/eclim"
-;;       )
-
-;; for auto-complete, add the emacs-eclim source
-;; (require 'ac-emacs-eclim-source)
-;; (ac-emacs-eclim-config)
-
-;; for company-mode
-;; (require 'company-emacs-eclim)
-
-;; (company-emacs-eclim-setup)
-(add-hook 'java-mode-hook
-          (lambda ()
-            (eclim-mode)
-            
-            (setq-local company-backends
-                        (cons 'company-emacs-eclim
-                              (remove-if (lambda (b) (find b '(company-nxml company-eclim)))
-                                         company-backends)))
-            (add-to-list (make-local-variable 'company-backends)
-                         'company-emacs-eclim)))
-
-
-;; (global-eclim-mode t)
-
-
-;;; [ company-eclim ]
-
-(require 'company-emacs-eclim)
-
-(add-hook 'java-mode-hook
-          (lambda ()
-            (add-to-list (make-local-variable 'company-backends)
-                         'company-eclim)))
-
-
-;;; [ java-complete ]
 
 
 
