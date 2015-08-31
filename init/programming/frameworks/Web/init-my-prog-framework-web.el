7f96bcbee632c31aaccda2fe69db8562e292381a
stardiviner <numbchild@gmail.com>
2015-08-31 21:00:21 +0800

    enable company-tern support in web-mode.

--- init/programming/frameworks/Web/init-my-prog-framework-web.el
+++ init/programming/frameworks/Web/init-my-prog-framework-web.el
@@ -356,6 +356,19 @@
 (add-to-list 'web-mode-expanders '("%/" . "<%= | %>")) ; ruby erb: <%= | %>.
 (add-to-list 'web-mode-expanders '("R/" . "<% | %>")) ; ruby erb: <% | %>.
 
+;;;_. company-mode support
+
+;; Enable JavaScript completion between <script>...</script> etc.
+(defadvice company-tern (before web-mode-set-up-ac-sources activate)
+  "Set `tern-mode' based on current language before running company-tern."
+  (if (equal major-mode 'web-mode)
+      (let ((web-mode-cur-language
+             (web-mode-language-at-pos)))
+        (if (or (string= web-mode-cur-language "javascript")
+               (string= web-mode-cur-language "jsx")
+               )
+            (unless tern-mode (tern-mode))
+          (if tern-mode (tern-mode))))))
 
 ;;;_. auto-complete support
 ;;
