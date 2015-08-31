a634910cf926ea4809ad1d766510ef53e48cdcda
stardiviner <numbchild@gmail.com>
2015-09-01 00:32:56 +0800

    add web browser init file.

--- init.el
+++ init.el
@@ -285,6 +285,7 @@
 
 ;;; Frameworks
 (require 'init-my-prog-framework-web)
+(require 'init-my-prog-web-browser)
 ;; (require 'init-my-prog-framework-arduino)
 (require 'init-my-prog-ruby-on-rails)
 ;; (require 'init-my-prog-nginx)
---
+++ init/programming/frameworks/Web/init-my-prog-web-browser.el
@@ -0,0 +1,24 @@
+;;; init-my-prog-web-browser.el --- init for Web Browser
+;;; -*- coding: utf-8 -*-
+
+;;; Commentary:
+
+
+
+;;; Code:
+
+;;; [ dom ]
+
+
+;;; [ kite ] -- WebKit
+
+
+
+;;; [ livid-mode ] -- Live browser eval of JavaScript every time a buffer changes.
+
+
+
+
+(provide 'init-my-prog-web-browser)
+
+;;; init-my-prog-web-browser.el ends here
