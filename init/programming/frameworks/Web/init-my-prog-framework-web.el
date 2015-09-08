a24dfcab53f81fc3cdaa4d80ad94632d2c5c2a49
stardiviner <numbchild@gmail.com>
2015-09-08 14:20:03 +0800

    auto format with web-beautify before saving a file.

--- init/programming/frameworks/Web/init-my-prog-framework-web.el
+++ init/programming/frameworks/Web/init-my-prog-framework-web.el
@@ -458,6 +458,33 @@
 ;; (eval-after-load 'css-mode
 ;;   '(define-key css-mode-map (kbd "C-c b") 'web-beautify-css))
 
+;; automatically format before saving a file
+(eval-after-load 'js2-mode
+  '(add-hook 'js2-mode-hook
+             (lambda ()
+               (add-hook 'before-save-hook 'web-beautify-js-buffer t t))))
+
+;; Or if you're using 'js-mode' (a.k.a 'javascript-mode')
+(eval-after-load 'js
+  '(add-hook 'js-mode-hook
+             (lambda ()
+               (add-hook 'before-save-hook 'web-beautify-js-buffer t t))))
+
+(eval-after-load 'json-mode
+  '(add-hook 'json-mode-hook
+             (lambda ()
+               (add-hook 'before-save-hook 'web-beautify-js-buffer t t))))
+
+(eval-after-load 'sgml-mode
+  '(add-hook 'html-mode-hook
+             (lambda ()
+               (add-hook 'before-save-hook 'web-beautify-html-buffer t t))))
+
+(eval-after-load 'css-mode
+  '(add-hook 'css-mode-hook
+             (lambda ()
+               (add-hook 'before-save-hook 'web-beautify-css-buffer t t))))
+
 ;;;_ restclient
 
 ;;; This is a tool to manually explore and test HTTP REST webservices. Runs
