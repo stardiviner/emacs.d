efb41d2715c7e1c5b6d7fd54d0a0e92762b08247
stardiviner <numbchild@gmail.com>
2015-09-29 20:20:35 +0800

    improve Tramp config file comments.

--- init/Emacs/init-my-emacs-tramp.el
+++ init/Emacs/init-my-emacs-tramp.el
@@ -9,24 +9,37 @@
 
 ;;; [ Tramp ]
 
-;; Usage:
-;; - [C-x C-f /sudo::/path/to/file] -- edit stuff with sudo.
-;; - [C-x C-f /su::/path/to/file] -- edit stuff as root.
-;; - [ssh]
-;; - [telnet]
-
 (require 'tramp)
 
-(setq tramp-default-method "ssh"        ; default value is "scpc" (ssh and scp)
-      ;; tramp-methods
-      ;; tramp-chunksize 500
-      ;;; for debugging
-      ;; tramp-debug-buffer-name t
-      ;; tramp-verbose 10
-      )
+;; <default method>
+;; (setq tramp-default-method "ssh") ; default "scpc" (ssh + scp),
+;; (add-to-list 'tramp-default-method-alist '("" ""))
+
+;; <default user>
+;; ssh connect: host, user
+;; (add-to-list 'tramp-default-user-alist
+;;              '("ssh" ".*\\.somewhere\\.else\\'" "john"))
+
+;; <default host>
+;; (add-to-list 'tramp-default-host-alist)
+
+;; <default proxy>
+;; (add-to-list 'tramp-default-proxies-alist)
+
+;; Tramp completion
+;; (add-to-list 'tramp-completion-function-alist)
+
+;; <predefined connection property>
+;; (add-to-list 'tramp-connection-properties
+;;              (list (regexp-quote "/ssh:user@randomhost.your.domain:")
+;;                    "busybox" t))
+
+;; tramp-debug-buffer-name t
+;; tramp-verbose 10
+
 
 ;; change SHELL environment variable to solve Tramp hangs issue.
-(eval-after-load 'tramp '(setenv "SHELL" "/bin/bash"))
+;; (eval-after-load 'tramp '(setenv "SHELL" "/bin/bash"))
 
 
 
