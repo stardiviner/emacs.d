e031c8d8f67422bded6d65dcbdc0488a94214415
stardiviner <numbchild@gmail.com>
2015-10-01 09:53:00 +0800

    migrate message-mode config to email init file.

--- init/tools/init-my-tool-email.el
+++ init/tools/init-my-tool-email.el
@@ -16,6 +16,54 @@
       compose-mail-user-agent-warnings nil
       )
 
+(add-hook 'message-mode-hook
+          (lambda ()
+            (turn-on-orgstruct) ; Org-struct minor mode
+            (turn-on-orgstruct++)
+            ;; enable Orgtbl minor mode in message-mode.
+            (turn-on-orgtbl)))
+
+
+;;; [ boxquote ]
+
+;; ,----[ Sample quote ]
+;; | This is a sample quote.  Notice that it looks good even if you
+;; | send it to someone who reads mail in a proportional font (probably
+;;                                                             | a non-hacker...).
+;; `----
+
+;;; Usage:
+;;
+;; - `boxquote-region' :: boxquote region.
+
+(require 'boxquote)
+
+;; (setq boxquote-title-format "[ %s ]")
+
+;; `message-completion-function' (like capf)
+;; (setq message-expand-name-databases '(bbdb eudb))
+
+(define-key narrow-map (kbd "q") 'boxquote-narrow-to-boxquote-content)
+
+(unless (boundp 'my-boxquote-map)
+  (define-prefix-command 'my-boxquote-map))
+(define-key my-tools-prefix (kbd "q") 'my-boxquote-map)
+
+(define-key my-boxquote-map (kbd "q") 'boxquote-boxquote)
+(define-key my-boxquote-map (kbd "u") 'boxquote-unbox)
+(define-key my-boxquote-map (kbd "t") 'boxquote-text)
+(define-key my-boxquote-map (kbd "U") 'boxquote-unbox-region)
+(define-key my-boxquote-map (kbd "r") 'boxquote-region)
+(define-key my-boxquote-map (kbd "b") 'boxquote-buffer)
+(define-key my-boxquote-map (kbd "f") 'boxquote-defun)
+(define-key my-boxquote-map (kbd "c") 'boxquote-shell-command)
+(define-key my-boxquote-map (kbd "F") 'boxquote-describe-function)
+(define-key my-boxquote-map (kbd "K") 'boxquote-describe-key)
+(define-key my-boxquote-map (kbd "V") 'boxquote-describe-variable)
+(define-key my-boxquote-map (kbd "C-w") 'boxquote-kill)
+(define-key my-boxquote-map (kbd "C-y") 'boxquote-yank)
+(define-key my-boxquote-map (kbd "p") 'boxquote-paragraph)
+
 
 ;;; [ encrypt email ]
 
@@ -28,10 +76,8 @@
 
 
 
-(require 'init-my-tool-email-message-mode)
 ;; (require 'init-my-tool-email-gnus)
 (require 'init-my-tool-email-mu4e)
-
 
 
 (provide 'init-my-tool-email)
