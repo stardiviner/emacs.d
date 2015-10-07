658603f8bbaaf00e61e31834f71d2c42901122b1
stardiviner <numbchild@gmail.com>
2015-10-07 13:04:01 +0800

    fix Common Lisp docset name.

--- init/programming/document/init-my-prog-document.el
+++ init/programming/document/init-my-prog-document.el
@@ -118,7 +118,7 @@
           ;; "CoffeeScript" "NodeJS"
           "jQuery" "AngularJS" "React" "D3JS"
           ;; "EmberJS" "ExtJS" "BackboneJS" "KnockoutJS" "MomentJS" "PrototypeJS" "RequireJS" "UnderscoreJS"
-          "Common_Lisp"
+          "Common Lisp"
           "Clojure"
           "C" ; "C++"
           "Go" ; "Swift" ; "Rust"
