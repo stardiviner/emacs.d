aad16190768cb399fc5c60b9f361caf8029ce45a
stardiviner <numbchild@gmail.com>
2015-10-15 12:19:50 +0800

    remove popup extensions custom to revert to default popup style.

--- init/Emacs/init-my-emacs-popup.el
+++ init/Emacs/init-my-emacs-popup.el
@@ -14,25 +14,17 @@
 (require 'tooltip)
 (tooltip-mode t)
 
-(setq-default tooltip-delay 0.3         ; default 0.7
-              tooltip-hide-delay 10     ; default 10
-              tooltip-short-delay 0.1   ; default 0.1
-              ;; tooltip-functions '(tooltip-help-tips)
-              ;; tooltip-hide-time nil
-              tooltip-x-offset 5
-              tooltip-y-offset 20
-              tooltip-frame-parameters '((name . "tooltip")
-                                         (internal-border-width . 2)
-                                         (border-width . 2))
-              )
-
-(set-face-attribute 'tooltip nil
-                    :inherit nil
-                    :inverse-video nil
-                    :foreground "black" :background "dim gray"
-                    :distant-foreground "gray"
-                    :family "DejaVu Sans Mono"
-                    )
+;; (setq-default tooltip-delay 0.3         ; default 0.7
+;;               tooltip-hide-delay 10     ; default 10
+;;               tooltip-short-delay 0.1   ; default 0.1
+;;               ;; tooltip-functions '(tooltip-help-tips)
+;;               ;; tooltip-hide-time nil
+;;               tooltip-x-offset 5
+;;               tooltip-y-offset +20
+;;               tooltip-frame-parameters '((name . "tooltip")
+;;                                          (internal-border-width . 2)
+;;                                          (border-width . 2))
+;;               )
 
 
 ;;; [ popup ]
@@ -49,51 +41,10 @@
 
 (require 'popup)
 
-(set-face-attribute 'popup-face nil
-                    :foreground "black" :background "gray"
-                    :family "DejaVu Sans Mono")
-(set-face-attribute 'popup-menu-face nil
-                    :inherit 'popup-face
-                    :foreground "black" :background "white")
-(set-face-attribute 'popup-menu-selection-face nil
-                    :inherit 'popup-face
-                    :foreground "white" :background "black"
-                    )
-(set-face-attribute 'popup-menu-mouse-face nil
-                    :inherit 'popup-face
-                    :foreground "black"
-                    :background "orange"
-                    )
-(set-face-attribute 'popup-tip-face nil
-                    :inherit 'popup-face
-                    :background "yellow"
-                    )
-(set-face-attribute 'popup-summary-face nil
-                    :inherit 'popup-face
-                    :foreground "dim gray")
-(set-face-attribute 'popup-menu-summary-face nil
-                    :inherit 'popup-face
-                    :background "purple")
-(set-face-attribute 'popup-isearch-match nil
-                    :inherit 'popup-face
-                    :inverse-video nil
-                    :foreground "deep pink"
-                    :weight 'bold
-                    :underline '(:color "dim gray" :style line)
-                    )
-(set-face-attribute 'popup-scroll-bar-background-face nil
-                    :background "gray")
-(set-face-attribute 'popup-scroll-bar-foreground-face nil
-                    :foreground "black")
-
 ;; add some shotcuts in popup menu mode
 (define-key popup-menu-keymap (kbd "M-n") 'popup-next)
 (define-key popup-menu-keymap (kbd "M-p") 'popup-previous)
 (define-key popup-menu-keymap (kbd "M-j") 'popup-select)
-;; (define-key popup-menu-keymap (kbd "TAB") 'popup-next)
-;; (define-key popup-menu-keymap (kbd "<tab>") 'popup-next)
-;; (define-key popup-menu-keymap (kbd "<backtab>") 'popup-previous)
-
 
 
 ;;; [ pos-tip ]
@@ -101,40 +52,23 @@
 (require 'pos-tip)
 
 (setq pos-tip-use-relative-coordinates nil
-      ;; pos-tip-frame-offset
-      ;; pos-tip-frame-offset-array
-      pos-tip-border-width 2
-      pos-tip-internal-border-width 1
-      ;; pos-tip-tab-width
+      pos-tip-border-width 1
+      pos-tip-internal-border-width 2
+      ;; pos-tip-tab-width nil
       )
-
-(setq pos-tip-foreground-color "black"
-      pos-tip-background-color "green yellow")
-
-;; (setq pos-tip-background-color "#002630" ; '(color-darken-name (face-background 'default) 1)
-;;       pos-tip-foreground-color "dark gray")
 
 
 ;;; [ popup-pos-tip ]
 
 ;; (require 'popup-pos-tip)
 
-
 
 ;;; [ showtip ]
 
 (require 'showtip)
 
-(setq showtip-timeout 20
+(setq showtip-timeout 15
       showtip-top-adjust 30)
-
-(set-face-attribute 'showtip-face nil
-                    :inherit nil
-                    :inverse-video nil
-                    :foreground "black" :background "white"
-                    ;; :family "DejaVu Sans Mono"
-                    ;; :family "WenQuanYi Zen Hei Mono"
-                    )
 
 
 (provide 'init-my-emacs-popup)
