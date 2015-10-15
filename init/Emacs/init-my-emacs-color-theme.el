d0b5425df1b5e24203fe06397faad31ec05bcb09
stardiviner <numbchild@gmail.com>
2015-10-15 12:15:41 +0800

    remove solarized color-theme options custom.

--- init/Emacs/init-my-emacs-color-theme.el
+++ init/Emacs/init-my-emacs-color-theme.el
@@ -32,26 +32,11 @@
 
 ;;; color-theme-solarized
 
-(setq solarized-termcolors 256
-      ;; solarized-degrade t
-      solarized-bold t
-      solarized-underline t
-      solarized-italic t
-      solarized-contrast 'normal ; 'normal, 'hight, 'low
-      ;; solarized-broken-srgb nil    ; nil, t
-      )
-
-;;; This allows you to have a mix of light and dark frames. I tend to use light
-;;; frames in the GUI and dark frames in my terminal:
-;;
-;; (add-hook 'after-make-frame-functions
-;;           (lambda (frame)
-;;             (let ((mode (if (display-graphic-p frame) 'light 'dark)))
-;;               (set-frame-parameter frame 'background-mode mode)
-;;               (set-terminal-parameter frame 'background-mode mode))
-;;             (enable-theme 'solarized)))
+(require 'color-theme-solarized)
 
 (color-theme-solarized)
+
+;; (load-theme 'solarized t)
 
 
 ;;; color-theme-monokai
