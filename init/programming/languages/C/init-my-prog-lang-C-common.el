a665e66423db62033999fe29d47ce19ef0ecb573
stardiviner <numbchild@gmail.com>
2015-10-20 14:40:48 +0800

    add company-irony-c-headers to company-backends.

--- init/programming/languages/C/init-my-prog-lang-C-common.el
+++ init/programming/languages/C/init-my-prog-lang-C-common.el
@@ -104,10 +104,13 @@
 
 (add-hook 'irony-mode-hook
           (lambda ()
-            (add-to-list (make-local-variable 'company-backends)
-                         'company-irony)
-            ;; (optional) adds CC special commands to `company-begin-commands' in order to
-            ;; trigger completion at interesting places, such as after scope operator.
+            (make-local-variable 'company-backends)
+            (add-to-list 'company-backends 'company-irony-c-headers)
+            (add-to-list 'company-backends 'company-irony)
+            
+            ;; (optional) adds CC special commands to `company-begin-commands'
+            ;; in order to trigger completion at interesting places, such as
+            ;; after scope operator.
             ;;     std::|
             (company-irony-setup-begin-commands)
             ))
