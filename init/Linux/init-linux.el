3e98800d46437079e38d4c1af3368a6f6816340c
stardiviner <numbchild@gmail.com>
2015-10-20 22:30:21 +0800

    update systemd files extension auto-mode-alist.

--- init/Linux/init-linux.el
+++ init/Linux/init-linux.el
@@ -18,16 +18,18 @@
 ;; - [C-c C-o] :: open links to documentation in a unit.
 ;;
 
-(add-to-list 'auto-mode-alist '("\\.automount\\'" . systemd-mode))
-(add-to-list 'auto-mode-alist '("\\.busname\\'" . systemd-mode))
-(add-to-list 'auto-mode-alist '("\\.mount\\'" . systemd-mode))
-(add-to-list 'auto-mode-alist '("\\.service\\'" . systemd-mode))
-(add-to-list 'auto-mode-alist '("\\.socket\\'" . systemd-mode))
-(add-to-list 'auto-mode-alist '("\\.target\\'" . systemd-mode))
-(add-to-list 'auto-mode-alist '("\\.timer\\'" . systemd-mode))
-(add-to-list 'auto-mode-alist '("\\.link\\'" . systemd-mode))
-(add-to-list 'auto-mode-alist '("\\.netdev\\'" . systemd-mode))
-(add-to-list 'auto-mode-alist '("\\.network\\'" . systemd-mode))
+(add-to-list 'auto-mode-alist '("\\.service\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.timer\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.target\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.mount\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.automount\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.slice\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.socket\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.path\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.netdev\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.network\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.link\\'" . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.automount\\'" . conf-unix-mode))
 
 ;; built-in default.
 ;; (add-hook 'systemd-mode-hook
