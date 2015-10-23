9cc41f06f5ef96accaeea2e379b9d445aa2861a7
stardiviner <numbchild@gmail.com>
2015-10-23 17:00:40 +0800

    disable context completion mode when not in specific context of web-mode.

--- init/programming/frameworks/Web/init-my-prog-framework-web.el
+++ init/programming/frameworks/Web/init-my-prog-framework-web.el
@@ -295,7 +295,9 @@
   (if (equal major-mode 'web-mode)
       (let ((web-mode-cur-language (web-mode-language-at-pos)))
         (if (string= web-mode-cur-language "css")
-            (unless css-mode (css-mode))))))
+            (unless css-mode (css-mode))
+          (if css-mode (css-mode -1))
+          ))))
 
 ;; Enable JavaScript completion between <script>...</script> etc.
 (defadvice company-tern (before web-mode-set-up-ac-sources activate)
@@ -303,9 +305,9 @@
   (if (equal major-mode 'web-mode)
       (let ((web-mode-cur-language (web-mode-language-at-pos)))
         (if (or (string= web-mode-cur-language "javascript")
-               (string= web-mode-cur-language "jsx"))
+                (string= web-mode-cur-language "jsx"))
             (unless tern-mode (tern-mode))
-          ;; (if tern-mode (tern-mode))
+          (if tern-mode (tern-mode -1))
           ))))
 
 ;; Enable Ruby on Rails completion between rhtml tag <% ... %> or <%= ... %>.
@@ -314,7 +316,8 @@
   (if (equal major-mode 'web-mode)
       (let ((web-mode-cur-language (web-mode-language-at-pos)))
         (if (string= web-mode-cur-language "erb")
-            (unless robe-mode (robe-mode))))))
+            (unless robe-mode (robe-mode))
+          (if robe-mode (robe-mode -1))))))
 
 ;;;_. auto-complete support
 ;;
