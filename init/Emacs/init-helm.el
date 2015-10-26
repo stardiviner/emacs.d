323106b49f87bee74859e4053832b64ccaa7bcc9
stardiviner <numbchild@gmail.com>
2015-10-26 14:12:30 +0800

    set helm M-x keybinding face.

--- init/Emacs/init-helm.el
+++ init/Emacs/init-helm.el
@@ -111,6 +111,9 @@
 ;; But you can press [M-x C-u align-regexp RET].
 (setq helm-M-x-fuzzy-match nil
       helm-M-x-reverse-history nil)
+;; [M-x] Emacs keybinding beside command
+(set-face-attribute 'helm-M-x-key nil
+                    :foreground "cyan")
 (global-set-key (kbd "M-x") 'helm-M-x)
 ;; If you prefer the helm version of the file finder, you can bind it to C-x C-f
 ;; to replace the standard find-file:
