c84d120faab0b51c81e7aab5154099bf4d9ea2d4
stardiviner <numbchild@gmail.com>
2015-11-20 09:50:51 +0800

    use js2-mode as default major-mode for JavaScript.

--- init/programming/languages/JavaScript/init-my-prog-lang-javascript.el
+++ init/programming/languages/JavaScript/init-my-prog-lang-javascript.el
@@ -22,7 +22,7 @@
 
 ;; (add-to-list js-enabled-frameworks 'reat)
 
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js3-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
 
 ;;; [ javascript-mode (js-mode) ]
