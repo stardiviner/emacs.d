7d11009fd10d863097b1830a62e1fdbf27fc8549
stardiviner <numbchild@gmail.com>
2015-12-08 14:20:08 +0800

    configure ghc package.
    add `ghc-init` to haskell-mode-hook to auto start ghc process.

--- init/programming/languages/Haskell/init-my-prog-lang-haskell.el
+++ init/programming/languages/Haskell/init-my-prog-lang-haskell.el
@@ -58,7 +58,15 @@
 
 ;;; [ ghc ]
 
-(use-package ghc)
+(use-package ghc
+  :config
+  (autoload 'ghc-init "ghc" nil t)
+  (autoload 'ghc-debug "ghc" nil t)
+  (add-hook 'haskell-mode-hook '(lambda () (ghc-init)))
+
+  ;; if you wish to display error each goto next/prev error,
+  (setq ghc-display-error 'minibuffer)
+  )
 
 
 ;;; [ ghci-completion ]
