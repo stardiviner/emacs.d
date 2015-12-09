3f79952ea6f9e87a7289ab1ecd897b452e78a9f4
stardiviner <numbchild@gmail.com>
2015-12-09 11:29:20 +0800

    wrap w3m config with use-package.

--- init/tools/init-my-tool-browser.el
+++ init/tools/init-my-tool-browser.el
@@ -81,14 +81,15 @@
 ;;
 ;; - [M-x w3m]
 
-;; (require 'w3m)
-
-(setq w3m-coding-system 'utf-8
-      w3m-file-coding-system 'utf-8
-      w3m-file-name-coding-system 'utf-8
-      w3m-input-coding-system 'utf-8
-      w3m-output-coding-system 'utf-8
-      w3m-terminal-coding-system 'utf-8)
+(use-package w3m
+  :config
+  (setq w3m-coding-system 'utf-8
+        w3m-file-coding-system 'utf-8
+        w3m-file-name-coding-system 'utf-8
+        w3m-input-coding-system 'utf-8
+        w3m-output-coding-system 'utf-8
+        w3m-terminal-coding-system 'utf-8)
+  )
 
 
 (provide 'init-my-tool-browser)
