c48dd8f68dde883463327fc75db701f447be9d60
stardiviner <numbchild@gmail.com>
2015-12-09 11:30:42 +0800

    setup w3m keybindings.

--- init/tools/init-my-tool-browser.el
+++ init/tools/init-my-tool-browser.el
@@ -89,6 +89,24 @@
         w3m-input-coding-system 'utf-8
         w3m-output-coding-system 'utf-8
         w3m-terminal-coding-system 'utf-8)
+
+  ;; setup keybindings
+  (setq w3m-mode-map (make-sparse-keymap))
+
+  (define-key w3m-mode-map (kbd "RET") 'w3m-view-this-url)
+  (define-key w3m-mode-map (kbd "q") 'bury-buffer)
+  (define-key w3m-mode-map (kbd "<mouse-1>") 'w3m-maybe-url)
+  (define-key w3m-mode-map [f5] 'w3m-reload-this-page)
+  (define-key w3m-mode-map (kbd "C-c C-d") 'haskell-w3m-open-haddock)
+  (define-key w3m-mode-map (kbd "M-<left>") 'w3m-view-previous-page)
+  (define-key w3m-mode-map (kbd "M-<right>") 'w3m-view-next-page)
+  (define-key w3m-mode-map (kbd "M-.") 'w3m-haddock-find-tag)
+
+  (defun w3m-maybe-url ()
+    (interactive)
+    (if (or (equal '(w3m-anchor) (get-text-property (point) 'face))
+            (equal '(w3m-arrived-anchor) (get-text-property (point) 'face)))
+        (w3m-view-this-url)))
   )
 
 
