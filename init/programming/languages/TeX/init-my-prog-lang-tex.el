b0664547afbae5d47d819110e311252936b39770
stardiviner <numbchild@gmail.com>
2015-12-12 18:33:13 +0800

    configure LaTeX-mode-hook to auto guess master file.

--- init/programming/languages/TeX/init-my-prog-lang-tex.el
+++ init/programming/languages/TeX/init-my-prog-lang-tex.el
@@ -29,6 +29,44 @@
 (setq TeX-auto-save t
       TeX-parse-self t)
 
+(setq-default TeX-master nil)
+
+;; automatic detection of master file
+(defun guess-TeX-master (filename)
+  "Guess the master file for FILENAME from currently open .tex files."
+  (let ((candidate nil)
+        (filename (file-name-nondirectory filename)))
+    (save-excursion
+      (dolist (buffer (buffer-list))
+        (with-current-buffer buffer
+          (let ((name (buffer-name))
+                (file buffer-file-name))
+            (if (and file (string-match "\\.tex$" file))
+                (progn
+                  (goto-char (point-min))
+                  (if (re-search-forward (concat "\\\\input{" filename "}") nil t)
+                      (setq candidate file))
+                  (if (re-search-forward (concat "\\\\include{" (file-name-sans-extension filename) "}") nil t)
+                      (setq candidate file))))))))
+    (if candidate
+        (message "TeX master document: %s" (file-name-nondirectory candidate)))
+    candidate))
+
+(add-hook 'LaTeX-mode-hook
+          '(lambda ()
+             (setq TeX-master (guess-TeX-master (buffer-file-name)))
+             ))
+
+
+;; (setq TeX-macro-global '())
+;; (setq TeX-outline-extra t)
+
+;; preview-latex config
+;; (setq preview-transparent-color '(highlight :background)
+;;       preview-auto-reveal
+;;       preview-auto-cache-preamble 'ask
+;;       )
+
 
 ;;; [ company-auctex ]
 ;;; [ company-math ]
