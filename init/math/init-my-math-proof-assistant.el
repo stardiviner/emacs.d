d628c71796806df5d84bc4282fb2dbf42854a61f
stardiviner <numbchild@gmail.com>
2015-12-26 13:23:29 +0800

    add Mathematics / Computer Science Proof assistant init file

---
+++ init/math/init-my-math-proof-assistant.el
@@ -0,0 +1,17 @@
+;;; init-my-math-proof-assistant.el --- init for Proof assistant
+;;; -*- coding: utf-8 -*-
+
+;;; Commentary:
+
+
+
+;;; Code:
+
+
+
+
+
+
+(provide 'init-my-math-proof-assistant)
+
+;;; init-my-math-proof-assistant.el ends here
