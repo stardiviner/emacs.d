0a15171076f0a7092db2d96dcb73660279ebed8a
stardiviner <numbchild@gmail.com>
2015-12-27 21:29:49 +0800

    remove org-download usage comments.

--- init/org-mode/init-my-org-extensions.el
+++ init/org-mode/init-my-org-extensions.el
@@ -162,65 +162,6 @@
 
 ;;; [ org-download ] -- drag and drop images to Emacs org-mode.
 
-;;; Usage:
-;;
-;; *This extension facilitates moving images from point A to point B.*
-;;
-;; * Point A (the source) can be:
-;;
-;;   - An image inside your browser that you can drag to Emacs.
-;;
-;;   - An image on your file system that you can drag to Emacs.
-;;
-;;   - A local or remote image address in kill-ring. Use the org-download-yank
-;;     command for this. Remember that you can use "=[0 w]=" in dired to get an
-;;     address.
-;;
-;;   - An screenshot taken using /gnome-screenshot/ or /scrot/ or /gm/. Use the
-;;     org-download-screenshot command for this. Customize the backend with
-;;     org-download-screenshot-method.
-;;
-;; * Point B (the target) is an Emacs org-mode buffer where the inline link will
-;;   be inserted. Several customization options will determine where exactly on
-;;   the file system the file will be stored.
-;;
-;;They are: org-download-method:
-;;
-;; 1) 'attach => use org-mode attachment machinery
-;;
-;; 2) 'directory => construct the directory in two stages:
-;;
-;;   1. first part of the folder name is:
-;;      - either "." (current folder)
-;;
-;;      - or org-download-image-dir (if it's not nil).
-;;
-;;      - ~org-download-image-dir~ becomes buffer-local when set, so each file can customize this value, e.g with:
-;;
-;;        #+BEGIN_EXAMPLE
-;;        -*- mode: Org; org-download-image-dir: "~/Pictures/foo"; -*-
-;;        #+END_EXAMPLE
-;;
-;;        To set it for all files at once, use this:
-;;        
-;;        #+BEGIN_SRC emacs-lisp
-;;        (setq-default org-download-image-dir "~/Pictures/foo")
-;;        #+END_SRC
-;;
-;;   2. second part is:
-;;
-;;       - ~org-download-heading-lvl~ is ~nil~ => ""
-;;
-;;       - ~org-download-heading-lvl~ is ~n~ => the name of current heading with level n.
-;;
-;;         Level count starts with 0, i.e. * is 0, ** is 1, *** is 2
-;;         etc. org-download-heading-lvl becomes buffer-local when set, so each
-;;         file can customize this value, e.g with:
-;;
-;;         #+BEGIN_EXAMPLE
-;;         -*- mode: Org; org-download-heading-lvl: nil; -*-
-;;         #+END_EXAMPLE
-
 (org-download-enable)
 
 (use-package org-download
