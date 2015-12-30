c5cff6cbccd8e929b2d148a32f164d48a53b2ddc
stardiviner <numbchild@gmail.com>
2015-12-30 15:01:29 +0800

    wrap gnuplot-mode config with use-package.

--- init/programming/languages/gnuplot/init-my-prog-lang-gnuplot.el
+++ init/programming/languages/gnuplot/init-my-prog-lang-gnuplot.el
@@ -6,26 +6,31 @@
 
 ;;; [ gnuplot-mode ]
 
-;; add gnuplot info file.
-;; FIXME: this does not work for `gnuplot-info-lookup-symbol'
-;; (with-eval-after-load 'info
-;;   (info-initialize)
-;;   (add-to-list 'Info-directory-list "~/.emacs.d/elpa/gnuplot-*/"))
+(use-package gnuplot-mode
+  :ensure t
+  :defer t
+  :config
+  ;; add gnuplot info file.
+  ;; FIXME: this does not work for `gnuplot-info-lookup-symbol'
+  ;; (with-eval-after-load 'info
+  ;;   (info-initialize)
+  ;;   (add-to-list 'Info-directory-list "~/.emacs.d/elpa/gnuplot-*/"))
 
-(setq auto-mode-alist (append (list
-                               '("\\.gp\\'" . gnuplot-mode)
-                               '("\\.plt\\'" . gnuplot-mode)
-                               '("\\.gnuplot\\'" . gnuplot-mode)
-                               )
-                              auto-mode-alist))
+  (setq auto-mode-alist (append (list
+                                 '("\\.gp\\'" . gnuplot-mode)
+                                 '("\\.plt\\'" . gnuplot-mode)
+                                 '("\\.gnuplot\\'" . gnuplot-mode)
+                                 )
+                                auto-mode-alist))
 
-(setq gnuplot-info-display 'window)
+  (setq gnuplot-info-display 'window)
 
-(add-hook 'gnuplot-mode-hook
-          (lambda ()
-            (define-key gnuplot-mode-map (kbd "<f5>") 'gnuplot-make-buffer)
-            (define-key gnuplot-mode-map (kbd "C-h d d") 'gnuplot-info-lookup-symbol)
-            ))
+  (add-hook 'gnuplot-mode-hook
+            '(lambda ()
+               (define-key gnuplot-mode-map (kbd "<f5>") 'gnuplot-make-buffer)
+               (define-key gnuplot-mode-map (kbd "C-h d d") 'gnuplot-info-lookup-symbol)
+               ))
+  )
 
 
 
