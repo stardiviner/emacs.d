dcd7fcacd4676a319ccdd389bf7328542d0a1798
stardiviner <numbchild@gmail.com>
2016-01-01 16:30:54 +0800

    add more vagrant keybindings.

--- init/programming/tools/init-my-prog-tools-vagrant.el
+++ init/programming/tools/init-my-prog-tools-vagrant.el
@@ -34,17 +34,18 @@
     (define-prefix-command 'my-vagrant-map))
   (define-key my-prog-tools-map (kbd "v") 'my-vagrant-map)
   
-  (define-key my-vagrant-map (kbd "v") 'vagrant-box-list)
   (define-key my-vagrant-map (kbd "l") 'vagrant-box-list)
   (define-key my-vagrant-map (kbd "u") 'vagrant-up)
   (define-key my-vagrant-map (kbd "s") 'vagrant-ssh)
   (define-key my-vagrant-map (kbd "t") 'vagrant-tramp-term)
   (define-key my-vagrant-map (kbd "S") 'vagrant-status)
+  (define-key my-vagrant-map (kbd "P") 'vagrant-suspend)
   (define-key my-vagrant-map (kbd "r") 'vagrant-resume)
   (define-key my-vagrant-map (kbd "R") 'vagrant-reload)
   (define-key my-vagrant-map (kbd "h") 'vagrant-halt)
   (define-key my-vagrant-map (kbd "D") 'vagrant-destroy)
   (define-key my-vagrant-map (kbd "p") 'vagrant-provision)
+  (define-key my-vagrant-map (kbd "e") 'vagrant-edit)
   )
 
 
