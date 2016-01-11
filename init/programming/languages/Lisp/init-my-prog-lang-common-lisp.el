96239793286b7dbdb7128a2cd45bb25c51398ce8
stardiviner <numbchild@gmail.com>
2016-01-11 15:58:24 +0800

    enable `sly-mode' in Lisps.

--- init/programming/languages/Lisp/init-my-prog-lang-common-lisp.el
+++ init/programming/languages/Lisp/init-my-prog-lang-common-lisp.el
@@ -272,6 +272,16 @@
             (lambda ()
               (unless (sly-connected-p)
                 (save-excursion (sly)))))
+
+  ;; enable `sly-mode' in Lisps
+  ;;
+  ;; (dolist (hook '(sly-mode-hook
+  ;;                 sly-mrepl-mode-hook
+  ;;                 lisp-mode-hook
+  ;;                 lisp-interaction-mode-hook
+  ;;                 common-lisp-lisp-mode-hook
+  ;;                 ))
+  ;;   (add-hook hook 'sly-mode))
   )
 
 
