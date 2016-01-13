6faab3c33812dac9302e390f0fe908023f1be8c5
stardiviner <numbchild@gmail.com>
2016-01-13 20:30:43 +0800

    remove some rbenv config options.

--- init/programming/languages/Ruby/init-my-prog-lang-ruby.el
+++ init/programming/languages/Ruby/init-my-prog-lang-ruby.el
@@ -258,29 +258,12 @@
   (setq rbenv-installation-dir "~/.rbenv/") ; "/usr/local/rbenv"
   :config
   (setq rbenv-show-active-ruby-in-modeline t
-        ;; rbenv-executable "~/.rbenv/bin/rbenv"
-        ;; rbenv-binary-paths '((shims-path . "~/.rbenv/shims")
-        ;;                      (bin-path . "~/.rbenv/bin"))
-        ;; rbenv-ruby-shim "~/.rbenv/shims/ruby"
-        rbenv-modeline-function 'rbenv--modeline-plain ; 'rbenv--modeline-with-face
+        rbenv-modeline-function 'rbenv--modeline-plain
         )
-
-  ;; (defun rbenv--modeline-with-face (current-ruby)
-  ;;   (append '(" [")
-  ;;           (list (propertize current-ruby 'face 'rbenv-active-ruby-face))
-  ;;           '("]")))
-
-  ;; (defun rbenv--modeline-plain (current-ruby)
-  ;;   (list " [" current-ruby "]"))
-
-  ;; Setting rbenv path
-  ;; (setenv "PATH" (concat (getenv "HOME") "/.rbenv/shims:" (getenv "HOME") "/.rbenv/bin:" (getenv "PATH")))
-  ;; (setq exec-path (cons (concat (getenv "HOME") "/.rbenv/shims") (cons (concat (getenv "HOME") "/.rbenv/bin") exec-path)))
 
   (global-rbenv-mode 1)
 
   (rbenv-use-global)
-  ;; (rbenv-use "2.2.0")
   )
 
 
