a676ad39a5468e67ff454107b6ff0436f9ec34e9
stardiviner <numbchild@gmail.com>
2016-01-16 14:16:20 +0800

    adjust code indent.

--- init/programming/frameworks/Web/init-my-prog-framework-web.el
+++ init/programming/frameworks/Web/init-my-prog-framework-web.el
@@ -324,8 +324,8 @@
 ;; - courtesy of Patrick @halbtuerke
 (defadvice web-mode-highlight-part (around tweak-jsx activate)
   (if (equal web-mode-content-type "jsx")
-    (let ((web-mode-enable-part-face nil))
-      ad-do-it)
+      (let ((web-mode-enable-part-face nil))
+        ad-do-it)
     ad-do-it))
 
 
