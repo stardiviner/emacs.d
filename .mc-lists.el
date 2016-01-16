2587d30207ef7908e77fd80d49f117f84eefa720
stardiviner <numbchild@gmail.com>
2016-01-16 14:07:34 +0800

    update multiple-cursors allowed commands.

--- .mc-lists.el
+++ .mc-lists.el
@@ -3,9 +3,30 @@
 
 (setq mc/cmds-to-run-for-all
       '(
+        backward-sexp
+        fill-paragraph
+        forward-sexp
         kill-region
+        my/org-insert-key
+        org-beginning-of-line
+        org-ctrl-c-ctrl-c
+        org-ctrl-c-star
         org-delete-char
+        org-end-of-line
+        org-kill-line
+        org-metaleft
+        org-metaright
+        org-open-line
         org-self-insert-command
+        org-yank
+        paredit-backward
+        paredit-close-square
+        paredit-forward-delete
+        paredit-kill
+        paredit-open-square
+        paredit-semicolon
+        sp-remove-active-pair-overlay
+        yas-expand
         ))
 
 (setq mc/cmds-to-run-once
