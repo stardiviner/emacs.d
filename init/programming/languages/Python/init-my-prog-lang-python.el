bd0078672c85d985ac9a17e1fe4950ec2c670f16
stardiviner <numbchild@gmail.com>
2016-01-16 14:18:00 +0800

    show Python object docstring in eldoc.

--- init/programming/languages/Python/init-my-prog-lang-python.el
+++ init/programming/languages/Python/init-my-prog-lang-python.el
@@ -208,7 +208,25 @@
 ;; options.
 
 (use-package anaconda-mode
-  :init
+  :config
+  
+  ;; show object docstring in eldoc.
+  (defun anaconda-mode-docstring-eldoc-function ()
+    (anaconda-mode-call "goto_definitions" 'anaconda-mode-docstring-eldoc-callback))
+
+  (defun anaconda-mode-docstring-eldoc-callback (result)
+    (eldoc-message
+     (s-join " " (--map
+                  (s-collapse-whitespace (cdr (assoc 'docstring it))) result))))
+
+  (defun docstring-eldoc-init ()
+    (make-local-variable 'eldoc-documentation-function)
+    (setq-local eldoc-documentation-function
+                'anaconda-mode-docstring-eldoc-function))
+
+  (add-hook 'python-mode-hook 'docstring-eldoc-init)
+
+  ;; enable anaconda-mode in python-mode.
   (add-hook 'python-mode-hook 'anaconda-mode)
   (add-hook 'python-mode-hook 'eldoc-mode)
   )
