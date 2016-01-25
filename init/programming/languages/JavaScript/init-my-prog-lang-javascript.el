8772e3c1dc5d59ac51eae33d1475fcd49a389992
stardiviner <numbchild@gmail.com>
2016-01-25 16:11:32 +0800

    fix js2-mode-map & js3-mode-map available issue.

--- init/programming/languages/JavaScript/init-my-prog-lang-javascript.el
+++ init/programming/languages/JavaScript/init-my-prog-lang-javascript.el
@@ -36,8 +36,14 @@
   (newline-and-indent))
 
 (define-key js-mode-map  (kbd "C-o") 'my/open-new-line-upper)
-(define-key js2-mode-map (kbd "C-o") 'my/open-new-line-upper)
-(define-key js3-mode-map (kbd "C-o") 'my/open-new-line-upper)
+(use-package js2-mode
+  :ensure t
+  :config
+  (define-key js2-mode-map (kbd "C-o") 'my/open-new-line-upper))
+(use-package js3-mode
+  :ensure t
+  :config
+  (define-key js3-mode-map (kbd "C-o") 'my/open-new-line-upper))
 
 
 ;;; [ javascript-mode (js-mode) ]
