091c67b10ec28e5771bc288e1d438ecf0a717d6a
stardiviner <numbchild@gmail.com>
2016-02-02 11:12:44 +0800

    disable migemo package to fix isearch is replaced by migemo-isearch with advice.

--- init/languages/init-my-language-japanese.el
+++ init/languages/init-my-language-japanese.el
@@ -9,22 +9,23 @@
 
 ;;; [ migemo ] -- provides Japanese increment search with 'Romanization of Japanese'(ローマ字).
 
-(use-package migemo
-  :ensure t
-  :config
-  (autoload 'migemo "migemo" "" t)
-
-  (setq migemo-command "cmigemo"
-        migemo-options '("-q" "--emacs")
-        ;; Set your installed path
-        ;; migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict"
-        ;; migemo-user-dictionary nil
-        ;; migemo-regex-dictionary nil
-        migemo-coding-system 'utf-8-unix
-        )
-
-  (migemo-init)
-  )
+;; (use-package migemo
+;;   :ensure t
+;;   :config
+;;   (autoload 'migemo "migemo" "" t)
+;;
+;;   (setq migemo-command "cmigemo"
+;;         migemo-options '("-q" "--emacs")
+;;         ;; Set your installed path
+;;         ;; migemo-dictionary "/usr/local/share/migemo/utf-8/migemo-dict"
+;;         ;; migemo-user-dictionary nil
+;;         ;; migemo-regex-dictionary nil
+;;         migemo-coding-system 'utf-8-unix
+;;         )
+;;
+;;   ;; (migemo-init)
+;;   (define-key my-search-language-prefix (kbd "j") 'migemo-isearch-toggle-migemo)
+;;   )
 
 
 ;;; [ oniisama ]
