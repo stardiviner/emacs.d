3e5d347d0ae2e1f5a1baa4fa9e0edfe8d31321f0
stardiviner <numbchild@gmail.com>
2016-02-19 19:50:12 +0800

    disable kite-mini package.

--- init/programming/frameworks/Web/init-my-prog-web-browser.el
+++ init/programming/frameworks/Web/init-my-prog-web-browser.el
@@ -70,16 +70,16 @@
 ;;; Usage:
 ;;
 ;; 1. open Chrome with remote debugging enabled:
-;;    $ chromium --remote-debugging-port=9222
-;; 2. kite-mini-mode in `js-mode', or `css-mode'.
+;;    $ chrome --remote-debugging-port=9222
+;; 2. enable `kite-mini-mode' in `js-mode', or `css-mode'.
 
-(use-package kite-mini
-  :ensure t
-  :config
-  ;; Automatically Turn on the mode for your buffer of choice.
-  (add-hook 'js-mode-hook (lambda () (kite-mini-mode t)))
-  (add-hook 'css-mode-hook (lambda () (kite-mini-mode t)))
-  )
+;; (use-package kite-mini
+;;   :ensure t
+;;   :config
+;;   ;; Automatically Turn on the mode for your buffer of choice.
+;;   (add-hook 'js-mode-hook (lambda () (kite-mini-mode t)))
+;;   (add-hook 'css-mode-hook (lambda () (kite-mini-mode t)))
+;;   )
 
 
 (provide 'init-my-prog-web-browser)
