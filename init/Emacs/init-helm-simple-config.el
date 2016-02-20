7b8b7db3de0c94c822a3e722508d96731c8db3d6
stardiviner <numbchild@gmail.com>
2016-02-20 14:58:06 +0800

    update simple helm config options.

--- init/Emacs/init-helm-simple-config.el
+++ init/Emacs/init-helm-simple-config.el
@@ -13,41 +13,31 @@
   :config
   (helm-autoresize-mode t)
 
-  (setq helm-full-frame nil ; use current window as popup. helm popup window position.
+  (setq helm-full-frame nil
         helm-always-two-windows t
         helm-autoresize-max-height 25
         helm-autoresize-min-height 6
-        ;; display
         ;; helm-display-function 'popwin:pop-to-buffer
-        ;; split
-        helm-split-window-in-side-p t ; force split inside selected window.
+        helm-split-window-in-side-p t
         helm-split-window-default-side 'below
         ;; helm-split-window-preferred-function
-        ;; 'helm-split-window-default-fn, 'popwin:pop-to-buffer
-        ;; helm-split-window-preferred-function 'popwin:pop-to-buffer
-        helm-quick-update t ; do not display invisible candidates
-        ;; helm-scroll-amount 8 ; scroll 8 lines other window using M-<next>/M-<prior>
+        helm-quick-update nil
         helm-case-fold-search t
         helm-buffers-fuzzy-matching nil
         helm-recentf-fuzzy-match nil
-        helm-move-to-line-cycle-in-source nil ; nil: not just cycling current limited candicates section.
+        helm-move-to-line-cycle-in-source nil
         ;; find-file
-        helm-ff-search-library-in-sexp t ; search for library in `require' and `declare-function' sexp.
-        helm-ff-file-name-history-use-recentf t ; use recentf
-        helm-ff-transformer-show-only-basename t ; show basename in find file candidates. toggled by C-].
+        helm-ff-search-library-in-sexp nil
+        helm-ff-file-name-history-use-recentf nil
+        helm-ff-transformer-show-only-basename t
         ;; helm-sources-using-default-as-input
         )
 
   ;; [ Helm faces ]
   ;; FIXME: this does not apply.
   (set-face-attribute 'helm-match nil
-                      ;; 1. dim already matched, leave eye on the un-matched strings.
                       :foreground "dim gray"
                       :background (color-darken-name (face-background 'default) 5)
-                      ;; 2. highlight/underline already matched
-                      ;; :foreground "white"
-                      ;; :underline t
-                      ;; :weight 'normal
                       )
   (set-face-attribute 'helm-selection nil
                       ;; 1. different font
@@ -74,7 +64,6 @@
   (set-face-attribute 'helm-source-header nil
                       :foreground "green" :background "#222222"
                       :overline "yellow" :weight 'bold
-                      ;; :family "DejaVu Sans Mono" :height 1.0
                       )
   (set-face-attribute 'helm-prefarg nil
                       :foreground "cyan")
