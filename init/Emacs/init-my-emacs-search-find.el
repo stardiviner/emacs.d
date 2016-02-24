8685880d1320c465459d4d022f3a8ec647b07554
stardiviner <numbchild@gmail.com>
2016-02-24 23:22:28 +0800

    add a function to set ffip-project-root smartly use current directory.

--- init/Emacs/init-my-emacs-search-find.el
+++ init/Emacs/init-my-emacs-search-find.el
@@ -12,8 +12,28 @@
 (use-package find-file-in-project
   :ensure t
   :config
-  (setq ffip-project-root "~")
-  ;; (setq ffip-project-root-function)
+  (setq ffip-project-root nil)
+  (setq ffip-project-root-function ffip-project-root)
+  (defun ffip-project-root ()
+    "Return the root of the project."
+    (let ((project-root (or ffip-project-root
+                            (if (functionp ffip-project-root-function)
+                                (funcall ffip-project-root-function)
+                              (if (listp ffip-project-file)
+                                  (cl-some (apply-partially 'locate-dominating-file
+                                                            default-directory)
+                                           ffip-project-file)
+                                (locate-dominating-file default-directory
+                                                        ffip-project-file))))))
+      (or project-root
+          (progn (message "No project was defined for the current file.")
+                 "."
+                 ;; nil
+                 ))))
+  
+  ;; default project root
+  ;; (setq ffip-project-root "~") ; home directory
+  ;; (setq ffip-project-root ".") ; current directory
 
   ;; (setq ffip-match-path-instead-of-filename t)
   
