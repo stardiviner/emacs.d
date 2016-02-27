e7840d3e110c553c1a8d4e33585677d74afd293f
stardiviner <numbchild@gmail.com>
2016-02-27 14:34:10 +0800

    add an advice to load Org-mode babel languages lazily.

--- init/org-mode/init-my-org-babel.el
+++ init/org-mode/init-my-org-babel.el
@@ -141,6 +141,16 @@
       )
 
 
+
+(defadvice org-babel-execute-src-block (around load-language nil activate)
+  "Load language if needed"
+  (let ((language (org-element-property :language (org-element-at-point))))
+    (unless (cdr (assoc (intern language) org-babel-load-languages))
+      (add-to-list 'org-babel-load-languages (cons (intern language) t))
+      (org-babel-do-load-languages 'org-babel-load-languages org-babel-load-languages))
+    ad-do-it))
+
+
 (add-to-list 'org-src-lang-modes (cons "SQL" 'sql))
 
 ;; (use-package js2-mode
