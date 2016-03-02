8ce1dc6ab253f9daba7581e80c2ceab9f892594f
stardiviner <numbchild@gmail.com>
2016-03-02 14:55:53 +0800

    toggle org footnote fill after inline note extraction.

--- init/org-mode/init-my-org-document-structure.el
+++ init/org-mode/init-my-org-document-structure.el
@@ -78,9 +78,9 @@
 (setq org-footnote-auto-label 'confirm
       org-footnote-auto-adjust t
       org-footnote-define-inline nil ; define foot-note inline, instead of separate section.
-      ;; org-footnote-fill-after-inline-note-extraction
-      ;; org-footnote-section
-      ;; org-footnote-tag-for-non-org-mode-files
+      org-footnote-fill-after-inline-note-extraction t
+      ;; org-footnote-section "Footnotes"
+      ;; org-footnote-tag-for-non-org-mode-files "Footnotes:"
       )
 
 
