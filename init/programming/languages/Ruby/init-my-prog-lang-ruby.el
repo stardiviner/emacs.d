2f537735faee7afd7600c8f4573d12f4964d286c
stardiviner <numbchild@gmail.com>
2016-03-04 14:39:54 +0800

    don't let yard-mode override eldoc-documentation-function for ruby-mode.

--- init/programming/languages/Ruby/init-my-prog-lang-ruby.el
+++ init/programming/languages/Ruby/init-my-prog-lang-ruby.el
@@ -209,12 +209,15 @@
 (use-package yard-mode
   :ensure t
   :config
+  (setq yard-use-eldoc nil)
+  
   (add-hook 'ruby-mode-hook 'yard-mode)
   (add-hook 'enh-ruby-mode-hook 'yard-mode)
 
   ;; If you would also like eldoc support, so that the expected syntax for the tag
   ;; beneath your cursor is displayed in the minibuffer, add that hook too:
-  (add-hook 'ruby-mode-hook 'eldoc-mode)
+  ;; (add-hook 'ruby-mode-hook 'eldoc-mode)
+  ;; (add-hook 'enh-ruby-mode-hook 'eldoc-mode)
   )
 
 
