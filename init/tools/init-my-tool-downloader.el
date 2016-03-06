1912b48bf9c5a085a488b7a7afe3cdaba6c586f6
stardiviner <numbchild@gmail.com>
2016-03-06 22:26:07 +0800

    add package download-region.

--- init/tools/init-my-tool-downloader.el
+++ init/tools/init-my-tool-downloader.el
@@ -7,9 +7,15 @@
 
 ;;; Code:
 
+;;; [ download-region ] -- simple in-buffer download manager for Emacs.
 
-
-
+(use-package download-region
+  :ensure t
+  :config
+  (set-face-attribute 'download-region-downloading nil
+                      :background "#194854" :foreground ""
+                      )
+  )
 
 
 (provide 'init-my-tool-downloader)
