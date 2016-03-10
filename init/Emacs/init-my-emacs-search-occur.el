1e5bedbc1c59f2bacaacaeceeca0f30f6f57e7b1
stardiviner <numbchild@gmail.com>
2016-03-10 16:44:06 +0800

    configure helm-swoop.

--- init/Emacs/init-my-emacs-search-occur.el
+++ init/Emacs/init-my-emacs-search-occur.el
@@ -39,7 +39,30 @@
 ;;; [ helm-swoop ]
 
 (use-package helm-swoop
-  :ensure t)
+  :ensure t
+  :config
+  ;; Save buffer when helm-multi-swoop-edit complete
+  (setq helm-multi-swoop-edit-save t)
+  ;; If this value is t, split window inside the current window
+  (setq helm-swoop-split-with-multiple-windows t)
+  ;; Split direction. 'split-window-vertically or 'split-window-horizontally
+  (setq helm-swoop-split-direction 'split-window-vertically)
+  ;; If nil, you can slightly boost invoke speed in exchange for text color
+  (setq helm-swoop-speed-or-color nil)
+  ;; Go to the opposite side of line from the end or beginning of line
+  (setq helm-swoop-move-to-line-cycle t)
+  ;; Optional face for line numbers
+  ;; Face name is `helm-swoop-line-number-face`
+  (setq helm-swoop-use-line-number-face nil)
+
+  ;; keybindings
+  (define-key my-search-prefix (kbd "C-o") 'helm-swoop)
+  (define-key my-search-prefix (kbd "C-S-o") 'helm-swoop-back-to-last-point)
+  (define-key my-search-prefix (kbd "C-M-o") 'helm-multi-swoop) ; `helm-multi-swoop-all'
+  ;; When doing isearch, hand the word over to helm-swoop
+  (define-key isearch-mode-map (kbd "M-o") 'helm-swoop-from-isearch)
+  (define-key helm-swoop-map (kbd "M-o") 'helm-multi-swoop-all-from-helm-swoop)
+  )
 
 
 (provide 'init-my-emacs-search-occur)
