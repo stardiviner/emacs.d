560124af08bfa90e1d65d346cbb31b436f40fb08
stardiviner <numbchild@gmail.com>
2016-03-18 13:05:30 +0800

    fix #+CALL: babel execute failed because this advice

--- init/org-mode/init-my-org-babel.el
+++ init/org-mode/init-my-org-babel.el
@@ -187,12 +187,16 @@
 
 
 (defadvice org-babel-execute-src-block (around load-language nil activate)
-  "Load language if needed"
+  "Load language if needed."
   (let ((language (org-element-property :language (org-element-at-point))))
-    (unless (cdr (assoc (intern language) org-babel-load-languages))
-      (require (intern (concat "ob-" language)))
-      (add-to-list 'org-babel-load-languages (cons (intern language) t))
-      (org-babel-do-load-languages 'org-babel-load-languages org-babel-load-languages))
+    ;; workaround for #+CALL: babel. (`language' will be `nil')
+    (if language
+        ;; whether language is already loaded in `org-babel-load-languages'.
+        (unless (cdr (assoc (intern language) org-babel-load-languages))
+          (require (intern (concat "ob-" language)))
+          (add-to-list 'org-babel-load-languages (cons (intern language) t))
+          (org-babel-do-load-languages 'org-babel-load-languages org-babel-load-languages))
+      )
     ad-do-it))
 
 
