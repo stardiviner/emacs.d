e89e9d96d6fd97a00620c8ef4c3e8c43de26fd45
stardiviner <numbchild@gmail.com>
2016-03-19 20:26:27 +0800

    add package company-arduino.

--- init/programming/frameworks/Arduino/init-my-prog-framework-arduino.el
+++ init/programming/frameworks/Arduino/init-my-prog-framework-arduino.el
@@ -16,6 +16,29 @@
   )
 
 
+;;; [ company-arduino ]
+
+(use-package company-arduino
+  :ensure t
+  :init
+  (setenv "ARDUINO_HOME" "/usr/share/arduino/")
+  :config
+  (add-hook 'arduino-mode-hook
+            (lambda ()
+              (irony-mode 1)
+              (company-arduino-turn-on)
+              ;; (add-to-list 'company-backends 'company-irony)
+              ;; (add-to-list 'company-backends 'company-c-headers)
+              ))
+  
+  ;; (defun my-company-c-headers-get-system-path ()
+  ;;   "Return the system include path for the current buffer."
+  ;;   (let ((default '("/usr/include/" "/usr/local/include/")))
+  ;;     (company-arduino-append-include-dirs default t)))
+  ;; (setq company-c-headers-path-system 'my-company-c-headers-get-system-path)
+  )
+
+
 (provide 'init-my-prog-framework-arduino)
 
 ;;; init-my-prog-framework-arduino.el ends here
