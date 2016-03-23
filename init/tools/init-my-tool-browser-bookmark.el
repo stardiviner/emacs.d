eb4b79fc202b7793f1452307090a1139143d8757
stardiviner <numbchild@gmail.com>
2016-03-23 23:08:31 +0800

    re-enable many init files.

--- init/tools/init-my-tool-browser-bookmark.el
+++
@@ -1,16 +0,0 @@
-;;; init-my-tool-browser-bookmark.el --- init for Browser Bookmarks
-;;; -*- coding: utf-8 -*-
-
-;;; Commentary:
-
-
-
-;;; Code:
-
-
-
-
-
-(provide 'init-my-tool-browser-bookmark)
-
-;;; init-my-tool-browser-bookmark.el ends here
