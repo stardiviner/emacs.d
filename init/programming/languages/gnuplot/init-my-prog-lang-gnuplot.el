d94d30e38f67f816f6a2fb639e9a1195eabeb518
stardiviner <numbchild@gmail.com>
2016-04-10 10:18:56 +0800

    use my custom setting to replace gnuplot-context-sensitive-mode.

--- init/programming/languages/gnuplot/init-my-prog-lang-gnuplot.el
+++ init/programming/languages/gnuplot/init-my-prog-lang-gnuplot.el
@@ -22,17 +22,41 @@
                                  )
                                 auto-mode-alist))
   :config
+  (require 'gnuplot-context)
+  
   (setq gnuplot-info-display 'window
         gnuplot-tab-completion t
-        gnuplot-eldoc-mode t)
+        gnuplot-use-context-sensitive-completion nil
+        gnuplot-eldoc-mode t
+        )
 
   (add-hook 'gnuplot-mode-hook
             (lambda ()
               ;; context sensitive for gnuplot completion & eldoc-mode.
-              (gnuplot-context-sensitive-mode 1)
+              ;; - `completion-at-point'
+              ;; - `gnuplot-info-at-point' / [C-c C-/]
+              ;; - `gnuplot-help-function' / [C-c C-d]
+              ;;
+              ;; (gnuplot-context-sensitive-mode 1)
 
+              ;; (setq gnuplot-completion-at-point-function
+              ;;       #'gnuplot-context-completion-at-point)
+              (add-to-list (make-local-variable 'completion-at-point-functions)
+                           #'gnuplot-context-completion-at-point)
+              
+              ;; Setup Eldoc
+              (eldoc-mode 1)
+              (set (make-local-variable 'eldoc-documentation-function)
+                   'gnuplot-eldoc-function)
+              (eldoc-add-command 'completion-at-point)     ; Check for eldoc after completion
+              (when (fboundp 'comint-dynamic-complete)
+                (eldoc-add-command 'comint-dynamic-complete))
+              
+              
               (define-key gnuplot-mode-map (kbd "<f5>") 'gnuplot-make-buffer)
               (define-key gnuplot-mode-map (kbd "C-h d d") 'gnuplot-info-lookup-symbol)
+              (define-key gnuplot-mode-map (kbd "C-c C-d") 'gnuplot-info-at-point)
+              (define-key gnuplot-mode-map (kbd "C-c C-/") 'gnuplot-help-function)
               (define-key gnuplot-mode-map (kbd "C-c M-i") 'gnuplot-inline-display-mode)
 
               (define-key gnuplot-mode-map (kbd "C-c C-s") 'run-gnuplot)
