b69f4afd6e43bfe58f8397f01bffafc5276d6f17
stardiviner <numbchild@gmail.com>
2016-04-13 15:15:05 +0800

    improve org-mode list bullets symbol.

--- init/org-mode/init-my-org-view.el
+++ init/org-mode/init-my-org-view.el
@@ -90,6 +90,24 @@
             ))
 
 
+;;; better list bullets
+
+;; Asterisks and dashes for bullet lists are fine, but having an actual circular
+;; bullet, is just nice:
+
+;; match "- list item"
+(font-lock-add-keywords
+ 'org-mode
+ '(("^\\([-+]\\) "
+    (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+;; match " - list item"
+(font-lock-add-keywords
+ 'org-mode
+ '(("^ +\\([-+]\\) "
+    (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+
 ;;; [ org-beautify-theme ] -- A sub-theme to make org-mode more beautiful.
 
 ;; (require 'org-beautify-theme)
