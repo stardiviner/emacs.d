4c498b3e26324a54b7dc0c255093f67a8db84356
stardiviner <numbchild@gmail.com>
2016-04-25 00:11:11 +0800

    add package org-page.

--- init/tools/init-my-tool-blog-org.el
+++ init/tools/init-my-tool-blog-org.el
@@ -7,6 +7,28 @@
 
 ;;; Code:
 
+;;; [ org-page ]
+
+(use-package org-page
+  :ensure t
+  :config
+  (setq op/repository-directory (expand-file-name "~/Org/Blog/org-page")
+        ;; op/repository-html-branch "master"
+        ;; op/repository-org-branch "source"
+        op/site-domain "http://stardiviner.github.io/"
+        op/site-main-title "stardiviner's blog"
+        op/site-sub-title "get over the world!"
+        ;; op/theme-root-directory
+        ;; op/theme 'mdo
+        op/personal-github-link "https://github.com/stardiviner"
+        op/personal-avatar "https://stardiviner.github.com/assets/images/avatar.png"
+        ;; [ commenting ]
+        op/personal-disqus-shortname "stardiviner-dark"
+        ;; op/personal-duoshuo-shortname ""
+        ;; [ google analytics ]
+        op/personal-google-analytics-id "UA-63992234-1"
+        )
+  )
 
 
 
