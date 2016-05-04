b9e7f4be78b679412779f8f0ea4f6e316b2ea14f
stardiviner <numbchild@gmail.com>
2016-05-04 23:01:38 +0800

    add a mouse disable minor mode.

--- init/Emacs/init-my-emacs-mouse.el
+++ init/Emacs/init-my-emacs-mouse.el
@@ -24,6 +24,26 @@
 (setq make-pointer-invisible t)
 
 
+;;; disable mouse minor mode
+
+(define-minor-mode disable-mouse-mode
+  "A minor-mode that disables all mouse keybinds."
+  :global t
+  :lighter " 🐭"
+  :keymap (make-sparse-keymap))
+
+(dolist (type '(mouse down-mouse drag-mouse
+                      double-mouse triple-mouse))
+  (dolist (prefix '("" C- M- S- M-S- C-M- C-S- C-M-S-))
+    ;; Yes, I actually HAD to go up to 7 here.
+    (dotimes (n 7)
+      (let ((k (format "%s%s-%s" prefix type n)))
+        (define-key disable-mouse-mode-map
+          (vector (intern k)) #'ignore)))))
+
+(disable-mouse-mode 1)
+
+
 ;;; [ Drag & Drop (dnd protocol) ]
 
 ;; (setq dnd-open-file-other-window t)
