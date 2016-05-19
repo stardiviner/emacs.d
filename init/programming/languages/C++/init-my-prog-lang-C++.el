b751190d1943ef6d8bf7fd6f6e2e2c45736cc972
stardiviner <numbchild@gmail.com>
2016-05-19 16:18:20 +0800

    add package modern-cpp-font-lock.

--- init/programming/languages/C++/init-my-prog-lang-C++.el
+++ init/programming/languages/C++/init-my-prog-lang-C++.el
@@ -7,9 +7,15 @@
 
 ;;; Code:
 
+;;; [ modern-cpp-font-lock ] -- font-locking for C++ mode.
 
-
-
+(use-package modern-cpp-font-lock
+  :ensure t
+  :config
+  (modern-c++-font-lock-global-mode t)
+  ;; local for current buffer
+  ;; (add-hook 'c++-mode-hook 'modern-c++-font-lock-mode)
+  )
 
 
 (provide 'init-my-prog-lang-C++)
