0dc4d29ed41e40e899b974286b9005b6f1fd72ad
stardiviner <numbchild@gmail.com>
2016-05-22 13:56:33 +0800

    add my asm-mode settings.

--- init/programming/languages/Assembly/init-my-prog-lang-assembly.el
+++ init/programming/languages/Assembly/init-my-prog-lang-assembly.el
@@ -9,10 +9,16 @@
 
 ;;; [ asm-mode ]
 
-(use-package asm-mode
-  :ensure t
-  :config
+(require 'asm-mode)
+
+(defun my-asm-mode-settings ()
+  ;; you can use `comment-dwim' (M-;) for this kind of behaviour anyway
+  (local-unset-key (vector asm-comment-char))
+  ;; asm-mode sets it locally to nil, to "stay closer to the old TAB behaviour".
+  (setq-local tab-always-indent (default-value 'tab-always-indent))
   )
+
+(add-hook 'asm-mode-hook #'my-asm-mode-settings)
 
 
 ;;; [ nasm-mode ]
