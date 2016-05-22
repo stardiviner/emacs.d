8c53cd82bee7f58a6c3861eaddc2c2c5afe2348b
stardiviner <numbchild@gmail.com>
2016-05-22 10:47:22 +0800

    add package blog-admin.

--- init/tools/init-my-tool-blog.el
+++ init/tools/init-my-tool-blog.el
@@ -13,6 +13,13 @@
 (define-key my-tools-prefix (kbd "l") 'blog-map)
 
 
+;;; [ blog-admin ] -- Write blog in emacs with many backends: hexo/org-page/nikola
+
+(use-package blog-admin
+  :ensure t
+  )
+
+
 
 ;; (require 'init-my-tool-blog-jekyll)
 ;; (require 'init-my-tool-blog-octopress)
