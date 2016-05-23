5e2ddd65ef5f1a1df2e66874efc4426d2aeffdb9
stardiviner <numbchild@gmail.com>
2016-05-23 20:11:06 +0800

    add mml select my GPG key into list.

--- customize.el
+++ customize.el
@@ -3,6 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(mml-secure-key-preferences
+   '((OpenPGP
+      (sign
+       ("numbchild@gmail.com" "F09F650D7D674819892591401B5DF1C95AE89AC3"))
+      (encrypt))
+     (CMS
+      (sign)
+      (encrypt))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
