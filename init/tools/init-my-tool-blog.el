652eb01ad0f19c33b28470b289a00f809d852c67
stardiviner <numbchild@gmail.com>
2016-05-24 13:55:23 +0800

    configure org-page for blog-admin.

--- init/tools/init-my-tool-blog.el
+++ init/tools/init-my-tool-blog.el
@@ -16,7 +16,29 @@
 ;;; [ blog-admin ] -- Write blog in emacs with many backends: hexo/org-page/nikola
 
 (use-package org-page
-  :ensure t)
+  :ensure t
+  :config
+  (setq op/repository-directory (expand-file-name "~/Org/Blog/org-page")
+        ;; op/repository-html-branch "master"
+        ;; op/repository-org-branch "source"
+        op/site-domain "http://stardiviner.github.io/"
+        op/site-main-title "stardiviner's blog"
+        op/site-sub-title "get over the world!"
+        op/theme-root-directory (concat user-emacs-directory "org-page/themes")
+        op/theme 'peach_blossom
+        op/personal-github-link "https://github.com/stardiviner"
+        op/personal-avatar "https://stardiviner.github.com/media/img/avatar.png"
+        ;; [ commenting ]
+        op/personal-disqus-shortname "stardiviner-dark"
+        ;; op/personal-duoshuo-shortname ""
+        ;; [ google analytics ]
+        op/personal-google-analytics-id "63992234"
+        )
+
+  (define-key blog-map (kbd "n") 'op/new-post)
+  (define-key blog-map (kbd "p") 'op/do-publication)
+  (define-key blog-map (kbd "P") 'op/do-publication-and-preview-site)
+  )
 
 (use-package blog-admin
   :ensure t
