583a29bc02074e362021577cf37d4af7f9fbe6e3
stardiviner <numbchild@gmail.com>
2016-06-10 15:23:28 +0800

    wrap sr-speedbar with use-package.
    improve sr-speedbar config.

--- init/programming/code browser/init-my-prog-speedbar.el
+++ init/programming/code browser/init-my-prog-speedbar.el
@@ -17,35 +17,33 @@
 ;; - [sr-speedbar-toggle] :: toggle sr-speedbar window.
 ;; - [sr-speedbar-select-window] :: select sr-speedbar window.
 
-(setq sr-speedbar-width 30
-      sr-speedbar-width-x 30
-      sr-speedbar-width-console 30
-      sr-speedbar-max-width 30
-      sr-speedbar-delete-windows nil ; delete other windows before showing sr-speedbar up
-      sr-speedbar-skip-other-window-p nil ; skip sr-speedbar in other-window cycle.
-      sr-speedbar-auto-refresh t
-      sr-speedbar-right-side nil ; nil for left. t for righ.t
-      )
+(use-package sr-speedbar
+  :ensure t
+  :config
+  (setq sr-speedbar-width 30
+        sr-speedbar-width-x 30
+        sr-speedbar-width-console 30
+        sr-speedbar-max-width 30
+        sr-speedbar-delete-windows nil
+        sr-speedbar-skip-other-window-p nil
+        sr-speedbar-auto-refresh t
+        sr-speedbar-right-side nil
+        )
+  (add-hook 'speedbar-mode-hook
+            (lambda ()
+              (linum-mode -1)))
 
-(add-hook 'speedbar-mode-hook
-          (lambda ()
-            (linum-mode -1)))
-
-
-
-
-(defun my-sr-speedbar-toggle-and-switch ()
-  "Toggle sr-speedbar or switch to sr-speedbar window if already opened."
-  (interactive)
-  (if (sr-speedbar-exist-p) ; if sr-speedbar is opened.
-      (if (equal (buffer-name (current-buffer)) sr-speedbar-buffer-name) ; if currently in the sr-speedbar window.
-          (sr-speedbar-close)
-        (sr-speedbar-select-window))
-    (sr-speedbar-open)
-    ;; (other-window 1) ; switch back to previous window.
-    ))
-
-;; (setq sr-speedbar-skip-other-window-p t)
+  (defun my-sr-speedbar-toggle-and-switch ()
+    "Toggle sr-speedbar or switch to sr-speedbar window if already opened."
+    (interactive)
+    (if (sr-speedbar-exist-p)
+        (if (equal (buffer-name (current-buffer)) sr-speedbar-buffer-name) ; if currently in the sr-speedbar window.
+            (sr-speedbar-close)
+          (sr-speedbar-select-window))
+      (sr-speedbar-open)
+      ;; (other-window 1) ; switch back to previous window.
+      ))
+  )
 
 
 
