e5e4354e177695271d5ca328966e72c3dd100019
stardiviner <numbchild@gmail.com>
2016-06-10 08:35:00 +0800

    add package helm-gtags.

--- init/programming/tags/init-my-prog-tags-gtags.el
+++ init/programming/tags/init-my-prog-tags-gtags.el
@@ -37,6 +37,35 @@
   )
 
 
+;;; [ helm-gtags ] -- helm interface for gtags.
+
+(use-package helm-gtags
+  :ensure t
+  :config
+  (setq helm-gtags-ignore-case t
+        helm-gtags-auto-update t
+        helm-gtags-use-input-at-cursor t
+        helm-gtags-pulse-at-cursor t
+        helm-gtags-prefix-key "\C-cg"
+        helm-gtags-suggested-key-mapping t
+        )
+
+  ;; Enable helm-gtags-mode
+  (add-hook 'dired-mode-hook 'helm-gtags-mode)
+  (add-hook 'eshell-mode-hook 'helm-gtags-mode)
+  (add-hook 'c-mode-hook 'helm-gtags-mode)
+  (add-hook 'c++-mode-hook 'helm-gtags-mode)
+  (add-hook 'asm-mode-hook 'helm-gtags-mode)
+
+  (define-key helm-gtags-mode-map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
+  (define-key helm-gtags-mode-map (kbd "C-j") 'helm-gtags-select)
+  (define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
+  (define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
+  (define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
+  (define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
+  )
+
+
 (provide 'init-my-prog-tags-gtags)
 
 ;;; init-my-prog-tags-gtags.el ends here
