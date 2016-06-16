6aa152722209ca1c58f14f222136c7fadc62e42e
stardiviner <numbchild@gmail.com>
2016-06-16 14:22:55 +0800

    disable GPG agent in Emacs for Gnus.

--- init/tools/init-gnus.el
+++ init/tools/init-gnus.el
@@ -357,39 +357,39 @@ This moves them into the Spam folder."
 
 ;;; GnuPG Agent
 
-(defun gpg-agent-restart ()
-  "This kills and restarts the gpg-agent.
-
-To kill gpg-agent, we use killall. If you know that the agent is
-OK, you should just reload the environment file using
-`gpg-agent-reload-info'."
-  (interactive)
-  (shell-command "killall gpg-agent")
-  (shell-command "gpg-agent --daemon --enable-ssh-support --write-env-file")
-  ;; read the environment file instead of parsing the output
-  (gpg-agent-reload-info))
-
-(defun gpg-agent-reload-info ()
-  "Reload the ~/.gpg-agent-info file."
-  (interactive)
-  (with-temp-buffer
-    (insert-file (expand-file-name "~/.gpg-agent-info"))
-    (goto-char (point-min))
-    (while (re-search-forward "\\([A-Z_]+\\)=\\(.*\\)" nil t)
-      (setenv (match-string 1) (match-string 2)))))
-
-(defun gpg-agent-startup ()
-  "Initialize the gpg-agent if necessary.
-
-Note that sometimes the gpg-agent can be up and running and still
-be useless, in which case you should restart it using
-`gpg-agent-restart'."
-  (gpg-agent-reload-info)
-  (unless (member (string-to-number (getenv "SSH_AGENT_PID"))
-                  (list-system-processes))
-    (gpg-agent-restart)))
-
-(gpg-agent-startup)
+;; (defun gpg-agent-restart ()
+;;   "This kills and restarts the gpg-agent.
+;;
+;; To kill gpg-agent, we use killall. If you know that the agent is
+;; OK, you should just reload the environment file using
+;; `gpg-agent-reload-info'."
+;;   (interactive)
+;;   (shell-command "killall gpg-agent")
+;;   (shell-command "gpg-agent --daemon --enable-ssh-support --write-env-file")
+;;   ;; read the environment file instead of parsing the output
+;;   (gpg-agent-reload-info))
+;;
+;; (defun gpg-agent-reload-info ()
+;;   "Reload the ~/.gpg-agent-info file."
+;;   (interactive)
+;;   (with-temp-buffer
+;;     (insert-file (expand-file-name "~/.gpg-agent-info"))
+;;     (goto-char (point-min))
+;;     (while (re-search-forward "\\([A-Z_]+\\)=\\(.*\\)" nil t)
+;;       (setenv (match-string 1) (match-string 2)))))
+;;
+;; (defun gpg-agent-startup ()
+;;   "Initialize the gpg-agent if necessary.
+;;
+;; Note that sometimes the gpg-agent can be up and running and still
+;; be useless, in which case you should restart it using
+;; `gpg-agent-restart'."
+;;   (gpg-agent-reload-info)
+;;   (unless (member (string-to-number (getenv "SSH_AGENT_PID"))
+;;                   (list-system-processes))
+;;     (gpg-agent-restart)))
+;;
+;; (gpg-agent-startup)
 
 
 ;;; [ Newsgroup ]
