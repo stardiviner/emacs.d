d5dca583fced8ee244413911c0ada385f8c5c61d
stardiviner <numbchild@gmail.com>
2016-06-19 10:29:25 +0800

    add package chinese-pyim and chinese-pyim-greatdict.
    add package chinese-pyim-greatdict.

--- init/Emacs/init-my-emacs-input-method.el
+++ init/Emacs/init-my-emacs-input-method.el
@@ -36,11 +36,51 @@
 
 ;;; [ Chinese Input Method ]
 
-;; custom
-;; (let ((quail-current-package (assoc "chinese-py" quail-package-alist)))
-;;   (quail-define-rules ((append . t))
-;;                       ("^han" ?漢)
-;;                       ))
+
+;;; [ chinese-pyim ]
+
+(use-package chinese-pyim
+  :ensure t
+  :bind
+  (("M-j" . pyim-convert-pinyin-at-point)
+   ;; ("C-;" . pyim-delete-word-from-personal-buffer)
+   )
+  
+  :config
+  (setq default-input-method "chinese-pyim")
+  ;; use double pinyin
+  (setq pyim-default-pinyin-scheme 'default)
+  
+  (setq-default pyim-english-input-switch-functions
+                '(pyim-probe-dynamic-english
+                  pyim-probe-isearch-mode
+                  pyim-probe-program-mode
+                  pyim-probe-org-structure-template))
+  
+  (setq-default pyim-punctuation-half-width-functions
+                '(pyim-probe-punctuation-line-beginning
+                  pyim-probe-punctuation-after-punctuation))
+
+  ;; enable pinyin search functionality
+  (setq pyim-isearch-enable-pinyin-search t)
+  
+  (setq pyim-use-tooltip 'pos-tip
+        pyim-page-length 5)
+
+  ;; for company-mode complete chinese
+  (require 'chinese-pyim-company)
+  (setq pyim-company-max-length 6)
+  ;; disable company-mode completion for chinese
+  ;; (setq pyim-company-complete-chinese-enable nil)
+  )
+
+
+;;; [ chinese-pyim-greatdict ]
+
+(use-package chinese-pyim-greatdict
+  :ensure t
+  :config
+  (chinese-pyim-greatdict-enable))
 
 
 ;;; [ TeX Input Method ]
