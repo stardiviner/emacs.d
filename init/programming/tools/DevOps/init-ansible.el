a36dcf95ba82a649e9aa46ea99bc82923e538d3e
stardiviner <numbchild@gmail.com>
2016-06-24 08:37:03 +0800

    add Ansible modes.

--- init/programming/tools/DevOps/init-ansible.el
+++ init/programming/tools/DevOps/init-ansible.el
@@ -7,9 +7,28 @@
 
 ;;; Code:
 
+;;; [ ansible ] -- Ansible minor mode
 
+(use-package ansible
+  :ensure t)
 
+
+;;; [ ansible-doc ] -- Ansible documentation for GNU Emacs
 
+(use-package ansible-doc
+  :ensure t
+  :config
+  (add-hook 'yaml-mode-hook #'ansible-doc-mode) ; [C-c ?] `ansible-doc'.
+  )
+
+
+;;; [ company-ansible ]
+
+(use-package company-ansible
+  :ensure t
+  :config
+  ;; (add-to-list 'company-backends 'company-ansible)
+  )
 
 
 (provide 'init-ansible)
