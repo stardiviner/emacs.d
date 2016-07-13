a28689706a47ddee0ccb5a162eba27474df3b226
stardiviner <numbchild@gmail.com>
2016-07-13 15:43:11 +0800

    add CQL init file.

---
+++ init/programming/languages/Databases/init-my-prog-lang-database-cql.el
@@ -0,0 +1,14 @@
+;;; init-my-prog-lang-database-cql.el --- init for CQL
+;;; -*- coding: utf-8 -*-
+
+;;; Commentary:
+
+
+
+;;; Code:
+
+
+
+(provide 'init-my-prog-lang-database-cql)
+
+;;; init-my-prog-lang-database-cql.el ends here
--- init/programming/languages/Databases/init-my-prog-lang-database.el
+++ init/programming/languages/Databases/init-my-prog-lang-database.el
@@ -20,14 +20,19 @@
 (define-key my-inferior-db-map (kbd "n") 'my-inferior-db-nosql-map)
 
 
+;; SQL
 (require 'init-my-prog-lang-database-sql)
 (require 'init-my-prog-lang-database-sqlite)
 (require 'init-my-prog-lang-database-mysql)
 (require 'init-my-prog-lang-database-postgresql)
 
+;; NoSQL
 (require 'init-my-prog-lang-database-nosql)
 (require 'init-my-prog-lang-database-mongodb)
 (require 'init-my-prog-lang-database-redis)
+
+;;; CQL
+(require 'init-my-prog-lang-database-cql)
 
 
 (provide 'init-my-prog-lang-database)
