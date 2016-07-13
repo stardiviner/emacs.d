94adbccce41c0fbe0e5de0328309a8d336662c19
stardiviner <numbchild@gmail.com>
2016-07-13 18:58:18 +0800

    add package evalator.

--- init/programming/eval/init-my-prog-eval.el
+++ init/programming/eval/init-my-prog-eval.el
@@ -13,6 +13,24 @@
 (global-set-key (kbd "C-c e") 'my-prog-eval-map)
 
 
+;;; [ evalator ]
+
+(use-package evalator
+  :ensure t
+  :config
+  (define-key my-prog-eval-map (kbd "e") 'evalator)
+  (define-key my-prog-eval-map (kbd "x") 'evalator-explicit)
+  (define-key my-prog-eval-map (kbd "r") 'evalator-resume)
+  (define-key my-prog-eval-map (kbd "i") 'evalator-insert-equiv-expr)
+
+  ;; auto detect context
+  (setq evalator-config-mode-context-alist nil)
+  (add-to-list 'evalator-config-mode-context-alist
+               '(ruby-mode . evalator-ruby-context))
+  (add-to-list 'evalator-config-mode-context-alist
+               '(clojure-mode . evalator-clojure-context))
+  )
+
 (provide 'init-my-prog-eval)
 
 ;;; init-my-prog-eval.el ends here
