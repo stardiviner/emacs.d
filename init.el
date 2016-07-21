69ca7131bd69733df912c31f5484f26aab7ac27d
stardiviner <numbchild@gmail.com>
2016-07-21 15:36:39 +0800

    add xwidget init file.

--- init.el
+++ init.el
@@ -169,6 +169,7 @@
 (require 'init-my-emacs-comint)
 (require 'init-my-emacs-subprocess)
 (require 'init-my-emacs-encrypt)
+(require 'init-my-emacs-xwidget)
 (require 'init-my-emacs-customize)
 
 
---
+++ init/Emacs/init-my-emacs-xwidget.el
@@ -0,0 +1,23 @@
+;;; init-my-emacs-xwidget.el --- init for XWdieget.
+;;; -*- coding: utf-8 -*-
+
+;;; Commentary:
+
+
+
+;;; Code:
+
+;;; [ xwidget ]
+
+(require 'xwidget)
+
+
+;;; [ xwidgete ] -- Enhance usability for Xwidget browser in Emacs 25.
+
+;; (use-package xwidgete
+;;   :ensure t)
+
+
+(provide 'init-my-emacs-xwidget)
+
+;;; init-my-emacs-xwidget.el ends here
