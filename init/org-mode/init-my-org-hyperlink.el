9a96cccd8f5fecb141a27132ab8602837114ce25
stardiviner <numbchild@gmail.com>
2016-07-26 09:22:41 +0800

    remove org-mode wiki: link, replace with package kiwix.el

--- init/org-mode/init-my-org-hyperlink.el
+++ init/org-mode/init-my-org-hyperlink.el
@@ -126,20 +126,6 @@ With prefix argument, also display headlines without a TODO keyword."
 
 (org-add-link-type "tag" 'org-tag-link-open)
 
-;; [[wiki:]]
-;; for open wiki search query with local application database.
-(defvar kiwix-browser "google-chrome-stable")
-
-(defun org-wiki-link-open (query)
-  "Search `QUERY' with Kiwix."
-  (let* ((browser kiwix-browser)
-         (kiwix-server "http://127.0.0.1:8000/")
-         (kiwix-library "wikipedia_zh_all_2015-11")
-         (url (concat kiwix-server kiwix-library "/A/" query ".html")))
-    (shell-command (concat browser " " url))))
-
-(org-add-link-type "wiki" 'org-wiki-link-open)
-
 
 (setq org-link-frame-setup
       '((vm . vm-visit-folder-other-frame)
@@ -170,7 +156,6 @@ With prefix argument, also display headlines without a TODO keyword."
         ("Wikipedia" . "http://en.wikipedia.org/w/index.php?search=%s")
         ("Wikia" . "http://www.wikia.com/index.php?search=%s")
         ("Baidu_BaiKe" . "http://baike.baidu.com/search/none?word=%s")
-        ("Wikipedia_Local" . "http://127.0.0.1:8000/wikipedia_zh_all_2015-11/A/%s.html")
         ;; Q & A
         ("Quora" . "https://www.quora.com/search?q=%s")
         ("ZhiHu" . "http://www.zhihu.com/search?q=%s&type=question")
