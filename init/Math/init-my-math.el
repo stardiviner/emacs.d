88ff4d226a817f070aaa931729777cc534bb8c23
stardiviner <numbchild@gmail.com>
2016-08-08 10:53:26 +0800

    add package wolfram-mode.

--- init/Math/init-my-math.el
+++ init/Math/init-my-math.el
@@ -21,6 +21,21 @@
 (use-package gmpl-mode
   :ensure t)
 
+
+;;; [ Wolfram ]
+
+(use-package wolfram-mode
+  :ensure t
+  :init
+  (autoload 'wolfram-mode "wolfram-mode" nil t)
+  (autoload 'run-wolfram "wolfram-mode" nil t)
+  (add-to-list 'auto-mode-alist '("\\.m$" . wolfram-mode))
+  :config
+  (setq wolfram-program "tungsten"
+        ;; wolfram-program-arguments
+        wolfram-indent 4
+        )
+  )
 
 
 (provide 'init-my-math)
