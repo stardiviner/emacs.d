7a324dd1a3a4a5f25239454fbe47fa047e18f20d
stardiviner <numbchild@gmail.com>
2016-08-08 10:50:17 +0800

    update sage-shell-mode and ob-sage configs.

--- init/Math/init-my-math-sage-math.el
+++ init/Math/init-my-math-sage-math.el
@@ -10,13 +10,23 @@
 ;;; [ sage-shell-mode ] -- A front-end for Sage Math.
 
 (use-package sage-shell-mode
-  :ensure t)
+  :ensure t
+  :config
+  (sage-shell:define-alias) ; define command `run-sage'.
+  ;; (setq sage-shell:completion-function )
+  ;; (setq sage-shell:completion-ignore-case t)
+  ;; (setq sage-shell-edit:display-function)
+  )
 
 
 ;;; [ ob-sagemath ] -- org-babel functions for SageMath evaluation.
 
 (use-package ob-sagemath
-  :ensure t)
+  :ensure t
+  :config
+  ;; (setq ob-sagemath-output-display-function)
+  ;; (setq org-babel-header-args:sage '())
+  )
 
 
 (provide 'init-my-math-sage-math)
