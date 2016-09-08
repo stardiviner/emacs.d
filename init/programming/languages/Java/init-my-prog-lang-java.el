3df245e1383e6e60bdf96739dadad1c131c59c7e
stardiviner <numbchild@gmail.com>
2016-09-08 15:26:23 +0800

    improve Eclim config.

--- init/programming/languages/Java/init-my-prog-lang-java.el
+++ init/programming/languages/Java/init-my-prog-lang-java.el
@@ -20,18 +20,25 @@
         ;; eclim-executable
         ;; eclimd-executable "~/.eclipse/org.eclipse.platform_4.5.1_155965261_linux_gtk_x86_64/eclimd"
         ;; eclimd-port 45620
-        eclimd-wait-for-process t)
+        eclimd-wait-for-process t
+        )
 
+  ;; auto enable `eclim-mode' in `java-mode'.
+  (add-hook 'java-mode-hook 'eclim-mode)
+  ;; (global-eclim-mode t)
+
+
+  ;; auto start eclimd
   (defun eclimd-auto-start ()
     (interactive)
-    (if (yes-or-no-p "start eclimd? ")
-        ;; (start-eclimd (file-name-directory (buffer-file-name)))
-        (call-interactively 'start-eclimd)
-      ))
+    (unless (and (get-buffer "*eclimd*")
+                 (process-live-p (get-process "eclimd")))
+      (if (yes-or-no-p "start eclimd? ")
+          ;; (start-eclimd (file-name-directory (buffer-file-name)))
+          (call-interactively 'start-eclimd)))
+    )
 
-  ;; (remove-hook 'java-mode-hook 'eclimd-auto-start)
-
-  ;; (global-eclim-mode t)
+  (add-hook 'java-mode-hook 'eclimd-auto-start)
   )
 
 
