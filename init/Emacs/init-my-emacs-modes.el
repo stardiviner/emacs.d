6b50a8d01c789673e36456a5661cf91cad6466a2
stardiviner <numbchild@gmail.com>
2016-09-10 19:23:18 +0800

    configure polymode.

--- init/Emacs/init-my-emacs-modes.el
+++ init/Emacs/init-my-emacs-modes.el
@@ -64,90 +64,16 @@
 
 ;;; [ polymode ] -- Object oriented framework for multiple emacs modes based on indirect buffers.
 
-;;; Usage:
-;;
-;; Basic Usage
-;;
-;; All polymode keys start with the prefix defined by polymode-prefix-key,
-;; default is [M-n]. The polymode-mode-map is the parent of all polymodes' maps:
-;;
-;; - `polymode-minor-mode' :: enable polymode on this buffer.
-;;
-;; [ prefix ]
-;;
-;; - [M-n] -- `polymode-prefix-key'
-;;
-;;  [ BACKENDS ]
-;;
-;;    e polymode-export
-;;
-;;    E polymode-set-exporter
-;;
-;;    w polymode-weave
-;;
-;;    W polymode-set-weaver
-;;
-;;    t polymode-tangle ;; not implemented yet
-;;
-;;    T polymode-set-tangler ;; not implemented yet
-;;
-;;    $ polymode-show-process-buffer
-;;  
-;;  [ NAVIGATION ]
-;;  
-;;  - C-n polymode-next-chunk
-;;  
-;;  - C-p polymode-previous-chunk
-;;  
-;;  - C-M-n polymode-next-chunk-same-type
-;;  
-;;  - C-M-p polymode-previous-chunk-same-type
-;;  
-;;  [ MANIPULATION ]
-;;  
-;;  - M-k polymode-kill-chunk
-;;  
-;;  - M-m polymode-mark-or-extend-chunk
-;;  
-;;  - C-t polymode-toggle-chunk-narrowing
-;;  
-;;  - M-i polymode-insert-new-chunk
-
-;;; Activation of Polymodes
-;;
-;; Polymodes are functions, just like ordinary emacs modes. The can be used in
-;; place of emacs major or minor modes alike. There are two main ways to
-;; automatically activate emacs (poly)modes:
-
-;; 1. By registering a file extension by adding modes to `auto-mode-alist':
-;;; MARKDOWN
-;; (add-to-list 'auto-mode-alist '("\\.md\\'" . poly-markdown-mode))
-;; (add-to-list 'auto-mode-alist '("\\.Rmd\\'" . poly-markdown+r-mode))
-
-;;; 2. By setting local mode variable in you file:
-;;; // -*- mode: poly-C++R -*-
-;;; ## -*- mode: poly-brew+R; -*-
-
-;;; Developing with Polymode (custom modes)
-;;
-;; check out polymode/modes/readme.md
-
-;; (require 'polymode)
-;;
-;; ;; modes
-;; (require 'poly-base)
-;; (require 'poly-org)
-;; (require 'poly-markdown)
-;; (require 'poly-noweb)
-;; (require 'poly-R)
-;; (require 'poly-erb)
-;; (require 'poly-slim)
-;;
-;;
-;; (setq polymode-prefix-key '(kbd "M-n"))
-;;
-;; (add-to-list 'auto-mode-alist '("\\.md\\'" . poly-markdown))
-
+(use-package polymode
+  :ensure t
+  :init
+  (require 'poly-markdown-mode)
+  (require 'poly-noweb-mode)
+  (require 'poly-noweb+r-mode)
+  (require 'poly-ess-help+r-mode)
+  (require 'poly-org-mode)
+  :config
+  )
 
 
 (provide 'init-my-emacs-modes)
