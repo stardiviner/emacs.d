f715d033486d4a01cbe8e201d430d89db4d9911e
stardiviner <numbchild@gmail.com>
2016-09-11 16:10:21 +0800

    configure SuperCollider.

--- init/electric music/init-my-electric-music.el
+++ init/electric music/init-my-electric-music.el
@@ -7,8 +7,24 @@
 
 ;;; Code:
 
+;;; [ SuperCollider ] -- an audio server, programming language, and IDE for sound synthesis and algorithmic composition.
 
+(add-to-list 'load-path "/usr/share/emacs/site-lisp/SuperCollider/")
 
+(require 'sclang)
+
+;;; Sclang Interface
+(setq sclang-auto-scroll-post-buffer t
+      sclang-show-workspace-on-startup nil
+      sclang-use-symbol-table t
+      sclang-main-run nil
+      sclang-main-stop nil
+      )
+
+;;; Sclang mode
+(setq sclang-indent-level 2)
+
+;;; Sclang minor mode
 
 
 
