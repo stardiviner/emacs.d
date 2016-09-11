8483d48413755776d7db0754c66bdc2612a33aba
stardiviner <numbchild@gmail.com>
2016-09-11 17:34:51 +0800

    change Babel src block background color.

--- init/org-mode/init-my-org-babel.el
+++ init/org-mode/init-my-org-babel.el
@@ -529,6 +529,261 @@ This can be useful for snippets to select kernel interactively."
   :ensure t)
 
 
+;;; change Babel src block background color.
+
+(with-eval-after-load 'org
+  ;; * Colored src blocks
+  ;; based on patches from Rasmus <rasmus@gmx.us>
+
+  ;; This function overwrites the org-src function to make src blocks be colored again.
+  (defun org-src-font-lock-fontify-block (lang start end)
+    "Fontify code block.
+        LANG is the language of the block.  START and END are positions of
+        the block.  This function is called by Emacs automatic
+        fontification, as long as `org-src-fontify-natively' is non-nil."
+    (let ((lang-mode (org-src--get-lang-mode lang)))
+      (when (fboundp lang-mode)
+        (let ((string (buffer-substring-no-properties start end))
+              (modified (buffer-modified-p))
+              (org-buffer (current-buffer))
+              (block-faces (let ((face-name (intern (format "org-block-%s" lang))))
+                             (append (and (facep face-name) (list face-name))
+                                     '(org-block)))))
+          (remove-text-properties start end '(face nil))
+          (with-current-buffer
+              (get-buffer-create
+               (format " *org-src-fontification:%s*" lang-mode))
+            (erase-buffer)
+            (insert string " ") ;; so there's a final property change
+            (unless (eq major-mode lang-mode) (funcall lang-mode))
+            (org-font-lock-ensure)
+            (let ((pos (point-min)) next)
+              (while (setq next (next-single-property-change pos 'face))
+                (let ((new-face (get-text-property pos 'face)))
+                  (put-text-property
+                   (+ start (1- pos)) (1- (+ start next)) 'face
+                   (list :inherit (append (and new-face (list new-face))
+                                          block-faces))
+                   org-buffer))
+                (setq pos next))
+              ;; Add the face to the remaining part of the font.
+              (put-text-property (1- (+ start pos))
+                                 end 'face
+                                 (list :inherit block-faces) org-buffer)))
+          (add-text-properties
+           start end
+           '(font-lock-fontified t fontified t font-lock-multiline t))
+          (set-buffer-modified-p modified)))))
+
+  (defun org-fontify-meta-lines-and-blocks-1 (limit)
+    "Fontify #+ lines and blocks."
+    (let ((case-fold-search t))
+      (if (re-search-forward
+           "^\\([ \t]*#\\(\\(\\+[a-zA-Z]+:?\\| \\|$\\)\\(_\\([a-zA-Z]+\\)\\)?\\)[ \t]*\\(\\([^ \t\n]*\\)[ \t]*\\(.*\\)\\)\\)"
+           limit t)
+          (let ((beg (match-beginning 0))
+                (block-start (match-end 0))
+                (block-end nil)
+                (lang (match-string 7))
+                (beg1 (line-beginning-position 2))
+                (dc1 (downcase (match-string 2)))
+                (dc3 (downcase (match-string 3)))
+                end end1 quoting block-type ovl)
+            (cond
+             ((and (match-end 4) (equal dc3 "+begin"))
+              ;; Truly a block
+              (setq block-type (downcase (match-string 5))
+                    quoting (member block-type org-protecting-blocks))
+              (when (re-search-forward
+                     (concat "^[ \t]*#\\+end" (match-string 4) "\\>.*")
+                     nil t)  ;; on purpose, we look further than LIMIT
+                (setq end (min (point-max) (match-end 0))
+                      end1 (min (point-max) (1- (match-beginning 0))))
+                (setq block-end (match-beginning 0))
+                (when quoting
+                  (org-remove-flyspell-overlays-in beg1 end1)
+                  (remove-text-properties beg end
+                                          '(display t invisible t intangible t)))
+                (add-text-properties
+                 beg end '(font-lock-fontified t font-lock-multiline t))
+                (add-text-properties beg beg1 '(face org-meta-line))
+                (org-remove-flyspell-overlays-in beg beg1)
+                (add-text-properties  ; For end_src
+                 end1 (min (point-max) (1+ end)) '(face org-meta-line))
+                (org-remove-flyspell-overlays-in end1 end)
+                (cond
+                 ((and lang (not (string= lang "")) org-src-fontify-natively)
+                  (org-src-font-lock-fontify-block lang block-start block-end)
+                  (add-text-properties beg1 block-end '(src-block t)))
+                 (quoting
+                  (add-text-properties beg1 (min (point-max) (1+ end1))
+                                       (let ((face-name (intern (format "org-block-%s" lang))))
+                                         (append (and (facep face-name) (list face-name))
+                                                 '(face org-block))))) ; end of source block
+                 ((not org-fontify-quote-and-verse-blocks))
+                 ((string= block-type "quote")
+                  (add-text-properties beg1 (min (point-max) (1+ end1)) '(face org-quote)))
+                 ((string= block-type "verse")
+                  (add-text-properties beg1 (min (point-max) (1+ end1)) '(face org-verse))))
+                (add-text-properties beg beg1 '(face org-block-begin-line))
+                (add-text-properties (min (point-max) (1+ end)) (min (point-max) (1+ end1))
+                                     '(face org-block-end-line))
+                t))
+             ((member dc1 '("+title:" "+author:" "+email:" "+date:"))
+              (org-remove-flyspell-overlays-in
+               (match-beginning 0)
+               (if (equal "+title:" dc1) (match-end 2) (match-end 0)))
+              (add-text-properties
+               beg (match-end 3)
+               (if (member (intern (substring dc1 1 -1)) org-hidden-keywords)
+                   '(font-lock-fontified t invisible t)
+                 '(font-lock-fontified t face org-document-info-keyword)))
+              (add-text-properties
+               (match-beginning 6) (min (point-max) (1+ (match-end 6)))
+               (if (string-equal dc1 "+title:")
+                   '(font-lock-fontified t face org-document-title)
+                 '(font-lock-fontified t face org-document-info))))
+             ((equal dc1 "+caption:")
+              (org-remove-flyspell-overlays-in (match-end 2) (match-end 0))
+              (remove-text-properties (match-beginning 0) (match-end 0)
+                                      '(display t invisible t intangible t))
+              (add-text-properties (match-beginning 1) (match-end 3)
+                                   '(font-lock-fontified t face org-meta-line))
+              (add-text-properties (match-beginning 6) (+ (match-end 6) 1)
+                                   '(font-lock-fontified t face org-block))
+              t)
+             ((member dc3 '(" " ""))
+              (org-remove-flyspell-overlays-in beg (match-end 0))
+              (add-text-properties
+               beg (match-end 0)
+               '(font-lock-fontified t face font-lock-comment-face)))
+             (t ;; just any other in-buffer setting, but not indented
+              (org-remove-flyspell-overlays-in (match-beginning 0) (match-end 0))
+              (remove-text-properties (match-beginning 0) (match-end 0)
+                                      '(display t invisible t intangible t))
+              (add-text-properties beg (match-end 0)
+                                   '(font-lock-fontified t face org-meta-line))
+              t))))))
+
+
+  (defface org-block-org
+    `((t (:background ,(color-lighten-name (face-background 'default) 2))))
+    "Face for Org-mode src blocks")
+  
+  (defface org-block-emacs-lisp
+    `((t (:background ,(color-darken-name (face-background 'default) 4))))
+    "Face for elisp src blocks")
+
+  (defface org-block-lisp
+    `((t (:background "Darkorchid4")))
+    "Face for Common Lisp src blockss")
+
+  (defface org-block-scheme
+    `((t (:background "DarkMagenta")))
+    "Face for Scheme src blockss")
+
+  (defface org-block-sh
+    `((t (:background "#282828")))
+    "Face for shell blocks")
+
+  (defface org-block-latex
+    `((t (:background "DarkRed")))
+    "Face for LaTeX blocks")
+
+  (defface org-block-clojure
+    `((t (:background "DarkSlateGray")))
+    "Face for Clojure blocks")
+  
+  (defface org-block-python
+    `((t (:background "DarkSlateGray")))
+    "Face for python blocks")
+
+  (defface org-block-ipython
+    `((t (:background "CadetBlue")))
+    "Face for IPython blocks")
+
+  (defface org-block-ruby
+    `((t (:background "DarkSlateGray")))
+    "Face for Ruby blocks")
+
+  (defface org-block-C
+    `((t (:background "DarkGreen")))
+    "Face for C blocks")
+
+  (defface org-block-C++
+    `((t (:background "DarkGreen")))
+    "Face for C++ blocks")
+
+  (defface org-block-java
+    `((t (:background "DarkOliveGreen")))
+    "Face for Java blocks")
+
+  (defface org-block-javascript
+    `((t (:background "DarkSlateBlue")))
+    "Face for JavaScript blocks")
+
+  (defface org-block-haskell
+    `((t (:background "Darkorchid4")))
+    "Face for Haskell blocks")
+
+  (defface org-block-ocaml
+    `((t (:background "SaddleBrown")))
+    "Face for OCaml blocks")
+
+  (defface org-block-sql
+    `((t (:background "DarkCyan")))
+    "Face for SQL blocks")
+
+  (defface org-block-sqlite
+    `((t (:background "DarkCyan")))
+    "Face for SQLite blocks")
+
+  (defface org-block-gnuplot
+    `((t (:background "SeaGreen")))
+    "Face for gnuplot blocks")
+
+  (defface org-block-R
+    `((t (:background "LightSeaGreen")))
+    "Face for R blocks")
+
+  (defface org-block-julia
+    `((t (:background "LightSeaGreen")))
+    "Face for Julia blocks")
+  
+  (defface org-block-octave
+    `((t (:background "LightSeaGreen")))
+    "Face for Octave blocks")
+
+  (defface org-block-matlab
+    `((t (:background "LightSeaGreen")))
+    "Face for Matlab blocks")
+  
+  (defface org-block-gnuplot
+    `((t (:background "LightSeaGreen")))
+    "Face for gnuplot blocks")
+
+  (defface org-block-sclang
+    `((t (:background "MidnightBlue")))
+    "Face for SuperCollider sclang blocks")
+
+  (defface org-block-ditaa
+    `((t (:background "orange")))
+    "Face for Ditaa blocks")
+
+  (defface org-block-dot
+    `((t (:background "orange")))
+    "Face for Dot blocks")
+  
+  (defface org-block-plantuml
+    `((t (:background "orange")))
+    "Face for plantuml blocks")
+
+  (defface org-block-ledger
+    `((t (:background "orange")))
+    "Face for ledger blocks")
+  )
+
+
 (provide 'init-my-org-babel)
 
 ;;; init-my-org-babel.el ends here
