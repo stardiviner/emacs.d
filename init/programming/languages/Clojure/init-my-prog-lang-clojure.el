7872005f720da5acb86832af74312632cf458fb7
stardiviner <numbchild@gmail.com>
2016-09-13 15:47:51 +0800

    add some fontify for Clojure.

--- init/programming/languages/Clojure/init-my-prog-lang-clojure.el
+++ init/programming/languages/Clojure/init-my-prog-lang-clojure.el
@@ -17,6 +17,28 @@
   (add-hook 'clojure-mode-hook #'subword-mode)
   (add-hook 'clojure-mode-hook #'eldoc-mode)
   ;; (add-hook 'clojure-mode-hook 'smartparens-strict-mode)
+
+  
+  (eval-after-load 'clojure-mode
+    '(font-lock-add-keywords
+      'clojure-mode `(("(\\(fn\\)[\[[:space:]]"
+                       (0 (progn (compose-region (match-beginning 1)
+                                                 (match-end 1) "λ")
+                                 nil))))))
+
+  (eval-after-load 'clojure-mode
+    '(font-lock-add-keywords
+      'clojure-mode `(("\\(#\\)("
+                       (0 (progn (compose-region (match-beginning 1)
+                                                 (match-end 1) "ƒ")
+                                 nil))))))
+
+  (eval-after-load 'clojure-mode
+    '(font-lock-add-keywords
+      'clojure-mode `(("\\(#\\){"
+                       (0 (progn (compose-region (match-beginning 1)
+                                                 (match-end 1) "∈")
+                                 nil))))))
   )
 
 
