db2187b1003f59a665a236bea1b4b5484505e017
stardiviner <numbchild@gmail.com>
2016-09-18 17:19:30 +0800

    merge clojure some special form fontify into one.

--- init/programming/languages/Clojure/init-my-prog-lang-clojure.el
+++ init/programming/languages/Clojure/init-my-prog-lang-clojure.el
@@ -21,26 +21,23 @@
   (add-hook 'clojure-repl-mode-hook #'my-lisp-repl-common-settings)
   
   
-  (eval-after-load 'clojure-mode
-    '(font-lock-add-keywords
-      'clojure-mode `(("(\\(fn\\)[\[[:space:]]"
-                       (0 (progn (compose-region (match-beginning 1)
-                                                 (match-end 1) "λ")
-                                 nil))))))
-
-  (eval-after-load 'clojure-mode
-    '(font-lock-add-keywords
-      'clojure-mode `(("\\(#\\)("
-                       (0 (progn (compose-region (match-beginning 1)
-                                                 (match-end 1) "ƒ")
-                                 nil))))))
-
-  (eval-after-load 'clojure-mode
-    '(font-lock-add-keywords
-      'clojure-mode `(("\\(#\\){"
-                       (0 (progn (compose-region (match-beginning 1)
-                                                 (match-end 1) "∈")
-                                 nil))))))
+  (with-eval-after-load 'clojure-mode
+    (font-lock-add-keywords
+     'clojure-mode `(("(\\(fn\\)[\[[:space:]]" ; (fn
+                      (0 (progn (compose-region (match-beginning 1)
+                                                (match-end 1) "λ")
+                                nil)))))
+    (font-lock-add-keywords
+     'clojure-mode `(("\\(#\\)(" ; #(
+                      (0 (progn (compose-region (match-beginning 1)
+                                                (match-end 1) "ƒ")
+                                nil)))))
+    (font-lock-add-keywords
+     'clojure-mode `(("\\(#\\){" ; #{
+                      (0 (progn (compose-region (match-beginning 1)
+                                                (match-end 1) "∈")
+                                nil)))))
+    )
   )
 
 
