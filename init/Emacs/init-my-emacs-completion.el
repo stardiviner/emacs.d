1251a5137db6491e2ca9a84e12607b5403db9001
stardiviner <numbchild@gmail.com>
2016-09-21 15:58:04 +0800

    wrap smex with use-package macro.

--- init/Emacs/init-my-emacs-completion.el
+++ init/Emacs/init-my-emacs-completion.el
@@ -75,13 +75,14 @@
 
 ;; work with ido will show recent frequently used commands in [M-x].
 
-(require 'smex)
+(use-package smex
+  :ensure t
+  :config
+  (setq smex-save-file (expand-file-name ".smex-items" user-emacs-directory))
 
-(setq smex-save-file (expand-file-name ".smex-items" user-emacs-directory))
-
-(smex-initialize)
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+  (smex-initialize)
+  ;; (global-set-key (kbd "M-X") 'smex-major-mode-commands)
+  )
 
 
 
