cae4c576a9e2b6416e3a614d8c8b24f81fd502c1
stardiviner <numbchild@gmail.com>
2016-09-24 14:19:04 +0800

    refactor minimal-init.el file.

--- minimal-init.el
+++ minimal-init.el
@@ -22,25 +22,9 @@
 ;; (require 'init-my-emacs-benchmark)
 
 
-;;; [ things before load ]
-
-;; Turn off mouse interface early in startup to avoid momentary display
-(when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-
-;; No splash screen please... jeez
-(setq inhibit-startup-screen t)
-
-(toggle-frame-maximized)
-
-
 ;;; some settings for easy usage
 
 (defalias 'yes-or-no-p 'y-or-n-p)
-
-
-
-(setq projectile-rails-keymap-prefix (kbd "C-c C-r")) ; or [C-c C-p]
 
 
 ;;; add my init files directory
@@ -56,28 +40,15 @@
 
 ;;; [ package.el ]
 
-(load "~/.emacs.d/init/init-my-packages.el")
 (load "~/.emacs.d/init/init-my-pm-package.el")
 (require 'init-my-pm-package)
 
-(require 'use-package)
-
 (require 'color)
-(require 'color-theme)
+(use-package color-theme
+  :ensure t)
 
 
 ;;; my custom functions
 (require 'init-my-library)
 (require 'init-my-functions)
 
-
-
-(require 'init-my-keybindings)
-(require 'init-my-emacs-keybinding)
-(require 'init-my-prog-programming)
-
-(require 'init-my-emacs-completion)
-(require 'init-my-prog-complete)
-
-(require 'init-my-prog-lang-lisp)
-(require 'init-my-prog-lang-emacs-lisp)
