c4c0b362c355761cf4ac9ee0047509b885303eb6
stardiviner <numbchild@gmail.com>
2016-09-24 12:49:56 +0800

    improve sh-mode shell type setting.

--- init/programming/languages/Shell/init-my-prog-lang-shell.el
+++ init/programming/languages/Shell/init-my-prog-lang-shell.el
@@ -25,8 +25,7 @@
   (interactive)
   (if (and (buffer-file-name) ; filer out non-file buffers which will returns nil
            (string-match "\\.zsh$" (buffer-file-name)))
-      (sh-set-shell "zsh"))
-  )
+      (sh-set-shell "zsh")))
 
 (add-hook 'sh-mode-hook #'my-sh-mode-zsh-setup)
 
@@ -63,23 +62,6 @@
 ;;   ;;              (setq-local completion-at-point-functions
 ;;   ;;                          '(bash-completion-dynamic-complete))))
 ;;   )
-
-
-;;; [ Zsh ]
-
-(add-to-list 'auto-mode-alist '("\\.zsh\\'" . sh-mode))
-
-;; A programmatic way of selecting a flavor when you don't want to use the
-;; shebang is doing this in a sh-mode buffer:
-(defun my-sh-mode-zsh-setup ()
-  "Setup `sh-mode' engine to Zsh."
-  (interactive)
-  (if (and (buffer-file-name) ; filer out non-file buffers which will returns nil
-           (string-match "\\.zsh$" (buffer-file-name)))
-      (sh-set-shell "zsh"))
-  )
-
-(add-hook 'sh-mode-hook #'my-sh-mode-zsh-setup)
 
 
 ;;; [ insert-shebang ]
