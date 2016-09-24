cb17dbf59516f9c54c52104d4329ec38ae9291ed
stardiviner <numbchild@gmail.com>
2016-09-24 14:43:45 +0800

    reduce the size of minimal-init.el

--- minimal-init.el
+++ minimal-init.el
@@ -4,51 +4,57 @@
 
 
 ;;; Code:
-
-;;; [ Debug ]
 
+;;; [ Debug ]
 (setq debug-on-error t
       debug-on-signal nil
       debug-on-quit nil)
 
-
 ;;; [ profiler ]
-
 ;; (profiler-start 'cpu+mem)
 
-
 ;;; benchmark
-
 ;; (require 'init-my-emacs-benchmark)
 
-
-;;; some settings for easy usage
-
-(defalias 'yes-or-no-p 'y-or-n-p)
-
-
 ;;; add my init files directory
-(let ((default-directory "~/.emacs.d/init/"))
-  (setq load-path
-        (append
-         (let ((load-path (copy-sequence load-path))) ; shadow
-           (append
-            (copy-sequence (normal-top-level-add-to-load-path '(".")))
-            (normal-top-level-add-subdirs-to-load-path)))
-         load-path)))
+;; (let ((default-directory "~/.emacs.d/init/"))
+;;   (setq load-path
+;;         (append
+;;          (let ((load-path (copy-sequence load-path))) ; shadow
+;;            (append
+;;             (copy-sequence (normal-top-level-add-to-load-path '(".")))
+;;             (normal-top-level-add-subdirs-to-load-path)))
+;;          load-path)))
 
-
 ;;; [ package.el ]
+(require 'cl)
+(require 'package)
+(setq package-menu-async t)
+(setq package-user-dir "~/.emacs.d/elpa")
+(setq-default package-archives
+              '(("org"   . "http://orgmode.org/elpa/")
+                ("melpa" . "http://melpa.org/packages/")
+                ;; ("melpa-stable" . "http://stable.melpa.org/packages/")
+                ;; ("marmalade" . "http://marmalade-repo.org/packages/")
+                ;; ("gnu" . "https://elpa.gnu.org/packages/")
+                ))
 
-(load "~/.emacs.d/init/init-my-pm-package.el")
-(require 'init-my-pm-package)
+(package-initialize)
 
-(require 'color)
-(use-package color-theme
-  :ensure t)
+;;; [ use-package ]
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
-
+;; (require 'color)
+;; (use-package color-theme
+;;   :ensure t)
+
 ;;; my custom functions
-(require 'init-my-library)
-(require 'init-my-functions)
+;; (require 'init-my-library)
+;; (require 'init-my-functions)
 
+;;; add your customizations from here
+
+(use-package magithub
+  :ensure t)
