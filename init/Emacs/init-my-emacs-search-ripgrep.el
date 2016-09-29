a06bc480e54f874aabd8d855b075d06e0d5f2e72
stardiviner <numbchild@gmail.com>
2016-09-29 12:33:15 +0800

    add ripgrep init file.

---
+++ init/Emacs/init-my-emacs-search-ripgrep.el
@@ -0,0 +1,22 @@
+;;; init-my-emacs-search-ripgrep.el --- init for ripgrep
+;;; -*- coding: utf-8 -*-
+
+;;; Commentary:
+
+
+
+;;; Code:
+;;; ----------------------------------------------------------------------------
+;;; [ ripgrep ] -- Emacs front-end for ripgrep, a command line search tool.
+
+(use-package ripgrep
+  :ensure t
+  :config
+  (define-key my-search-prefix (kbd "s") 'ripgrep-regexp)
+  )
+
+;;; ----------------------------------------------------------------------------
+
+(provide 'init-my-emacs-search-ripgrep)
+
+;;; init-my-emacs-search-ripgrep.el ends here
