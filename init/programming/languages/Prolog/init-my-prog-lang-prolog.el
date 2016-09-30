ad17dead6ff2c05f6334d84c0b63eb45a492fc48
stardiviner <numbchild@gmail.com>
2016-09-30 15:51:04 +0800

    update prolog.el config.

--- init/programming/languages/Prolog/init-my-prog-lang-prolog.el
+++ init/programming/languages/Prolog/init-my-prog-lang-prolog.el
@@ -12,24 +12,17 @@
 ;;; A major mode for Prolog programming under Emacs and offers to my knowledge
 ;;; the best Prolog IDE out there, especially when used from within XEmacs.
 
-;;; Usage:
-;;
-;; -
-
 (require 'prolog)
 
 (autoload 'run-prolog "prolog" "Start a Prolog sub-process." t)
 (autoload 'prolog-mode "prolog" "Major mode for editing Prolog programs." t)
 (autoload 'mercury-mode "prolog" "Major mode for editing Mercury programs." t)
 
-(setq prolog-system 'swi                ; which Prolog are you using? 'swi, 'gnu,
-      )
+;; which Prolog are you using? 'swi, 'gnu,
+(setq prolog-system 'swi)
 
 (add-to-list 'auto-mode-alist '("\\.prolog\\'" . prolog-mode))
 (add-to-list 'auto-mode-alist '("\\.m\\'" . mercury-mode))
-
-
-;;; [ prolog-inferior-mode ]
 
 
 ;;; [ ediprolog ] -- Emacs Does Interactive Prolog
