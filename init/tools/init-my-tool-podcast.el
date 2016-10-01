ee55d51739499a55fcef864fcc7346b169717e51
stardiviner <numbchild@gmail.com>
2016-10-01 16:25:32 +0800

    add podcast init file.

---
+++ init/tools/init-my-tool-podcast.el
@@ -0,0 +1,15 @@
+;;; init-my-tool-podcast.el --- init for Podcast in Emacs
+;;; -*- coding: utf-8 -*-
+
+;;; Commentary:
+
+
+
+;;; Code:
+;;; ----------------------------------------------------------------------------
+
+;;; ----------------------------------------------------------------------------
+
+(provide 'init-my-tool-podcast)
+
+;;; init-my-tool-podcast.el ends here
