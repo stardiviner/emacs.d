39922db856eaff94e1c6a1f40229def5bbf03841
stardiviner <numbchild@gmail.com>
2016-10-04 15:56:32 +0800

    add company-mode to minimal init file.

--- minimal-init.el
+++ minimal-init.el
@@ -55,5 +55,99 @@
   (global-set-key (kbd "C-x C-j") 'ace-window)
   )
 
+(use-package company
+  :ensure t
+  :config
+  (setq company-minimum-prefix-length 3
+        company-idle-delay 0.2
+        company-tooltip-align-annotations t
+        company-tooltip-flip-when-above nil
+        company-tooltip-limit 10
+        company-tooltip-minimum 3
+        )
+
+  (setq-default company-backends
+                '(company-files         ; files & directory
+                  (company-capf         ; `completion-at-point-functions'
+                   :with
+                   company-yasnippet)
+                  company-dabbrev-code  ; company-dabbrev
+                  company-abbrev
+                  company-keywords      ; keywords
+                  )
+                )
+
+  (defun my-company-add-backend-locally (backend)
+    "Add a backend in my custom way.
+
+\(my-company-add-backend-locally 'company-robe\)
+"
+    (if (local-variable-if-set-p 'company-backends)
+        (add-to-list 'company-backends `(,backend :with company-yasnippet))
+      (add-to-list (make-local-variable 'company-backends)
+                   `(,backend :with company-yasnippet))
+      ))
+
+  (global-company-mode 1)
+  
+  (define-key company-mode-map (kbd "C-M-i") 'company-complete)
+  (define-key company-mode-map (kbd "M-<tab>") 'company-complete)
+
+  ;; yasnippet
+  ;; `yas-expand', `yas-expand-from-trigger-key'
+  (define-key company-active-map [tab] 'yas-expand-from-trigger-key)
+
+  ;; navigation
+  (define-key company-active-map "\t" nil)
+  (define-key company-active-map [tab] nil)
+  (define-key company-active-map (kbd "<tab>") nil)
+  (define-key company-active-map (kbd "<S-tab>") nil)
+  (define-key company-active-map (kbd "C-n") nil)
+  (define-key company-active-map (kbd "C-p") nil)
+  (define-key company-active-map (kbd "C-j") nil)
+  (define-key company-active-map (kbd "C-g") 'company-abort)
+  (define-key company-active-map (kbd "M-n") 'company-select-next)
+  (define-key company-active-map (kbd "M-p") 'company-select-previous)
+  (define-key company-active-map (kbd "M-j") 'company-complete-selection)
+  (define-key company-active-map (kbd "M-i") 'company-complete-common)
+  (define-key company-active-map [mouse-1] 'company-complete-mouse)
+  (define-key company-active-map [mouse-3] 'company-select-mouse)
+
+  ;; help
+  (define-key company-active-map (kbd "<f1>") 'company-show-doc-buffer)
+  (define-key company-active-map (kbd "M-h") 'company-show-doc-buffer)
+
+  (define-key company-active-map (kbd "M-l") 'company-show-location)
+
+  ;; search
+  (define-key company-active-map (kbd "M-s") 'company-filter-candidates)
+  (define-key company-active-map (kbd "C-M-s") 'company-search-candidates)
+  (define-key company-search-map (kbd "C-g") 'company-search-abort)
+  (define-key company-search-map (kbd "M-s") 'company-search-repeat-forward)
+  (define-key company-search-map (kbd "M-r") 'company-search-repeat-backward)
+  (define-key company-search-map (kbd "M-n") 'company-search-repeat-forward)
+  (define-key company-search-map (kbd "M-p") 'company-search-repeat-backward)
+  (define-key company-search-map (kbd "M-o") 'company-search-kill-others)
+  (define-key company-search-map (kbd "M-j") 'company-complete-selection)
+
+  (defun company-new-line ()
+    "insert a literal return new line."
+    (interactive)
+    ;; (company-abort)
+    (company-cancel 'abort)
+    (newline-and-indent)
+    )
+
+  (define-key company-active-map [return] 'company-new-line)
+  (define-key company-active-map "\r" 'company-new-line)
+
+
+  ;; [ company-abbrev / company-dabbrev ]
+  (setq company-dabbrev-other-buffers t)
+
+  ;; [ company-tempo ]
+  (setq company-tempo-expand t)
+  )
+
 (use-package magithub
   :ensure t)
