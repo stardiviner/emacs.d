7ba54f99aa95e7bcfb3e87619e2fa6bf4ae3ced3
stardiviner <numbchild@gmail.com>
2016-10-07 09:10:43 +0800

    add package magit-gerrit.

--- init/programming/vcs/init-my-prog-vcs-review.el
+++ init/programming/vcs/init-my-prog-vcs-review.el
@@ -7,9 +7,18 @@
 
 ;;; Code:
 ;;; ----------------------------------------------------------------------------
+;;; [ magit-gerrit ] -- Magit plugin for Gerrit Code Review
 
+(use-package magit-gerrit
+  :ensure t
+  :config
+  ;; if remote url is not using the default gerrit port and
+  ;; ssh scheme, need to manually set this variable
+  ;; (setq-default magit-gerrit-ssh-creds "myid@gerrithost.org")
 
-
+  ;; if necessary, use an alternative remote instead of 'origin'
+  ;; (setq-default magit-gerrit-remote "gerrit")
+  )
 
 ;;; ----------------------------------------------------------------------------
 
