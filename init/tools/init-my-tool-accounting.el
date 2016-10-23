c94fc6d204d5d9c1a37ac39be2a0d91a1a012469
stardiviner <numbchild@gmail.com>
2016-10-23 11:06:02 +0800

    add Accounting init file.

---
+++ init/tools/init-my-tool-accounting.el
@@ -0,0 +1,16 @@
+;;; init-my-tool-accounting.el --- init for Accounting in Emacs.
+;;; -*- coding: utf-8 -*-
+
+;;; Commentary:
+
+
+
+;;; Code:
+;;; ----------------------------------------------------------------------------
+
+
+;;; ----------------------------------------------------------------------------
+
+(provide 'init-my-tool-accounting)
+
+;;; init-my-tool-accounting.el ends here
