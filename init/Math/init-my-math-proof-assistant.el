17f9c3d794f9aa5a823fa4256fe689b95dac8613
stardiviner <numbchild@gmail.com>
2016-10-23 23:40:33 +0800

    refactor company-coq config.

--- init/Math/init-my-math-proof-assistant.el
+++ init/Math/init-my-math-proof-assistant.el
@@ -36,14 +36,7 @@
   :defer t
   :init
   (require 'proof-site nil t)
-  
   (add-hook 'coq-mode-hook #'company-coq-mode)
-  (add-hook 'coq-mode-hook
-            '(lambda ()
-               (set (make-local-variable 'prettify-symbols-alist)
-                    '((":=" . ?≜) ("Proof." . ?∵) ("Qed." . ?■)
-                      ("Defined." . ?□) ("Time" . ?⏱) ("Admitted." . ?😱)))))
-
   :config
   (setq company-coq-dynamic-autocompletion t)
   ;; (setq company-coq-autocomplete-modules nil)
@@ -51,6 +44,12 @@
   ;; (setq company-coq-autocomplete-symbols nil)
   ;; (setq company-coq-autocomplete-block-end nil)
   ;; (setq company-coq-autocomplete-search-results nil)
+
+  (add-hook 'coq-mode-hook
+            '(lambda ()
+               (set (make-local-variable 'prettify-symbols-alist)
+                    '((":=" . ?≜) ("Proof." . ?∵) ("Qed." . ?■)
+                      ("Defined." . ?□) ("Time" . ?⏱) ("Admitted." . ?😱)))))
   )
 
 
