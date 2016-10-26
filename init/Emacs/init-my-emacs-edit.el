71408bc86354722a258cc8cb136c74d15e4196ae
stardiviner <numbchild@gmail.com>
2016-10-26 09:30:37 +0800

    migrate edit-server, atomic-chrome into a separate init file.

--- init/Emacs/init-my-emacs-edit.el
+++ init/Emacs/init-my-emacs-edit.el
@@ -20,43 +20,6 @@
 ;;; - [C-x SPC] / [C-x r r m] (custom keybinding) :: `rectangle-mark-mode'
 
 
-;;;_ [ edit-server ]
-
-;; A Chrome "clone" of It's All Text for spawning an editor to edit text areas in browsers with Emacs.
-;; https://github.com/stsquad/emacs_chrome
-
-(use-package edit-server
-  :ensure t
-  :if window-system
-  :defer t
-  :init
-  (edit-server-start)
-  
-  :config
-  (setq edit-server-new-frame t)
-  (setq edit-server-url-major-mode-alist
-        '(("github\\.com" . markdown-mode)
-          ;; Stack Overflow
-          ("stackoverflow\\.com" . markdown-mode)
-          ("stack\\(exchange\\|overflow\\)\\.com\\.[a-z0-9]+\\.txt" . markdown-mode)
-          (".*\\.stackexchange\\.com/.*" . markdown-mode)
-          ("segmentfault\\.com" . markdown-mode)
-          ;; Jupyter notebooks
-          ;; localhost:8888/notebooks/Untitled1.ipynb?kernel_name=clojure
-          ("localhost:8888/notebooks/.*\\.ipynb\\?kernel_name=clojure" . clojure-mode)
-          ("localhost:8888/notebooks/.*\\.ipynb\\?kernel_name=ruby" . ruby-mode)
-          ("localhost:8888/notebooks/.*\\.ipynb\\?kernel_name=julia" . julia-mode)
-          ("localhost:8888/notebooks/.*\\.ipynb\\?kernel_name=python" . python-mode)
-          ))
-  )
-
-;;; [ atomic-chrome ] -- with websocket improvement on `edit-server'.
-
-(use-package atomic-chrome
-  :ensure t
-  :config
-  (atomic-chrome-start-server)
-  )
 
 ;;; [ 0xc ] -- Easy base conversion made easy in Emacs.
 
@@ -73,7 +36,7 @@
 (require 'init-my-emacs-edit-tabulate)
 (require 'init-my-emacs-edit-multiple-cursors)
 (require 'init-my-emacs-edit-sudo)
-
+(require 'init-my-emacs-edit-server)
 
 (provide 'init-my-emacs-edit)
 
