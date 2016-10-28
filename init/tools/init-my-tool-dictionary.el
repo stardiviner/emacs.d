9d72cb9e461d49b2fa98648c79de7a63afbbe22f
stardiviner <numbchild@gmail.com>
2016-10-28 09:38:23 +0800

    use package sdcv.

--- init/tools/init-my-tool-dictionary.el
+++ init/tools/init-my-tool-dictionary.el
@@ -9,39 +9,30 @@
 
 ;;; [ sdcv.el ]
 
-(use-package pos-tip
-  :ensure t)
+(use-package sdcv
+  :ensure t
+  :init
+  (unless (boundp 'dictionary-map)
+    (define-prefix-command 'dictionary-map))
+  (define-key my-tools-prefix (kbd "d") 'dictionary-map)
 
-(load "~/.emacs.d/init/extensions/sdcv.el")
+  (define-key dictionary-map (kbd "d") 'sdcv-search-pointer+)
+  (define-key dictionary-map (kbd "C-d") 'sdcv-search-input)
 
-(setq sdcv-dictionary-simple-list ;; a simple dictionary list for popup display
-      '(;; "WordNet"
-        "牛津英汉双解美化版"
-        ))
-
-(setq sdcv-dictionary-complete-list ;; a complete dictionary list for buffer display
-      '("WordNet"
-        "牛津英汉双解美化版"
-        "朗道英汉字典5.0"
-        "朗道汉英字典5.0"
-        ))
-
-(define-key sdcv-mode-map (kbd "n") 'sdcv-next-dictionary)
-(define-key sdcv-mode-map (kbd "p") 'sdcv-previous-dictionary)
-(define-key sdcv-mode-map (kbd "I") 'sdcv-search-input)
-(define-key sdcv-mode-map (kbd "i") 'sdcv-search-input+)
-(define-key sdcv-mode-map (kbd "Q") 'sdcv-search-pointer)
-(define-key sdcv-mode-map (kbd "q") 'sdcv-search-pointer+)
-(define-key sdcv-mode-map (kbd "V") 'show-entry)
-(define-key sdcv-mode-map (kbd "v") 'hide-entry)
-
-(unless (boundp 'dictionary-map)
-  (define-prefix-command 'dictionary-map))
-(define-key my-tools-prefix (kbd "d") 'dictionary-map)
-
-(define-key dictionary-map (kbd "d") 'sdcv-search-pointer+)
-(define-key dictionary-map (kbd "C-d") 'sdcv-search-input)
-
+  :config
+  ;; a simple dictionary list for popup display
+  (setq sdcv-dictionary-simple-list
+        '(;; "WordNet"
+          "牛津英汉双解美化版"
+          ))
+  ;; a complete dictionary list for buffer display
+  (setq sdcv-dictionary-complete-list
+        '("WordNet"
+          "牛津英汉双解美化版"
+          "朗道英汉字典5.0"
+          "朗道汉英字典5.0"
+          ))
+  )
 
 ;;; [ babel ] -- An Emacs interface to different translation services available on the Internet.
 
