c66f760baba83158623d796354f04ebdafd9fafd
stardiviner <numbchild@gmail.com>
2016-10-30 16:38:58 +0800

    auto adding CUSTOM_ID property for heading.

--- init/org-mode/init-my-org-property.el
+++ init/org-mode/init-my-org-property.el
@@ -7,9 +7,10 @@
 
 ;;; Code:
 
+;;; auto adding CUSTOM_ID property for heading
 
-
-
+(require 'org-id)
+(setq org-id-link-to-org-use-id 'create-if-interactive-and-no-custom-id)
 
 
 (provide 'init-my-org-property)
