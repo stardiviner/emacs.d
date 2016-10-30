7086e1e5d2cbd706cf9ca005daab309e12f1797e
stardiviner <numbchild@gmail.com>
2016-10-30 17:33:21 +0800

    disable org-mode heading ID.

--- init/org-mode/init-my-org-property.el
+++ init/org-mode/init-my-org-property.el
@@ -9,8 +9,8 @@
 
 ;;; auto adding CUSTOM_ID property for heading
 
-(require 'org-id)
-(setq org-id-link-to-org-use-id 'create-if-interactive-and-no-custom-id)
+;; (require 'org-id)
+;; (setq org-id-link-to-org-use-id 'create-if-interactive-and-no-custom-id)
 
 
 (provide 'init-my-org-property)
