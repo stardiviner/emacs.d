89a377b6331654e283b2aa0da00fdabca8eb90c4
stardiviner <numbchild@gmail.com>
2016-11-05 22:40:14 +0800

    improve gnuplot-mode settings.

--- init/programming/languages/gnuplot/init-my-prog-lang-gnuplot.el
+++ init/programming/languages/gnuplot/init-my-prog-lang-gnuplot.el
@@ -15,7 +15,6 @@
 
 (use-package gnuplot-mode
   :ensure t
-  :defer t
   :init
   (setq auto-mode-alist
         (append (list
@@ -33,38 +32,39 @@
         gnuplot-eldoc-mode t
         )
 
-  (add-hook 'gnuplot-mode-hook
-            (lambda ()
-              ;; context sensitive for gnuplot completion & eldoc-mode.
-              ;; - `completion-at-point'
-              ;; - `gnuplot-info-at-point' / [C-c C-/]
-              ;; - `gnuplot-help-function' / [C-c C-d]
-              ;;
-              ;; (gnuplot-context-sensitive-mode 1)
+  (defun my-gnuplot-mode-settings ()
+    ;; context sensitive for gnuplot completion & eldoc-mode.
+    ;; - `completion-at-point'
+    ;; - `gnuplot-info-at-point' / [C-c C-/]
+    ;; - `gnuplot-help-function' / [C-c C-d]
+    ;;
+    ;; (gnuplot-context-sensitive-mode 1)
 
-              ;; (setq gnuplot-completion-at-point-function
-              ;;       #'gnuplot-context-completion-at-point)
-              (add-hook 'completion-at-point-functions
-                        'gnuplot-context-completion-at-point nil t)
-              
-              ;; Setup Eldoc
-              (eldoc-mode 1)
-              (set (make-local-variable 'eldoc-documentation-function)
-                   'gnuplot-eldoc-function)
-              (eldoc-add-command 'completion-at-point)     ; Check for eldoc after completion
-              (when (fboundp 'comint-dynamic-complete)
-                (eldoc-add-command 'comint-dynamic-complete))
-              
-              
-              (define-key gnuplot-mode-map (kbd "<f5>") 'gnuplot-make-buffer)
-              (define-key gnuplot-mode-map (kbd "C-h d d") 'gnuplot-info-at-point)
-              (define-key gnuplot-mode-map (kbd "C-c C-d") 'gnuplot-info-lookup-symbol)
-              (define-key gnuplot-mode-map (kbd "C-c C-/") 'gnuplot-help-function)
-              (define-key gnuplot-mode-map (kbd "C-c M-i") 'gnuplot-inline-display-mode)
+    ;; (setq gnuplot-completion-at-point-function
+    ;;       #'gnuplot-context-completion-at-point)
+    (add-hook 'completion-at-point-functions
+              'gnuplot-context-completion-at-point nil t)
+    
+    ;; Setup Eldoc
+    (eldoc-mode 1)
+    (set (make-local-variable 'eldoc-documentation-function)
+         'gnuplot-eldoc-function)
+    (eldoc-add-command 'completion-at-point)     ; Check for eldoc after completion
+    (when (fboundp 'comint-dynamic-complete)
+      (eldoc-add-command 'comint-dynamic-complete))
+    
+    
+    (define-key gnuplot-mode-map (kbd "<f5>") 'gnuplot-make-buffer)
+    (define-key gnuplot-mode-map (kbd "C-h d d") 'gnuplot-info-at-point)
+    (define-key gnuplot-mode-map (kbd "C-c C-d") 'gnuplot-info-lookup-symbol)
+    (define-key gnuplot-mode-map (kbd "C-c C-/") 'gnuplot-help-function)
+    (define-key gnuplot-mode-map (kbd "C-c M-i") 'gnuplot-inline-display-mode)
 
-              (define-key gnuplot-mode-map (kbd "C-c C-s") 'run-gnuplot)
-              (define-key gnuplot-mode-map (kbd "C-c C-z") 'run-gnuplot)
-              ))
+    (define-key gnuplot-mode-map (kbd "C-c C-s") 'run-gnuplot)
+    (define-key gnuplot-mode-map (kbd "C-c C-z") 'run-gnuplot)
+    )
+  
+  (add-hook 'gnuplot-mode-hook #'my-gnuplot-mode-settings)
 
   ;; auto enable `gnuplot-inline-display-mode' in gnuplot comint process buffer.
   (add-hook 'gnuplot-comint-mode-hook 'gnuplot-inline-display-mode)
