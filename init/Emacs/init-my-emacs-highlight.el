4b1a3c8f2b7142b48944400dcc7e321476dcd6d2
stardiviner <numbchild@gmail.com>
2016-11-14 20:59:56 +0800

    update highlight-prefix keybindings.

--- init/Emacs/init-my-emacs-highlight.el
+++ init/Emacs/init-my-emacs-highlight.el
@@ -24,25 +24,16 @@
   (unbind-key (kbd "C-x w p") hi-lock-map)
   (unbind-key (kbd "C-x w r") hi-lock-map)
   )
-;; re-bind keybindings
-(unless (boundp 'hi-lock-prefix)
-  (define-prefix-command 'hi-lock-prefix))
-(define-key my-highlight-prefix (kbd "h") 'hi-lock-prefix)
 
-(define-key hi-lock-prefix (kbd "s") 'highlight-symbol-at-point)
-(define-key hi-lock-prefix (kbd "p") 'highlight-phrase)
-(define-key hi-lock-prefix (kbd "r") 'highlight-regexp)
-(define-key hi-lock-prefix (kbd "u") 'unhighlight-regexp)
-(define-key hi-lock-prefix (kbd "F") 'hi-lock-find-patterns)
-(define-key hi-lock-prefix (kbd "P") 'hi-lock-write-interactive-patterns)
-(define-key hi-lock-prefix (kbd "l") 'highlight-lines-matching-regexp)
-
+;; rebind commands
+(define-key my-highlight-prefix (kbd "C-s") 'highlight-regexp)
+(define-key my-highlight-prefix (kbd "M-u") 'unhighlight-regexp)
+(define-key my-highlight-prefix (kbd "M-s") 'highlight-lines-matching-regexp)
 
 ;;; [ highlight-symbol ] -- automatic and manual symbol highlighting for Emacs
 
 (use-package highlight-symbol
   :ensure t
-  :defer t
   :init
   (add-hook 'prog-mode-hook 'highlight-symbol-mode)
   :config
@@ -54,23 +45,18 @@
         highlight-symbol-foreground-color nil ; nil: keep original color.
         )
 
-  (unless (boundp 'my-highlight-symbol-prefix)
-    (define-prefix-command 'my-highlight-symbol-prefix))
-  (define-key my-highlight-prefix (kbd "s") 'my-highlight-symbol-prefix)
-
   ;; setting up keybindings
-  (define-key my-highlight-symbol-prefix (kbd "h") 'highlight-symbol-at-point)
-  (define-key my-highlight-symbol-prefix (kbd "n") 'highlight-symbol-next)
-  (define-key my-highlight-symbol-prefix (kbd "p") 'highlight-symbol-prev)
-  (define-key my-highlight-symbol-prefix (kbd "N") 'highlight-symbol-next-in-defun)
-  (define-key my-highlight-symbol-prefix (kbd "P") 'highlight-symbol-prev-in-defun)
-  (define-key my-highlight-symbol-prefix (kbd "l") 'highlight-symbol-list-all)
-  (define-key my-highlight-symbol-prefix (kbd "o") 'highlight-symbol-occur)
-  (define-key my-highlight-symbol-prefix (kbd "C") 'highlight-symbol-count)
-  (define-key my-highlight-symbol-prefix (kbd "r") 'highlight-symbol-query-replace)
-  (define-key my-highlight-symbol-prefix (kbd "U") 'highlight-symbol-remove-all)
-  (define-key my-highlight-symbol-prefix (kbd "u") 'highlight-symbol-at-point)
-  (define-key my-highlight-symbol-prefix (kbd "m") 'highlight-symbol-nav-mode)
+  (define-key my-highlight-prefix (kbd "h") 'highlight-symbol-at-point)
+  (define-key my-highlight-prefix (kbd "n") 'highlight-symbol-next)
+  (define-key my-highlight-prefix (kbd "p") 'highlight-symbol-prev)
+  (define-key my-highlight-prefix (kbd "M-n") 'highlight-symbol-next-in-defun)
+  (define-key my-highlight-prefix (kbd "M-p") 'highlight-symbol-prev-in-defun)
+  (define-key my-highlight-prefix (kbd "l") 'highlight-symbol-list-all)
+  (define-key my-highlight-prefix (kbd "o") 'highlight-symbol-occur)
+  (define-key my-highlight-prefix (kbd "C") 'highlight-symbol-count)
+  (define-key my-highlight-prefix (kbd "M-%") 'highlight-symbol-query-replace)
+  (define-key my-highlight-prefix (kbd "u") 'highlight-symbol-remove-all)
+  (define-key my-highlight-prefix (kbd "m") 'highlight-symbol-nav-mode)
   )
 
 
