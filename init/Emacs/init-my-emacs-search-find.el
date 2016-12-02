d85fba2aaaa9f7989c5a962f02d051ad24f4a1fc
stardiviner <numbchild@gmail.com>
2016-12-02 23:07:31 +0800

    enable package f3.

--- init/Emacs/init-my-emacs-search-find.el
+++ init/Emacs/init-my-emacs-search-find.el
@@ -9,11 +9,10 @@
 
 ;;; [ f3 ] -- The Fantastic File Finder: a helm interface for searching files really fast.
 
-;; (use-package f3
-;;   :ensure t
-;;   :config
-;;   (global-set-key (kbd "C-x f") 'f3)
-;;   )
+(use-package f3
+  :ensure t
+  :bind ("C-x f" . f3)
+  )
 
 
 (provide 'init-my-emacs-search-find)
