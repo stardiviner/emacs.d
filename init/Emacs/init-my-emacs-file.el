633e603eb830b5c498cfc053a78ae7264a2d84c1
stardiviner <numbchild@gmail.com>
2016-12-07 19:14:40 +0800

    move file to another place with user command `move-file'.

--- init/Emacs/init-my-emacs-file.el
+++ init/Emacs/init-my-emacs-file.el
@@ -72,6 +72,26 @@
 (use-package snapshot-timemachine
   :ensure t)
 
+;;; move file to another place
+(defun move-file (new-location)
+  "Write this file to NEW-LOCATION, and delete the old one."
+  (interactive
+   (list (if buffer-file-name
+             (read-file-name "Move file to: ")
+           (read-file-name "Move file to: "
+                           default-directory
+                           (expand-file-name (file-name-nondirectory (buffer-name))
+                                             default-directory)))))
+  (when (file-exists-p new-location)
+    (delete-file new-location))
+  (let ((old-location (buffer-file-name)))
+    (write-file new-location t)
+    (when (and old-location
+               (file-exists-p new-location))
+      (delete-file old-location))))
+
+(bind-key "C-x M-w" #'move-file)
+
 
 (provide 'init-my-emacs-file)
 
