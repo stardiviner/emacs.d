c406e9dd77da6322f3b2d2e8d569bc62f7f4fcd4
stardiviner <numbchild@gmail.com>
2016-12-12 13:55:04 +0800

    update multiple-cursors ignored commands list.

--- .mc-lists.el
+++ .mc-lists.el
@@ -6,9 +6,16 @@
         artist-backward-char
         artist-forward-char
         backward-sexp
+        counsel-yank-pop
         fill-paragraph
         forward-sexp
         kill-region
+        lispy-braces
+        lispy-brackets
+        lispy-delete
+        lispy-kill
+        lispy-parens
+        lispy-quotes
         my/fill-or-unfill
         my/org-insert-key
         org-beginning-of-line
@@ -22,6 +29,7 @@
         org-kill-line
         org-metaleft
         org-metaright
+        org-next-visible-heading
         org-open-line
         org-return-indent
         org-self-insert-command
@@ -36,6 +44,7 @@
         paredit-forward-delete
         paredit-forward-kill-word
         paredit-kill
+        paredit-open-round
         paredit-open-square
         paredit-semicolon
         paredit-splice-sexp-killing-backward
@@ -45,10 +54,81 @@
         sp-delete-char
         sp-kill-hybrid-sexp
         sp-remove-active-pair-overlay
+        special-digit-argument
+        special-lh-knight/body
+        special-lispy-ace-char
+        special-lispy-ace-paren
+        special-lispy-ace-subword
+        special-lispy-ace-symbol
+        special-lispy-ace-symbol-replace
+        special-lispy-alt-multiline
+        special-lispy-back
+        special-lispy-barf
+        special-lispy-beginning-of-defun
+        special-lispy-clone
+        special-lispy-convolute
+        special-lispy-convolute-left
+        special-lispy-different
+        special-lispy-down
+        special-lispy-down-slurp
+        special-lispy-edebug-stop
+        special-lispy-ediff-regions
+        special-lispy-eval
+        special-lispy-eval-and-insert
+        special-lispy-eval-other-window
+        special-lispy-flow
+        special-lispy-follow
+        special-lispy-goto
+        special-lispy-goto-def-ace
+        special-lispy-goto-def-down
+        special-lispy-goto-elisp-commands
+        special-lispy-goto-local
+        special-lispy-goto-mode
+        special-lispy-goto-projectile
+        special-lispy-goto-recursive
+        special-lispy-join
+        special-lispy-left
+        special-lispy-mark-list
+        special-lispy-move-down
+        special-lispy-move-left
+        special-lispy-move-right
+        special-lispy-move-up
+        special-lispy-narrow
+        special-lispy-new-copy
+        special-lispy-occur
+        special-lispy-oneline
+        special-lispy-other-mode
+        special-lispy-other-space
+        special-lispy-outline-goto-child
+        special-lispy-outline-next
+        special-lispy-outline-prev
+        special-lispy-paste
+        special-lispy-quit
+        special-lispy-raise
+        special-lispy-raise-some
+        special-lispy-repeat
+        special-lispy-right
+        special-lispy-shifttab
+        special-lispy-slurp
+        special-lispy-splice
+        special-lispy-stringify
+        special-lispy-tab
+        special-lispy-teleport
+        special-lispy-tilde
+        special-lispy-underscore
+        special-lispy-undo
+        special-lispy-up
+        special-lispy-up-slurp
+        special-lispy-view
+        special-lispy-visit
+        special-lispy-widen
+        special-lispy-x
+        special-pop-tag-mark
         yas-expand
         ))
 
 (setq mc/cmds-to-run-once
       '(
         ace-window
+        lispy-cursor-down
         ))
