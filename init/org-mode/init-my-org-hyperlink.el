f215539fa7b4eb0432d1ede2c2799c3d712c4237
stardiviner <numbchild@gmail.com>
2016-12-31 16:09:36 +0800

    open .swf files in Org-mode with firefox.

--- init/org-mode/init-my-org-hyperlink.el
+++ init/org-mode/init-my-org-hyperlink.el
@@ -60,7 +60,8 @@
         ("\\.mp3\\'" . "mpg123 %s")
         ))
 
-(add-to-list 'org-file-apps '("\\.swf\\'" . "swfdec-player %s"))
+;; (add-to-list 'org-file-apps '("\\.swf\\'" . "swfdec-player %s"))
+(add-to-list 'org-file-apps '("\\.swf\\'" . "firefox %s"))
 (add-to-list 'org-file-apps '("\\.jar\\'" . "java -jar %s"))
 
 ;; System wise: xdg-open, kde-open, gnome-open.
