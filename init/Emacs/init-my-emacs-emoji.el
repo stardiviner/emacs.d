f009432a3298c4f5a790472ca30764990d764ebf
stardiviner <numbchild@gmail.com>
2017-01-06 16:05:30 +0800

    add emoji init file.

---
+++ init/Emacs/init-my-emacs-emoji.el
@@ -0,0 +1,17 @@
+;;; init-my-emacs-emoji.el --- init Emoji for Emacs.
+
+;;; Commentary:
+
+
+
+;;; Code:
+;;; ----------------------------------------------------------------------------
+
+
+
+
+;;; ----------------------------------------------------------------------------
+
+(provide 'init-my-emacs-emoji)
+
+;;; init-my-emacs-emoji.el ends here
