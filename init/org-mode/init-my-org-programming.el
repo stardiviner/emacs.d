fd06f886c79316f35b0491ab1f12137cc7a8e539
stardiviner <numbchild@gmail.com>
2017-01-12 15:27:48 +0800

    add package org-projectile.

--- init/org-mode/init-my-org-programming.el
+++ init/org-mode/init-my-org-programming.el
@@ -6,8 +6,28 @@
 
 ;;; Code:
 ;;; ----------------------------------------------------------------------------
+;;; [ org-projectile ] -- Manage org-mode TODOs for your projectile projects.
 
+(use-package org-projectile
+  :ensure t
+  :bind (("C-c n p" . org-projectile:project-todo-completing-read))
+  :config
+  ;; global projects todo file
+  (setq org-projectile:projects-file
+        (concat org-directory "/Programming/Projects/"))
+  
+  ;; per-project todo files
+  (org-projectile:per-repo)
+  (setq org-projectile:per-repo-filename "TODO.org")
 
+  ;; prompt mode
+  (org-projectile:prompt)
+
+  ;; add files to `org-agenda'.
+  (setq org-agenda-files (append org-agenda-files (org-projectile:todo-files)))
+
+  (add-to-list 'org-capture-templates (org-projectile:project-todo-entry "p"))
+  )
 
 
 ;;; ----------------------------------------------------------------------------
