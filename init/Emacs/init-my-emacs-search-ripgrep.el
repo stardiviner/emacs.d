ed30aca121b9b272af2ab7ae30df8c59a59d1c90
stardiviner <numbchild@gmail.com>
2017-01-16 22:28:03 +0800

    enable package ripgrep

--- init/Emacs/init-my-emacs-search-ripgrep.el
+++ init/Emacs/init-my-emacs-search-ripgrep.el
@@ -9,12 +9,12 @@
 ;;; ----------------------------------------------------------------------------
 ;;; [ ripgrep ] -- Emacs front-end for ripgrep, a command line search tool.
 
-;; (use-package ripgrep
-;;   :ensure t
-;;   :defer t
-;;   :init
-;;   (define-key my-search-prefix (kbd "s") 'ripgrep-regexp)
-;;   )
+(use-package ripgrep
+  :ensure t
+  :bind (:map my-search-prefix
+              ("s" . ripgrep-regexp))
+  )
+
 
 ;;; ----------------------------------------------------------------------------
 
