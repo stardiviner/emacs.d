0c0f99a98f652fb9ff4c884bdd0142831cb54034
stardiviner <numbchild@gmail.com>
2017-02-17 21:36:06 +0800

    add Org-mode minimal config to minimal init file.

--- minimal-init.el
+++ minimal-init.el
@@ -156,4 +156,165 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(use-package org
+  :ensure t)
+
+(use-package org-plus-contrib
+  :ensure t)
+
+(require 'init-my-org-mode)
+
+(setq org-modules
+      '(org-pcomplete
+        org-faces
+        ;; org-fstree
+        org-table org-compat
+        ;; org-protocol
+        org-timer org-clock org-habit org-notify
+        org-info org-bibtex org-docview
+        org-plot
+        org-irc ; org-gnus org-mhe org-rmail
+        ;; org-w3m
+        ))
+
+;;; Org-mode Babel
+
+(setq org-confirm-babel-evaluate nil)
+(setq org-babel-no-eval-on-ctrl-c-ctrl-c nil)
+(setq org-confirm-shell-link-function 'yes-or-no-p)
+(setq org-confirm-elisp-link-function 'yes-or-no-p)
+
+(setq org-babel-default-header-args
+      '((:session . "none")
+        (:results . "replace")
+        (:exports . "both") (:cache . "no") (:hlines . "no")
+        (:noweb . "no") (:tangle . "no")
+        (:mkdirp . "yes")
+        (:padline . "true") (:comments . "links")
+        ))
+
+(setq org-babel-noweb-error-all-langs t)
+
+;; babel src block editing
+(setq org-src-fontify-natively t
+      ;; nil: preserve org indent, t: preserve export indent.
+      org-src-preserve-indentation nil
+      ;; 0: fix `diff' babel syntax highlighting invalid issue.
+      org-edit-src-content-indentation 0
+      org-src-tab-acts-natively nil ; make [Tab] work native as in major mode.
+      org-src-window-setup 'current-window ; 'reorganize-frame, 'current-window
+      org-src-ask-before-returning-to-edit-buffer nil
+      org-edit-src-auto-save-idle-delay 0 ; 0: don't auto save.
+      )
+
+;; latex fragments preview
+(setq org-startup-with-latex-preview t)
+
+;; inline image preview
+(setq org-startup-with-inline-images nil)
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((emacs-lisp . t)                     ; Emacs Lisp
+   (org . t)                            ; Org-mode
+   (sh . t)                             ; Shell
+   (ruby . t)                           ; Ruby
+   (python . t)                         ; Python
+   (C . t)                              ; C
+   (lisp . t)                           ; Lisp
+   (scheme . t)                         ; Scheme
+   (clojure . t)                        ; Clojure
+   (haskell . t)                        ; Haskell
+   (js . t)                             ; JavaScript
+   (css . t)                            ; CSS
+   (latex . t)                          ; LaTeX
+   (sql . t)                            ; SQL
+   (sqlite . t)                         ; SQLite
+   ))
+
+(setq org-todo-keywords
+      '(
+        ;; Status
+        (sequence "URGENT(u!)" "INPROGRESS(g!)" "TODO(t@/!)" "LATER(l!)" "SOMEDAY(s@/!)" "FAILED(x@/!)" "CANCELLED(C@/!)" "|" "DONE(d@/!)")
+        ;; Clock
+        (sequence "STARTED(!)" "|" "DONE(d@/!)")
+        ;; Habit
+        (sequence "HABIT(h)" "|" "DONE(d)")
+        ;; Types
+        (type "CODE(c@/!)" "PROJECT(P@/!)" "Org(o@/!)" "|" "DONE(d@/!)")
+        ;; Code
+        (sequence "BUG(b!)" "ISSUE(i!)" "ERROR(e!)" "FEATURE(f!)" "Pull-Request(p!)" "|" "DONE(d@/!)")
+        ;; Work
+        (type "WORK(w@/!)" "MEETING(m@/!)" "|" "DONE(d@/!)")
+        ;; Learn
+        (type "LEARN(n!)" "REVIEW(r!)" "|" "DONE(d@/!)")
+        ;; Life
+        (type "SEX(X@/!)" "|" "DONE(d@/!)")
+        ;; org-trello
+        ;; (type "TODO" "INPROGRESS" "|" "DONE")
+        ))
+
+(setq org-todo-keyword-faces
+      '(;;; todo keywords
+        ("TODO" :foreground "orange"
+         :box '(:color "black" :line-width -1))
+        ("URGENT" :foreground "red" :background "black"
+         :box '(:color "black" :line-width -1))
+        ("STARTED" :foreground "green"
+         :box '(:color "red" :line-width -1))
+        ("HABIT" :foreground "cyan" :background "black"
+         :box '(:color "green" :line-width -1))
+        ("SOMEDAY" :foreground "dim gray"
+         :box '(:color "black" :line-width -1))
+        ("INPROGRESS" :foreground "cyan"
+         :box '(:color "black" :line-width -1))
+        ("LATER" :foreground "dim gray" :background "black"
+         :box '(:color "dark red" :line-width -1))
+        ("DONE" :foreground "black"
+         :strike-through t
+         :box '(:color "black" :line-width -1))
+        ("FAILED" :foreground "#444444"
+         :underline "dark red"
+         :box '(:color "black" :line-width -1))
+        ("CANCELLED"
+         :foreground "black"
+         :strike-through t
+         :box '(:color "black" :line-width -1))
+        ;; code programming
+        ("BUG" :foreground "red"
+         :box '(:color "red" :line-width -1 :style nil))
+        ("ISSUE" :foreground "red"
+         :box '(:color "dark red" :line-width -1 :style nil))
+        ("ERROR" :foreground "red"
+         :box '(:color "red" :line-width -1 :style nil))
+        ("FIXME" :foreground "black" :background "red"
+         :box '(:color "dark red" :line-width -1 :style nil))
+        ("FEATURE" :foreground "cyan"
+         :box '(:color "cyan" :line-width -1 :style nil))
+        ("Pull-Request" :foreground "yellow"
+         :box '(:color "yellow" :line-width -1 :style nil))
+        ;; types
+        ("Org" :foreground "cyan" :background "#004A5D"
+         :box '(:color "cyan" :line-width -1 :style nil))
+        ("CODE" :foreground "white" :background "#004A5D"
+         :box '(:color "cyan" :line-width -1 :style nil))
+        ("PROJECT" :foreground "white" :background "#004A5D"
+         :box '(:color "cyan" :line-width -1 :style nil))
+        ;; life
+        ("SEX" :foreground "deep pink"
+         :box '(:color "deep pink" :line-width -1 :style nil))
+        ;; work
+        ("WORK" :foreground "orange"
+         :box '(:color "black" :line-width -1 :style nil))
+        ("MEETING" :foreground "cornflower blue"
+         :box '(:color "cyan" :line-width -1 :style nil))
+        ;; learn
+        ("LEARN" :foreground "green yellow"
+         :box '(:color "black" :line-width -1))
+        ("REVIEW" :foreground "yellow"
+         :box '(:color "black" :line-width -1))
+        ))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 
