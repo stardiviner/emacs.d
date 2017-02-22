0c8683485794d190dc67a8defc03718d72acfeae
stardiviner <numbchild@gmail.com>
2017-02-22 16:25:22 +0800

    update allout config.

--- init/Emacs/init-my-emacs-outline.el
+++ init/Emacs/init-my-emacs-outline.el
@@ -32,40 +32,11 @@
                 allout-plain-bullets-string-len 5
                 allout-plain-bullets-string "*+#>." ; + -> #N -> > -> *
                 )
-
   
   (allout-minor-mode 1)
-
-  ;; so that you can active/inactive allout-minor-mode to edit/navigate/folding with it.
-  ;; (define-key my-edit-prefix (kbd "o") 'allout-minor-mode)
-  ;; activate outline mode for current buffer, and establish a default file-var setting for `allout-layout'.
-  (defun my-allout-toggle ()
-    "Toggle allout for current buffer."
-    (interactive)
-    (if (allout-mode-p)
-        (allout-mode -1)
-      (outlineify-sticky)
-      (allout-hide-bodies)))
-
-  (define-key allout-mode-map (kbd "C-c SPC C-l") 'allout-hide-bodies)
-  
-  ;; (define-key my-edit-prefix (kbd "o") 'outlineify-sticky)
-  (define-key outline-prefix (kbd "@") 'my-allout-toggle)
-
-  ;; (unless (boundp 'outline-prefix)
-  ;;   (define-prefix-command 'outline-prefix))
-  ;; (define-key my-edit-prefix (kbd "o") 'outline-prefix)
-  ;;
-  ;; (define-key outline-prefix (kbd "n") 'allout-next-heading)
   )
 
 
 (provide 'init-my-emacs-outline)
 
 ;;; init-my-emacs-outline.el ends here
-;;;_* Dummy outline topic header -- see ‘allout-mode’ docstring: ‘C-h m’.
-;;;_* Local emacs vars.
-;;;_ + Local variables:
-;;;_ + allout-layout: (-1 : 0)
-;;;_ + End:
-
