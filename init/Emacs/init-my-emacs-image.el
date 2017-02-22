376719d360b2205659a42b4ae9f8a63823c5e230
stardiviner <numbchild@gmail.com>
2017-02-22 16:22:22 +0800

    remove package image+.

--- init/Emacs/init-my-emacs-image.el
+++ init/Emacs/init-my-emacs-image.el
@@ -30,26 +30,6 @@
   (add-hook 'eshell-mode-hook 'iimage-mode)
   )
 
-
-;;; [ image+ ] -- Emacs image extension
-
-;; (use-package image+
-;;   :ensure t
-;;   :defer t
-;;   :config
-;;   (with-eval-after-load 'image+
-;;     (when (require 'hydra nil t)
-;;       (defhydra imagex-sticky-binding (global-map "C-x C-l")
-;;         "Manipulating Image"
-;;         ("+" imagex-sticky-zoom-in "zoom in")
-;;         ("-" imagex-sticky-zoom-out "zoom out")
-;;         ("M" imagex-sticky-maximize "maximize")
-;;         ("O" imagex-sticky-restore-original "restore original")
-;;         ("S" imagex-sticky-save-image "save file")
-;;         ("r" imagex-sticky-rotate-right "rotate right")
-;;         ("l" imagex-sticky-rotate-left "rotate left"))))
-;;   )
-
 ;;; [ picpocket ] -- image viewer
 
 (use-package picpocket
