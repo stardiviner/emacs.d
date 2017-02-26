90a728d015399b3d93ccdd5377e3024e775b3592
stardiviner <numbchild@gmail.com>
2017-02-26 10:53:50 +0800

    enable async irony for company-mode.

--- init/programming/languages/C/init-my-prog-lang-C-common.el
+++ init/programming/languages/C/init-my-prog-lang-C-common.el
@@ -69,13 +69,13 @@
   :config
   ;; replace the `completion-at-point' and `complete-symbol' bindings in
   ;; irony-mode's buffers by irony-mode's function
-  ;; (add-hook 'irony-mode-hook
-  ;;           (lambda ()
-  ;;             (define-key irony-mode-map [remap completion-at-point]
-  ;;               'irony-completion-at-point-async)
-  ;;             (define-key irony-mode-map [remap complete-symbol]
-  ;;               'irony-completion-at-point-async)
-  ;;             ))
+  (defun my-irony-mode-hook ()
+    (define-key irony-mode-map [remap completion-at-point]
+      'irony-completion-at-point-async)
+    (define-key irony-mode-map [remap complete-symbol]
+      'irony-completion-at-point-async))
+  (add-hook 'irony-mode-hook 'my-irony-mode-hook)
+  (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
   )
 
 
