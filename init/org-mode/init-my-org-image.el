6eb6a8a3b3adca2ae5a69e6d261fbb39728844e6
stardiviner <numbchild@gmail.com>
2017-02-27 12:51:56 +0800

    enable Org-mode startup show inline image.

--- init/org-mode/init-my-org-image.el
+++ init/org-mode/init-my-org-image.el
@@ -13,7 +13,7 @@
 ;; [C-c C-x C-v] - `org-toggle-inline-images'.
 ;; [C-c C-x C-M-v] - `org-redisplay-inline-images'
 
-(setq org-startup-with-inline-images nil
+(setq org-startup-with-inline-images t
       org-image-actual-width nil ; inline image scale width.
       ;; you need to use:
       ;; - #+ATTR_ORG: :width 400
