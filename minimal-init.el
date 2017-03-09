75621974366952ade8ed2e6d7743bcaa4ed198b6
stardiviner <numbchild@gmail.com>
2017-03-09 17:28:38 +0800

    update minimal init file.

--- minimal-init.el
+++ minimal-init.el
@@ -209,9 +209,8 @@
 
 ;; latex fragments preview
 (setq org-startup-with-latex-preview t)
-
 ;; inline image preview
-(setq org-startup-with-inline-images nil)
+(setq org-startup-with-inline-images t)
 
 (org-babel-do-load-languages
  'org-babel-load-languages
@@ -232,94 +231,4 @@
    (sqlite . t)                         ; SQLite
    ))
 
-(setq org-todo-keywords
-      '(
-        ;; Status
-        (sequence "URGENT(u!)" "INPROGRESS(g!)" "TODO(t@/!)" "LATER(l!)" "SOMEDAY(s@/!)" "FAILED(x@/!)" "CANCELLED(C@/!)" "|" "DONE(d@/!)")
-        ;; Clock
-        (sequence "STARTED(!)" "|" "DONE(d@/!)")
-        ;; Habit
-        (sequence "HABIT(h)" "|" "DONE(d)")
-        ;; Types
-        (type "CODE(c@/!)" "PROJECT(P@/!)" "Org(o@/!)" "|" "DONE(d@/!)")
-        ;; Code
-        (sequence "BUG(b!)" "ISSUE(i!)" "ERROR(e!)" "FEATURE(f!)" "Pull-Request(p!)" "|" "DONE(d@/!)")
-        ;; Work
-        (type "WORK(w@/!)" "MEETING(m@/!)" "|" "DONE(d@/!)")
-        ;; Learn
-        (type "LEARN(n!)" "REVIEW(r!)" "|" "DONE(d@/!)")
-        ;; Life
-        (type "SEX(X@/!)" "|" "DONE(d@/!)")
-        ;; org-trello
-        ;; (type "TODO" "INPROGRESS" "|" "DONE")
-        ))
-
-(setq org-todo-keyword-faces
-      '(;;; todo keywords
-        ("TODO" :foreground "orange"
-         :box '(:color "black" :line-width -1))
-        ("URGENT" :foreground "red" :background "black"
-         :box '(:color "black" :line-width -1))
-        ("STARTED" :foreground "green"
-         :box '(:color "red" :line-width -1))
-        ("HABIT" :foreground "cyan" :background "black"
-         :box '(:color "green" :line-width -1))
-        ("SOMEDAY" :foreground "dim gray"
-         :box '(:color "black" :line-width -1))
-        ("INPROGRESS" :foreground "cyan"
-         :box '(:color "black" :line-width -1))
-        ("LATER" :foreground "dim gray" :background "black"
-         :box '(:color "dark red" :line-width -1))
-        ("DONE" :foreground "black"
-         :strike-through t
-         :box '(:color "black" :line-width -1))
-        ("FAILED" :foreground "#444444"
-         :underline "dark red"
-         :box '(:color "black" :line-width -1))
-        ("CANCELLED"
-         :foreground "black"
-         :strike-through t
-         :box '(:color "black" :line-width -1))
-        ;; code programming
-        ("BUG" :foreground "red"
-         :box '(:color "red" :line-width -1 :style nil))
-        ("ISSUE" :foreground "red"
-         :box '(:color "dark red" :line-width -1 :style nil))
-        ("ERROR" :foreground "red"
-         :box '(:color "red" :line-width -1 :style nil))
-        ("FIXME" :foreground "black" :background "red"
-         :box '(:color "dark red" :line-width -1 :style nil))
-        ("FEATURE" :foreground "cyan"
-         :box '(:color "cyan" :line-width -1 :style nil))
-        ("Pull-Request" :foreground "yellow"
-         :box '(:color "yellow" :line-width -1 :style nil))
-        ;; types
-        ("Org" :foreground "cyan" :background "#004A5D"
-         :box '(:color "cyan" :line-width -1 :style nil))
-        ("CODE" :foreground "white" :background "#004A5D"
-         :box '(:color "cyan" :line-width -1 :style nil))
-        ("PROJECT" :foreground "white" :background "#004A5D"
-         :box '(:color "cyan" :line-width -1 :style nil))
-        ;; life
-        ("SEX" :foreground "deep pink"
-         :box '(:color "deep pink" :line-width -1 :style nil))
-        ;; work
-        ("WORK" :foreground "orange"
-         :box '(:color "black" :line-width -1 :style nil))
-        ("MEETING" :foreground "cornflower blue"
-         :box '(:color "cyan" :line-width -1 :style nil))
-        ;; learn
-        ("LEARN" :foreground "green yellow"
-         :box '(:color "black" :line-width -1))
-        ("REVIEW" :foreground "yellow"
-         :box '(:color "black" :line-width -1))
-        ))
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(use-package cider
-  :ensure t
-  :config
-  (setq cider-show-eval-spinner t)
-  (setq cider-allow-jack-in-without-project t)
-  )
