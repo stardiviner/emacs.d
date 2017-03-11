d19edcc09d377f96f1f04c5d2739b01ef03ac61a
stardiviner <numbchild@gmail.com>
2017-03-11 15:33:18 +0800

    use new package pomidor to replace tomatinho.

--- init/tools/init-my-tool-pomodoro.el
+++ init/tools/init-my-tool-pomodoro.el
@@ -13,22 +13,17 @@
 (global-set-key (kbd "<f12>") 'pomodoro-prefix)
 ;; (define-key my-tools-prefix (kbd "C-p") 'pomodoro-prefix)
 
+;;; [ pomidor ] -- Pomidor is a simple and cool pomodoro technique timer.
 
-;;; [ tomatinho ] -- A simple and beautiful pomodoro technique timer that runs on Emacs.
-
-(use-package tomatinho
+(use-package pomidor
   :ensure t
   :bind (:map pomodoro-prefix
-              ("<f12>" . tomatinho)
-              :map tomatinho-map
-              ("n" . tomatinho-interactive-new-pomodoro)
-              ("p" . tomatinho-interactive-deliberate-pause)
-              ("<DEL>" . tomatinho-interactive-reset)
-              ("TAB" . tomatinho-interactive-toggle-display)
-              ("q" . tomatinho-interactive-kill-buffer)
-              ("Q" . tomatinho-interactive-quit))
+              ("<f12>" . pomidor))
   :config
-  ;; (setq tomatinho-bar-length 25)
+  ;; disable sounds
+  ;; (setq pomidor-sound-tick nil
+  ;;       pomidor-sound-tack nil
+  ;;       pomidor-sound-overwork nil)
   )
 
 
