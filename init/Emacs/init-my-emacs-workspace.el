1e87d65523e035161340d14f0e25a6e78011fc9f
stardiviner <numbchild@gmail.com>
2017-04-05 22:58:47 +0800

    disable perspeen.

--- init/Emacs/init-my-emacs-workspace.el
+++ init/Emacs/init-my-emacs-workspace.el
@@ -8,16 +8,16 @@
 ;;; ----------------------------------------------------------------------------
 ;;; [ perspeen ] -- A emacs plugin for multi workspace.
 
-(use-package perspeen
-  :ensure t
-  :config
-  (setq perspeen-use-tab t)
-  (setq perspeen-keymap-prefix (kbd "C-z"))
-  (define-key perspeen-command-map (kbd "k") 'perspeen-delete-ws)
-  (define-key perspeen-command-map (kbd "r") 'perspeen-rename-ws)
-  (define-key perspeen-command-map (kbd "s") 'perspeen-goto-ws)
-  (perspeen-mode 1)
-  )
+;; (use-package perspeen
+;;   :ensure t
+;;   :config
+;;   (setq perspeen-use-tab t)
+;;   (setq perspeen-keymap-prefix (kbd "C-z"))
+;;   (define-key perspeen-command-map (kbd "k") 'perspeen-delete-ws)
+;;   (define-key perspeen-command-map (kbd "r") 'perspeen-rename-ws)
+;;   (define-key perspeen-command-map (kbd "s") 'perspeen-goto-ws)
+;;   (perspeen-mode 1)
+;;   )
 
 
 ;;; ----------------------------------------------------------------------------
