24a5b5dc9ef84b65045f6100821c12dbd6897627
stardiviner <numbchild@gmail.com>
2017-04-26 12:21:01 +0800

    add package mastodon.

--- init/tools/init-my-tool-social-network.el
+++ init/tools/init-my-tool-social-network.el
@@ -6,8 +6,15 @@
 
 ;;; Code:
 ;;; ----------------------------------------------------------------------------
+;;; [ mastodon ] -- A GNU Social-compatible microblogging server.
 
-
+(use-package mastodon
+  :ensure t
+  :config
+  (setq mastodon-instance-url "https://social.bytesexual.net/" ; "https://mastodon.social"
+        ;; mastodon-token-file (concat user-emacs-directory "mastodon.plstore")
+        )
+  )
 
 
 ;;; ----------------------------------------------------------------------------
