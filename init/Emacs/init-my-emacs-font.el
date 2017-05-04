e67b077dd9ef55bc030e345166f167959b65ba03
stardiviner <numbchild@gmail.com>
2017-05-05 00:14:04 +0800

    reuse WenQuanYi Micro Hei font for CJK Chinese faces.

--- init/Emacs/init-my-emacs-font.el
+++ init/Emacs/init-my-emacs-font.el
@@ -137,8 +137,8 @@ for variable-pitch face."
 (dolist (charset '(kana han symbol cjk-misc bopomofo))
   (set-fontset-font (frame-parameter nil 'font)
                     charset
-                    (font-spec :family "Noto Sans CJK SC" :size 13)
-                    ;; (font-spec :family "WenQuanYi Micro Hei" :size 13)
+                    ;; (font-spec :family "Noto Sans CJK SC" :size 12)
+                    (font-spec :family "WenQuanYi Micro Hei" :size 13)
                     ;; (font-spec :family "HanaMinA" :size 14)
                     ))
 
