e67431ec79f0276103d523eb831fea0db6278576
stardiviner <numbchild@gmail.com>
2017-05-24 16:10:37 +0800

    enable company-racer.

--- init/programming/languages/Rust/init-my-prog-lang-rust.el
+++ init/programming/languages/Rust/init-my-prog-lang-rust.el
@@ -44,27 +44,25 @@
   :ensure t
   :init
   ;; config racer in the path.
-  (unless (getenv "RUST_SRC_PATH")
-    (setenv "RUST_SRC_PATH" (expand-file-name "~/Code/Rust/rust/src")))
+  (let ((rust-src-path (getenv "RUST_SRC_PATH")))
+    (if (and rust-src-path (file-directory-p rust-src-path))
+        (setenv "RUST_SRC_PATH" (expand-file-name "~/Code/Rust/rust/src"))
+      (warn (format "Rust SRC path %s does not exist." rust-src-path))))
   ;; (setq racer-cmd (or (executable-find "racer") "/usr/local/bin/racer"))
   ;; (setq racer-rust-src-path (or (getenv "RUST_SRC_PATH")
   ;;                               "/usr/local/src/rust/src"))
   
   ;; auto start racer for rust-mode.
   (add-hook 'rust-mode-hook #'racer-mode)
+
+  (use-package company-racer
+    :ensure t
+    :config
+    (add-hook 'rust-mode-hook
+              (lambda ()
+                (my-company-add-backend-locally 'company-racer)))
+    )
   )
-
-
-;;; [ company-racer ]
-
-;; (use-package company-racer
-;;   :ensure t
-;;   :init
-;;   (add-hook 'rust-mode-hook
-;;             (lambda ()
-;;               (my-company-add-backend-locally 'company-racer)))
-;;   )
-
 
 ;;; [ cargo ] -- Emacs Cargo client.
 
