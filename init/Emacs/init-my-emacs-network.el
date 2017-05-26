cad7380cecc611b7528d82a0b39d23da954af83b
stardiviner <numbchild@gmail.com>
2017-05-26 10:13:25 +0800

    setup Emacs network proxy.

--- init/Emacs/init-my-emacs-network.el
+++ init/Emacs/init-my-emacs-network.el
@@ -14,7 +14,37 @@
 
 ;;; [ Proxy ]
 
-;; (setq url-proxy-services '(()))
+(setq url-proxy-services
+      '(
+        ;; ("http"  . "http://b.qypac.net:57008")
+        ;; ("https" . "127.0.0.1:1080")
+        ;; ("ftp"   . "b.qypac.net:57008")
+        ;; don't use `localhost', avoid robe server (For Ruby) can't response.
+        ;; ("no_proxy" . "127.0.0.1")
+        ;; ("no_proxy" . "^.*\\(baidu\\|sina)\\.com")
+        ))
+
+;; (setq url-using-proxy "http://b.qypac.net:57008")
+
+;;; AUTHORITY
+;; (setq url-http-proxy-basic-auth-storage
+;;       (list (list "proxy.com:8080"
+;; 		  (cons "Input your LDAP UID !"
+;; 			(base64-encode-string "LOGIN:PASSWORD")))))
+
+
+(defun proxy-toggle ()
+  (interactive)
+  (if (getenv "HTTP_PROXY")
+      (progn
+        (setenv "HTTP_PROXY"  nil)
+        (setenv "HTTPS_PROXY" nil)
+        )
+    (setenv "HTTP_PROXY"  "http://b.qypac.net:57008")
+    (setenv "HTTPS_PROXY" "http://b.qypac.net:57008")
+    )
+  )
+
 
 ;;; ----------------------------------------------------------------------------
 
