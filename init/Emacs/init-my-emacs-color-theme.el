3db8e4948758d9e0658a2fe7c8b60b39551d28eb
stardiviner <numbchild@gmail.com>
2017-05-31 11:08:44 +0800

    use org-beautify-theme only in leuven-theme.

--- init/Emacs/init-my-emacs-color-theme.el
+++ init/Emacs/init-my-emacs-color-theme.el
@@ -42,6 +42,12 @@
 ;;   :config
 ;;   (setq leuven-scale-outline-headlines t)
 ;;   (load-theme 'leuven t)
+;;
+;;   ;; [ org-beautify-theme ] -- A sub-theme to make org-mode more beautiful.
+;;   (use-package org-beautify-theme
+;;     :ensure t
+;;     :config
+;;     (load-theme 'org-beautify))
 ;;   )
 
 ;;; [ nord-theme ] -- An arctic, north-bluish clean and elegant Emacs theme.
--- init/org-mode/init-my-org-view.el
+++ init/org-mode/init-my-org-view.el
@@ -101,10 +101,6 @@
 
 ;;; [ org-beautify-theme ] -- A sub-theme to make org-mode more beautiful.
 
-;; (use-package org-beautify-theme
-;;   :ensure t
-;;   :config
-;;   (load-theme 'org-beautify))
 
 
 (provide 'init-my-org-view)
