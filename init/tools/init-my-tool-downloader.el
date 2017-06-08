c38ba6693d96b17eab8a1e665037d97696d74d98
stardiviner <numbchild@gmail.com>
2017-06-08 08:29:26 +0800

    add package aria2.

--- init/tools/init-my-tool-downloader.el
+++ init/tools/init-my-tool-downloader.el
@@ -7,6 +7,14 @@
 
 ;;; Code:
 
+;;; [ aria2 ] -- control `aria2c' commandline tool from Emacs.
+
+(use-package aria2
+  :ensure t
+  :config
+  (setq aria2-download-directory (expand-file-name "~/Downloads"))
+  )
+
 ;;; [ download-region ] -- simple in-buffer download manager for Emacs.
 
 (use-package download-region
