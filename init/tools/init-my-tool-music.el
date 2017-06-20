334b9658ba0c9ba3b7785bf318c26b88307079ca
stardiviner <numbchild@gmail.com>
2017-06-20 09:20:23 +0800

    refactor MPC.

--- init/tools/init-my-tool-music.el
+++ init/tools/init-my-tool-music.el
@@ -17,31 +17,35 @@
 
 (use-package mpc
   :ensure t
-  :defer t
-  :init
-  (advice-add 'mpc :after
-              (lambda ()
-                (switch-to-buffer-other-window "*MPC-Songs*")))
+  :config
+  (setq mpc-mpd-music-directory "~/Music/")
+  ;; Assign shortcut [Win+M] for Emacs MPC in KDE.
+  (setq mpc-frame-alist '((name . "MPC") (tool-bar-lines . 0)
+                          ;; (font . "WenQuanYi Micro Hei Mono")
+                          ;; (alist-get 'height (frame-parameters))
+                          ))
+  
+  (define-key mpc-mode-map (kbd "s") 'mpc-songs-search)
+  (define-key mpc-mode-map (kbd "L") 'mpc-songs-kill-search)
+  (define-key mpc-mode-map (kbd "p") 'mpc-toggle-play)
+  (define-key mpc-mode-map (kbd "M-y") 'mpc-toggle-single)
+  (define-key mpc-mode-map (kbd "M-r") 'mpc-toggle-repeat)
+  (define-key mpc-mode-map (kbd "M-s") 'mpc-toggle-shuffle)
 
-  (define-key my-music-prefix (kbd "M") 'mpc)
+  (define-key my-music-prefix (kbd "p") 'mpc-toggle-play)
   (define-key my-music-prefix (kbd "y") 'mpc-toggle-single)
   (define-key my-music-prefix (kbd "r") 'mpc-toggle-repeat)
   (define-key my-music-prefix (kbd "s") 'mpc-toggle-shuffle)
-  (define-key my-music-prefix (kbd "t") 'mpc-toggle-play)
-
-  :config
-  (defun my-mpc-songs-search ()
-    (interactive)
-    (call-interactively 'mpc-songs-search)
-    (switch-to-buffer-other-window "*MPC-Songs*"))
   
-  (define-key mpc-mode-map (kbd "l") 'my-mpc-songs-search)
-  (define-key mpc-mode-map (kbd "L") 'mpc-songs-kill-search)
+  (defun my/mpc ()
+    (interactive)
+    (select-frame (make-frame mpc-frame-alist))
+    (mpc))
 
-  (define-key mpc-mode-map (kbd "y") 'mpc-toggle-single)
-  (define-key mpc-mode-map (kbd "r") 'mpc-toggle-repeat)
-  (define-key mpc-mode-map (kbd "s") 'mpc-toggle-shuffle)
-  (define-key mpc-mode-map (kbd "t") 'mpc-toggle-play)
+  (define-key my-music-prefix (kbd "M") 'my/mpc)
+  
+  ;; auto start MPC frame.
+  ;; (funcall-interactively 'my/mpc)
   )
 
 ;;; [ dionysos ] -- A simple music player for Emacs by support backends like: MPD, VLC, MPlayer.
