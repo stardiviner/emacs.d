6d926bfc1806e3bd873c67157302292000183b38
stardiviner <numbchild@gmail.com>
2017-06-23 10:43:42 +0800

    add org-crypt for public key encryption for Org entries.

--- init/org-mode/init-my-org-extensions.el
+++ init/org-mode/init-my-org-extensions.el
@@ -30,6 +30,29 @@
 ;;   (add-hook 'org-mode-hook 'org-eww-mode)
 ;;   )
 
+;;; [ org-crypt ] -- public key encryption for Org entries
+
+(require 'org-crypt)
+
+;; You can change the tag to any complex tag matching string by
+;; setting the `org-crypt-tag-matcher' variable.
+(setq org-crypt-tag-matcher "encrypt")
+
+;;; add `org-crypt' required tag to default tag list.
+(setq org-tag-alist
+      (append '((:startgroup . nil) ("encrypt" . ?E) (:endgroup . nil))
+              org-tag-alist))
+
+;;; set your public keyring.
+(setq org-crypt-key "F09F650D7D674819892591401B5DF1C95AE89AC3")
+
+;;; To automatically encrypt all necessary entries when saving a file.
+;; (org-crypt-use-before-save-magic)
+
+;;; set keybindings for org-crypt functions.
+(define-key org-mode-map (kbd "C-c C-r") 'org-encrypt-entry)
+(define-key org-mode-map (kbd "C-c M-r") 'org-decrypt-entry)
+
 
 
 (provide 'init-my-org-extensions)
