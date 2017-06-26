2915fc41938a9da178e1b6b37f93d04299fc98c6
stardiviner <numbchild@gmail.com>
2017-06-27 07:26:40 +0800

    add package hierarchy.

--- init/init-my-library.el
+++ init/init-my-library.el
@@ -6,17 +6,23 @@
 
 
 ;;; Code:
-
+
 ;;; get the content of region.
 
 (defun region-content ()
   "Get the region content if region is active."
   (interactive)
-  (if (use-region-p) ; `region-active-p', `mark-active'
+  (if (use-region-p) ; `region-active-p'
       (buffer-substring-no-properties (region-beginning) (region-end))
     ))
 
 
+;;; [ hierarchy ] -- Emacs library to create, query, navigate and display hierarchy structures.
+
+(use-package hierarchy
+  :ensure t)
+
+
 
 (provide 'init-my-library)
 
