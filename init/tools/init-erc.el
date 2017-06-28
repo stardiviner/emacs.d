8c9df9ba1633aeffcf86ce3b91bd3bc63faf6f88
stardiviner <numbchild@gmail.com>
2017-06-28 22:22:21 +0800

    ERC auto ghost yourself (NickServ Identify) when bad network.

--- init/tools/init-erc.el
+++ init/tools/init-erc.el
@@ -38,6 +38,23 @@
         )
       )
 
+;;; auto ghost yourself.
+(defun erc-ghost-maybe (server nick)
+  "Send GHOST message to NickServ if NICK ends with `erc-nick-uniquifier'.
+The function is suitable for `erc-after-connect'."
+  (when (string-match (format "\\(.*?\\)%s+$" erc-nick-uniquifier) nick)
+    (let ((nick-orig (match-string 1 nick))
+          (password erc-session-password))
+      (when (y-or-n-p (format "Current nick is '%s'. Do you want to ghost?"
+                              nick))
+        (erc-message "PRIVMSG" (format "NickServ GHOST %s %s"
+				                               nick-orig password))
+	      (erc-cmd-NICK nick-orig)
+	      (erc-message "PRIVMSG" (format "NickServ identify %s %s"
+				                               nick-orig password))))))
+
+(add-hook 'erc-after-connect 'erc-ghost-maybe)
+
 ;; [ IRC Server ]
 (setq erc-server-auto-reconnect t)
 
