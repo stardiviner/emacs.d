d809a40c62fc251a502ca863c1a76c9218369332
stardiviner <numbchild@gmail.com>
2017-06-30 17:19:37 +0800

    disable org-mode disable [M-RET] split line.

--- init/org-mode/init-my-org-document-structure.el
+++ init/org-mode/init-my-org-document-structure.el
@@ -36,7 +36,7 @@
 
 ;; http://irreal.org/blog/?p=6297
 ;; https://www.reddit.com/r/emacs/comments/6iqtze/org_mreturn_annoyance/
-(setq org-M-RET-may-split-line '((default . nil)))
+;; (setq org-M-RET-may-split-line '((default . nil)))
 
 (setq org-columns-default-format
       "%25ITEM %TODO %3PRIORITY %TAGS %6effort(EFFORT){:}")
