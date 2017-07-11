efe204edae351584c927471b50d4a22cbaeda862
stardiviner <numbchild@gmail.com>
2017-07-11 17:59:14 +0800

    uppercase org-babel example markers.

--- init/org-mode/init-my-org-babel.el
+++ init/org-mode/init-my-org-babel.el
@@ -12,6 +12,9 @@
 (setq org-confirm-shell-link-function 'yes-or-no-p)
 (setq org-confirm-elisp-link-function 'yes-or-no-p)
 
+(setq org-babel-uppercase-example-markers t)
+(setq org-babel-hash-show-time t)
+
 ;;; source block header arguments
 (setq org-babel-default-header-args
       '((:session . "none")
