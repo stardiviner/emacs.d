d356929f438fe29448bded6561a44e8f625831f7
stardiviner <numbchild@gmail.com>
2017-07-27 11:22:21 +0800

    add package pyim.

--- init/Emacs/init-my-emacs-input-method.el
+++ init/Emacs/init-my-emacs-input-method.el
@@ -42,6 +42,44 @@
 ;;                       ("^\\alpha" ?ᵅ)
 ;;                       ))
 
+;;; [ pyim ] -- 一个 emacs 中文输入法，支持全拼，双拼和五笔.
+
+(use-package pyim
+  :ensure t
+  :config
+  (require 'pyim)
+  (require 'pyim-basedict) ; 拼音词库设置，五笔用户 *不需要* 此行设置
+  (pyim-basedict-enable)   ; 拼音词库，五笔用户 *不需要* 此行设置
+  (setq default-input-method "pyim")
+  (setq pyim-default-scheme 'quanpin)
+  ;; improve performance
+  (setq pyim-backends '(dcache-personal dcache-common pinyin-chars))
+  
+  (setq pyim-page-tooltip 'popup) ; 'popup, 'pos-tip,
+  ;; (setq pyim-page-tooltip 'pos-tip
+  ;;       x-gtk-use-system-tooltips t)
+  ;; (setq pyim-page-style 'one-line)
+  (setq pyim-page-length 5)
+
+  ;; (setq pyim-isearch-enable-pinyin-search t)
+
+  ;; 设置 pyim 探针设置，这是 pyim 高级功能设置，可以实现 *无痛* 中英文切换 :-)
+  ;; 我自己使用的中英文动态切换规则是：
+  ;; 1. 光标只有在注释里面时，才可以输入中文。
+  ;; 2. 光标前是汉字字符时，才能输入中文。
+  ;; 3. 使用 M-j 快捷键，强制将光标前的拼音字符串转换为中文。
+  (setq-default pyim-english-input-switch-functions
+                '(pyim-probe-dynamic-english
+                  pyim-probe-isearch-mode
+                  pyim-probe-program-mode
+                  pyim-probe-org-structure-template))
+
+  (setq-default pyim-punctuation-half-width-functions
+                '(pyim-probe-punctuation-line-beginning
+                  pyim-probe-punctuation-after-punctuation))
+  )
+
+
 
 (provide 'init-my-emacs-input-method)
 
