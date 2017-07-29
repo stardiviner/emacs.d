c33bfc07bffb484bb1ce845711dfe16fd5d8437e
stardiviner <numbchild@gmail.com>
2017-07-29 09:53:46 +0800

    update org-projectile config options.

--- init/org-mode/init-my-org-programming.el
+++ init/org-mode/init-my-org-programming.el
@@ -10,31 +10,15 @@
 
 (use-package org-projectile
   :ensure t
-  :bind (("C-c n p" . org-projectile:project-todo-completing-read))
+  :bind (("C-c n p" . org-projectile-project-todo-completing-read))
   :config
   ;; global projects todo file
-  (setq org-projectile:projects-file
+  (setq org-projectile-projects-file
         (concat org-directory "/Programming/Projects/"))
-  
-  ;; per-project todo files
-  (org-projectile:per-repo)
-  (setq org-projectile:per-repo-filename "TODO.org")
-
-  ;; prompt mode
-  (org-projectile:prompt)
-
   ;; add files to `org-agenda'.
-  (setq org-agenda-files (append org-agenda-files (org-projectile:todo-files)))
-
-  (setq org-capture-templates
-        (append '(("p" "[p]rogramming")
-                  ("pt" "[t]odo"
-                   ;; TODO
-                   ;; (org-projectile:project-todo-entry "pt")
-                   ))
-                org-capture-templates))
-  
-  ;; (add-to-list 'org-capture-templates (org-projectile:project-todo-entry "p"))
+  (setq org-agenda-files (append org-agenda-files (org-projectile-todo-files)))
+  ;; add org-mode capture templates.
+  (add-to-list 'org-capture-templates (org-projectile-project-todo-entry))
   )
 
 
