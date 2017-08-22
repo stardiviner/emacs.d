c4725a85883460ca7dc45ce381da6b652b8cae35
stardiviner <numbchild@gmail.com>
2017-08-22 15:31:32 +0800

    refactor Helm and switch helm-grep-ag to "ripgrep(rg)".

--- init/Emacs/init-helm.el
+++ init/Emacs/init-helm.el
@@ -6,30 +6,26 @@
 
 
 ;;; Code:
-
+
 ;;; [ Helm appearance ]
 
 (use-package helm
   :ensure t
-  :diminish helm-mode
-  :bind (
-         ;; ("M-x" . helm-M-x)
-         ("M-y" . helm-show-kill-ring)
-         ("C-x f" . helm-for-files)
-         ("C-x c /" . helm-find)
-         ("C-x c c" . helm-colors)
-         ("C-x c C-r" . helm-grep-it)
-         :map helm-map
-         ("<tab>" . helm-selection-action)
-         ("C-z" . helm-selection-action)
-         ("C-i" . helm-execute-persistent-action) ; make TAB works in terminal.
-         ("C-j" . helm-execute-persistent-action)
-         ("<RET>" . helm-maybe-exit-minibuffer)
-         ;; NOTE: this cause helm-dash open menu candidate error.
-         ;; ("<return>" . helm-confirm-and-exit-minibuffer)
-         )
+  ;; :diminish helm-mode
+  ;; :bind (
+  ;;        ("M-x" . helm-M-x)
+  ;;        ("M-y" . helm-show-kill-ring)
+  ;;        ("C-x f" . helm-for-files)
+  ;;        ("C-x c /" . helm-find)
+  ;;        ("C-x c c" . helm-colors)
+  ;;        ("C-x c C-r" . helm-grep-it)
+  ;;        )
   :config
+  (require 'helm)
   (require 'helm-config)
+
+  ;; (helm-mode 1)
+  ;; (helm-mode -1)
   
   (helm-autoresize-mode t)
   (setq helm-full-frame nil
@@ -54,11 +50,26 @@
         ;; helm-sources-using-default-as-input
         )
 
-  ;; (helm-mode 1)
+  ;; Grep (grep, ag, pt, rg)
+  (require 'helm-grep)
+  (setq helm-grep-ag-command
+        "rg --smart-case --no-heading --line-number %s %s %s")
+  ;; (setq helm-grep-ag-pipe-cmd-switches '(" --color-always --colors 'match:fg:black'" "--colors 'match:bg:yellow'"))
+  ;; (setq helm-grep-ag-command
+  ;;       "rg --color=always --colors 'match:fg:black' --colors 'match:bg:yellow' --smart-case --no-heading --line-number %s %s %s")
+  ;; [ wgrep ]
+  (define-key helm-grep-mode-map (kbd "C-c C-p") 'wgrep-change-to-wgrep-mode)
+
+  ;; Helm internal keybindings
+  ;; (define-key helm-map ("<tab>") . helm-selection-action)
+  ;; (define-key helm-map ("C-j") . helm-execute-persistent-action)
+  ;; (define-key helm-map ("<RET>") . helm-maybe-exit-minibuffer)
+  ;; NOTE: this cause helm-dash open menu candidate error.
+  ;; (define-key helm-map ("<return>") . helm-confirm-and-exit-minibuffer)
   )
 
 
-
+
 (provide 'init-helm)
 
 ;;; init-helm.el ends here
