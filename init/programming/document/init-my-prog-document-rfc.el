0961dfa67810d422faa5cb255a62f6eb2e0bf44f
stardiviner <numbchild@gmail.com>
2017-10-08 11:35:48 +0800

    refactor irfc package configuration.

--- init/programming/document/init-my-prog-document-rfc.el
+++ init/programming/document/init-my-prog-document-rfc.el
@@ -6,16 +6,16 @@
 
 
 ;;; Code:
-
+
 ;;; [ irfc ]
 
 (use-package irfc
   :ensure t
-  :defer t
-  :init
-  (define-key my-prog-help-document-map (kbd "r") 'irfc-visit)
+  :commands (irfc-visit)
+  :bind (:map my-prog-help-document-map
+              ("r" . irfc-visit))
   :config
-  (setq irfc-directory (concat user-emacs-directory "documentations/RFC")) ; set RFC database storage.
+  (setq irfc-directory (concat user-emacs-directory "documentations/RFC"))
   (setq irfc-assoc-mode t)        ; RFC documents are associated with `irfc-mode'.
 
   (setq irfc-highlight-requirement-keywords t
@@ -49,7 +49,7 @@
   ;; (define-key rfcview-mode-map (kbd "d") 'rfcview-imenu-index-function)
   )
 
-
+
 (provide 'init-my-prog-document-rfc)
 
 ;;; init-my-prog-document-rfc.el ends here
