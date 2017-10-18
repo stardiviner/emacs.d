eed9af756f754b73a47e58715132f6971e8defc5
stardiviner <numbchild@gmail.com>
2017-10-18 12:39:55 +0800

    remove package wolfram-mode.

--- init/Science/Math/init-my-math-wolfram.el
+++ init/Science/Math/init-my-math-wolfram.el
@@ -6,20 +6,7 @@
 
 
 ;;; Code:
-;;; ----------------------------------------------------------------------------
-;;; [ wolfram-mode ] -- Mathematica editing and inferior mode.
-
-(use-package wolfram-mode
-  :ensure t
-  :defer t
-  :mode ("\\.m$" . wolfram-mode)
-  :config
-  (setq wolfram-program "tungsten"
-        ;; wolfram-program-arguments
-        wolfram-indent 4
-        )
-  )
-
+
 ;;; [ wolfram.el ] -- Wolfram Alpha integration by showing result with Org-mode.
 
 (use-package wolfram
@@ -31,8 +18,7 @@
   (setq wolfram-alpha-app-id "YX2WUR-2J7GPTXY44")
   )
 
-;;; ----------------------------------------------------------------------------
-
+
 (provide 'init-my-math-wolfram)
 
 ;;; init-my-math-wolfram.el ends here
