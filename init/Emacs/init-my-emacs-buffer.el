755dd51bdafbc3ab62dc389d4ca3d22037d98170
stardiviner <numbchild@gmail.com>
2017-11-06 19:20:06 +0800

    improve recentf and ibuffer config.

--- init/Emacs/init-my-emacs-buffer.el
+++ init/Emacs/init-my-emacs-buffer.el
@@ -5,45 +5,22 @@
 
 
 ;;; Code:
+
+;;; [ auto-revert-mode ] -- auto-reload external file changes.
 
-;;; Auto-reload external file changes
-
-;; (global-auto-revert-mode)
 (setq global-auto-revert-non-file-buffers nil
       auto-revert-verbose nil
       auto-revert-use-notify t)
 
+;; (global-auto-revert-mode)
 
 ;;; [ ibuffer ]
 
-(global-set-key (kbd "C-x C-b") 'ibuffer)
-
-;;; [ ibuffer-vc ]
-
-;; - Group your buffers by their parent vc root directory
-;; - See the VC status of the associated files
-;; - Sort buffers by their VC status
-
-;;; Usage:
-;;
-;; - [M-x ibuffer-vc]
-
-;; (require 'ibuffer-vc)
-
-
-;;; [ ibuffer-git ]
-
-
-;;; [ ibuffer-tramp ]
-
-
-;;; [ ibuffer-projectile ]
-
-
-
-
-(setq buffers-menu-max-size 30)
-
+(use-package ibuffer
+  :bind ("C-x C-b" . ibuffer)
+  :config
+  (setq buffers-menu-max-size 30)
+  )
 
 ;;; [uniquify] -- meaningful names for buffers with the same name
 
@@ -55,8 +32,7 @@
       uniquify-ignore-buffers-re "^\\*" ; don't muck with special buffers
       )
 
-
-;;; [ recentf ]
+;;; [ recentf ] -- setup a menu of recently opened files.
 
 (require 'recentf)
 
@@ -64,14 +40,10 @@
 
 (setq recentf-save-file (expand-file-name ".temp/recentf" user-emacs-directory)
       recentf-exclude '("/tmp/" "/ssh:")
-      recentf-max-menu-items 25
-      recentf-max-saved-items 100
-      recentf-auto-cleanup 'mode ; 'mode, 'never.
-      recentf-case-fold-search t
+      ;; recentf-auto-cleanup 'mode ; 'mode, 'never.
       )
 
-
-
+
 (provide 'init-my-emacs-buffer)
 
 ;;; init-my-emacs-buffer.el ends here
