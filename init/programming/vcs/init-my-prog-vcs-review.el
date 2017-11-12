b61217a5ce9c82d55ea77356fc383941f2c7fb5a
stardiviner <numbchild@gmail.com>
2017-11-12 12:22:42 +0800

    delay magit-gerrit.

--- init/programming/vcs/init-my-prog-vcs-review.el
+++ init/programming/vcs/init-my-prog-vcs-review.el
@@ -6,12 +6,13 @@
 
 
 ;;; Code:
-;;; ----------------------------------------------------------------------------
+
 ;;; [ magit-gerrit ] -- Magit plugin for Gerrit Code Review
 
 (use-package magit-gerrit
   :ensure t
-  :config
+  :defer t
+  ;; :config
   ;; if remote url is not using the default gerrit port and
   ;; ssh scheme, need to manually set this variable
   ;; (setq-default magit-gerrit-ssh-creds "myid@gerrithost.org")
@@ -20,8 +21,7 @@
   ;; (setq-default magit-gerrit-remote "gerrit")
   )
 
-;;; ----------------------------------------------------------------------------
-
+
 (provide 'init-my-prog-vcs-review)
 
 ;;; init-my-prog-vcs-review.el ends here
