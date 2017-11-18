f36fdd20b37c57fa04e129631f30480a44f4e785
stardiviner <numbchild@gmail.com>
2017-11-18 09:39:15 +0800

    add an init file for password manager.

---
+++ init/tools/init-my-tool-password-manager.el
@@ -0,0 +1,18 @@
+;;; init-my-tool-password-manager.el --- init for password manager.
+
+;;; Commentary:
+
+
+
+;;; Code:
+
+;;; [ pass ] -- packages around command line utility "pass".
+
+(require 'init-pass)
+
+
+
+
+(provide 'init-my-tool-password-manager)
+
+;;; init-my-tool-password-manager.el ends here
