478192237ae2b82533e35cdd621269a9c28a4575
stardiviner <numbchild@gmail.com>
2017-12-05 13:18:12 +0800

    add helper functions for insert running Docker containers name
    for Org-Babel docker-tramp :dir header argument.

--- init/programming/tools/DevOps/container/init-docker.el
+++ init/programming/tools/DevOps/container/init-docker.el
@@ -16,9 +16,18 @@
               ("i" . docker-images)
               ("v" . docker-volumes)
               ("n" . docker-networks))
+  :init
+  (require 'docker-process)
+  (setq docker-keymap-prefix "C-c t c")
   :config
   (setq docker-containers-show-all t)
   (docker-global-mode 1) ; enable global docker minor mode
+
+  (autoload 'docker-read-container-name "docker-containers.el")
+  (defun docker-insert-container (container-name)
+    "A helper function to insert container ID or name."
+    (interactive (list (docker-read-container-name "Docker container name: ")))
+    (insert container-name))
   )
 
 ;;; [ dockerfile-mode ] -- Dockerfile
@@ -39,6 +48,20 @@
   (setq docker-tramp-use-names t
         ;; docker-tramp-docker-options nil
         )
+
+  (defun docker-tramp-insert-running-container (container)
+    "A helper function to insert the running `CONTAINER' name.
+For Org-babel header argument :dir /docker:<name>:."
+    (interactive (let ((containers-name (mapcar 'cdr (docker-tramp--running-containers)))
+                       (containers-id (docker-tramp--running-containers)))
+                   ;; `docker-tramp--running-containers' return a list of the form (ID NAME):
+                   ;; '(("24dbb476cdd8"  "container_name")
+                   ;;   ("bbd4f62e9f1d"  "other_name"))
+                   ;; I think completing-read will recognize it as collection,
+                   ;; but if you want to use the container names, you need to use:
+                   ;; (mapcar 'cdr (docker-tramp--running-containers))
+                   (list (completing-read "Docker container name: " containers-name))))
+    (insert container))
   )
 
 ;;; [ docker-api ] -- Emacs interface to the Docker API.
