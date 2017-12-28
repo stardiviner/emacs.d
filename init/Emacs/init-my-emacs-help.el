a3b069375d87e8d8fdca25746ac4060661215ed1
stardiviner <numbchild@gmail.com>
2017-12-28 13:05:42 +0800

    migrate Help and Info configs.

--- init/Emacs/init-my-emacs-help.el
+++ init/Emacs/init-my-emacs-help.el
@@ -7,26 +7,27 @@
 
 ;;; Code:
 
-;; (setq text-quoting-style nil)
-
 ;;; [ Info ]
 
-;; (require 'info)
-
-(add-to-list 'Info-directory-list
-             (expand-file-name "info/" user-emacs-directory))
-;; - `Info-default-directory-list'
-
-;; let `[s]' search like =isearch= for incremental search.
-(setq Info-isearch-search t)
-
-;; - `Info-mode-hook'
+(use-package info
+  :bind ("C-h i" . info-display-manual)
+  :init
+  ;; - `Info-default-directory-list'
+  (add-to-list 'Info-directory-list
+               (expand-file-name "info/" user-emacs-directory))
+  (add-to-list 'display-buffer-alist
+               '("\\*info\\*" . (display-buffer-below-selected)))
+  ;; let `[s]' search like =isearch= for incremental search.
+  (setq Info-isearch-search t)
+  ;; - `Info-mode-hook'
+  )
 
 
 ;;; [ info+ ]
 
 (use-package info+
   :ensure t
+  :defer t
   :config
   (setq Info-breadcrumbs-in-header-flag t
         ;; Info-display-node-header-fn
@@ -35,9 +36,38 @@
         Info-fontify-quotations-flag t
         Info-fontify-reference-items-flag t
         Info-fontify-single-quote-flag t
-        Info-saved-nodes t
-        )
-  )
+        Info-saved-nodes t))
+
+
+;; (use-package niceify-info
+;;   :ensure t
+;;   :init
+;;   (add-hook 'Info-selection-hook #'niceify-info))
+
+;;; [ sicp ] -- Structure and Interpretation of Computer Programs in info format.
+
+(use-package sicp
+  :ensure t)
+
+
+(add-to-list 'display-buffer-alist
+             '("^\\*Help\\*$" (display-buffer-below-selected)))
+
+;;; [ helpful ] -- A better Emacs *help* buffer.
+
+;; (use-package helpful
+;;   :ensure t
+;;   :config
+;;   ;; replace Emacs default keybindings.
+;;   (global-set-key (kbd "C-h f") #'helpful-callable)
+;;   (global-set-key (kbd "C-h v") #'helpful-variable)
+;;   (global-set-key (kbd "C-h k") #'helpful-key)
+;;   ;; convenient keybinding:
+;;   (global-set-key (kbd "C-h C-.") #'helpful-at-point)
+;;  
+;;   (add-to-list 'display-buffer-alist
+;;                '("^\\*helpful.*\\*$" (display-buffer-below-selected)))
+;;   )
 
 
 (provide 'init-my-emacs-help)
