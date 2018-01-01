dad2d35a1530adf4b735499c30f17d2580464011
stardiviner <numbchild@gmail.com>
2018-01-01 12:38:39 +0800

    add package auto-sudoedit.

--- init/Emacs/init-my-emacs-edit-sudo.el
+++ init/Emacs/init-my-emacs-edit-sudo.el
@@ -14,6 +14,13 @@
   :bind ("C-x C-r" . sudo-edit)
   )
 
+;;; [ auto-sudoedit ] -- automatic do sudo by tramp when need root file.
+
+(use-package auto-sudoedit
+  :ensure t
+  :config
+  (auto-sudoedit-mode 1))
+
 
 (provide 'init-my-emacs-edit-sudo)
 
