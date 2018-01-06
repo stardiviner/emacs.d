72657a76862453f57d5e1fbe36669dbc56f70d2e
stardiviner <numbchild@gmail.com>
2018-01-06 19:55:11 +0800

    set default Org-mode inline image background color based on color-theme background color.
    Improve by using an advice.

--- init/org-mode/init-my-org-image.el
+++ init/org-mode/init-my-org-image.el
@@ -23,123 +23,20 @@
   )
 
 
-;;; set default Org-mode inline image background color.
-(defcustom org-inline-image-background nil
-  "The color used as the default background for inline images.
-When nil, use the default face background."
-  :group 'org
-  :type '(choice color (const nil)))
+;;; set default Org-mode inline image background color based on color-theme background color.
+(defun org-display-inline-images--with-color-theme-background-color (args)
+  "Specify background color of Org-mode inline image through modify `ARGS'."
+  (let* ((file (car args))
+         (type (cadr args))
+         (data-p (caddr args))
+         (props (cdddr args)))
+    ;; get this return result style from `create-image'
+    (append (list file type data-p)
+            (list :background (face-background 'default))
+            props)))
 
-;;; set default background color based on different stategies.
-(defun my-org-mode-image-set-face (&args)
-  (setq org-inline-image-background
-        ;; a static color.
-        ;; "#FFFFFF"
-        ;; same color with color-theme background.
-        (face-background 'default)
-        ;; slight darker than color-theme.
-        ;; (cl-case (alist-get 'background-mode (frame-parameters))
-        ;;   ('light
-        ;;    (color-darken-name (face-background 'default) 10))
-        ;;   ('dark
-        ;;    (color-darken-name (face-background 'default) 2)))
-        ))
-
-(add-hook 'circadian-after-load-theme-hook #'my-org-mode-image-set-face)
-
-(defun org-display-inline-images (&optional include-linked refresh beg end)
-  "Modified version of original `org-display-inline-images'.
-Append `:background' in `create-image'."
-  (interactive "P")
-  (when (display-graphic-p)
-    (unless refresh
-      (org-remove-inline-images)
-      (when (fboundp 'clear-image-cache) (clear-image-cache)))
-    (org-with-wide-buffer
-     (goto-char (or beg (point-min)))
-     (let ((case-fold-search t)
-           (file-extension-re (image-file-name-regexp)))
-       (while (re-search-forward "[][]\\[\\(?:file\\|[./~]\\)" end t)
-         (let ((link (save-match-data (org-element-context))))
-           ;; Check if we're at an inline image.
-           (when (and (equal (org-element-property :type link) "file")
-                      (or include-linked
-                          (not (org-element-property :contents-begin link)))
-                      (let ((parent (org-element-property :parent link)))
-                        (or (not (eq (org-element-type parent) 'link))
-                            (not (cdr (org-element-contents parent)))))
-                      (org-string-match-p file-extension-re
-                                          (org-element-property :path link)))
-             (let ((file (expand-file-name
-                          (org-link-unescape
-                           (org-element-property :path link)))))
-               (when (file-exists-p file)
-                 (let ((width
-                        ;; Apply `org-image-actual-width' specifications.
-                        (cond
-                         ((not (image-type-available-p 'imagemagick)) nil)
-                         ((eq org-image-actual-width t) nil)
-                         ((listp org-image-actual-width)
-                          (or
-                           ;; First try to find a width among
-                           ;; attributes associated to the paragraph
-                           ;; containing link.
-                           (let ((paragraph
-                                  (let ((e link))
-                                    (while (and (setq e (org-element-property
-                                                         :parent e))
-                                                (not (eq (org-element-type e)
-                                                         'paragraph))))
-                                    e)))
-                             (when paragraph
-                               (save-excursion
-                                 (goto-char (org-element-property :begin paragraph))
-                                 (when
-                                     (re-search-forward
-                                      "^[ \t]*#\\+attr_.*?: +.*?:width +\\(\\S-+\\)"
-                                      (org-element-property
-                                       :post-affiliated paragraph)
-                                      t)
-                                   (string-to-number (match-string 1))))))
-                           ;; Otherwise, fall-back to provided number.
-                           (car org-image-actual-width)))
-                         ((numberp org-image-actual-width)
-                          org-image-actual-width)))
-                       (old (get-char-property-and-overlay
-                             (org-element-property :begin link)
-                             'org-image-overlay)))
-                   (if (and (car-safe old) refresh)
-                       (image-refresh (overlay-get (cdr old) 'display))
-                     (let ((image (create-image file
-                                                (and width 'imagemagick)
-                                                nil
-                                                :width width
-                                                :background org-inline-image-background)))
-                       (when image
-                         (let* ((link
-                                 ;; If inline image is the description
-                                 ;; of another link, be sure to
-                                 ;; consider the latter as the one to
-                                 ;; apply the overlay on.
-                                 (let ((parent
-                                        (org-element-property :parent link)))
-                                   (if (eq (org-element-type parent) 'link)
-                                       parent
-                                     link)))
-                                (ov (make-overlay
-                                     (org-element-property :begin link)
-                                     (progn
-                                       (goto-char
-                                        (org-element-property :end link))
-                                       (skip-chars-backward " \t")
-                                       (point)))))
-                           (overlay-put ov 'display image)
-                           (overlay-put ov 'face 'default)
-                           (overlay-put ov 'org-image-overlay t)
-                           (overlay-put
-                            ov 'modification-hooks
-                            (list 'org-display-inline-remove-overlay))
-                           (push ov org-inline-image-overlays)))))))))))))))
+(advice-add 'create-image :filter-args
+            #'org-display-inline-images--with-color-theme-background-color)
 
 
 ;;; A workflow to quickly insert inline images to Org-mode.
