fa88e9f9013760a94e1595e5536cb879dc5e2707
stardiviner <numbchild@gmail.com>
2018-01-07 16:28:38 +0800

    disable ripgrep.

--- init/Emacs/init-my-emacs-search.el
+++ init/Emacs/init-my-emacs-search.el
@@ -17,12 +17,12 @@
 (require 'init-my-emacs-search-wgrep)
 (require 'init-my-emacs-search-ag)
 ;; (require 'init-my-emacs-search-pt)
-(require 'init-my-emacs-search-ripgrep)
+;; (require 'init-my-emacs-search-ripgrep)
 (require 'init-my-emacs-search-finder)
 (require 'init-my-emacs-search-engine)
 
 (with-eval-after-load 'counsel
-  (define-key search-prefix (kbd "s") 'counsel-rg))
+  (define-key search-prefix (kbd "s") 'counsel-ag))
 
 
 (provide 'init-my-emacs-search)
