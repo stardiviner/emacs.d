8db2d32c23f67d162ceeef38bb23e41211e466e5
stardiviner <numbchild@gmail.com>
2018-01-08 13:20:41 +0800

    add package ghub and github-browse-file.

--- init/programming/vcs/init-my-prog-vcs-git-github.el
+++ init/programming/vcs/init-my-prog-vcs-git-github.el
@@ -7,6 +7,14 @@
 
 ;;; Code:
 
+;;; [ ghub ] -- minuscule client library for the Github API.
+
+(use-package ghub
+  :ensure t
+  ;; :config
+  ;; (setq ghub-github-token-scopes)
+  )
+
 ;;; [ magithub ] -- Magit interfaces for GitHub.
 
 (use-package magithub
@@ -14,6 +22,18 @@
   :after magit
   :config
   (magithub-feature-autoinject t))
+
+;;; [ github-browse-file ] -- View the file you're editing in Emacs on GitHub.
+
+(use-package github-browse-file
+  :ensure t
+  :init
+  (unless (boundp 'git-gutter-prefix)
+    (define-prefix-command 'git-gutter-prefix))
+  :bind (:map git-gutter-prefix
+              ("f" . github-browse-file)
+              ("F" . github-browse-file-blame)
+              ("M-c" . github-browse-commit)))
 
 
 
