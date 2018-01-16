1628133ff0164116450bbf0b924f0a20f7bd8245
stardiviner <numbchild@gmail.com>
2018-01-16 12:30:45 +0800

    add config for view (view-mode).

--- init/Linux/init-log-tools.el
+++ init/Linux/init-log-tools.el
@@ -11,6 +11,29 @@
 (use-package logview
   :ensure t)
 
+;;; [ view-mode ]
+
+(use-package view
+  :config
+  (defun View-goto-line-last (&optional line)
+    "goto last line"
+    (interactive "P")
+    (goto-line (line-number-at-pos (point-max))))
+
+  (define-key view-mode-map (kbd "e") 'View-scroll-half-page-forward)
+  (define-key view-mode-map (kbd "u") 'View-scroll-half-page-backward)
+
+  ;; less like
+  (define-key view-mode-map (kbd "N") 'View-search-last-regexp-backward)
+  (define-key view-mode-map (kbd "?") 'View-search-regexp-backward?)
+  (define-key view-mode-map (kbd "g") 'View-goto-line)
+  (define-key view-mode-map (kbd "G") 'View-goto-line-last)
+  ;; Vim like
+  (define-key view-mode-map (kbd "h") 'backward-char)
+  (define-key view-mode-map (kbd "j") 'next-line)
+  (define-key view-mode-map (kbd "k") 'previous-line)
+  (define-key view-mode-map (kbd "l") 'forward-char))
+
 ;;; [ lognav-mode ] -- Navigate Log Error Messages with [M-n/p].
 
 (use-package lognav-mode
