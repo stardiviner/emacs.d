6b4fe944aaa87de54010ced083d8bc17ccf6ac9e
stardiviner <numbchild@gmail.com>
2018-01-24 10:46:32 +0800

    [color-theme] improve my color-themes config.

--- init/Emacs/init-my-emacs-color-theme.el
+++ init/Emacs/init-my-emacs-color-theme.el
@@ -15,6 +15,8 @@
 
 (global-font-lock-mode t)
 
+;;; [ Custom Themes ]
+
 ;;; [ Color Themes ]
 
 (use-package color-theme
@@ -40,9 +42,7 @@
 ;;               (color-theme-buffer-local 'color-theme-standard (current-buffer))))
 ;;   )
 
-;;; [ Custom Themes ]
-
-(add-to-list 'custom-theme-load-path "~/.emacs.d/color-themes/")
+;; (add-to-list 'custom-theme-load-path "~/.emacs.d/color-themes/")
 
 ;;; [ leuven-theme ] -- Awesome Emacs color theme on white background.
 
@@ -53,31 +53,6 @@
   (setq leuven-scale-outline-headlines t)
   ;; (load-theme 'leuven t)
   )
-
-;;; [ nord-theme ] -- An arctic, north-bluish clean and elegant Emacs theme.
-
-;; (use-package nord-theme
-;;   :ensure t
-;;   :config
-;;   (load-theme 'nord t)
-;;   )
-
-;;; [ doom-themes ] -- Emacs themes inspired by Atom One
-
-;; (use-package doom-themes
-;;   :ensure t
-;;   :config
-;;   (setq doom-enable-bold t
-;;         doom-enable-italic t)
-;;   (load-theme 'doom-one t)
-;;   )
-
-;;; [ gruvbox-theme ] -- Gruvbox is a retro groove color scheme for Emacs.
-
-;; (use-package gruvbox-theme
-;;   :ensure t
-;;   :config
-;;   (load-theme 'gruvbox t))
 
 ;;; [ spacemacs-theme ] -- Spacemacs default color-theme.
 
@@ -93,8 +68,7 @@
 ;; (use-package kaolin-themes
 ;;   :ensure t
 ;;   :config
-;;   (load-theme 'kaolin-dark t)
-;;   )
+;;   (load-theme 'kaolin-dark t))
 
 ;; (set-face-attribute 'underline nil
 ;;                     :underline (cl-case (alist-get 'background-mode (frame-parameters))
@@ -120,25 +94,10 @@
 ;; built-in function.
 (set-face-attribute 'font-lock-builtin-face nil
                     :slant 'italic)
-
 (set-face-attribute 'font-lock-function-name-face nil
-                    :background (color-lighten-name (face-background 'default) 2)
-                    )
+                    :background (color-lighten-name (face-background 'default) 2))
 (set-face-attribute 'font-lock-type-face nil
-                    :background (color-lighten-name (face-background 'default) 3)
-                    )
-
-;;; apply color-theme on new frame to fix issue.
-;; (defun my-new-frame-apply-things (arg)
-;;   (load-theme 'leuven t))
-;;
-;; (add-hook 'after-make-frame-functions #'my-new-frame-apply-things)
-
-;;; [ select-themes ] -- select color-theme with completing-read.
-
-;; (use-package select-themes
-;;   :ensure t
-;;   :defer t)
+                    :background (color-lighten-name (face-background 'default) 3))
 
 ;;; [ circadian ] -- theme-switching for Emacs based on daytime.
 
@@ -148,14 +107,10 @@
   (setq calendar-location-name "Shaoxing Town"
         calendar-time-zone +480
         calendar-latitude 29.72
-        calendar-longitude 120.20
-        )
-
-  (setq circadian-themes '((:sunrise . leuven) ; spacemacs-light
-                           (:sunset . spacemacs-dark)
-                           ))
-  (circadian-setup)
-  )
+        calendar-longitude 120.20)
+  (setq circadian-themes '((:sunrise . leuven)
+                           (:sunset . spacemacs-dark)))
+  (circadian-setup))
 
 
 (provide 'init-my-emacs-color-theme)
