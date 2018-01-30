3644192bf07b628f34b925a45ad3a8c47519cb97
stardiviner <numbchild@gmail.com>
2018-01-30 14:48:06 +0800

    [ebook] enable nov-mode and refactor ereader-mode.

--- init/Emacs/init-my-emacs-ebook.el
+++ init/Emacs/init-my-emacs-ebook.el
@@ -12,17 +12,19 @@
 (use-package ereader
   :ensure t
   :load (org-ebook)
+  :mode (("\\.epub\\'" . ereader-mode))
   :config
-  (push '("\\.epub\\'" . ereader-mode) auto-mode-alist)
-  (add-to-list 'org-file-apps '("\\.epub\\'" . auto-mode))
-  )
+  (with-eval-after-load "org"
+    (add-to-list 'org-file-apps '("\\.epub\\'" . auto-mode))))
 
 ;;; [ nov ] -- featureful EPUB reader mode.
 
-;; (use-package nov
-;;   :ensure t
-;;   :mode ("\\.epub\\'" . nov-mode)
-;;   )
+(use-package nov
+  :ensure t
+  :mode ("\\.epub\\'" . nov-mode)
+  :config
+  (with-eval-after-load "org"
+    (add-to-list 'org-file-apps '("\\.epub\\'" . auto-mode))))
 
 
 (provide 'init-my-emacs-ebook)
