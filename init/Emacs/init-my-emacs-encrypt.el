0ae73fbba2b1b6811d1f1a9de022dd0b3cc99265
stardiviner <numbchild@gmail.com>
2018-02-04 12:08:35 +0800

    update Emacs encrypt init file
    - fix encrypt-prefix conflict with emms-prefix.
    - update configs for use-package style.
    - fix inline-crypt-prefix name.

--- init/Emacs/init-my-emacs-encrypt.el
+++ init/Emacs/init-my-emacs-encrypt.el
@@ -9,7 +9,7 @@
   (define-prefix-command 'tools-prefix))
 (unless (boundp 'encrypt-prefix)
   (define-prefix-command 'encrypt-prefix))
-(define-key tools-prefix (kbd "e") 'encrypt-prefix)
+(define-key tools-prefix (kbd "M-e") 'encrypt-prefix)
 
 ;;; [ password ]
 
@@ -33,16 +33,15 @@
 
 (use-package epa
   :ensure t
+  :load (epa-file)
   :init
-  (require 'epa-file)
   ;; force Emacs to use its own internal password prompt instead of an external
   ;; pin entry program.
   (setenv "GPG_AGENT_INFO" nil)
-  (epa-file-enable)
-  :config
   ;; let EasyPG Assistant to use loopback for pinentry.
   (setq epa-pinentry-mode 'loopback)
-  
+  (epa-file-enable)
+  :config
   (setq epa-file-encrypt-to "stardiviner")
   (setq epa-file-select-keys (case epa-file-encrypt-to
                                ('nil t)
@@ -58,21 +57,6 @@
                '("^\\*Keys\\*" (display-buffer-below-selected)))
   )
 
-;;; FAQ
-;; How to use a non-graphical password prompt for gpg?
-;;
-;; In X, Emacs 23 seems to pop up a graphical window to ask you the
-;; keyphrase. How to let it ask the password in minibuffer?
-;;
-;; I found the cause for it. The graphical window appears if you run gpg with
-;; the --use-agent option. EasyPG adds it (see epg.el) if it sees an environment
-;; variable like this one:
-;; GPG_AGENT_INFO=/tmp/seahorse-nDQm50/S.gpg-agent:6321:1 (check that with the
-;; env command). And you have this variable if for instance you have the program
-;; seahorse installed and running (which is the case in Ubuntu). If you
-;; uninstall Seahorse, the prompt will always be text instead of graphical. You
-;; may have to relogin to X to force Seahorse to close.
-
 ;;; [ inline-crypt ] -- simple inline encryption via openssl.
 
 (use-package inline-crypt
@@ -81,17 +65,17 @@
              inline-crypt-decrypt-region
              inline-crypt-encrypt-string
              inline-crypt-decrypt-string)
-  :config
-  ;; (setq inline-crypt-openssl-command "openssl")
-
-  (unless (boundp 'inline-crypt)
+  :preface
+  (unless (boundp 'inline-crypt-prefix)
     (define-prefix-command 'inline-crypt-prefix))
   (define-key encrypt-prefix (kbd "i") 'inline-crypt-prefix)
-
-  (define-key inline-crypt-prefix (kbd "r") 'inline-crypt-encrypt-region)
-  (define-key inline-crypt-prefix (kbd "C-r") 'inline-crypt-decrypt-region)
-  (define-key inline-crypt-prefix (kbd "s") 'inline-crypt-encrypt-string)
-  (define-key inline-crypt-prefix (kbd "C-s") 'inline-crypt-decrypt-string)
+  :bind (:map inline-crypt-prefix
+              ("r" . inline-crypt-encrypt-region)
+              ("C-r" . inline-crypt-decrypt-region)
+              ("s" . inline-crypt-encrypt-string)
+              ("C-s" . inline-crypt-decrypt-string))
+  :config
+  ;; (setq inline-crypt-openssl-command "openssl")
   )
 
 ;;; [ letterbox-mode ] -- a simple minor mode to add letterboxing to sensitive text.
