f8db761c04eb4300a9375746a694f1c76c3c4000
stardiviner <numbchild@gmail.com>
2018-02-05 13:32:05 +0800

    [Solidity] add packages solidity-mode, company-solidity for Solidity.

--- init/programming/frameworks/Ethereum/init-my-prog-framework-ethereum.el
+++ init/programming/frameworks/Ethereum/init-my-prog-framework-ethereum.el
@@ -6,9 +6,20 @@
 
 ;;; Code:
 
+;;; [ solidity-mode ]
 
+(use-package solidity-mode
+  :ensure t)
 
+;;; [ company-solidity ]
 
+(use-package company-solidity
+  :ensure t
+  :config
+  (defun my-company-solidity-setup ()
+    (my-company-add-backend-locally 'company-solidity))
+  (add-hook 'solidity-mode-hook #'my-company-solidity-setup)
+  )
 
 
 
