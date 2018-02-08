b9e16c8b1b1f4714f3cec075fe9ca9877d3f2a66
stardiviner <numbchild@gmail.com>
2018-02-08 23:18:26 +0800

    [Arduino] switch arduino-mode and company-arduino to my fork.

--- init/programming/frameworks/Arduino/init-my-prog-framework-arduino.el
+++ init/programming/frameworks/Arduino/init-my-prog-framework-arduino.el
@@ -10,23 +10,10 @@
 ;;; [ arduino-mode ]
 
 (use-package arduino-mode
-  ;; :ensure t
   :ensure-system-package arduino
-  ;; TODO: switch to official package after the maintainer accepted new PRs.
-  ;; https://github.com/bookest/arduino-mode/network
-  :preface
-  ;; fix bug (void-function ede-project-autoload)
-  (require 'ede)
-  (defvar ede-arduino-preferences-file (expand-file-name "~/.arduino/preferences.txt"))
-  
-  :defines ede-arduino-preferences-file
-  :quelpa (arduino-mode
-           :fetcher github
-           :repo "bwachter/arduino-mode"
-           :upgrade t) ; "bwachter/arduino-mode", "mavit/arduino-mode"
-  :ensure ede
-  :bind (:map arduino-mode-map
-              ("C-c C-c" . arduino-upload))
+  ;; :ensure t
+  :quelpa (arduino-mode :fetcher github :repo "stardiviner/arduino-mode" :upgrade t)
+  ;; :load-path "~/Code/Emacs/arduino-mode"
   ;; :config
   ;; (setq arduino-font-lock-extra-types '()) ; this depend on c-font-lock like c-lang-defvar etc.
   )
@@ -35,10 +22,13 @@
 ;;; [ company-arduino ]
 
 (use-package company-arduino
-  :ensure t
+  ;; :ensure t
+  :load-path "~/Code/Emacs/company-arduino"
+  :after arduino-mode
+  :preface
+  (setq company-arduino-home
+        (setenv "ARDUINO_HOME" (expand-file-name "~/Arduino/")))
   :init
-  (setenv "ARDUINO_HOME" "/usr/share/arduino")
-  
   (add-hook 'arduino-mode-hook #'irony-mode)
   (add-hook 'arduino-mode-hook #'company-arduino-turn-on)
   :config
