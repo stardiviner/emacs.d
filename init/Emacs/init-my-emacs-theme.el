e4e7f46527ce03bb1dba7549d43358838b5b932f
stardiviner <numbchild@gmail.com>
2018-02-08 23:21:54 +0800

    [face] custom Emacs built-in font-lock faces with circadian hook.

--- init/Emacs/init-my-emacs-theme.el
+++ init/Emacs/init-my-emacs-theme.el
@@ -49,35 +49,6 @@
 ;;   :config
 ;;   (load-theme 'kaolin-dark t))
 
-;; (set-face-attribute 'underline nil
-;;                     :underline (cl-case (alist-get 'background-mode (frame-parameters))
-;;                                  ('light
-;;                                   (color-darken-name (face-background 'default) 50))
-;;                                  ('dark
-;;                                   (color-lighten-name (face-background 'default) 10))
-;;                                  )
-;;                     )
-
-;; (set-face-attribute 'region nil
-;;                     :inherit nil :inverse-video nil
-;;                     :background (cl-case (alist-get 'background-mode (frame-parameters))
-;;                                   ('light
-;;                                    (color-darken-name (face-background 'default) 5))
-;;                                   ('dark
-;;                                    (color-lighten-name (face-background 'default) 8)))
-;;                     )
-
-;; comment
-(set-face-attribute 'font-lock-comment-face nil
-                    :slant 'italic)
-;; built-in function.
-(set-face-attribute 'font-lock-builtin-face nil
-                    :slant 'italic)
-(set-face-attribute 'font-lock-function-name-face nil
-                    :background (color-lighten-name (face-background 'default) 2))
-(set-face-attribute 'font-lock-type-face nil
-                    :background (color-lighten-name (face-background 'default) 3))
-
 ;;; [ circadian ] -- theme-switching for Emacs based on daytime.
 
 (use-package circadian
@@ -90,6 +61,41 @@
   (setq circadian-themes '((:sunrise . leuven)
                            (:sunset . spacemacs-dark)))
   (circadian-setup))
+
+
+(defun my:font-lock-extra-setup (theme)
+  "Set some built-in faces."
+  (set-face-attribute 'underline nil
+                      :underline (cl-case (alist-get 'background-mode (frame-parameters))
+                                   ('light
+                                    (color-darken-name (face-background 'default) 50))
+                                   ('dark
+                                    (color-lighten-name (face-background 'default) 10))
+                                   )
+                      )
+
+  ;; (set-face-attribute 'region nil
+  ;;                     :inherit nil :inverse-video nil
+  ;;                     :background (cl-case (alist-get 'background-mode (frame-parameters))
+  ;;                                   ('light
+  ;;                                    (color-darken-name (face-background 'default) 5))
+  ;;                                   ('dark
+  ;;                                    (color-lighten-name (face-background 'default) 8)))
+  ;;                     )
+
+  ;; comment
+  (set-face-attribute 'font-lock-comment-face nil
+                      :slant 'italic)
+  ;; built-in function.
+  (set-face-attribute 'font-lock-builtin-face nil
+                      :slant 'italic)
+  (set-face-attribute 'font-lock-function-name-face nil
+                      :background (color-lighten-name (face-background 'default) 2))
+  (set-face-attribute 'font-lock-type-face nil
+                      :background (color-lighten-name (face-background 'default) 3))
+  )
+
+(add-hook 'circadian-after-load-theme-hook #'my:font-lock-extra-setup)
 
 
 (provide 'init-my-emacs-theme)
