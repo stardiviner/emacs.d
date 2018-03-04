f2fdc13823c73002c62fb209caf9486a08d3a3d8
stardiviner <numbchild@gmail.com>
2018-03-04 18:08:29 +0800

    [Dired] remove duplicate all-the-icons-dired.

--- init/programming/code browser/init-prog-sidebar.el
+++ init/programming/code browser/init-prog-sidebar.el
@@ -11,19 +11,12 @@
 
 (use-package dired-sidebar
   :ensure t
+  :defer t
   :commands (dired-sidebar-toggle-sidebar)
   :bind ("<f8>" . dired-sidebar-toggle-sidebar)
   :config
-  (use-package all-the-icons-dired
-    :ensure t)
-  (if (and (display-graphic-p) (featurep 'all-the-icons-dired))
-      (progn
-        (add-hook 'dired-sidebar-mode-hook #'all-the-icons-dired-mode)
-        (setq dired-sidebar-theme 'icons))
-    (setq dired-sidebar-theme 'nerd))
   ;; (setq dired-sidebar-use-custom-font t)
   ;; (setq dired-sidebar-face '(:family "Monaco" :height 120))
-
   (setq dired-sidebar-delay-auto-revert-updates nil)
   )
 
