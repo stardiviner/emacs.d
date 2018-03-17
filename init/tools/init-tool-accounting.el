e5613c9382715d6ee4321a5babe8f745e2f4efa4
stardiviner <numbchild@gmail.com>
2018-03-17 14:41:11 +0800

    [ledger] load ob-ledger initially.

--- init/tools/init-tool-accounting.el
+++ init/tools/init-tool-accounting.el
@@ -25,80 +25,77 @@
     (add-hook 'ledger-mode-hook
               (lambda ()
                 (flycheck-mode 1)
-                (flycheck-select-checker 'ledger)))
-    )
-
-  ;; [ ob-ledger ]
-  (use-package ob-ledger
-    :config
-    (add-to-list 'org-babel-load-languages '(ledger . t))
-    (org-babel-do-load-languages 'org-babel-load-languages org-babel-load-languages)
-    (add-to-list 'org-babel-tangle-lang-exts '("ledger" . "ledger"))
-
-    (setq org-babel-default-header-args:ledger
-          '((:results . "output") (:cmdline . "bal"))
-          )
-
-    (with-eval-after-load 'org-capture
-      (setq org-capture-templates
-            (append '(("l" "[L]edger entries")
-                      ;; Expense
-                      ("le" "[E]xpenses")
-                      ("les" "[S]hopping" plain
-                       (file (concat org-directory "/Accounting/my.ledger"))
-                       "%(org-read-date) %^{Event}\n expenses:shopping:%^{category}  %^{Amount}")
-                      ("lef" "[F]ood" plain
-                       (file (concat org-directory "/Accounting/my.ledger"))
-                       "%(org-read-date) %^{Event}\n expenses:food:%^{meat,breakfast,lunch,dinner}  %^{Amount}")
-                      ("let" "[T]raffic" plain
-                       (file (concat org-directory "/Accounting/my.ledger"))
-                       "%(org-read-date) %^{Event}\n expenses:traffic:%^{bus,train,plane}  %^{Amount}")
-                      ("leh" "[H]ouse Rent" plain
-                       (file (concat org-directory "/Accounting/my.ledger"))
-                       "%(org-read-date) %^{Event}\n expenses:house rent:  %^{Amount}")
-                      
-                      ;; Income
-                      ("li" "[I]ncome")
-                      ("lis" "[S]alary" plain
-                       (file (concat org-directory "/Accounting/my.ledger"))
-                       "%(org-read-date) %^{Event}\n income:salary:%^{account}  %^{Amount}")
-
-                      ;; Transfer
-                      ("lt" "[T]ransfer")
-                      ("ltb" "Take out money from [B]ank"
-                       (file (concat org-directory "/Accounting/my.ledger"))
-                       "%(org-read-date) %^{Event}\n transfer:%^{source} -> %^{bank}  %^{Amount}"
-                       )
-                      ("lto" "save moeny on [o]nline account"
-                       (file (concat org-directory "/Accounting/my.ledger"))
-                       "%(org-read-date) %^{Event}\n transfer:%^{source} -> %^{ZhiFuBao}  %^{Amount}"
-                       )
-                      ("ltc" "take out moeny to [C]ash"
-                       (file (concat org-directory "/Accounting/my.ledger"))
-                       "%(org-read-date) %^{Event}\n transfer:%^{source} -> cash  %^{Amount}"
-                       )
-
-                      ;; Debt
-                      ("ld" "[D]ebt")
-                      ("ldr" "[R]ent" plain
-                       (file (concat org-directory "/Accounting/my.ledger"))
-                       "%(org-read-date) %^{Event}\n debt:rent:%^{people}  %^{Amount}")
-                      ("ldb" "[B]orrow" plain
-                       (file (concat org-directory "/Accounting/my.ledger"))
-                       "%(org-read-date) %^{Event}\n debt:borrow:%^{people}  %^{Amount}")
-                      
-                      ;; Assets
-                      ("la" "[A]ssets")
-                      ("lab" "[B]ank" plain
-                       (file (concat org-directory "/Accounting/my.ledger"))
-                       "%(org-read-date) %^{Event}\n assets:bank:%^{bank}  %^{Amount}")
-                      ("lao" "[O]nline Accounts" plain
-                       (file (concat org-directory "/Accounting/my.ledger"))
-                       "%(org-read-date) %^{Event}\n assets:online-account:%^{ZhiFuBao}  %^{Amount}")
-                      )
-                    org-capture-templates)))
-    )
+                (flycheck-select-checker 'ledger))))
   )
+
+;; [ ob-ledger ]
+
+(require 'ob-ledger)
+(add-to-list 'org-babel-load-languages '(ledger . t))
+(org-babel-do-load-languages 'org-babel-load-languages org-babel-load-languages)
+(add-to-list 'org-babel-tangle-lang-exts '("ledger" . "ledger"))
+
+(setq org-babel-default-header-args:ledger
+      '((:results . "output") (:cmdline . "bal")))
+
+(with-eval-after-load 'org-capture
+  (setq org-capture-templates
+        (append '(("l" "[L]edger entries")
+                  ;; Expense
+                  ("le" "[E]xpenses")
+                  ("les" "[S]hopping" plain
+                   (file (concat org-directory "/Accounting/my.ledger"))
+                   "%(org-read-date) %^{Event}\n expenses:shopping:%^{category}  %^{Amount}")
+                  ("lef" "[F]ood" plain
+                   (file (concat org-directory "/Accounting/my.ledger"))
+                   "%(org-read-date) %^{Event}\n expenses:food:%^{meat,breakfast,lunch,dinner}  %^{Amount}")
+                  ("let" "[T]raffic" plain
+                   (file (concat org-directory "/Accounting/my.ledger"))
+                   "%(org-read-date) %^{Event}\n expenses:traffic:%^{bus,train,plane}  %^{Amount}")
+                  ("leh" "[H]ouse Rent" plain
+                   (file (concat org-directory "/Accounting/my.ledger"))
+                   "%(org-read-date) %^{Event}\n expenses:house rent:  %^{Amount}")
+                  
+                  ;; Income
+                  ("li" "[I]ncome")
+                  ("lis" "[S]alary" plain
+                   (file (concat org-directory "/Accounting/my.ledger"))
+                   "%(org-read-date) %^{Event}\n income:salary:%^{account}  %^{Amount}")
+
+                  ;; Transfer
+                  ("lt" "[T]ransfer")
+                  ("ltb" "Take out money from [B]ank"
+                   (file (concat org-directory "/Accounting/my.ledger"))
+                   "%(org-read-date) %^{Event}\n transfer:%^{source} -> %^{bank}  %^{Amount}"
+                   )
+                  ("lto" "save moeny on [o]nline account"
+                   (file (concat org-directory "/Accounting/my.ledger"))
+                   "%(org-read-date) %^{Event}\n transfer:%^{source} -> %^{ZhiFuBao}  %^{Amount}"
+                   )
+                  ("ltc" "take out moeny to [C]ash"
+                   (file (concat org-directory "/Accounting/my.ledger"))
+                   "%(org-read-date) %^{Event}\n transfer:%^{source} -> cash  %^{Amount}"
+                   )
+
+                  ;; Debt
+                  ("ld" "[D]ebt")
+                  ("ldr" "[R]ent" plain
+                   (file (concat org-directory "/Accounting/my.ledger"))
+                   "%(org-read-date) %^{Event}\n debt:rent:%^{people}  %^{Amount}")
+                  ("ldb" "[B]orrow" plain
+                   (file (concat org-directory "/Accounting/my.ledger"))
+                   "%(org-read-date) %^{Event}\n debt:borrow:%^{people}  %^{Amount}")
+                  
+                  ;; Assets
+                  ("la" "[A]ssets")
+                  ("lab" "[B]ank" plain
+                   (file (concat org-directory "/Accounting/my.ledger"))
+                   "%(org-read-date) %^{Event}\n assets:bank:%^{bank}  %^{Amount}")
+                  ("lao" "[O]nline Accounts" plain
+                   (file (concat org-directory "/Accounting/my.ledger"))
+                   "%(org-read-date) %^{Event}\n assets:online-account:%^{ZhiFuBao}  %^{Amount}")
+                  )
+                org-capture-templates)))
 
 ;;; [ hledger-mode ] -- Major mode for editing hledger.
 
