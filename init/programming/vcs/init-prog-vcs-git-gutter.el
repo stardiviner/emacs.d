47c3138e64d82f0ec576931397965119588f1f4b
stardiviner <numbchild@gmail.com>
2018-03-17 14:38:33 +0800

    [diff] add package diff-hl.

--- init/programming/vcs/init-prog-vcs-git-gutter.el
+++ init/programming/vcs/init-prog-vcs-git-gutter.el
@@ -113,6 +113,24 @@
         git-gutter+-diff-option "-w")
   )
 
+;;; [ diff-hl ] -- highlighting uncommitted changes with continuous fringe vertical block.
+
+(use-package diff-hl
+  :ensure t
+  :defer t
+  :commands (diff-hl-next-hunk diff-hl-previous-hunk diff-hl-diff-goto-hunk)
+  :init
+  (add-hook 'prog-mode-hook 'diff-hl-mode)
+  (add-hook 'dired-mode-hook 'diff-hl-dired-mode)
+  :bind (:map git-gutter-prefix
+              ("M-n" . diff-hl-next-hunk)
+              ("M-p" . diff-hl-previous-hunk)
+              ("M-=" . diff-hl-diff-goto-hunk))
+  :config
+  (setq vc-git-diff-switches '("--histogram"))
+  (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
+  )
+
 
 
 (provide 'init-prog-vcs-git-gutter)
