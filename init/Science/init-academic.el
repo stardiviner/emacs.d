57a83247aedfcad127077b8713e1bf86373eb3cc
stardiviner <numbchild@gmail.com>
2018-03-22 13:42:16 +0800

    [Academic] add academic init file.

---
+++ init/Science/init-academic.el
@@ -0,0 +1,20 @@
+;;; init-academic.el --- init for Academic
+
+;;; Commentary:
+
+
+
+;;; Code:
+
+;;; [ academic-phrases ] -- Bypass that mental block when writing your papers.
+
+(use-package academic-phrases
+  :ensure t
+  :defer t
+  :commands (academic-phrases academic-phrases-by-section))
+
+
+
+(provide 'init-academic)
+
+;;; init-academic.el ends here
