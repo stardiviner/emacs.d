33108941296c7bffa91ada9cd338a135e34ced63
stardiviner <numbchild@gmail.com>
2018-03-30 12:25:18 +0800

    [LilyPond] add lilypond-mode and ob-lilypond.

--- init/Music/init-musician.el
+++ init/Music/init-musician.el
@@ -6,6 +6,24 @@
 
 ;;; Code:
 
+;;; [ LilyPond ]
+
+(use-package lilypond-mode
+  :preface
+  (autoload 'LilyPond-mode "lilypond-mode" "LilyPond Editing Mode" t)
+  :mode (("\\.ly$" . LilyPond-mode)
+         ("\\.ily$" . LilyPond-mode))
+  :init (add-hook 'LilyPond-mode-hook (lambda () (turn-on-font-lock)))
+  (add-to-list 'display-buffer-alist
+               '("\\*lilypond\\*" . (display-buffer-below-selected)))
+  )
+
+;;; [ ob-lilypond ]
+
+(require 'ob-lilypond)
+(add-to-list 'org-babel-load-languages '(lilypond . t))
+(org-babel-do-load-languages 'org-babel-load-languages org-babel-load-languages)
+
 ;;; [ gregorio-mode ] -- Gregorio Mode for .gabc files.
 
 (use-package gregorio-mode
