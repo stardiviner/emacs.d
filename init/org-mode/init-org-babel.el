aeb575b6b27b695c62d29349e7c88b2a85a9c0c0
stardiviner <numbchild@gmail.com>
2018-04-06 15:12:35 +0800

    [org-export] only export code by default to avoid evaluation src blcoks when exporting.

--- init/org-mode/init-org-babel.el
+++ init/org-mode/init-org-babel.el
@@ -19,7 +19,7 @@
                 (:noweb . "no") (:hlines . "no")
                 (:tangle . "no") (:comments . "links")
                 (:cache . "yes")
-                (:results . "replace") (:exports . "both")
+                (:results . "replace") (:exports . "code")
                 ))
 
 ;;; don't evaluate babel when exporting. For helper functions like `my:org-convert-region-to-html' etc.
