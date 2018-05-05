5c0cfeeb446fd7ebc17e9acfe80cd035e879aa00
stardiviner <numbchild@gmail.com>
2018-05-05 23:02:06 +0800

    [ob-clojure] set ob-clojure :ruselts type to pp by default.

--- init/programming/languages/Clojure/init-prog-lang-clojure.el
+++ init/programming/languages/Clojure/init-prog-lang-clojure.el
@@ -379,7 +379,7 @@ Usage: (my/cider-repl-eval \"\(clojure expr\)\")"
 (add-to-list 'org-babel-default-header-args:clojure
              '(:noweb . "yes"))
 (add-to-list 'org-babel-default-header-args:clojure
-             '(:results . "value"))
+             '(:results . "pp"))
 
 ;; No timeout when executing calls on Cider via nrepl
 (setq org-babel-clojure-sync-nrepl-timeout 30)
