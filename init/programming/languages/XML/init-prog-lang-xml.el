1d601ff84601f03a1a45ef80ff4b20ddceae9eec
stardiviner <numbchild@gmail.com>
2018-05-14 08:55:47 +0800

    [XML] improve XML configs

--- init/programming/languages/XML/init-prog-lang-xml.el
+++ init/programming/languages/XML/init-prog-lang-xml.el
@@ -10,48 +10,33 @@
 ;;; [ nxml ]
 
 (use-package nxml
-  :defer t
   :mode "\\.xml\\'"
   :config
-  (setq nxml-child-indent 2
-        nxml-attribute-indent 2
-        nxml-auto-insert-xml-declaration-flag nil
-        )
-
-  ;;; XML completion
-  (setq nxml-bind-meta-tab-to-complete-flag t ; M-Tab to complete
-        nxml-slash-auto-complete-flag nil ; </ to complete
-        )
   ;; company-nxml
   (defun my-company-nxml-settings ()
     (setq-local company-minimum-prefix-length 1)
-    (add-to-list (make-local-variable 'company-backends)
-                 'company-nxml)
-    )
-
-  (add-hook 'nxml-mode-hook 'my-company-nxml-settings)
+    (add-to-list (make-local-variable 'company-backends) 'company-nxml))
+  (add-hook 'nxml-mode-hook #'my-company-nxml-settings)
 
   ;; format XML with `xmllint'.
-  (defun my-xml-format ()
+  (defun my:xml-lint ()
     "Format an XML buffer with `xmllint'."
     (interactive)
-    (shell-command-on-region (point-min) (point-max)
-                             "xmllint -format -"
-                             (current-buffer)
-                             t
-                             "*Xmllint Error Buffer*"
-                             t))
-
-  (define-key nxml-mode-map (kbd "C-c m f") 'my-xml-format)
+    (shell-command-on-region
+     (point-min) (point-max)
+     "xmllint -format -"
+     (current-buffer)
+     t
+     "*Xmllint Error Buffer*"
+     t))
+  (define-key nxml-mode-map (kbd "C-c m f") 'my:xml-lint)
   )
 
-
 ;;; [ ob-xml ]
 
 (with-eval-after-load 'nxml-mode
   (add-to-list 'org-src-lang-modes '("xml" . nxml)))
 
-
 ;;; [ x-path-walker ] -- navigation for JSON/XML/HTML based on path (imenu like)
 
 (use-package x-path-walker
@@ -64,8 +49,7 @@
                   json-mode-hook
                   ))
     (add-hook hook
-              (lambda ()
-                (local-set-key (kbd "C-c C-j") 'helm-x-path-walker))))
+              (lambda () (local-set-key (kbd "C-c C-j") 'helm-x-path-walker))))
   )
 
 
