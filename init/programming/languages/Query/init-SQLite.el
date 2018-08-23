fc13f210c9bbf5ebeff3332dd98d916e6d8a5c5f
stardiviner <numbchild@gmail.com>
2018-08-23 08:29:15 +0800

    [ob-sqlite] update default header arguments.

--- init/programming/languages/Query/init-SQLite.el
+++ init/programming/languages/Query/init-SQLite.el
@@ -19,11 +19,19 @@
 (add-to-list 'org-babel-tangle-lang-exts '("sqlite" . "sqlite"))
 
 (setq org-babel-default-header-args:sqlite
-      '((:db . "temp.db")
-        (:results . "raw")
-        ;; (:echo . t)
-        (:column . t)
-        (:nullvalue . "Null")))
+      '((:column . t)
+        ;; (:line . "yes")
+        (:header . "yes")
+        ;; (:echo . "yes")
+        ;; (:bail . "yes")
+        ;; (:separator . ",")
+        ;; (:csv . t)
+        ;; (:html . t)
+        (:results . "verbatim")
+        ;; (:wrap . "example")
+        (:nullvalue . "Null")
+        (:db . "data/code/temp.db") ; this is essential necessary for every src block.
+        ))
 
 
 ;;; [ esqlite ] -- sqlite file manipulate utilities from Emacs.
