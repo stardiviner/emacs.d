0c69f9b748b8a40960ca1350e051beecafcd380b
stardiviner <numbchild@gmail.com>
2018-09-19 20:00:58 +0800

    [Windows] add package dotnet.

--- init/Microsoft Windows/init-microsoft-windows.el
+++ init/Microsoft Windows/init-microsoft-windows.el
@@ -36,6 +36,19 @@
 (use-package powershell
   :ensure t
   :commands (powershell))
+
+;;; [ .NET ]
+
+(use-package dotnet
+  :ensure t
+  :commands (dotnet-add-package
+             dotnet-new
+             dotnet-run dotnet-run-with-args
+             dotnet-build
+             dotnet-publish
+             dotnet-test dotnet-test-rerun
+             dotnet-sln-new dotnet-sln-add dotnet-sln-remove dotnet-sln-list))
+
 ;;; ----------------------------------------------------------------------------
 
 (provide 'init-microsoft-windows)
