5d49ebc4162ca82907cb7a8f7477e54bc33a7f08
stardiviner <numbchild@gmail.com>
2018-09-30 08:45:04 +0800

    [Edit] autoload package floobits commands.

--- init/Emacs/init-emacs-edit-collaborate.el
+++ init/Emacs/init-emacs-edit-collaborate.el
@@ -12,6 +12,19 @@
 (use-package floobits
   :ensure t
   :defer t
+  :commands (floobits-join-workspace
+             floobits-add-to-workspace
+             floobits-remove-from-workspace
+             floobits-leave-workspace
+             floobits-delete-workspace
+             floobits-open-workspace-in-browser
+             floobits-workspace-settings
+             floobits-share-dir-private
+             floobits-share-dir-public
+             floobits-summon
+             floobits-follow-user
+             floobits-follow-mode-toggle
+             floobits-clear-highlights)
   :config
   (add-to-list 'display-buffer-alist
                '("\\*Floobits\\*" . (display-buffer-reuse-window display-buffer-below-selected))))
