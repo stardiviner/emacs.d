474d7aaf48f2042bbdb42216c4c2e8b327500825
stardiviner <numbchild@gmail.com>
2018-10-14 09:41:16 +0800

    [JavaScript] enable lsp-mode before lsp-javascript-typescript-enable.

--- init/programming/languages/JavaScript/init-prog-lang-javascript.el
+++ init/programming/languages/JavaScript/init-prog-lang-javascript.el
@@ -196,9 +196,16 @@
 (use-package lsp-javascript-typescript
   :ensure t
   :init (require 'lsp-javascript-typescript)
+  (add-hook 'js-mode-hook #'lsp-mode)
   (add-hook 'js-mode-hook #'lsp-javascript-typescript-enable)
-  (add-hook 'typescript-mode-hook #'lsp-javascript-typescript-enable) ;; for typescript support
+  
+  (add-hook 'js2-mode-hook #'lsp-mode)
+  (add-hook 'js2-mode-hook #'lsp-javascript-typescript-enable)
+
+  (add-hook 'js3-mode-hook #'lsp-mode)
   (add-hook 'js3-mode-hook #'lsp-javascript-typescript-enable) ;; for js3-mode support
+  
+  (add-hook 'typescript-mode-hook #'lsp-javascript-typescript-enable)
   (add-hook 'rjsx-mode #'lsp-javascript-typescript-enable) ;; for rjsx-mode support
   )
 
