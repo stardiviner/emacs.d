b456243c042971d7d0eb2ec0f69ede34d7e33fa1
stardiviner <numbchild@gmail.com>
2018-10-30 12:08:58 +0800

    [sidebar] disable ibuffer in sidebar. only dired-sidebar toggle.

--- init/programming/code browser/init-prog-sidebar.el
+++ init/programming/code browser/init-prog-sidebar.el
@@ -13,35 +13,36 @@
   :ensure t
   :defer t
   :commands (dired-sidebar-toggle-sidebar)
-  ;; :bind ("<f8>" . dired-sidebar-toggle-sidebar)
+  :bind ("<f8>" . dired-sidebar-toggle-sidebar)
   :preface (setq dired-sidebar-disable-dired-collapse nil) ; affect Dired performance hard!!!
   :config
-  ;; (setq dired-sidebar-use-custom-font t)
-  ;; (setq dired-sidebar-face '(:family "Monaco" :height 120))
-  (setq dired-sidebar-delay-auto-revert-updates nil)
-  (setq dired-sidebar-close-sidebar-on-file-open t)
+  (setq dired-sidebar-close-sidebar-on-file-open t
+        dired-sidebar-delay-auto-revert-updates nil
+        ;; dired-sidebar-use-custom-font t
+        ;; dired-sidebar-face '(:family "Monaco" :height 120)
+        )
   )
 
-(use-package ibuffer-sidebar
-  :ensure t
-  :ensure ibuffer-projectile
-  :ensure dired-sidebar
-  :defer t
-  :load (ibuffer-projectile)
-  :commands (ibuffer-sidebar-toggle-sidebar)
-  :init
-  ;; be toggled together with dired-sidebar.
-  (defun +sidebar-toggle ()
-    "Toggle both `dired-sidebar' and `ibuffer-sidebar'."
-    (interactive)
-    (dired-sidebar-toggle-sidebar)
-    (ibuffer-sidebar-toggle-sidebar)
-    (ibuffer-projectile-set-filter-groups))
-  (global-set-key (kbd "<f8>") '+sidebar-toggle)
-  :config
-  (setq ibuffer-sidebar-use-custom-font nil)
-  (setq ibuffer-sidebar-face `(:family "Helvetica" :height 140))
-  )
+;; (use-package ibuffer-sidebar
+;;   :ensure t
+;;   :ensure ibuffer-projectile
+;;   :ensure dired-sidebar
+;;   :defer t
+;;   :load (ibuffer-projectile)
+;;   :commands (ibuffer-sidebar-toggle-sidebar)
+;;   :init
+;;   ;; be toggled together with dired-sidebar.
+;;   (defun +sidebar-toggle ()
+;;     "Toggle both `dired-sidebar' and `ibuffer-sidebar'."
+;;     (interactive)
+;;     (dired-sidebar-toggle-sidebar)
+;;     (ibuffer-sidebar-toggle-sidebar)
+;;     (ibuffer-projectile-set-filter-groups))
+;;   (global-set-key (kbd "<f8>") '+sidebar-toggle)
+;;   :config
+;;   (setq ibuffer-sidebar-use-custom-font nil)
+;;   (setq ibuffer-sidebar-face `(:family "Helvetica" :height 140))
+;;   )
 
 
 
