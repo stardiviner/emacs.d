f68e3ddfe86ed5eff476c2e3a5ea7b89ffb96e36
stardiviner <numbchild@gmail.com>
2018-11-02 07:28:45 +0800

    [backup, auto-save] improve configs.

--- init/Emacs/init-emacs-backup.el
+++ init/Emacs/init-emacs-backup.el
@@ -6,52 +6,53 @@
 
 ;;; Usage:
 
-;; - `recover-file' :: recover file from file~.
+;; - `recover-file' :: recover file from backup file suffix with "file~".
 
 
 ;;; Code:
 
-
+;;; [ backup ]
 ;; .#foo.txt
 ;; modify same file by different processes
 (setq create-lockfiles t)
 ;; foo.txt~
 ;; backup the latest recent file
 (setq make-backup-files t)
-;; #foo.txt#
-;; periodly save, otherwise lost data when accidently power-off.
-(setq auto-save-default t)
-
 
 (setq backup-by-copying t
       backup-by-copying-when-mismatch t
       backup-by-copying-when-privileged-mismatch t
       backup-by-copying-when-linked t
-      version-control nil ; use versioned backups filename.ext.~1~
-      vc-make-backup-files nil ; do not backup files in vc.
-      ;; backup-inhibited t ; do not generate backup
-      delete-old-versions t             ; auto delete old versions.
-      kept-new-versions 2               ; number of new versions.
-      kept-old-versions 2               ; number of old versions.
-      )
-
-(setq backup-directory-alist
+      backup-directory-alist
       '(("." . ".emacs_backups")) ; save backups in $(pwd)/.emacs_backups/filename.bak
       ;; `(("." . ,(expand-file-name "~/.emacs.d/.backups")))
       ;; `((".*" . ,temporary-file-directory)) ; put all under directory /tmp.
       )
 
+;;; [ version control ]
+;;; version control with versioned backup filename like "filename.ext.~1~".
+
+(setq version-control nil               ; toggle version control backup?
+      vc-make-backup-files nil          ; do not backup files in vc.
+      ;; backup-inhibited t                ; do not generate backup
+      delete-old-versions t             ; auto delete old versions.
+      kept-new-versions 2               ; number of new versions.
+      kept-old-versions 2               ; number of old versions.
+      )
 
 ;;; [ auto-save-mode ] -- toggle auto-saving in the current buffer.
 
 ;;; From time to time, Emacs automatically saves each visited file in a separate file,
 ;;; without altering the file you actually use. This is called “auto-saving”.
+;;
+;; create filename with "#foo.txt#".
+;; periodly save, otherwise lost data when accidently power-off.
 
-(setq auto-save-default t               ; create #autosave# files
-      auto-save-list-file-prefix "~/.emacs.d/.auto-save-list/saves-"
-      auto-save-interval (* 60 10)
-      ;; delete-auto-save-files nil ; don't delete auto-save file when a buffer is saved or killed.
-      )
+;; (setq auto-save-default t
+;;       auto-save-list-file-prefix (concat user-emacs-directory "auto-save-list/.saves-")
+;;       auto-save-interval (* 60 10)
+;;       ;; delete-auto-save-files nil ; don't delete auto-save file when a buffer is saved or killed.
+;;       )
 ;; (auto-save-mode 1)
 
 ;;; [ auto-save-visited-mode ]
@@ -63,19 +64,6 @@
 ;; save-related hooks. See Info node `Saving' for details of the save process.
 
 (auto-save-visited-mode t)
-
-;;; [ auto-save ]
-
-;;; https://www.emacswiki.org/emacs/auto-save.el
-;;; https://www.emacswiki.org/emacs/init-auto-save.el
-;;; ;; 禁用Emacs备份机制
-;;; (setq make-backup-files nil)
-;;; (setq auto-save-default nil)
-;;; ;; 启用 LazyCat 的自动备份插件
-;;; (require 'auto-save)
-;;; (auto-save-enable)
-;;; (setq auto-save-slient t)
-;;; (setq auto-save-delete-trailing-whitespace t)
 
 
 (provide 'init-emacs-backup)
