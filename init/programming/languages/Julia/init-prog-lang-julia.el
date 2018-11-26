a2b19e24cb4e79379ef5aee856dc2663fd42af10
stardiviner <numbchild@gmail.com>
2018-11-26 19:19:44 +0800

    [Julia] remove my custom julia REPL command.

--- init/programming/languages/Julia/init-prog-lang-julia.el
+++ init/programming/languages/Julia/init-prog-lang-julia.el
@@ -13,47 +13,19 @@
   :ensure t
   :ensure-system-package julia
   :defer t
-  :commands (run-julia)
-  :config
-  (setq julia-indent-offset 2)
-  )
-
+  :commands (run-julia inferior-julia)
+  :config (setq julia-indent-offset 2))
 
 ;;; [ ESS - Julia ]
 
 (use-package ess
   :ensure t
   :defer t
+  :commands (julia)
   :init
+  ;; add `julia-mode' to `prog-mode''.
   (add-hook 'julia-mode-hook
-            (lambda ()
-              ;; add julia-mode to prog-mode.
-              (unless (derived-mode-p 'prog-mode)
-                (run-hooks 'prog-mode-hook))
-              ))
-  
-  ;; - `julia' :: from ess-julia.
-  ;; - `inferior-julia' :: from julia-mode.
-  (unless (boundp 'ess-prefix)
-    (define-prefix-command 'ess-prefix))
-  (define-key ess-prefix (kbd "j") 'my-ess-inferior-julia)
-  
-  :config
-  ;; (setq inferior-julia-args)
-
-  (defun my-ess-inferior-julia (&optional process-buffer-name)
-    "Start or switch to inferior-julia process buffer PROCESS-BUFFER-NAME."
-    (interactive)
-    (if (get-buffer-process (or process-buffer-name "*julia*"))
-        ;; the inferior julia process exist
-        (switch-to-buffer (or process-buffer-name "*julia*"))
-      ;; create a new inferior julia process
-      (julia)
-      ;; (julia)
-      ;; kill old process
-      ;; (kill-process (get-buffer-process (or process-buffer-name "*julia*"))
-      )
-    )
+            (lambda () (unless (derived-mode-p 'prog-mode) (run-hooks 'prog-mode-hook))))
   )
 
 ;;; [ ob-julia ]
@@ -61,16 +33,14 @@
 (use-package ess
   :ensure t
   :load (ess-site ess-custom)
-  :init (setq inferior-julia-program "julia"))
-
-(require 'ob-julia)
-(setq org-babel-julia-command "julia")
-(add-to-list 'org-babel-load-languages '(julia . t))
-(org-babel-do-load-languages 'org-babel-load-languages org-babel-load-languages)
-
-;; (setq org-babel-default-header-args:julia
-;;       '((:results . "output replace")
-;;         (:padnewline . "yes")))
+  :config
+  (require 'ob-julia)
+  (add-to-list 'org-babel-load-languages '(julia . t))
+  (org-babel-do-load-languages 'org-babel-load-languages org-babel-load-languages)
+  ;; (setq org-babel-default-header-args:julia
+  ;;       '((:results . "output replace")
+  ;;         (:padnewline . "yes")))
+  )
 
 ;;; [ julia-shell ] -- Emacs major mode for an interactive Julia shell.
 
