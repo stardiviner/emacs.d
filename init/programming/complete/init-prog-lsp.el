7e51cbf68c8b01b05f452eed1aad1b425203e890
stardiviner <numbchild@gmail.com>
2019-01-24 17:18:53 +0800

    [LSP] add LSP support for Org Mode Babel source blocks.

--- init/programming/complete/init-prog-lsp.el
+++ init/programming/complete/init-prog-lsp.el
@@ -10,9 +10,41 @@
 
 (use-package lsp-mode
   :ensure t
-  :commands lsp
+  :commands (lsp lsp-describe-session)
   :load (lsp-clients) ; load `lsp-clients' for auto configuration of language server clients.
-  :init (setq lsp-auto-guess-root t))
+  ;; :hook (prog-mode . lsp)
+  :init (setq lsp-auto-guess-root t
+              lsp-prefer-flymake nil) ; use `flycheck' and `lsp-ui'.
+  :config
+  ;; Support LSP in Org Babel with header argument `:file'.
+  ;; https://github.com/emacs-lsp/lsp-mode/issues/377
+  (cl-defmacro lsp-org-babel-enbale (lang)
+    "Support LANG in org source code block."
+    ;; (cl-check-type lang symbolp)
+    (let* ((edit-pre (intern (format "org-babel-edit-prep:%s" lang)))
+           (intern-pre (intern (format "lsp--%s" (symbol-name edit-pre)))))
+      `(progn
+         (defun ,intern-pre (info)
+           ;; TODO: `lsp-file' can be improved by auto generate under /tmp.
+           (let ((lsp-file (or (->> info caddr (alist-get :file))
+                               buffer-file-name)))
+             (setq-local buffer-file-name lsp-file)
+             (setq-local lsp-buffer-uri (lsp--path-to-uri lsp-file))
+             (lsp)))
+         (if (fboundp ',edit-pre)
+             (advice-add ',edit-pre :after ',intern-pre)
+           (progn
+             (defun ,edit-pre (info)
+               (,intern-pre info))
+             (put ',edit-pre 'function-documentation
+                  (format "Prepare local buffer environment for org source block (%s)."
+                          (upcase ,lang))))))))
+
+  (defvar org-babel-lang-list
+    '("shell" "python" "ipython" "ruby" "js" "css" "C" "rust" "java" "go"))
+  (dolist (lang org-babel-lang-list)
+    (eval `(lsp-org-babel-enbale ,lang)))
+  )
 
 ;; [ lsp-ui ] -- UI modules for lsp-mode.
 
@@ -22,7 +54,8 @@
   :hook (lsp-mode . lsp-ui-mode)
   :bind (:map lsp-ui-mode-map
               ([remap xref-find-definitions] . lsp-ui-peek-find-definitions)
-              ([remap xref-find-references] . lsp-ui-peek-find-references))
+              ([remap xref-find-references] . lsp-ui-peek-find-references)
+              ("C-c C-j" . lsp-ui-imenu))
   :init (setq lsp-ui-doc-enable nil
               lsp-ui-doc-header t
               lsp-ui-doc-include-signature t
@@ -32,10 +65,9 @@
 ;; [ company-lsp ] -- company-mode completion backend for lsp-mode.
 
 (use-package company-lsp
-  :after company
   :ensure t
   :commands company-lsp
-  :init (setq company-lsp-enable-recompletion t)
+  :init
   (defun my:company-lsp-enable ()
     (my-company-add-backend-locally 'company-lsp))
   (add-hook 'lsp-mode-hook #'my:company-lsp-enable))
