8b0271ce2851ce57ff62172ec067c4ac89e83ef8
stardiviner <numbchild@gmail.com>
2019-01-30 16:11:55 +0800

    [mode-line] doom-modeline updated to use global minor mode now.

--- init/Emacs/init-doom-modeline.el
+++ init/Emacs/init-doom-modeline.el
@@ -1,6 +1,6 @@
 ;;; init-doom-modeline.el --- init for doom-modeline.
 
-;;; Time-stamp: <2019-01-18 13:46:01 stardiviner>
+;;; Time-stamp: <2019-01-30 16:11:34 stardiviner>
 
 ;;; Commentary:
 
@@ -12,9 +12,9 @@
 
 (use-package doom-modeline
   :ensure t
-  :hook (after-init . doom-modeline-init)
+  :hook (after-init . doom-modeline-mode)
   :init (setq doom-modeline-buffer-file-name-style 'buffer-name
-              ;; doom-modeline-icon nil ; don't use icon will be faster
+              doom-modeline-icon nil ; don't use icon will be faster
               doom-modeline-github nil))
 
 
