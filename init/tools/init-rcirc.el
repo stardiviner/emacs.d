df4faa5bc79f63a8b719d0624bc372099ea0f4a4
stardiviner <numbchild@gmail.com>
2019-02-12 14:25:44 +0800

    [IRC] improve rcirc extension rcirc-alertify installation by stripping.

--- init/tools/init-rcirc.el
+++ init/tools/init-rcirc.el
@@ -1,6 +1,6 @@
 ;;; init-rcirc.el --- A simple IRC client for Emacs.
 
-;;; Time-stamp: <2018-11-11 14:33:54 stardiviner>
+;;; Time-stamp: <2019-02-12 14:24:36 stardiviner>
 
 ;;; Commentary:
 
@@ -21,9 +21,10 @@
         rcirc-default-full-name "Christopher M. Miles")
 
   ;; /msg NickServ identify <password>
-  (setq rcirc-authinfo
+  (setq rcirc-auto-authenticate-flag t
+        rcirc-authinfo
         `(("freenode" nickserv "stardiviner" ,(my/json-read-value my/account-file 'erc))
-          ;; ("freenode" chanserv "stardiviner" )
+          ;; ("freenode" chanserv "stardiviner" "PASSWORD")
           ;; ("bitlbee" bitlbee "stardiviner" "PASSWORD")
           ))
 
@@ -56,16 +57,15 @@
   (add-to-list 'rcirc-omit-responses "AWAY")
   (add-hook 'rcirc-mode-hook #'rcirc-omit-mode)
 
-  ;; [ rcirc-alertify ] -- Cross platform notifications for rcirc with tight integration.
-  (use-package rcirc-alertify
-    :ensure t
-    :init (setq alert-default-style 'libnotify)
-    :config (rcirc-alertify-enable))
-
   ;; [ Edit ]
+  (add-hook 'rcirc-mode-hook #'flyspell-mode))
 
-  (add-hook 'rcirc-mode-hook #'flyspell-mode)
-  )
+;; [ rcirc-alertify ] -- Cross platform notifications for rcirc with tight integration.
+
+(use-package rcirc-alertify
+  :ensure t
+  :init (setq alert-default-style 'libnotify)
+  :config (rcirc-alertify-enable))
 
 
 
