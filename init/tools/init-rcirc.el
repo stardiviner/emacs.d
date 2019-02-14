2750512d9d56b433e8c08c52b3199625304c0ed7
stardiviner <numbchild@gmail.com>
2019-02-14 09:44:36 +0800

    Revert "[IRC] fix delay rcirc loading."
    This reverts commit 9534be5ac2dd37d6cbe6e80a5221a1e0520b784a.

--- init/tools/init-rcirc.el
+++ init/tools/init-rcirc.el
@@ -11,61 +11,59 @@
 ;;; Code:
 
 
-(with-eval-after-load 'rcirc
-  (use-package rcirc
-    :defer t
-    :bind (:map tools-prefix ("i" . rcirc))
-    :init
-    ;; server, account info
-    (setq rcirc-default-nick "stardiviner"
-          rcirc-default-user-name "stardiviner"
-          rcirc-default-full-name "Christopher M. Miles")
+(use-package rcirc
+  :defer t
+  :bind (:map tools-prefix ("i" . rcirc))
+  :config
+  ;; server, account info
+  (setq rcirc-default-nick "stardiviner"
+        rcirc-default-user-name "stardiviner"
+        rcirc-default-full-name "Christopher M. Miles")
 
-    ;; /msg NickServ identify <password>
-    (setq rcirc-auto-authenticate-flag t
-          rcirc-authinfo
-          `(("freenode" nickserv "stardiviner" ,(my/json-read-value my/account-file 'erc))
-            ;; ("freenode" chanserv "stardiviner" "PASSWORD")
-            ;; ("bitlbee" bitlbee "stardiviner" "PASSWORD")
-            ))
+  ;; /msg NickServ identify <password>
+  (setq rcirc-auto-authenticate-flag t
+        rcirc-authinfo
+        `(("freenode" nickserv "stardiviner" ,(my/json-read-value my/account-file 'erc))
+          ;; ("freenode" chanserv "stardiviner" "PASSWORD")
+          ;; ("bitlbee" bitlbee "stardiviner" "PASSWORD")
+          ))
 
-    (setq rcirc-server-alist
-          '(("irc.freenode.net"
-             :channels ("#emacs" "#lisp" "#archlinux")
-             :nick "stardiviner"
-             :user-name "stardiviner"
-             :encryption tls :port 6697)))
-    ;; (add-to-list 'rcirc-server-alist
-    ;;              '("irc.gimp.org" :channels ("#gimp")))
-    ;; (add-to-list 'rcirc-server-alist
-    ;;              '("irc.gnome.org" :channels ("#gnome")))
-    ;; (add-to-list 'rcirc-server-alist
-    ;;              '("irc.oftc.net" :channels ()))
+  (setq rcirc-server-alist
+        '(("irc.freenode.net"
+           :channels ("#emacs" "#lisp" "#archlinux")
+           :nick "stardiviner"
+           :user-name "stardiviner"
+           :encryption tls :port 6697)))
+  ;; (add-to-list 'rcirc-server-alist
+  ;;              '("irc.gimp.org" :channels ("#gimp")))
+  ;; (add-to-list 'rcirc-server-alist
+  ;;              '("irc.gnome.org" :channels ("#gnome")))
+  ;; (add-to-list 'rcirc-server-alist
+  ;;              '("irc.oftc.net" :channels ()))
 
-    ;; Reconnecting after you have lost the connection with command `/reconnect'.
+  ;; Reconnecting after you have lost the connection with command `/reconnect'.
 
-    ;; [ Channel ] -- [C-c C-l] (set low priority for very active channel)
-    ;; [ track mode ] -- [C-c C-<SPC>]
-    (add-hook 'rcirc-mode-hook #'rcirc-track-minor-mode)
+  ;; [ Channel ] -- [C-c C-l] (set low priority for very active channel)
+  ;; [ track mode ] -- [C-c C-<SPC>]
+  (add-hook 'rcirc-mode-hook #'rcirc-track-minor-mode)
 
-    ;; [ People ] -- `/ignore', `/bright', `/dim'
+  ;; [ People ] -- `/ignore', `/bright', `/dim'
 
-    ;; [ Highlighting ] -- `/keyword'
+  ;; [ Highlighting ] -- `/keyword'
 
-    ;; [ Notices ] -- [C-c C-o] omit notices.
+  ;; [ Notices ] -- [C-c C-o] omit notices.
 
-    ;; ("JOIN" "PART" "QUIT" "NICK")
-    (add-to-list 'rcirc-omit-responses "AWAY")
-    (add-hook 'rcirc-mode-hook #'rcirc-omit-mode)
+  ;; ("JOIN" "PART" "QUIT" "NICK")
+  (add-to-list 'rcirc-omit-responses "AWAY")
+  (add-hook 'rcirc-mode-hook #'rcirc-omit-mode)
 
-    ;; [ Edit ]
-    (add-hook 'rcirc-mode-hook #'flyspell-mode)))
+  ;; [ Edit ]
+  (add-hook 'rcirc-mode-hook #'flyspell-mode))
 
 ;; [ rcirc-alertify ] -- Cross platform notifications for rcirc with tight integration.
 
 (use-package rcirc-alertify
   :ensure t
-  :defer t
   :init (setq alert-default-style 'libnotify)
   :config (rcirc-alertify-enable))
 
