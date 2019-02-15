176855ce8fc191695d849a6f4812490ebc86c644
stardiviner <numbchild@gmail.com>
2019-02-15 12:11:55 +0800

    [Matrix] add Org link `matrix:` support.

--- init/tools/init-communication.el
+++ init/tools/init-communication.el
@@ -14,7 +14,13 @@
   :bind (:map matrix-client-mode-map ("C-c C-b" . matrix-client-switch-buffer))
   :init (setq matrix-client-ng-save-token t
               matrix-client-use-tracking t
-              matrix-client-show-images t))
+              matrix-client-show-images t)
+  :config
+  ;; matrix link support for Org.
+  (defun org-matrix-link-open (room-id)
+    "Open Matrix ROOM-ID in `matrix-client'."
+    (matrix-join-room (car matrix-client-sessions) room-id))
+  (org-link-set-parameters "matrix" :follow #'org-matrix-link-open))
 
 ;;; [ slack ] -- Emacs interface for Slack.
 
