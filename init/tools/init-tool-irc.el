54edbc48cd8c26804bdf4a9feea1b373fb6f9ac0
stardiviner <numbchild@gmail.com>
2019-02-18 18:33:33 +0800

    [IRC] use circe to replace rcirc.

--- init/tools/init-tool-irc.el
+++ init/tools/init-tool-irc.el
@@ -9,8 +9,39 @@
 ;;; ERC is a powerful, modular, and extensible IRC client for Emacs.
 ;; (require 'init-erc)
 
-;;; A simple IRC client for Emacs.
-(require 'init-rcirc)
+;;; [ Circe ] -- have sane defaults, and integrates well with the rest of the editor.
+
+(use-package circe
+  :ensure t
+  :defer t
+  :commands (circe)
+  :config
+  ;; user and servers info
+  (setq circe-default-nick "stardiviner"
+        circe-network-options `(("Freenode"
+                                 :user "stardiviner"
+                                 :pass ,(my/json-read-value my/account-file 'erc)
+                                 :use-tls t
+                                 :channels ("#emacs" "#clojure"))))
+  ;; ignore
+  (setq circe-reduce-lurker-spam t)
+  ;; view
+  (setq circe-format-say "{nick:-10s} {body}") ; align nick names and messages.
+  (define-key circe-mode-map (kbd "C-c SPC") 'tracking-next-buffer)
+  ;; spelling checking
+  (setq lui-flyspell-p t
+        lui-flyspell-alist '(("#hamburg" "german8")
+                             (".*" "american")))
+  ;; auto use paste service for long single line.
+  ;; (require 'lui-autopaste)
+  ;; (add-hook 'circe-channel-mode-hook 'enable-lui-autopaste)
+  ;; Logging
+  (load "lui-logging" nil t)
+  (enable-lui-logging-globally)
+  ;; track bar
+  (enable-lui-track-bar))
+
+(define-key tools-prefix (kbd "i") 'circe)
 
 
 (provide 'init-tool-irc)
