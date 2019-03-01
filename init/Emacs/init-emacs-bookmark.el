d048473197f48b1d5ab60fa6a5e66fc52c519247
stardiviner <numbchild@gmail.com>
2019-03-01 22:47:18 +0800

    [bookmark] improve bm.el config

--- init/Emacs/init-emacs-bookmark.el
+++ init/Emacs/init-emacs-bookmark.el
@@ -36,45 +36,40 @@
   :ensure t
   :defer t
   :preface (setq bm-face 'highlight)
-  :init (add-to-list 'display-buffer-alist
-                     '("\\*bm-bookmarks\\*" (display-buffer-same-window)))
-  
   (unless (boundp 'bookmark-bm-prefix)
     (define-prefix-command 'bookmark-bm-prefix))
   (global-set-key (kbd "M-g b") 'bookmark-bm-prefix)
-
-  ;; mark
-  (define-key bookmark-bm-prefix (kbd "b") 'bm-toggle)
-  ;; navigate
-  (define-key bookmark-bm-prefix (kbd "n") 'bm-next)
-  (define-key bookmark-bm-prefix (kbd "p") 'bm-previous)
-  ;; show
-  (define-key bookmark-bm-prefix (kbd "j") 'bm-show)
-  (define-key bookmark-bm-prefix (kbd "l") 'bm-show-all)
-  (define-key bookmark-bm-prefix (kbd "N") 'bm-show-next)
-  (define-key bookmark-bm-prefix (kbd "P") 'bm-show-prev)
-  ;; persistent
-  (define-key bookmark-bm-prefix (kbd "t") 'bm-toggle-buffer-persistence)
-  ;; save to repository
-  (define-key bookmark-bm-prefix (kbd "s") 'bm-save) ; Save bookmarks to persistent repository.
-  (define-key bookmark-bm-prefix (kbd "C-s") 'bm-buffer-save) ; Save all bookmarks to repository.
-  (define-key bookmark-bm-prefix (kbd "M-s") 'bm-buffer-save-all) ; Save bookmarks in all buffers.
-  ;; remove/delete
-  (define-key bookmark-bm-prefix (kbd "c") 'bm-remove-all-current-buffer)
-  (define-key bookmark-bm-prefix (kbd "M-c") 'bm-remove-all-all-buffers)
-  
-  (setq bm-in-lifo-order t)
-  (setq bm-cycle-all-buffers nil)
-  (setq temporary-bookmark-p nil)
-  (setq bm-annotate-on-create t)
-  (setq bm-highlight-style 'bm-highlight-line-and-fringe)
-  (setq bm-marker 'bm-marker-left)
-  ;; store & restore bm session.
-  (setq bm-restore-repository-on-load t)
-  ;; this could affect Emacs increasing running usage Persistence
-  (setq bm-repository-file "~/.emacs.d/.bm-repository"
-        bm-repository-size 100)
-  )
+  :bind (:map bookmark-bm-prefix
+              ;; mark
+              ("b" . bm-toggle)
+              ;; navigate
+              ("n" . bm-next)
+              ("p" . bm-previous)
+              ;; show
+              ("j" . bm-show)
+              ("l" . bm-show-all)
+              ("N" . bm-show-next)
+              ("P" . bm-show-prev)
+              ;; persistent
+              ("t" . bm-toggle-buffer-persistence)
+              ;; save to repository
+              ("s" . bm-save) ; Save bookmarks to persistent repository.
+              ("C-s" . bm-buffer-save) ; Save all bookmarks to repository.
+              ("M-s" . bm-buffer-save-all) ; Save bookmarks in all buffers.
+              ;; remove/delete
+              ("c" . bm-remove-all-current-buffer)
+              ("M-c" . bm-remove-all-all-buffers))
+  :init (add-to-list 'display-buffer-alist
+                     '("\\*bm-bookmarks\\*" (display-buffer-same-window)))
+  (setq bm-in-lifo-order t
+        bm-cycle-all-buffers nil
+        bm-annotate-on-create t
+        bm-highlight-style 'bm-highlight-line-and-fringe
+        ;; store & restore bm session.
+        bm-restore-repository-on-load t
+        ;; this could affect Emacs increasing running usage Persistence
+        bm-repository-file "~/.emacs.d/.bm-repository"
+        bm-repository-size 100))
 
 
 (provide 'init-emacs-bookmark)
