661cd7150ff1691783a69a023b8926ddb6dd3542
stardiviner <numbchild@gmail.com>
2019-03-18 10:30:41 +0800

    [authoring] enable doom-modeline word counting in authoring mode.

--- init/authoring/init-authoring.el
+++ init/authoring/init-authoring.el
@@ -16,6 +16,10 @@
   (org-babel-do-load-languages 'org-babel-load-languages '((org-if . t)))
   (org-babel-do-load-languages 'org-babel-load-languages org-babel-load-languages))
 
+(if (featurep 'doom-modeline)
+    (with-eval-after-load 'doom-modeline
+      (setq doom-modeline-enable-word-count t)))
+
 ;;; [ org-wc ] count words under every org-mode trees
 
 (use-package org-wc
