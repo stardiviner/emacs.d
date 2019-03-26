0d77778ef68d279fcc816c43e035fd966d3efe72
stardiviner <numbchild@gmail.com>
2019-03-26 08:57:53 +0800

    [Jupyter] fix ob-jupyter languages not filtered out from ob-async.

--- init/programming/Data Science/init-data-science.el
+++ init/programming/Data Science/init-data-science.el
@@ -19,17 +19,20 @@
                org-babel-execute:jupyter-clojure
                org-babel-execute:jupyter-julia
                org-babel-execute:jupyter-ruby)
+    :init (when (featurep 'ob-async)
+            (with-eval-after-load 'ob-async
+              (add-to-list 'ob-async-no-async-languages-alist "jupyter-python")
+              (add-to-list 'ob-async-no-async-languages-alist "jupyter-clojure")
+              (add-to-list 'ob-async-no-async-languages-alist "jupyter-julia")
+              (add-to-list 'ob-async-no-async-languages-alist "jupyter-ruby")
+              (add-to-list 'ob-async-no-async-languages-alist "jupyter-sql"))
+            (use-package ob-python
+              :commands (org-babel-variable-assignments:python)))
     :config
     (add-to-list 'org-babel-load-languages '(jupyter . t) 'append)
     (org-babel-do-load-languages 'org-babel-load-languages org-babel-load-languages)
     (add-to-list 'display-buffer-alist
-                 '("^\\*jupyter-repl.*\\*" (display-buffer-below-selected)))
-    (if (featurep 'ob-async)
-        (with-eval-after-load 'ob-async
-          (add-to-list 'ob-async-no-async-languages-alist "jupyter-python")
-          (add-to-list 'ob-async-no-async-languages-alist "jupyter-clojure")
-          (add-to-list 'ob-async-no-async-languages-alist "jupyter-julia")
-          (add-to-list 'ob-async-no-async-languages-alist "jupyter-ruby")))))
+                 '("^\\*jupyter-repl.*\\*" (display-buffer-below-selected)))))
 
 ;;; [ Emacs IPython Notebook (EIN) ] -- IPython notebook client in Emacs
 
