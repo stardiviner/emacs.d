d1083f5b661e2c8e8fbedde4da7d79beae26308e
stardiviner <numbchild@gmail.com>
2019-04-07 09:05:25 +0800

    [Rust] enable Rust init file and format code.
    - replace package company-racer with racer.el default capf support.

--- init/programming/languages/init-prog-lang-rust.el
+++ init/programming/languages/init-prog-lang-rust.el
@@ -12,15 +12,9 @@
 (use-package rust-mode
   :ensure t
   :defer t
-  :config
-  (setq rust-indent-offset 4
-        rust-indent-method-chain t
-        rust-indent-where-clause t
-        rust-match-angle-brackets t
-        )
-  (add-hook 'rust-mode-hook #'eldoc-mode)
-  )
-
+  :init (setq rust-indent-method-chain t
+              rust-indent-where-clause t
+              rust-match-angle-brackets t))
 
 ;;; [ flycheck-rust ]
 
@@ -28,32 +22,15 @@
   :ensure t
   :defer t
   :after rust-mode
-  :init
-  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
-  )
-
+  :commands (flycheck-rust-setup)
+  :hook ((rust-mode . flycheck-mode) (flycheck-mode . flycheck-rust-setup)))
 
 ;;; [ racer ]
 
 (use-package racer
   :ensure t
-  :ensure-system-package (racer . "cargo install racer") ; "sudo pacman -S --noconfirm rust-racer"
-  :init
-  ;; config racer in the path.
-  (let ((rust-src-path (getenv "RUST_SRC_PATH")))
-    (if (and rust-src-path (file-directory-p rust-src-path))
-        (setenv "RUST_SRC_PATH" (expand-file-name "~/Code/Rust/rust/src"))
-      (warn (format "Rust SRC path %s does not exist." rust-src-path))))
-  ;; (setq racer-cmd (or (executable-find "racer") "/usr/local/bin/racer"))
-  ;; (setq racer-rust-src-path (or (getenv "RUST_SRC_PATH")
-  ;;                               "/usr/local/src/rust/src"))
-  
-  ;; auto start racer for rust-mode.
-  (add-hook 'rust-mode-hook #'racer-mode)
-  (use-package company-racer
-    :ensure t
-    :init (add-hook 'rust-mode-hook
-                    (lambda () (my-company-add-backend-locally 'company-racer))))
+  :ensure-system-package (racer . "cargo +nightly install racer")
+  :hook ((rust-mode . racer-mode) (racer-mode . eldoc-mode))
   :config
   (define-key racer-mode-map (kbd "C-c C-d C-d") 'racer-describe)
   (add-to-list 'display-buffer-alist
@@ -63,7 +40,9 @@
 
 (use-package cargo
   :ensure t
-  :defer t)
+  :defer t
+  :commands (cargo-minor-mode)
+  :hook (rust-mode . cargo-minor-mode))
 
 ;;; [ ob-rust ] -- Org-mode Babel support for Rust.
 
