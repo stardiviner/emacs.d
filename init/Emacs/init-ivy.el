3fec6fbc0764cf5ed8b0b3b9be6a51c9144173ce
stardiviner <numbchild@gmail.com>
2019-04-14 21:00:15 +0800

    [Ivy] enable and upgrade ivy config.

--- init/Emacs/init-ivy.el
+++ init/Emacs/init-ivy.el
@@ -26,10 +26,12 @@
 
 ;;; [ ivy-rich ] -- More friendly display transformer for ivy.
 
-;; (use-package ivy-rich
-;;   :ensure t
-;;   :config
-;;   (ivy-set-display-transformer 'ivy-switch-buffer 'ivy-rich-switch-buffer-transformer))
+(use-package ivy-rich
+  :ensure t
+  :defer t
+  :commands (ivy-rich-mode)
+  :init (ivy-rich-mode 1)
+  (setq ivy-format-function #'ivy-format-function-line))
 
 ;;; [ counsel ]
 
