11d851328085825f9e63df96b3d97f3a23804bfe
stardiviner <numbchild@gmail.com>
2019-05-26 10:17:36 +0800

    [org-publish] add jQuery script for org-publish exported website.

--- init/org-mode/init-org-publish.el
+++ init/org-mode/init-org-publish.el
@@ -114,6 +114,9 @@
                                    ;; icon
                                    "<link ref=\"icon\" type=\"image/png\" href=\"/assets/images/favicon-16x16.png\" sizes=\"16x16\">"
                                    "<link ref=\"icon\" type=\"image/png\" href=\"/assets/images/favicon-32x32.png\" sizes=\"32x32\">"
+                                   ;; JQuery
+                                   ;; "<script type=\"text/javascript\" src=\"http://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js\"></script>"
+                                   "<script type=\"text/javascript\" src=\"assets/scripts/jquery-3.4.1.min.js\"></script>"
                                    ;; MathJax
                                    "<script type=\"text/javascript\" src=\"https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-AMS_HTML\"></script>"
                                    )
