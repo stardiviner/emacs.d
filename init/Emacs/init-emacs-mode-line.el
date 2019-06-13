a2f6710b4ef9a03957a931bfacce8f184f8e8ff5
stardiviner <numbchild@gmail.com>
2019-06-14 07:21:26 +0800

    [mode-line] doom-modeline segment replace IRC buffer name to icons.

--- init/Emacs/init-emacs-mode-line.el
+++ init/Emacs/init-emacs-mode-line.el
@@ -27,6 +27,25 @@
               doom-modeline-icon t ; don't use icon will be faster
               doom-modeline-github nil
               doom-modeline-irc nil)
+  :config
+  ;; doom-modeline segment replace IRC buffer name to icons.
+  ;; https://github.com/seagle0128/doom-modeline/pull/110
+  (defun smf/irc-icons (buffer)
+    "Given a BUFFER name, return an icon. Else return buffer."
+    (cond
+     ((string-match "#mercurial" buffer)
+      (all-the-icons-faicon "mercury" :v-adjust .05))
+     ((string-match "#bitbucket" buffer)
+      (all-the-icons-faicon "bitbucket" :v-adjust .05))
+     ((string-match "#octobus-hg" buffer)
+      ;; this inserts a custom fonticon, in this case, octobus
+      (propertize "\xe900"
+                  'face '(:family "smf-custom-icons")
+                  'rear-nonsticky t
+                  'display '(raise -0.1)
+                  'font-lock-ignore t))
+     (t buffer)))
+  (setq doom-modeline-irc-stylize #'smf/irc-icons))
 
 
 (provide 'init-emacs-mode-line)
