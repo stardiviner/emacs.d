7c533b5ea420010358ec9eba4b6f060be99150cc
stardiviner <numbchild@gmail.com>
2019-06-15 13:49:58 +0800

    [startup] display Emacs startup time message.

--- init.el
+++ init.el
@@ -403,5 +403,12 @@
 ;;   :defer t
 ;;   :commands (playground-checkout playground-checkout-with-options))
 
+;; Use a hook so the message doesn't get clobbered by other messages.
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (message "Emacs ready in %s with %d garbage collections."
+                     (format "%.2f seconds"
+                             (float-time (time-subtract after-init-time before-init-time)))
+                     gcs-done)))
 
 ;;; init.el ends here
