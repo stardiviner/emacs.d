ff8f38bc773d0fbcc1abd484e98ff9f60062084a
stardiviner <numbchild@gmail.com>
2019-06-22 10:47:06 +0800

    [package] add leaf and leaf-keywords.

--- init/init-package.el
+++ init/init-package.el
@@ -138,6 +138,36 @@
 (use-package quelpa-use-package
   :ensure t)
 
+;;; [ leaf ] -- Simplify your init.el configuration, extended use-package.
+
+(prog1 "prepare leaf"
+  (prog1 "package"
+    (custom-set-variables
+     '(package-archives '(("org"   . "https://orgmode.org/elpa/")
+                          ("melpa" . "https://melpa.org/packages/")
+                          ("gnu"   . "https://elpa.gnu.org/packages/"))))
+    (package-initialize))
+
+  (prog1 "leaf"
+    (unless (package-installed-p 'leaf)
+      (unless (assoc 'leaf package-archive-contents)
+        (package-refresh-contents))
+      (condition-case err
+          (package-install 'leaf)
+        (error
+         (package-refresh-contents)       ; renew local melpa cache if fail
+         (package-install 'leaf))))
+
+    (leaf leaf-keywords
+      :ensure t
+      :config (leaf-keywords-init)))
+
+  (prog1 "optional packages for leaf-keywords"
+    ;; optional packages if you want to use :hydra, :el-get,,,
+    (leaf hydra :ensure t)
+    (leaf el-get :ensure t
+      :custom ((el-get-git-shallow-clone  . t)))))
+
 
 
 (provide 'init-package)
