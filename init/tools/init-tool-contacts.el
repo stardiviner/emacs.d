ba5e5c6a127e34c7ce41338c62f63dd47cdc0019
stardiviner <numbchild@gmail.com>
2019-07-07 18:09:57 +0800

    [tools] add package google-maps.

--- init/tools/init-tool-contacts.el
+++ init/tools/init-tool-contacts.el
@@ -8,6 +8,13 @@
 
 ;;; [ EBDB ] -- Emacs BBDB.
 
+;;; [ google-maps ] -- Google Maps
+
+;; (use-package google-maps
+;;   :ensure t
+;;   :defer t
+;;   :commands (google-maps))
+
 
 
 (provide 'init-tool-contacts)
