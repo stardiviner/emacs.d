0b3521d6f3856a5f41d9dde07d81ffd1692c47ee
stardiviner <numbchild@gmail.com>
2019-07-19 16:49:47 +0800

    [macOS] use differnt font for different operating systems.

--- init/Emacs/init-emacs-font.el
+++ init/Emacs/init-emacs-font.el
@@ -57,8 +57,8 @@
 
 ;; set default font.
 (set-face-attribute 'default nil
-		    :family "Fira Code"
-		    :foundry "PfEd")
+                    :family "Fira Code"
+                    :foundry "PfEd")
 
 ;; set Unicode characters font
 (when (display-graphic-p) ; for `set-fontset-font'
@@ -70,22 +70,51 @@
   (dolist (charset '(kana han symbol cjk-misc bopomofo))
     (set-fontset-font (frame-parameter nil 'font)
                       charset
-                      ;; (font-spec :family "Noto Sans CJK SC" :size 13)
-                      ;; (font-spec :family "Source Han" :size 12)
-                      ;; (font-spec :family "Hack" :size 12)
-                      (font-spec :family "WenQuanYi Micro Hei" :size 12)
-                      ;; (font-spec :family "NSimSun" :size 14)
-                      ;; (font-spec :family "BabelStone Han" :size 14)
-                      ;; 方正清刻本悦宋简体
-                      ;; (font-spec :family "FZQingKeBenYueSongS-R-GB" :size 15)
-                      ;; (font-spec :family "WenYue GuDianMingChaoTi-R-GB" :size 15)
-                      ;; (font-spec :family "WenYue HouXianDaiTi-R-GB" :size 15)
-                      ;; (font-spec :family "Roboto" :size 15)
-                      ;; (font-spec :family "WenyueType GutiFangsong-R-GB" :size 15)
-                      )))
+                      (cl-case system-type
+                        ('gnu/linux
+                         ;; (font-spec :family "Noto Sans CJK SC" :size 13)
+                         ;; (font-spec :family "Source Han" :size 12)
+                         ;; (font-spec :family "Hack" :size 12)
+                         (font-spec :family "WenQuanYi Micro Hei" :size 12)
+                         ;; (font-spec :family "NSimSun" :size 14)
+                         ;; (font-spec :family "BabelStone Han" :size 14)
+                         ;; (font-spec :family "FZQingKeBenYueSongS-R-GB" :size 15) ; 方正清刻本悦宋简体
+                         ;; (font-spec :family "WenYue GuDianMingChaoTi-R-GB" :size 15)
+                         ;; (font-spec :family "WenYue HouXianDaiTi-R-GB" :size 15)
+                         ;; (font-spec :family "Roboto" :size 15)
+                         ;; (font-spec :family "WenyueType GutiFangsong-R-GB" :size 15)
+                         )
+                       ('darwin
+                        ;; macOS Chinese fonts
+                        ;; (font-spec :family "Songti SC" :size 15)
+                        ;; (font-spec :family "Heiti SC" :size 15)
+                        ;; (font-spec :family "Hiragino Sans CNS" :size 15)
+                        ;; (font-spec :family "Lantinghei SC" :size 15)
+                        ;; (font-spec :family "LiHei Pro" :size 15)
+                        ;; (font-spec :family "LiSong Pro" :size 15)
+                        ;; (font-spec :family "PingFang SC" :size 15)
+                        ;; (font-spec :family "Yuanti SC" :size 15)
+                        ;; (font-spec :family "Apple LiGothic" :size 15)
+
+                        ;; macOS handwriting fonts
+                        ;; (font-spec :family "BiauKai" :size 13)
+                        (font-spec :family "Kaiti SC" :size 14)
+                        ;; (font-spec :family "Hannotate SC" :size 14)
+                        ;; (font-spec :family "Libian SC" :size 14)
+                        ;; (font-spec :family "Weibei SC" :size 14)
+                        ;; (font-spec :family "LingWai SC" :size 15)
+                        ;; (font-spec :family "HanziPen SC" :size 14)
+                        ;; (font-spec :family "Yuppy SC" :size 14)
+                        ;; (font-spec :family "Wawati SC" :size 14)
+                        ;; (font-spec :family "Xingkai SC" :size 14)
+                        )))))
 
 ;;; Text Scale
-(setq face-font-rescale-alist '(("WenQuanYi Micro Hei" . 1.3)))
+(cl-case system-type
+  ('gnu/linux
+   (setq face-font-rescale-alist '(("WenQuanYi Micro Hei" . 1.3))))
+  ('darwin
+   (setq face-font-rescale-alist '(("Kaiti SC" . 1.0)))))
 
 
 (provide 'init-emacs-font)
