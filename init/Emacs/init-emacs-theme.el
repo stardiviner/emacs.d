c8475b5d10a871f411501328e8595f60f516259b
stardiviner <numbchild@gmail.com>
2019-07-23 08:51:03 +0800

    [theme] replace three themes with theme 'adwaita.

--- init/Emacs/init-emacs-theme.el
+++ init/Emacs/init-emacs-theme.el
@@ -17,9 +17,7 @@
 
 (require 'color) ; for `color-lighten-name' and `color-darken-name'
 
-;; (use-package delight ; for `doom-themes'
-;;   :ensure t
-;;   :delight buffer-face-mode)
+(load-theme 'adwaita)
 
 ;;; [ leuven-theme ]
 
@@ -28,31 +26,6 @@
 ;;   :no-require t
 ;;   :init (require 'leuven-theme)
 ;;   :config (load-theme 'leuven t))
-
-;; (use-package parchment-theme
-;;   :ensure t
-;;   :config (load-theme 'parchment t))
-
-;;; [ eziam-theme ] -- A mostly monochrome theme, inspired by Tao and Leuven, with dark and light versions.
-
-;; (use-package eziam-theme
-;;   :ensure t
-;;   :no-require t
-;;   :load (eziam-light-theme))
-
-;;; [ spacemacs-theme ] -- Spacemacs default color-theme.
-
-;; (use-package spacemacs-theme
-;;   :ensure t
-;;   :no-require t
-;;   :config (load-theme 'spacemacs-dark t)
-;;   (set-face-attribute 'mode-line nil
-;;                       :background (color-darken-name (face-background 'default) 5)
-;;                       :box '(:color "DarkSlateBlue" :line-width 2))
-;;   (with-eval-after-load 'ivy
-;;     (set-face-attribute 'ivy-current-match nil
-;;                         :background "SeaGreen" :foreground "white"
-;;                         :weight 'normal)))
 
 ;;; [ one-themes ] -- One color scheme.
 
@@ -63,12 +36,6 @@
   :require t
   :config (load-theme 'one-dark t))
 
-;;; [ dimmer ] -- Interactively highlight which buffer is active by dimming the others.
-
-;; (use-package dimmer
-;;   :ensure t
-;;   :init (setq dimmer-exclusion-regexp "*company-*")
-;;   :config (dimmer-mode 1))
 
 
 (provide 'init-emacs-theme)
