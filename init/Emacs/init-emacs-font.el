f2180c290d6665562eeb5f1d997fb2893aeaf926
stardiviner <numbchild@gmail.com>
2019-07-29 13:58:10 +0800

    [font] fix invalid font on Linux.

--- init/Emacs/init-emacs-font.el
+++ init/Emacs/init-emacs-font.el
@@ -13,52 +13,18 @@
 
 ;; need to modify English font settings to suitable with chinese font.
 ;; the value is in 1/10pt, so 100 will give you 10pt, etc
-(set-frame-font (format "%s:pixelsize=%d" "Fira Code" 12) t)
+(cl-case system-type
+  ('gnu/linux
+   (set-frame-font (format "%s:pixelsize=%d" "DejaVu Sans Mono" 12) t)
+   (set-face-attribute 'default nil
+                       :family "DejaVu Sans Mono"
+                       :foundry "PfEd"))
+  ('darwin
+   (set-frame-font (format "%s:pixelsize=%d" "Fira Code" 12) t)
+   (set-face-attribute 'default nil
+                       :family "Fira Code"
+                       :foundry "PfEd")))
 
-;; For Fira Code font.
-;; (when (window-system)
-;;   (set-frame-font "Fira Code"))
-;; (let ((alist '((33 . ".\\(?:\\(?:==\\|!!\\)\\|[!=]\\)")
-;;                (35 . ".\\(?:###\\|##\\|_(\\|[#(?[_{]\\)")
-;;                (36 . ".\\(?:>\\)")
-;;                (37 . ".\\(?:\\(?:%%\\)\\|%\\)")
-;;                (38 . ".\\(?:\\(?:&&\\)\\|&\\)")
-;;                (42 . ".\\(?:\\(?:\\*\\*/\\)\\|\\(?:\\*[*/]\\)\\|[*/>]\\)")
-;;                (43 . ".\\(?:\\(?:\\+\\+\\)\\|[+>]\\)")
-;;                (45 . ".\\(?:\\(?:-[>-]\\|<<\\|>>\\)\\|[<>}~-]\\)")
-;;                ;; (46 . ".\\(?:\\(?:\\.[.<]\\)\\|[.=-]\\)")
-;;                (47 . ".\\(?:\\(?:\\*\\*\\|//\\|==\\)\\|[*/=>]\\)")
-;;                (48 . ".\\(?:x[a-zA-Z]\\)")
-;;                (58 . ".\\(?:::\\|[:=]\\)")
-;;                (59 . ".\\(?:;;\\|;\\)")
-;;                (60 . ".\\(?:\\(?:!--\\)\\|\\(?:~~\\|->\\|\\$>\\|\\*>\\|\\+>\\|--\\|<[<=-]\\|=[<=>]\\||>\\)\\|[*$+~/<=>|-]\\)")
-;;                (61 . ".\\(?:\\(?:/=\\|:=\\|<<\\|=[=>]\\|>>\\)\\|[<=>~]\\)")
-;;                (62 . ".\\(?:\\(?:=>\\|>[=>-]\\)\\|[=>-]\\)")
-;;                (63 . ".\\(?:\\(\\?\\?\\)\\|[:=?]\\)")
-;;                (91 . ".\\(?:]\\)")
-;;                (92 . ".\\(?:\\(?:\\\\\\\\\\)\\|\\\\\\)")
-;;                (94 . ".\\(?:=\\)")
-;;                (119 . ".\\(?:ww\\)")
-;;                (123 . ".\\(?:-\\)")
-;;                (124 . ".\\(?:\\(?:|[=|]\\)\\|[=>|]\\)")
-;;                (126 . ".\\(?:~>\\|~~\\|[>=@~-]\\)")
-;;                )
-;;              ))
-;;   (dolist (char-regexp alist)
-;;     (set-char-table-range composition-function-table (car char-regexp)
-;;                           `([,(cdr char-regexp) 0 font-shape-gstring]))))
-
-;;; Styled fonts
-;; (set-frame-font (format "%s:pixelsize=%d" "STALKER1" 16) t)
-
-;;; font "Inconsolata" is good for mixing English & Chinese.
-;; https://emacs.stackexchange.com/questions/10464/japanese-cjk-font-settings-for-proper-horizontal-alignment
-;; (set-frame-font "Inconsolata-12" t)
-
-;; set default font.
-(set-face-attribute 'default nil
-                    :family "Fira Code"
-                    :foundry "PfEd")
 
 ;; set Unicode characters font
 (when (display-graphic-p) ; for `set-fontset-font'
@@ -84,30 +50,30 @@
                          ;; (font-spec :family "Roboto" :size 15)
                          ;; (font-spec :family "WenyueType GutiFangsong-R-GB" :size 15)
                          )
-                       ('darwin
-                        ;; macOS Chinese fonts
-                        ;; (font-spec :family "Songti SC" :size 15)
-                        ;; (font-spec :family "Heiti SC" :size 15)
-                        ;; (font-spec :family "Hiragino Sans CNS" :size 15)
-                        ;; (font-spec :family "Lantinghei SC" :size 15)
-                        ;; (font-spec :family "LiHei Pro" :size 15)
-                        ;; (font-spec :family "LiSong Pro" :size 15)
-                        ;; (font-spec :family "PingFang SC" :size 15)
-                        ;; (font-spec :family "Yuanti SC" :size 15)
-                        ;; (font-spec :family "Apple LiGothic" :size 15)
+                        ('darwin
+                         ;; macOS Chinese fonts
+                         ;; (font-spec :family "Songti SC" :size 15)
+                         ;; (font-spec :family "Heiti SC" :size 15)
+                         ;; (font-spec :family "Hiragino Sans CNS" :size 15)
+                         ;; (font-spec :family "Lantinghei SC" :size 15)
+                         ;; (font-spec :family "LiHei Pro" :size 15)
+                         ;; (font-spec :family "LiSong Pro" :size 15)
+                         ;; (font-spec :family "PingFang SC" :size 15)
+                         ;; (font-spec :family "Yuanti SC" :size 15)
+                         ;; (font-spec :family "Apple LiGothic" :size 15)
 
-                        ;; macOS handwriting fonts
-                        ;; (font-spec :family "BiauKai" :size 13)
-                        (font-spec :family "Kaiti SC" :size 14)
-                        ;; (font-spec :family "Hannotate SC" :size 14)
-                        ;; (font-spec :family "Libian SC" :size 14)
-                        ;; (font-spec :family "Weibei SC" :size 14)
-                        ;; (font-spec :family "LingWai SC" :size 15)
-                        ;; (font-spec :family "HanziPen SC" :size 14)
-                        ;; (font-spec :family "Yuppy SC" :size 14)
-                        ;; (font-spec :family "Wawati SC" :size 14)
-                        ;; (font-spec :family "Xingkai SC" :size 14)
-                        )))))
+                         ;; macOS handwriting fonts
+                         ;; (font-spec :family "BiauKai" :size 13)
+                         (font-spec :family "Kaiti SC" :size 14)
+                         ;; (font-spec :family "Hannotate SC" :size 14)
+                         ;; (font-spec :family "Libian SC" :size 14)
+                         ;; (font-spec :family "Weibei SC" :size 14)
+                         ;; (font-spec :family "LingWai SC" :size 15)
+                         ;; (font-spec :family "HanziPen SC" :size 14)
+                         ;; (font-spec :family "Yuppy SC" :size 14)
+                         ;; (font-spec :family "Wawati SC" :size 14)
+                         ;; (font-spec :family "Xingkai SC" :size 14)
+                         )))))
 
 ;;; Text Scale
 (cl-case system-type
