b214e780296c558ddede922834b58fe349fed11c
stardiviner <numbchild@gmail.com>
2019-08-04 17:44:42 +0800

    [macOS] fix init-macOS.el load before init-emacs-face.el priority issue.

--- init/Emacs/init-emacs-face.el
+++ init/Emacs/init-emacs-face.el
@@ -8,18 +8,34 @@
 
 ;;; [ variable-pitch ] -- support for displaying proportional fonts.
 
-(set-face-attribute 'variable-pitch nil
-                    :family "Source Sans Pro"
-                    :height 105
-                    :weight 'normal :slant 'normal)
-(set-face-attribute 'fixed-pitch nil
-                    :family "Hack"
-                    :height 100
-                    :weight 'normal :slant 'normal)
-(set-face-attribute 'fixed-pitch-serif nil
-                    :family "Hack"
-                    :height 120
-                    :weight 'normal :slant 'italic)
+(cl-case system-type
+  ('gnu/linux
+   (set-face-attribute 'variable-pitch nil
+                       :family "Source Sans Pro"
+                       :height 105
+                       :weight 'normal :slant 'normal)
+   (set-face-attribute 'fixed-pitch nil
+                       :family "Hack"
+                       :height 100
+                       :weight 'normal :slant 'normal)
+   (set-face-attribute 'fixed-pitch-serif nil
+                       :family "Hack"
+                       :height 120
+                       :weight 'normal :slant 'italic))
+  ('darwin
+   (set-face-attribute 'variable-pitch nil
+                       :family "DejaVu Sans Mono"
+                       :height 120
+                       :weight 'normal :slant 'normal)
+   (set-face-attribute 'fixed-pitch nil
+                       :family "Hack"
+                       :height 120
+                       :weight 'normal :slant 'normal)
+   (set-face-attribute 'fixed-pitch-serif nil
+                       :family "Hack"
+                       :height 120
+                       :weight 'normal :slant 'italic)))
+
 
 
 
