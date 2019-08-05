960e6d5fb022a41585d64b98f0abf9954febc904
stardiviner <numbchild@gmail.com>
2019-08-05 13:50:34 +0800

    [face] set variable-pitch faces to a union font family.

--- init/Emacs/init-emacs-face.el
+++ init/Emacs/init-emacs-face.el
@@ -8,34 +8,18 @@
 
 ;;; [ variable-pitch ] -- support for displaying proportional fonts.
 
-(cl-case system-type
-  ('gnu/linux
-   (set-face-attribute 'variable-pitch nil
-                       :family "Source Sans Pro"
-                       :height 105
-                       :weight 'normal :slant 'normal)
-   (set-face-attribute 'fixed-pitch nil
-                       :family "Hack"
-                       :height 100
-                       :weight 'normal :slant 'normal)
-   (set-face-attribute 'fixed-pitch-serif nil
-                       :family "Hack"
-                       :height 120
-                       :weight 'normal :slant 'italic))
-  ('darwin
-   (set-face-attribute 'variable-pitch nil
-                       :family "DejaVu Sans Mono"
-                       :height 120
-                       :weight 'normal :slant 'normal)
-   (set-face-attribute 'fixed-pitch nil
-                       :family "Hack"
-                       :height 120
-                       :weight 'normal :slant 'normal)
-   (set-face-attribute 'fixed-pitch-serif nil
-                       :family "Hack"
-                       :height 120
-                       :weight 'normal :slant 'italic)))
-
+(set-face-attribute 'variable-pitch nil
+                    :family "DejaVu Sans Mono"
+                    :height 105
+                    :weight 'normal :slant 'normal)
+(set-face-attribute 'fixed-pitch nil
+                    :family "DejaVu Sans Mono"
+                    :height 105
+                    :weight 'normal :slant 'normal)
+(set-face-attribute 'fixed-pitch-serif nil
+                    :family "DejaVu Serif"
+                    :height 120
+                    :weight 'normal :slant 'italic)
 
 
 
