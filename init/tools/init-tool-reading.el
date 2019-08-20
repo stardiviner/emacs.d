2eeb2552f90ac2a321e1147b580a05576dc39321
stardiviner <numbchild@gmail.com>
2019-08-20 21:50:21 +0800

    [reading] add a command novel-reading-mode.

--- init/tools/init-tool-reading.el
+++ init/tools/init-tool-reading.el
@@ -7,6 +7,35 @@
 
 ;;; Code:
 
+;;; [ novel reading mode ]
+
+(defun novel-read-mode ()
+  "Setup current frame to be suitable for reading long novel/article text.
+
+• Set frame width to 70
+• Line wrap at word boundaries.
+• Line spacing is increased.
+• Proportional width font is used.
+
+Call again to toggle back.
+URL `http://ergoemacs.org/emacs/emacs_novel_reading_mode.html'
+Version: 2019-01-30
+Author: xah"
+  (interactive)
+  (let ()
+    (if (eq (frame-parameter (selected-frame) 'width) 70)
+        (progn
+          (set-frame-parameter (selected-frame) 'width 106)
+          (variable-pitch-mode 0)
+          (setq line-spacing nil)
+          (setq word-wrap nil))
+      (progn
+        (set-frame-parameter (selected-frame) 'width 70)
+        (variable-pitch-mode 1)
+        (setq line-spacing 0.4)
+        (setq word-wrap t))))
+  (redraw-frame (selected-frame)))
+
 ;;; [ spray ] -- [Emacs] an elisp implementation of OpenSpritz
 
 (use-package spray
