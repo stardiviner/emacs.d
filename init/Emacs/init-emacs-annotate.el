592541ac4b110241d3a1aba045035eafb64ad0d6
stardiviner <numbchild@gmail.com>
2019-09-09 09:24:09 +0800

    [annoate] disable annotate package.

--- init/Emacs/init-emacs-annotate.el
+++ init/Emacs/init-emacs-annotate.el
@@ -1,6 +1,6 @@
 ;;; init-emacs-annotate.el --- init for Emacs annotate.
 
-;;; Time-stamp: <2019-09-03 08:51:04 stardiviner>
+;;; Time-stamp: <2019-09-09 09:24:03 stardiviner>
 
 ;;; Commentary:
 
@@ -10,21 +10,21 @@
 
 ;;; [ annotate ] -- add annotations to arbitrary files without changing the files themselves.
 
-(use-package annotate
-  :ensure t
-  :defer t
-  :commands (annotate-mode
-             annotate-annotate annotate-next-annotation annotate-previous-annotation
-             annotate-clear-annotations annotate-integrate-annotations
-             annotate-export-annotations annotate-save-annotations)
-  :init
-  ;; (dolist (hook '(emacs-lisp-mode-hook
-  ;;                 lisp-mode-hook
-  ;;                 clojure-mode-hook
-  ;;                 python-mode-hook
-  ;;                 java-mode-hook))
-  ;;   (add-hook hook #'annotate-mode))
-  (setq annotate-file (concat user-emacs-directory ".annotations")))
+;; (use-package annotate
+;;   :ensure t
+;;   :defer t
+;;   :commands (annotate-mode
+;;              annotate-annotate annotate-next-annotation annotate-previous-annotation
+;;              annotate-clear-annotations annotate-integrate-annotations
+;;              annotate-export-annotations annotate-save-annotations)
+;;   :init
+;;   ;; (dolist (hook '(emacs-lisp-mode-hook
+;;   ;;                 lisp-mode-hook
+;;   ;;                 clojure-mode-hook
+;;   ;;                 python-mode-hook
+;;   ;;                 java-mode-hook))
+;;   ;;   (add-hook hook #'annotate-mode))
+;;   (setq annotate-file (concat user-emacs-directory ".annotations")))
 
 
 
