42c1d9a59ed6918be179085257dcf446bccb320f
stardiviner <numbchild@gmail.com>
2019-09-17 12:53:31 +0800

    [company] hide `company-ispell' echo message "Starting 'look' process"

--- init/Emacs/init-company-mode.el
+++ init/Emacs/init-company-mode.el
@@ -84,6 +84,10 @@
   ;; [ company-ispell ]
   ;; (setq company-ispell-dictionary "/usr/share/dict/words")
   ;; (add-to-list 'company-backend 'company-ispell)
+  ;; hide `company-ispell' echo message "Starting 'look' process".
+  (advice-add 'ispell-lookup-words :around
+              (lambda (orig &rest args)
+                (shut-up (apply orig args))))
   
   ;; keybindings
 
