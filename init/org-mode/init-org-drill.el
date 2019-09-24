7679c8d8a4e5f21005a2af69420ec7ff81c21a60
stardiviner <numbchild@gmail.com>
2019-09-25 07:31:22 +0800

    [drills] refactor org-drill config, wrap custom advice into use-package wrapper

--- init/org-mode/init-org-drill.el
+++ init/org-mode/init-org-drill.el
@@ -10,9 +10,15 @@
 
 (use-package org-drill
   :ensure t
-  :defer t
   :commands (org-drill)
   :init
+  (setq org-drill-use-visible-cloze-face-p nil ; t will caused [] invalid headline fontify.
+        org-drill-hide-item-headings-p nil
+        org-drill-save-buffers-after-drill-sessions-p nil
+        ;; org-drill-spaced-repetition-algorithm 'sm2
+        )
+  
+  :config
   ;; add org-drill topic property into default properties list.
   (add-to-list 'org-default-properties "DRILL_CARD_TYPE")
   (setq org-tag-alist
@@ -21,12 +27,16 @@
   (add-to-list 'org-tag-faces
                '("drill" :foreground "coral"))
 
-  (setq org-drill-use-visible-cloze-face-p nil ; t will caused [] invalid headline fontify.
-        org-drill-hide-item-headings-p nil
-        org-drill-save-buffers-after-drill-sessions-p nil
-        ;; org-drill-spaced-repetition-algorithm 'sm2
-        )
-  ;; record queried words to Org-mode drill files
+  (defun my-org-drill ()
+    "My wrapper helper function around `org-drill'."
+    (interactive)
+    (find-file my-org-drill-words-file)
+    (if (org-drill-entries-pending-p)
+        (org-drill-resume)
+      (org-drill)))
+  (define-key Org-prefix (kbd "w") 'my-org-drill)
+
+  ;; record (GoldenDict) queried words to Org-mode drill files for memory.
   (require 'org-capture) ; load `org-capture-templates'
 
   (setq my-org-drill-words-file (concat org-directory "/Drills/Words.org"))
@@ -46,58 +56,49 @@
                    :empty-lines 1))
                 org-capture-templates))
 
-  (defun my-org-drill ()
-    "My wrapper helper function around `org-drill'."
+  (defun my-org-drill-word-exist-p (word)
+    "Check word exist in Words.org file?"
+    (let ((drill-words (org-map-entries
+                        (lambda () (nth 4 (org-heading-components)))
+                        nil
+                        (list (expand-file-name my-org-drill-words-file)))))
+      (not ; reverse word exist boolean
+       (cl-some (lambda (drill) (string-equal drill word)) drill-words))))
+
+  (defun my-org-drill-record-word ()
+    "Record word to org-drill words file."
     (interactive)
-    (find-file my-org-drill-words-file)
-    (if (org-drill-entries-pending-p)
-        (org-drill-resume)
-      (org-drill)))
-  (define-key Org-prefix (kbd "w") 'my-org-drill)
+    (require 'expand-region)
+    (let ((word (downcase
+                 (substring-no-properties
+                  (if (region-active-p)
+                      (buffer-substring-no-properties (mark) (point))
+                    (thing-at-point 'word))))))
+      (unless (not (and (my-org-drill-word-exist-p word)
+                        (yes-or-no-p (format "org-drill record this word (%s): " word))))
+        ;; copy original source sentence for `org-capture' template `%c'.
+        (save-mark-and-excursion
+          (ignore-errors (er/mark-sentence))
+          (kill-ring-save (region-beginning) (region-end))
+          (if (region-active-p) (deactivate-mark)))
+        ;; region select word for `org-capture' template "`%i'".
+        (unless (region-active-p) (er/mark-word))
+        ;; call org-capture template programmatically.
+        (org-capture nil "w")
+        ;; disable region selection.
+        ;; FIXME: the current buffer is CAPTURE buffer.
+        (if (region-active-p) (deactivate-mark)))))
+
+  (autoload 'goldendict-dwim "goldendict")
+  (advice-add 'goldendict-dwim :after #'my-org-drill-record-word)
+
+  (define-key Org-prefix (kbd "C-w") 'my-org-drill-record-word)
   )
 
 
 (use-package stem-english
   :ensure t
   :defer t)
-
-(defun my-org-drill-check-not-exist (word)
-  "Check word exist in Words.org file?"
-  (let ((drill-words (org-map-entries
-                      (lambda () (nth 4 (org-heading-components)))
-                      nil
-                      (list (expand-file-name my-org-drill-words-file)))))
-    (not ; reverse word exist boolean
-     (cl-some (lambda (drill) (string-equal drill word)) drill-words))))
-
-(defun my-org-drill-record-word ()
-  "Record word to org-drill words file."
-  (interactive)
-  (require 'expand-region)
-  (let ((word (downcase
-               (substring-no-properties
-                (if (region-active-p)
-                    (buffer-substring-no-properties (mark) (point))
-                  (thing-at-point 'word))))))
-    (unless (not (and (my-org-drill-check-not-exist word)
-                      (yes-or-no-p (format "org-drill record this word (%s): " word))))
-      ;; copy original source sentence for `org-capture' template `%c'.
-      (save-mark-and-excursion
-        (ignore-errors (er/mark-sentence))
-        (kill-ring-save (region-beginning) (region-end))
-        (if (region-active-p) (deactivate-mark)))
-      ;; region select word for `org-capture' template "`%i'".
-      (unless (region-active-p) (er/mark-word))
-      ;; call org-capture template programmatically.
-      (org-capture nil "w")
-      ;; disable region selection.
-      ;; FIXME: the current buffer is CAPTURE buffer.
-      (if (region-active-p) (deactivate-mark)))))
-
-(autoload 'goldendict-dwim "goldendict")
-(advice-add 'goldendict-dwim :after #'my-org-drill-record-word)
-
-(define-key Org-prefix (kbd "C-w") 'my-org-drill-record-word)
 
 ;; TODO: still invalid [s] keybinding setup in org-drill session.
 ;; (defun org-drill-setup-in-file ()
