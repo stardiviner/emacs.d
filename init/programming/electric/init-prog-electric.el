cf51ff56c50e02c06fd97ea0288f8d46c9ff3ce5
stardiviner <numbchild@gmail.com>
2019-10-04 19:55:15 +0800

    [smartparens] disable package smartparens

--- init/programming/electric/init-prog-electric.el
+++ init/programming/electric/init-prog-electric.el
@@ -21,55 +21,46 @@
 
 ;;; [ smartparens ] -- deals with parens pairs and tries to be smart about it.
 
-(use-package smartparens
-  :ensure t
-  :defer t
-  :delight smartparens-mode
-  :config
-  (add-to-list 'sp-ignore-modes-list 'org-mode) ;`smartparens' is heavy in `org-self-insert-command'.
-  (add-to-list 'sp-ignore-modes-list 'emacs-lisp-mode)
-  (add-to-list 'sp-ignore-modes-list 'clojure-mode)
-  (add-to-list 'sp-ignore-modes-list 'lisp-mode)
-  (add-to-list 'sp-ignore-modes-list 'scheme-mode)
-  (add-to-list 'sp-ignore-modes-list 'python-mode)  
-
-  ;; (setq sp-navigate-consider-sgml-tags '(html-erb-mode
-  ;;                                        web-mode
-  ;;                                        nxml-mode sgml-mode
-  ;;                                        nxhtml-mode html-mode rhtml-mode
-  ;;                                        jinja2-mode)
-  ;;       )
-  ;; (setq sp-override-key-bindings '((\"C-M-f\" . sp-forward-sexp)
-  ;;                                  (\"C-<right>\" . nil)))
-
-  (sp-local-pair 'minibuffer-inactive-mode "'" nil :actions nil)
-
-  ;; `code` in clojure comment
-  (sp-with-modes '(clojure-mode) (sp-local-pair "`" "`"))
-
-  ;; smartparens for other modes.
-  (require 'smartparens-ruby)
-  (require 'smartparens-html)
-
-  (sp-with-modes '(rhtml-mode)
-    ;; (sp-local-pair "<" ">")
-    (sp-local-pair "<%" "%>"))
-
-  (sp-with-modes '(markdown-mode gfm-mode rst-mode)
-    (sp-local-pair "*" "*" :bind "C-*")
-    (sp-local-tag "2" "**" "**")
-    (sp-local-tag "s" "```scheme" "```")
-    (sp-local-tag "<"  "<_>" "</_>" :transform 'sp-match-sgml-tags))
-
-  ;; (set-face-attribute 'sp-show-pair-match-face nil
-  ;;                     :background "green yellow")
-
-  ;; NOTE: to make `sp-ignore-modes-list' work, put `smartparens-mode' at the end.
-  (smartparens-global-mode t)
-  (show-smartparens-global-mode t)
-  ;; (add-hook 'prog-mode-hook #'turn-on-smartparens-mode)
-  ;; (add-hook 'c-mode-common-hook 'smartparens-mode)
-  )
+;; (use-package smartparens
+;;   :ensure t
+;;   :defer t
+;;   :delight smartparens-mode
+;;   :config
+;;   (add-to-list 'sp-ignore-modes-list 'org-mode) ;`smartparens' is heavy in `org-self-insert-command'.
+;;   (add-to-list 'sp-ignore-modes-list 'emacs-lisp-mode)
+;;   (add-to-list 'sp-ignore-modes-list 'clojure-mode)
+;;   (add-to-list 'sp-ignore-modes-list 'lisp-mode)
+;;   (add-to-list 'sp-ignore-modes-list 'scheme-mode)
+;;   (add-to-list 'sp-ignore-modes-list 'python-mode)  
+;;
+;;   ;; smartparens for other modes.
+;;   (require 'smartparens-ruby)
+;;   (require 'smartparens-html)
+;;
+;;   (sp-local-pair 'minibuffer-inactive-mode "'" nil :actions nil)
+;;   (sp-local-pair 'emacs-lisp-mode "'" nil :actions nil)
+;;
+;;   ;; `code` in clojure comment
+;;   (sp-with-modes '(clojure-mode) (sp-local-pair "`" "`"))
+;;   (sp-local-pair 'clojure-mode "'" nil :actions nil)
+;;
+;;   (sp-with-modes '(rhtml-mode) (sp-local-pair "<%" "%>"))
+;;
+;;   (sp-with-modes '(markdown-mode gfm-mode rst-mode)
+;;     (sp-local-pair "*" "*" :bind "C-*")
+;;     (sp-local-tag "2" "**" "**")
+;;     (sp-local-tag "s" "```scheme" "```")
+;;     (sp-local-tag "<"  "<_>" "</_>" :transform 'sp-match-sgml-tags))
+;;
+;;   ;; (set-face-attribute 'sp-show-pair-match-face nil
+;;   ;;                     :background "green yellow")
+;;
+;;   ;; NOTE: to make `sp-ignore-modes-list' work, put `smartparens-mode' at the end.
+;;   (smartparens-global-mode t)
+;;   (show-smartparens-global-mode t)
+;;   ;; (add-hook 'prog-mode-hook #'turn-on-smartparens-mode)
+;;   ;; (add-hook 'c-mode-common-hook 'smartparens-mode)
+;;   )
 
 ;;; [ rainbow-identifiers ] -- highlight identifiers according to their names.
 
