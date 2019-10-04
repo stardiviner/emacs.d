ef4e8dabe4c11f14bf282bb716a1528d2d126eac
stardiviner <numbchild@gmail.com>
2019-10-04 22:07:48 +0800

    [comment] refactor comment package configs.

--- init/programming/comment/init-prog-comment.el
+++ init/programming/comment/init-prog-comment.el
@@ -8,16 +8,16 @@
 (unless (boundp 'prog-comment-prefix)
   (define-prefix-command 'prog-comment-prefix))
 (global-set-key (kbd "M-;") 'prog-comment-prefix)
+(global-set-key (kbd "M-g c") 'prog-comment-prefix)
 
 
 (setq comment-auto-fill-only-comments t
-      comment-multi-line t
-      )
+      comment-multi-line t)
 
 
 ;;; prefix: [M-;], `prog-comment-prefix'
 
-(define-key prog-comment-prefix (kbd "M-;") 'comment-dwim)
+(define-key prog-comment-prefix (kbd "c") 'comment-dwim)
 ;; or with [C-u N]
 (global-set-key (kbd "C-x C-;") 'comment-line)
 (define-key prog-comment-prefix (kbd "r") 'comment-or-uncomment-region)
@@ -39,11 +39,12 @@ column.  Place the point after the comment box."
     (goto-char e)
     (set-marker e nil)))
 
+;;; [ banner-comment ] -- turn a comment into a banner.
 
-
-(unless (boundp 'comment-tag-prefix)
-  (define-prefix-command 'comment-tag-prefix))
-(global-set-key (kbd "M-g c") 'comment-tag-prefix)
+(use-package banner-comment
+  :ensure t
+  :bind (:map prog-comment-prefix ("=" . banner-comment)))
+
 
 ;;; [ hl-todo ] -- highlight TODO and similar keywords.
 
@@ -51,11 +52,11 @@ column.  Place the point after the comment box."
   :ensure t
   :defer t
   :commands (hl-todo-next hl-todo-previous hl-todo-occur)
-  :bind (:map comment-tag-prefix
+  :bind (:map prog-comment-prefix
               ("n" . hl-todo-next)
               ("p" . hl-todo-previous)
               ("o" . hl-todo-occur)
-              ("i" . hl-todo-insert-keyword))
+              ("t" . hl-todo-insert-keyword))
   :init (add-hook 'prog-mode-hook #'hl-todo-mode)
   :config
   (add-to-list 'hl-todo-keyword-faces '("PERFORMANCE" . "#5f7f5f"))
@@ -83,22 +84,13 @@ column.  Place the point after the comment box."
                                     ('light
                                      (color-darken-name (face-background 'default) 10))
                                     ('dark
-                                     (color-darken-name (face-background 'default) 5))))
-  )
-
+                                     (color-darken-name (face-background 'default) 5)))))
 
 ;;; [ org-commentary ] -- insert/generate/update conventional library headers using Org-mode.
 
 (use-package org-commentary
   :ensure t
   :commands (org-commentary-update))
-
-;;; [ banner-comment ] -- turn a comment into a banner.
-
-(use-package banner-comment
-  :ensure t
-  :commands (banner-comment)
-  :bind (:map prog-comment-prefix ("M-b" . banner-comment)))
 
 
 (provide 'init-prog-comment)
