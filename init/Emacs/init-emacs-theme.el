bfdc7a80008d959125d6b2d75a33052b48e268ff
stardiviner <numbchild@gmail.com>
2019-12-08 21:32:02 +0800

    [theme] add package circadian to automatically switching theme

--- init/Emacs/init-emacs-theme.el
+++ init/Emacs/init-emacs-theme.el
@@ -19,19 +19,35 @@
 
 ;;; [ leuven-theme ]
 
-;; (use-package leuven-theme
-;;   :load-path "~/Code/Emacs/leuven-theme/"
-;;   :no-require t
-;;   :init (require 'leuven-theme)
-;;   :config (load-theme 'leuven t))
+(use-package leuven-theme
+  :load-path "~/Code/Emacs/leuven-theme/"
+  :no-require t
+  :defer t
+  :init (require 'leuven-theme)
+  ;; :config (load-theme 'leuven t)
+  )
 
 ;;; [ one-themes ] -- One color scheme.
 
 (use-package one-themes
   :load-path "~/Code/Emacs/one-themes"
   :no-require t
+  :defer t
   :init (require 'one-themes)
-  :config (load-theme 'one-dark t))
+  ;; :config (load-theme 'one-dark t)
+  )
+
+(use-package circadian
+  :ensure t
+  :config
+  (setq calendar-location-name "Shaoxing Town"
+        calendar-time-zone +480
+        calendar-latitude 29.72
+        calendar-longitude 120.20)
+  ;; NOTE: make sure to use `:defer' keyword for theme `use-package'.
+  (setq circadian-themes '((:sunrise . leuven)
+                           (:sunset . one-dark)))
+  (circadian-setup))
 
 
 (provide 'init-emacs-theme)
