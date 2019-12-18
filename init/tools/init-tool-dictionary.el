7d68cf5e6993a4bba208feb4bdcb5cc93474df9c
stardiviner <numbchild@gmail.com>
2019-12-18 21:07:38 +0800

    [dictionary] remove some don't used packages.
    - remove sdcv
    - remove youdao-dictionary
    - remove helm-baidu-fanyi-suggest
    - remove insert-translated-name

--- init/tools/init-tool-dictionary.el
+++ init/tools/init-tool-dictionary.el
@@ -11,42 +11,11 @@
   (define-prefix-command 'dictionary-prefix))
 (define-key tools-prefix (kbd "d") 'dictionary-prefix)
 
-;;; [ sdcv.el ]
-
-(leaf sdcv
-  :el-get (sdcv :url "https://github.com/manateelazycat/sdcv.git")
-  :ensure posframe
-  :commands (sdcv-search-pointer sdcv-search-pointer+ sdcv-search-input sdcv-search-input+)
-  :bind (:dictionary-prefix ("C-d" . sdcv-search-pointer+)
-                            ("M-d" . sdcv-search-input))
-  :init
-  (setq sdcv-say-word-p t)
-  ;; a simple dictionary list for popup display
-  (setq sdcv-dictionary-simple-list
-        '(;; "懒虫简明英汉词典"
-          ;; "懒虫简明汉英词典"
-          ;; "WordNet"
-          "牛津英汉双解美化版"))
-  ;; a complete dictionary list for buffer display
-  (setq sdcv-dictionary-complete-list
-        '("WordNet"
-          "牛津英汉双解美化版"
-          "朗道英汉字典5.0"
-          "朗道汉英字典5.0"))
-  ;; :config
-  ;; (set-face-attribute 'sdcv-tooltip-face nil
-  ;;                     :foreground (face-foreground 'default)
-  ;;                     :background (cl-case (alist-get 'background-mode (frame-parameters))
-  ;;                                   ('light
-  ;;                                    (color-darken-name (face-background 'default) 10))
-  ;;                                   ('dark
-  ;;                                    (color-lighten-name (face-background 'default) 5))))
-  )
-
 ;;; [ Goldendict ]
 
 (use-package goldendict
   :ensure t
+  :commands (goldendict-dwim)
   :bind (:map dictionary-prefix ("d" . goldendict-dwim)))
 
 ;;; [ google-translate ] -- Emacs interface to Google Translate.
@@ -114,36 +83,6 @@
   (add-to-list 'display-buffer-alist
                '("^\\*baidu-translate\\*" .
                  (display-buffer-reuse-window display-buffer-below-selected))))
-
-;;; [ youdao-dictionary ] -- Youdao Dictionary (有道词典) interface for Emacs
-
-(use-package youdao-dictionary
-  :ensure t
-  :defer t
-  :bind (:map dictionary-prefix ("y" . youdao-dictionary-search-at-point))
-  :init
-  (add-to-list 'display-buffer-alist
-               '("^\\*Youdao Dictionary\\*" .
-                 (display-buffer-reuse-window display-buffer-below-selected))))
-
-;;; [ insert-translated-name ] -- Insert translated string as variable, function name or sentence for comment.
-
-(leaf insert-translated-name
-  :el-get (insert-translated-name :url "https://github.com/manateelazycat/insert-translated-name.git")
-  :commands (insert-translated-name-insert)
-  :bind (:dictionary-prefix ("SPC" . insert-translated-name-insert))
-  :config (set-face-attribute 'insert-translated-name-font-lock-mark-word nil
-                              :foreground "hot pink")
-  ;; auto active in some modes:
-  (dolist (hook (list 'atomic-chrome-edit-mode-hook))
-    (add-hook hook '(lambda () (insert-translated-name-use-original-translation)))))
-
-;;; [ helm-baidu-fanyi-suggest ]
-
-(use-package helm-baidu-fanyi-suggest
-  :load-path (lambda () (expand-file-name "init/extension/" user-emacs-directory))
-  :defer t
-  :commands (helm-baidu-fanyi-suggest))
 
 
 (provide 'init-tool-dictionary)
