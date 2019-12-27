d54439ee5a5023e753bea6017b4350669268faa5
stardiviner <numbchild@gmail.com>
2019-12-27 18:22:39 +0800

    [indent] fix void `aggressive-indent-exclude-modes'.

--- init/programming/indent/init-prog-indent.el
+++ init/programming/indent/init-prog-indent.el
@@ -100,22 +100,24 @@
   :delight aggressive-indent-mode
   :commands (aggressive-indent-mode global-aggressive-indent-mode)
   :init (setq aggressive-indent-sit-for-time 0.1)
-  (defun my/aggressive-indent-enable ()
-    (unless (or (member major-mode aggressive-indent-excluded-modes)
-                (member major-mode aggressive-indent-dont-electric-modes))
-      (aggressive-indent-mode 1)))
-  (add-hook 'prog-mode-hook #'my/aggressive-indent-enable)
-  (add-hook 'emacs-lisp-mode-hook #'aggressive-indent-mode)
+  :config
+  ;; enable on mode locally
+  ;; (defun my/aggressive-indent-enable ()
+  ;;   (unless (or (member major-mode aggressive-indent-excluded-modes)
+  ;;               (member major-mode aggressive-indent-dont-electric-modes))
+  ;;     (aggressive-indent-mode 1)))
+  ;; (add-hook 'prog-mode-hook #'my/aggressive-indent-enable)
+  ;; (add-hook 'emacs-lisp-mode-hook #'aggressive-indent-mode)
 
+  ;; enable globally
   (add-to-list 'aggressive-indent-excluded-modes 'lua-mode)
   (add-to-list 'aggressive-indent-excluded-modes 'coq-mode)
   (add-to-list 'aggressive-indent-excluded-modes 'snippet-mode)
   (add-to-list 'aggressive-indent-excluded-modes 'cider-repl-mode)
   (add-to-list 'aggressive-indent-excluded-modes 'csharp-mode)
-
   (add-to-list 'aggressive-indent-dont-electric-modes 'python-mode)
   (global-aggressive-indent-mode 1)
-  :config
+
   ;; The variable `aggressive-indent-dont-indent-if' lets you customize when you
   ;; **don't** want indentation to happen.  For instance, if you think it's
   ;; annoying that lines jump around in `c++-mode' because you haven't typed the
