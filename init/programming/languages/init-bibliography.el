5d36200957fde4299518599fd302a869701fb4a4
stardiviner <numbchild@gmail.com>
2020-01-24 00:31:02 +0800

    fix dangling parentheses

--- init/programming/languages/init-bibliography.el
+++ init/programming/languages/init-bibliography.el
@@ -25,8 +25,7 @@
   (use-package bibtex-style
     :ensure t)
   (use-package bibtex-utils
-    :ensure t)
-  )
+    :ensure t))
 
 ;;; ----------------------------------------------------------------------------
 ;;; [ helm-bibtex ] -- Helm interface for bibliography manager.
@@ -34,39 +33,29 @@
 (use-package helm-bibtex
   :ensure t
   :defer t
-  :bind (:map bibliograph-prefix
-              ("h" . helm-bibtex))
+  :bind (:map bibliograph-prefix ("h" . helm-bibtex))
   :config
   (setq bibtex-completion-bibliography "~/Org/Bibliography/index.bib"
         bibtex-completion-library-path "~/Org/Bibliography/lib/"
         bibtex-completion-notes-path "~/Org/Bibliography/notes/"
         helm-bibtex-bibliography "~/Org/Bibliography/index.bib"
         helm-bibtex-library-path "~/Org/Bibliography/lib/")
-  (setq bibtex-completion-pdf-open-function 'org-open-file ; `find-file'
-        )
-  )
+  (setq bibtex-completion-pdf-open-function 'org-open-file))
 
 ;;; [ company-bibtex ] -- Emacs company-mode completion back-end for Bibtex keys.
 
 (use-package company-bibtex
   :ensure t
   :defer t
-  :init
-  (setq company-bibtex-bibliography
-        '("~/Org/Bibliography/index.bib"))
-
+  :init (setq company-bibtex-bibliography '("~/Org/Bibliography/index.bib"))
   (add-hook 'org-mode-hook
-            (lambda () (my-company-add-backend-locally 'company-bibtex)))
-  )
+            (lambda () (my-company-add-backend-locally 'company-bibtex))))
 
 ;;; [ ebib ] -- a BibTeX database manager.
 
 ;; (use-package ebib
 ;;   :ensure t
-;;   :config
-;;   (use-package ebib-handy
-;;     :ensure t)
-;;   )
+;;   :ensure ebib-handy)
 
 ;;; [ bibretrieve ] -- Retrieving BibTeX entries from the web.
 
