9ca91cc613743c6d0f5ae00d3e59dbfce2397aff
stardiviner <numbchild@gmail.com>
2020-01-30 13:05:12 +0800

    [kill ring] disable undo-tree package

--- init/Emacs/init-emacs-kill-ring.el
+++ init/Emacs/init-emacs-kill-ring.el
@@ -9,18 +9,18 @@
 
 ;;; [ undo-tree ] -- treat undo history as a tree.
 
-(use-package undo-tree
-  :ensure t
-  :init (global-undo-tree-mode 1)
-  (setq undo-tree-visualizer-diff t
-        undo-tree-visualizer-relative-timestamps t)
-  (add-to-list 'display-buffer-alist
-               '("^ \\*undo-tree\\*"
-                 display-buffer-reuse-window display-buffer-in-side-window
-                 (reusable-frames . visible)
-                 (side . right)
-                 (slot . 1)
-                 (window-width . 0.5))))
+;; (use-package undo-tree
+;;   :ensure t
+;;   :init (global-undo-tree-mode 1)
+;;   (setq undo-tree-visualizer-diff t
+;;         undo-tree-visualizer-relative-timestamps t)
+;;   (add-to-list 'display-buffer-alist
+;;                '("^ \\*undo-tree\\*"
+;;                  display-buffer-reuse-window display-buffer-in-side-window
+;;                  (reusable-frames . visible)
+;;                  (side . right)
+;;                  (slot . 1)
+;;                  (window-width . 0.5))))
 
 
 (provide 'init-emacs-kill-ring)
