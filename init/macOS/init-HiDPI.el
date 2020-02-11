80921bd60ab6854553b900ff40d8f0534613e322
stardiviner <numbchild@gmail.com>
2020-02-11 19:29:02 +0800

    [HiDPI] after setup .Xresources, Emacs only need to set default font for HiDPI

--- init/macOS/init-HiDPI.el
+++ init/macOS/init-HiDPI.el
@@ -1,6 +1,6 @@
 ;;; init-HiDPI.el ---  -*- lexical-binding: t; -*-
 
-;;; Time-stamp: <2020-02-10 21:59:39 stardiviner>
+;;; Time-stamp: <2020-02-11 19:27:58 stardiviner>
 
 ;;; Commentary:
 
@@ -9,28 +9,16 @@
 ;;; Code:
 
 ;;; set font
-(set-frame-font (format "%s:pixelsize=%d" "DejaVu Sans Mono" 22) t)
+;; (set-frame-font (format "%s:pixelsize=%d" "DejaVu Sans Mono" 22) t)
+
 (set-face-attribute 'default nil
                     :family "DejaVu Sans Mono"
-                    :font "DejaVu Sans Mono 18")
+                    :font "DejaVu Sans Mono 9")
 
-(set-face-attribute 'variable-pitch nil
-                    :family "DejaVu Sans Mono"
-                    :font "DejaVu Sans Mono 18"
-                    :weight 'normal :slant 'normal)
-(set-face-attribute 'fixed-pitch nil
-                    :family "DejaVu Sans Mono"
-                    :font "DejaVu Sans Mono 18"
-                    :weight 'normal :slant 'normal)
-(set-face-attribute 'fixed-pitch-serif nil
-                    :family "DejaVu Serif"
-                    :font "DejaVu Sans Mono 18"
-                    :weight 'normal :slant 'italic)
+;; (setq org-image-actual-width t)
 
-(set-face-attribute 'org-level-1 nil
-                    :font "DejaVu Sans Mono 22")
-(set-face-attribute 'org-level-2 nil
-                    :font "DejaVu Sans Mono 20")
+;; increase TeX/LaTeX preview scale size.
+;; (setq preview-scale-function 4)
 
 
 
