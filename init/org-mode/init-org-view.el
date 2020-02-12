0e313ce48e11baa325e7dfd14244b257d0b06228
stardiviner <numbchild@gmail.com>
2020-02-12 14:27:42 +0800

    [Org] re-enable org-bullets

--- init/org-mode/init-org-view.el
+++ init/org-mode/init-org-view.el
@@ -21,43 +21,45 @@
 
 ;;; [ org-bullets ]
 
-;; (use-package org-bullets
-;;   :ensure t
-;;   :ensure all-the-icons
-;;   :defer t
-;;   :init (add-hook 'org-mode-hook #'org-bullets-mode)
-;;   :config
-;;   (with-eval-after-load 'all-the-icons
-;;     (setq-default org-bullets-bullet-list
-;;                   `(;; disk -> folder -> file style
-;;                     ;; ,(all-the-icons-faicon "hdd-o")
-;;                     ;; ,(all-the-icons-faicon "cube")
-;;                     ,(all-the-icons-faicon "folder-open-o")
-;;                     ,(all-the-icons-faicon "folder-o")
-;;                     ,(all-the-icons-faicon "inbox")
-;;                     ;; ,(all-the-icons-faicon "book")
-;;                     ,(all-the-icons-faicon "archive")
-;;                     ,(all-the-icons-faicon "file-text-o")
-;;                     ,(all-the-icons-faicon "file-o")
-;;                     ,(all-the-icons-faicon "floppy-o")
-;;                     ,(all-the-icons-faicon "header")
-;;                     ,(all-the-icons-faicon "bookmark")
-;;                     ,(all-the-icons-faicon "bookmark-o")
-;;                     ,(all-the-icons-faicon "paperclip")
-;;                     ,(all-the-icons-faicon "hashtag")
-;;
-;;                     ;; battery style.
-;;                     ,(all-the-icons-faicon "battery-full")
-;;                     ,(all-the-icons-faicon "battery-three-quarters")
-;;                     ,(all-the-icons-faicon "battery-half")
-;;                     ,(all-the-icons-faicon "battery-quarter")
-;;                     ,(all-the-icons-faicon "battery-empty")
-;;
-;;                     ;; directions style
-;;                     ;; ,(all-the-icons-faicon "chevron-circle-down")
-;;                     ;; ,(all-the-icons-faicon "caret-square-o-right")
-;;                     ;; ,(all-the-icons-faicon "arrow-circle-o-down")
-;;                     ))))
+(use-package org-bullets
+  :ensure t
+  :ensure all-the-icons
+  :defer t
+  :hook (org-mode . org-bullets-mode)
+  :config
+  (with-eval-after-load 'all-the-icons
+    (setq-default org-bullets-bullet-list
+                  `(;; disk -> folder -> file style
+                    ;; folder -> open folder -> inbox -> book -> text -> file -> floppy -> paperclip -> hashtag
+                    ;;===========================================================
+                    ;; ,(all-the-icons-faicon "hdd-o")
+                    ;; ,(all-the-icons-faicon "cube")
+                    ,(all-the-icons-faicon "folder-o")
+                    ,(all-the-icons-faicon "folder-open-o")
+                    ,(all-the-icons-faicon "inbox")
+                    ;; ,(all-the-icons-faicon "book")
+                    ;; ,(all-the-icons-faicon "archive")
+                    ,(all-the-icons-faicon "file-text-o")
+                    ,(all-the-icons-faicon "file-o")
+                    ,(all-the-icons-faicon "floppy-o")
+                    ;; ,(all-the-icons-faicon "header")
+                    ;; ,(all-the-icons-faicon "bookmark")
+                    ;; ,(all-the-icons-faicon "bookmark-o")
+                    ,(all-the-icons-faicon "paperclip")
+                    ,(all-the-icons-faicon "hashtag")
+
+                    ;; battery style.
+                    ,(all-the-icons-faicon "battery-full")
+                    ,(all-the-icons-faicon "battery-three-quarters")
+                    ,(all-the-icons-faicon "battery-half")
+                    ,(all-the-icons-faicon "battery-quarter")
+                    ,(all-the-icons-faicon "battery-empty")
+
+                    ;; directions style
+                    ,(all-the-icons-faicon "chevron-circle-down")
+                    ,(all-the-icons-faicon "caret-square-o-right")
+                    ,(all-the-icons-faicon "arrow-circle-o-down")
+                    ))))
 
 
 (provide 'init-org-view)
