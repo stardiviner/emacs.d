42f4c2aa636eff767bfda160874cbf2e16465dd7
stardiviner <numbchild@gmail.com>
2020-02-15 21:47:41 +0800

    [Batch] put all Batch Script packages into a separate init file

---
+++ init/Microsoft Windows/init-bat.el
@@ -0,0 +1,33 @@
+;;; init-bat.el --- init for Windows Batch Script. -*- lexical-binding: t; -*-
+
+;;; Time-stamp: <2020-02-15 21:47:25 stardiviner>
+
+;;; Commentary:
+
+
+
+;;; Code:
+
+;;; [ bat-mode ] -- Emacs built-in major mode for editing DOS/Windows scripts.
+
+(require 'bat-mode)
+
+;;; [ bmx-mode ] -- Batch Mode eXtras.
+
+(use-package bmx-mode
+  :ensure t
+  :config
+  ;; (bmx-mode-setup-defaults)
+  (add-hook 'bat-mode-hook #'bmx-mode)
+  (defun my/bmx-mode-company-setup ()
+    (make-local-variable 'company-backends)
+    (add-to-list 'company-backends #'bmx--company-label-backend)
+    (add-to-list 'company-backends #'bmx--company-variable-backend)
+    (add-hook 'company-completion-finished-hook #'bmx--company-completion-finished-hook))
+  (add-hook 'bmx-mode-hook #'my/bmx-mode-company-setup))
+
+
+
+(provide 'init-bat)
+
+;;; init-bat.el ends here
--- init/Microsoft Windows/init-microsoft-windows.el
+++ init/Microsoft Windows/init-microsoft-windows.el
@@ -13,23 +13,9 @@
   :ensure t
   :mode "\\.ini\\'")
 
-;;; [ bat-mode ] -- Emacs built-in major mode for editing DOS/Windows scripts.
+;;; [ Batch Script ]
 
-(require 'bat-mode)
-
-;;; [ bmx-mode ] -- Batch Mode eXtras.
-
-(use-package bmx-mode
-  :ensure t
-  :config
-  ;; (bmx-mode-setup-defaults)
-  (add-hook 'bat-mode-hook #'bmx-mode)
-  (defun my/bmx-mode-company-setup ()
-    (make-local-variable 'company-backends)
-    (add-to-list 'company-backends #'bmx--company-label-backend)
-    (add-to-list 'company-backends #'bmx--company-variable-backend)
-    (add-hook 'company-completion-finished-hook #'bmx--company-completion-finished-hook))
-  (add-hook 'bmx-mode-hook #'my/bmx-mode-company-setup))
+(require 'init-bat)
 
 ;;; [ PowerShell ]
 
