7d08f135a7ba56efc2cfaeb72608d4943a2af58d
stardiviner <numbchild@gmail.com>
2020-03-14 11:03:30 +0800

    [package] disable playonline

--- init/programming/eval/init-prog-eval.el
+++ init/programming/eval/init-prog-eval.el
@@ -141,15 +141,15 @@
 
 ;;; [ play-code ] -- Play code with online playgrounds.
 
-(use-package playonline
-  :quelpa (playonline :fetcher github :repo "twlz0ne/playonline.el")
-  :commands (playonline)
-  :init (define-key eval-prefix (kbd "o") 'playonline)
-  (add-to-list 'display-buffer-alist
-               '("^\\*playonline\\*" (display-buffer-below-selected)))
-  (use-package ob-playonline
-    :quelpa (ob-playonline :fetcher github :repo "twlz0ne/ob-playonline")
-    :demand t))
+;; (use-package playonline
+;;   :quelpa (playonline :fetcher github :repo "twlz0ne/playonline.el")
+;;   :commands (playonline)
+;;   :init (define-key eval-prefix (kbd "o") 'playonline)
+;;   (add-to-list 'display-buffer-alist
+;;                '("^\\*playonline\\*" (display-buffer-below-selected)))
+;;   (use-package ob-playonline
+;;     :quelpa (ob-playonline :fetcher github :repo "twlz0ne/ob-playonline")
+;;     :demand t))
 
 ;;; [ sesman ] -- Session manager for Emacs based IDEs.
 
