2ade34c6a1742ec3f40263ac568c26be0d9e07f3
stardiviner <numbchild@gmail.com>
2020-04-15 15:07:42 +0800

    [audio] disable two packages: ecasound.el and sox.el

--- init/tools/init-tool-audio.el
+++ init/tools/init-tool-audio.el
@@ -27,19 +27,19 @@
 
 ;;; [ ecasound ] -- command-line multitrack audio processor.
 
-(use-package ecasound
-  :load-path "/usr/share/ecasound/"
-  :bind (:map audio-prefix ("e" . ecasound)))
+;; (use-package ecasound
+;;   :load-path "/usr/share/ecasound/"
+;;   :bind (:map audio-prefix ("e" . ecasound)))
 
 
 ;;; [ SoX.el ] -- helper for interacting with SoX, the Swiss Army knife of sound processing programs.
 
-(use-package sox
-  :quelpa (sox :fetcher github :repo "vxe/SoX.el")
-  :commands (sox)
-  :config
-  (add-to-list 'display-buffer-alist
-               '("^\\*sox\\*.*" (display-buffer-below-selected))))
+;; (use-package sox
+;;   :quelpa (sox :fetcher github :repo "vxe/SoX.el")
+;;   :commands (sox)
+;;   :config
+;;   (add-to-list 'display-buffer-alist
+;;                '("^\\*sox\\*.*" (display-buffer-below-selected))))
 
 ;;; ----------------------------------------------------------------------------
 
