8c285b26bf6f874c0797b985ff9812a947a7c2a2
stardiviner <numbchild@gmail.com>
2020-05-11 19:57:38 +0800

    [profiling] improve Emacs profiling tools to complete list

--- init/Emacs/init-emacs-profiler.el
+++ init/Emacs/init-emacs-profiler.el
@@ -1,6 +1,6 @@
 ;;; init-emacs-profiler.el --- init profilers for Emacs.
 
-;;; Time-stamp: <2020-05-11 06:46:48 stardiviner>
+;;; Time-stamp: <2020-05-11 19:56:52 stardiviner>
 
 ;;; Commentary:
 
@@ -10,10 +10,14 @@
 
 ;;; [ profiler ] -- Emacs buolt-in profiler.
 
-(setq profiler-report-cpu-line-format '((70 left)
-                                        (24 right ((19 right) (5 right))))
-      profiler-report-memory-line-format '((70 left)
-                                           (19 right ((14 right profiler-format-number) (5 right)))))
+(use-package profiler
+  :commands (profiler-start profiler-report profiler-stop)
+  :init
+  ;; make profiler report line length longer
+  (setq profiler-report-cpu-line-format '((70 left)
+                                          (24 right ((19 right) (5 right))))
+        profiler-report-memory-line-format '((70 left)
+                                             (19 right ((14 right profiler-format-number) (5 right))))))
 
 ;;; [ elp ] -- Emacs Lisp profiler.
 
@@ -24,6 +28,11 @@
              elp-results)
   :init (add-to-list 'display-buffer-alist
                      '("\\*ELP Profiling Results\\*" . (display-buffer-below-selected))))
+
+;;; [ benchmark ] -- support for benchmarking code.
+
+(use-package benchmark
+  :commands (benchmark benchmark-run benchmark-run-compiled benchmark-progn))
 
 ;;; [ trace ] -- tracing facility for Emacs Lisp functions.
 
