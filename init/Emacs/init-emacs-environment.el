44b846561a9868a75b8cc52410c1db03b22ceb33
stardiviner <numbchild@gmail.com>
2020-05-30 23:10:06 +0800

    [coding] auto guess file buffer encoding. (last is highest priority)

--- init/Emacs/init-emacs-environment.el
+++ init/Emacs/init-emacs-environment.el
@@ -32,8 +32,17 @@
 
 ;;; [ coding system ]
 
-(prefer-coding-system 'utf-8-unix)
 (set-default-coding-systems 'utf-8)
+
+;;; auto guess file buffer encoding. (last is highest priority)
+;; https://www.gnu.org/software/emacs/manual/html_node/emacs/Recognize-Coding.html#Recognize-Coding
+(prefer-coding-system 'cp950)
+(prefer-coding-system 'gb2312)
+(prefer-coding-system 'cp936)
+(prefer-coding-system 'gb18030)
+(prefer-coding-system 'utf-16)
+(prefer-coding-system 'utf-8-dos)
+(prefer-coding-system 'utf-8-unix)
 
 ;; (set-charset-priority 'unicode)
 ;; (set-language-environment 'utf-8)
