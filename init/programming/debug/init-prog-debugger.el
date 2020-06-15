ede5e32e312892f9cb6d9deab8df0a8f1b10d8a6
stardiviner <numbchild@gmail.com>
2020-06-15 19:44:13 +0800

    [debugger] refactor debuggers config

--- init/programming/debug/init-prog-debugger.el
+++ init/programming/debug/init-prog-debugger.el
@@ -13,20 +13,17 @@
   :ensure t
   :defer t
   :commands (gdb gdb-many-windows)
-  :init (setq gdb-many-windows t
-              gdb-show-main t))
+  :custom ((gdb-many-windows t)
+           (gdb-show-main t)))
 
 ;;; [ realgud ] -- A modular GNU Emacs front-end for interacting with external debuggers.
 
 (use-package realgud
   :ensure t
-  :requires t)
-
-;;; [ realgud-lldb ] -- realgud front-end to lldb.
-
-(use-package realgud-lldb
-  :ensure t
-  :defer t)
+  :config
+  ;; [ realgud-lldb ] -- realgud front-end to lldb.
+  (use-package realgud-lldb
+    :ensure t))
 
 ;;; [ moonshot ] -- Run executable file, debug and build commands on project +projectile, compilation-mode, ivy, realgud.
 
