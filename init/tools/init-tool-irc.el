dd1b25f260e7b5d613772df32dc0b1f1dfe28e64
stardiviner <numbchild@gmail.com>
2020-06-17 19:11:06 +0800

    [IRC] fix circe not auto login server with my account issue

--- init/tools/init-tool-irc.el
+++ init/tools/init-tool-irc.el
@@ -16,32 +16,30 @@
   :defer t
   :commands (circe)
   :bind (:map tools-prefix ("i" . circe))
-  :config
   ;; user and servers info
-  (setq circe-default-nick "stardiviner"
-        circe-network-options `(("Freenode"
-                                 :user "stardiviner"
-                                 :pass ,(my/json-read-value my/account-file 'irc)
-                                 :use-tls t
-                                 :channels ("#emacs"))))
-  ;; ignore
-  (setq circe-reduce-lurker-spam t)
-  ;; view
-  (setq circe-format-say "{nick:-10s} {body}") ; align nick names and messages.
+  :custom ((circe-default-nick "stardiviner")
+           (circe-network-options `(("Freenode"
+                                     :user "stardiviner"
+                                     :pass ,(my/json-read-value my/account-file 'irc)
+                                     :use-tls t
+                                     :channels ("#emacs"))))
+           (circe-channels '("#emacs"))
+           (circe-reduce-lurker-spam t)
+           (circe-format-say "{nick:-10s} {body}") ; align nick names and messages.
+           ;; spelling checking
+           (lui-flyspell-p t)
+           (lui-flyspell-alist '(("#hamburg" "german8") (".*" "american"))))
+  :config
   (define-key circe-mode-map (kbd "C-c SPC") 'tracking-next-buffer)
   ;; words completion
   (defun my/circe-company-setup ()
     (my-company-add-backend-locally 'company-ispell))
   (add-hook 'circe-channel-mode-hook #'my/circe-company-setup)
-  ;; spelling checking
-  (setq lui-flyspell-p t
-        lui-flyspell-alist '(("#hamburg" "german8")
-                             (".*" "american")))
   ;; auto use paste service for long single line.
   ;; (require 'lui-autopaste)
   ;; (add-hook 'circe-channel-mode-hook 'enable-lui-autopaste)
   ;; Logging
-  (load "lui-logging" nil t)
+  (require 'lui-logging)
   (enable-lui-logging-globally)
   ;; track bar
   (enable-lui-track-bar))
