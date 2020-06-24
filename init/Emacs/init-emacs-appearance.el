1eefbf5570837e054122d072bcc75678504630a8
stardiviner <numbchild@gmail.com>
2020-06-24 13:17:35 +0800

    [Emacs] disable line-spacing

--- init/Emacs/init-emacs-appearance.el
+++ init/Emacs/init-emacs-appearance.el
@@ -319,7 +319,7 @@
 ;; above or below the display line.
 
 ;; additional space to put between lines.
-(setq-default line-spacing 0.1)         ; nil, 0, 0.1, 1,
+;; (setq-default line-spacing 0.1)         ; nil, 0, 0.1, 1,
 
 ;;; [ line numbers ] -- Emacs native line number mode.
 
