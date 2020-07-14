0247dfbab2fda9bb8cf36b4e680d84c8d61b94e6
stardiviner <numbchild@gmail.com>
2020-07-14 20:16:20 +0800

    [Org] improve org-protocol config

--- init/org-mode/init-org-protocol.el
+++ init/org-mode/init-org-protocol.el
@@ -10,49 +10,47 @@
 ;;; [ org-protocol ] -- intercept calls from emacsclient to trigger custom actions.
 
 (use-package org-protocol
-  :defer t
   :config
   (with-eval-after-load 'org-protocol
     (unless (featurep 'server)
       (require 'server))
     (unless (server-running-p)
-      (server-start))))
+      (server-start)))
 
-;; ;; TODO: setup this option.
-;; ;; (module-name :property value property: value ...)
-;; (setq org-protocol-project-alist
-;;       '(("http://orgmode.org/worg/"
-;;          :online-suffix ".php"
-;;          :working-suffix ".org"
-;;          :base-url "http://orgmode.org/worg/"
-;;          :working-directory "/home/stardiviner/Org/Worg/")
-;;         ("http://localhost/org-notes/"
-;;          :online-suffix ".html"
-;;          :working-suffix ".org"
-;;          :base-url "http://localhost/org/"
-;;          :working-directory "/home/user/org/"
-;;          :rewrites (("org/?$" . "index.php")))))
+  ;; [ org-protocol capture ]
+  (setq org-capture-templates
+        (append `(("P" ,(format "%s\torg-protocol"
+                                (all-the-icons-faicon "external-link" :face 'all-the-icons-orange)))
+                  ("PP" ,(format "%s\tProtocol"
+                                 (all-the-icons-faicon "chrome" :face 'all-the-icons-orange))
+                   entry (file ,(concat org-directory "/Tasks/Tasks.org"))
+                   "* %^{Title}\nSource: %u, %c\n#+begin_quote\n%i\n#+end_quote\n\n\n%?"
+                   :prepend t
+                   :empty-lines 1)
+                  ("PL" ,(format "%s\tProtocol Link"
+                                 (all-the-icons-faicon "external-link" :face 'all-the-icons-orange))
+                   entry (file ,(concat org-directory "/Tasks/Tasks.org"))
+                   "* %? [[%:link][%:description]] \nCaptured On: %U"
+                   :prepend t
+                   :empty-lines 1))
+                org-capture-templates))
 
-;;; [ org-protocol capture ]
-
-(require 'org-protocol)
-
-(setq org-capture-templates
-      (append `(("P" ,(format "%s\torg-protocol"
-                              (all-the-icons-faicon "external-link" :face 'all-the-icons-orange)))
-                ("PP" ,(format "%s\tProtocol"
-                               (all-the-icons-faicon "chrome" :face 'all-the-icons-orange))
-                 entry (file ,(concat org-directory "/Tasks/Tasks.org"))
-                 "* %^{Title}\nSource: %u, %c\n #+begin_quote\n%i\n#+end_quote\n\n\n%?"
-                 :prepend t
-                 :empty-lines 1)
-                ("PL" ,(format "%s\tProtocol Link"
-                               (all-the-icons-faicon "external-link" :face 'all-the-icons-orange))
-                 entry (file ,(concat org-directory "/Tasks/Tasks.org"))
-                 "* %? [[%:link][%:description]] \nCaptured On: %U"
-                 :prepend t
-                 :empty-lines 1))
-              org-capture-templates))
+  
+  ;; ;; TODO: setup this option.
+  ;; ;; (module-name :property value property: value ...)
+  ;; (setq org-protocol-project-alist
+  ;;       '(("http://orgmode.org/worg/"
+  ;;          :online-suffix ".php"
+  ;;          :working-suffix ".org"
+  ;;          :base-url "http://orgmode.org/worg/"
+  ;;          :working-directory "/home/stardiviner/Org/Worg/")
+  ;;         ("http://localhost/org-notes/"
+  ;;          :online-suffix ".html"
+  ;;          :working-suffix ".org"
+  ;;          :base-url "http://localhost/org/"
+  ;;          :working-directory "/home/user/org/"
+  ;;          :rewrites (("org/?$" . "index.php")))))
+  )
 
 (use-package org-protocol-capture-html
   :ensure t
