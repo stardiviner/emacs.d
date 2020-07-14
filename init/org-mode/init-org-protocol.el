40e8029f7a46172be01321ebe19e94d1b30b2470
stardiviner <numbchild@gmail.com>
2020-07-14 20:23:41 +0800

    [Org] lower-case org-protocol keys in org-capture-templates

--- init/org-mode/init-org-protocol.el
+++ init/org-mode/init-org-protocol.el
@@ -19,15 +19,15 @@
 
   ;; [ org-protocol capture ]
   (setq org-capture-templates
-        (append `(("P" ,(format "%s\torg-protocol"
+        (append `(("p" ,(format "%s\torg-protocol"
                                 (all-the-icons-faicon "external-link" :face 'all-the-icons-orange)))
-                  ("PP" ,(format "%s\tProtocol"
+                  ("pp" ,(format "%s\tProtocol"
                                  (all-the-icons-faicon "chrome" :face 'all-the-icons-orange))
                    entry (file ,(concat org-directory "/Tasks/Tasks.org"))
                    "* %^{Title}\nSource: %u, %c\n#+begin_quote\n%i\n#+end_quote\n\n\n%?"
                    :prepend t
                    :empty-lines 1)
-                  ("PL" ,(format "%s\tProtocol Link"
+                  ("pl" ,(format "%s\tProtocol Link"
                                  (all-the-icons-faicon "external-link" :face 'all-the-icons-orange))
                    entry (file ,(concat org-directory "/Tasks/Tasks.org"))
                    "* %? [[%:link][%:description]] \nCaptured On: %U"
@@ -56,7 +56,7 @@
   :ensure t
   :config (setq org-capture-templates
                 (append
-                 `(("PH" ,(format "%s\torg-protocol-capture-html"
+                 `(("ph" ,(format "%s\torg-protocol-capture-html"
                                   (all-the-icons-faicon "html5" :face 'all-the-icons-pink))
                     entry (file "")
                     "* %a :website:\n\n%U %?\n\n%:initial"))
