83ee1faad7c43291a573811c4d128a72f87d5efa
stardiviner <numbchild@gmail.com>
2020-07-16 08:03:14 +0800

    Revert "[performance] disable doom-modeline"
    This reverts commit ca184d410daf745a44ec2cc47f959a6453ec33d0.

--- init/Emacs/init-emacs-mode-line.el
+++ init/Emacs/init-emacs-mode-line.el
@@ -17,14 +17,14 @@
 
 ;;; [ doom-modeline ] -- A minimal and modern mode-line.
 
-;; (use-package doom-modeline
-;;   :ensure t
-;;   :custom ((doom-modeline-buffer-file-name-style 'buffer-name)
-;;            (doom-modeline-icon t) ; don't use icon will be faster
-;;            (doom-modeline-github nil)
-;;            ;; Fix the laggy issue, by don't compact font caches during GC.
-;;            (inhibit-compacting-font-caches t))
-;;   :hook (after-init . doom-modeline-mode))
+(use-package doom-modeline
+  :ensure t
+  :custom ((doom-modeline-buffer-file-name-style 'buffer-name)
+           (doom-modeline-icon t) ; don't use icon will be faster
+           (doom-modeline-github nil)
+           ;; Fix the laggy issue, by don't compact font caches during GC.
+           (inhibit-compacting-font-caches t))
+  :hook (after-init . doom-modeline-mode))
 
 
 (provide 'init-emacs-mode-line)
