800fc432c9377527e05248b7c14ecea26551e4ba
stardiviner <numbchild@gmail.com>
2020-07-17 18:39:54 +0800

    [Org] fix org-protocol capture template key conflict with org-projectile

--- init/org-mode/init-org-protocol.el
+++ init/org-mode/init-org-protocol.el
@@ -19,15 +19,15 @@
 
   ;; [ org-protocol capture ]
   (setq org-capture-templates
-        (append `(("p" ,(format "%s\torg-protocol"
+        (append `(("P" ,(format "%s\torg-protocol"
                                 (all-the-icons-faicon "external-link" :face 'all-the-icons-orange)))
-                  ("pp" ,(format "%s\tProtocol"
+                  ("PP" ,(format "%s\tProtocol"
                                  (all-the-icons-faicon "chrome" :face 'all-the-icons-orange))
                    entry (file ,(concat org-directory "/Tasks/Tasks.org"))
                    "* %^{Title}\nSource: %u, %c\n#+begin_quote\n%i\n#+end_quote\n\n\n%?"
                    :prepend t
                    :empty-lines 1)
-                  ("pl" ,(format "%s\tSave Link to Bookmarks"
+                  ("PL" ,(format "%s\tSave Link to Bookmarks"
                                  (all-the-icons-faicon "external-link" :face 'all-the-icons-orange))
                    entry (file ,(concat org-directory "/Bookmarks/Bookmarks.org"))
                    "* [[%:link][%:description]]\n:PROPERTIES:\n:DATE: %U \n:END:\n %?"
@@ -57,7 +57,7 @@
   :ensure t
   :config
   (add-to-list 'org-capture-templates
-               `("ph" ,(format "%s\torg-protocol-capture-html"
+               `("PH" ,(format "%s\torg-protocol-capture-html"
                                (all-the-icons-faicon "html5" :face 'all-the-icons-pink))
                  entry (file ,(concat org-directory "/Bookmarks/Bookmarks.org"))
                  "* [[%:link][%:description]]\n:PROPERTIES:\n:DATE: %U \n:END:\n %? \n%:initial"
