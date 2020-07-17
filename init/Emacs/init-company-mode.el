893b7f95f69db7c533b642bf83c1212a1eab682c
stardiviner <numbchild@gmail.com>
2020-07-17 11:23:19 +0800

    [company] remove custom [Enter] and [SPACE] in company-tooltip

--- init/Emacs/init-company-mode.el
+++ init/Emacs/init-company-mode.el
@@ -89,16 +89,12 @@
       (shut-up (apply orig-func args)))
     (advice-add 'ispell-lookup-words :around 'ispell-silent))
   
-  ;; keybindings
+  ;; [ keybindings ]
 
-  ;; manually start completion (don't globally set, conflict with auto-complete
-  ;; in some modes which use auto-complete)
-  ;;
   ;; (global-set-key (kbd "<tab>") 'company-indent-or-complete-common)
   ;; (define-key company-mode-map (kbd "<tab>") 'company-complete)
   ;; (define-key company-mode-map (kbd "TAB") 'company-complete)
   ;; (define-key company-mode-map [tab] 'company-complete)
-  
   (define-key company-mode-map (kbd "C-M-i") 'company-complete)
   (define-key company-mode-map (kbd "M-<tab>") 'company-complete)
 
@@ -119,31 +115,14 @@
   (define-key company-active-map (kbd "M-s") 'company-search-candidates)
   (define-key company-active-map (kbd "C-M-s") 'company-filter-candidates)
   ;; nested searching map.
-  (define-key company-search-map (kbd "M-g") 'company-search-toggle-filtering)
   (define-key company-search-map (kbd "C-g") 'company-search-abort)
+  (define-key company-search-map (kbd "M-g") 'company-search-toggle-filtering)
   (define-key company-search-map (kbd "M-s") 'company-search-repeat-forward)
   (define-key company-search-map (kbd "M-r") 'company-search-repeat-backward)
   (define-key company-search-map (kbd "M-n") 'company-search-repeat-forward)
   (define-key company-search-map (kbd "M-p") 'company-search-repeat-backward)
   (define-key company-search-map (kbd "M-o") 'company-search-kill-others)
   (define-key company-search-map (kbd "M-j") 'company-complete-selection)
-
-  (defun company-new-line ()
-    "insert a literal return new line."
-    (interactive)
-    ;; (company-abort)
-    (company-cancel 'abort)
-    (newline-and-indent))
-  (define-key company-active-map [return] 'company-new-line)
-  (define-key company-active-map "\r" 'company-new-line)
-
-  (defun company-space ()
-    "insert a literal return new line."
-    (interactive)
-    ;; (company-abort)
-    (company-cancel 'abort)
-    (insert " "))
-  (define-key company-active-map (kbd "SPC") 'company-space)
 
   ;; [ company-yasnippet ]
   ;; make `company-yasnippet' work for prefix like `%link_to'.
