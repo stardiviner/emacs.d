e4c1b42b934feb7d5a3a3fa109c4a920b367e740
stardiviner <numbchild@gmail.com>
2020-07-19 17:03:40 +0800

    [web] add package web-beautify to Format HTML, CSS and JavaScript/JSON

--- init/programming/frameworks/Web/init-prog-framework-web.el
+++ init/programming/frameworks/Web/init-prog-framework-web.el
@@ -210,6 +210,24 @@
   :delight web-mode-edit-element-minor-mode
   :hook (web-mode-hook . web-mode-edit-element-minor-mode))
 
+;;; [ web-beautify ] -- Format HTML, CSS and JavaScript/JSON.
+
+(use-package web-beautify
+  :ensure t
+  :config
+  (eval-after-load 'js2-mode
+    '(add-hook 'js2-mode-hook
+               (lambda () (add-hook 'before-save-hook 'web-beautify-js-buffer t t))))
+  (eval-after-load 'json-mode
+    '(add-hook 'json-mode-hook
+               (lambda () (add-hook 'before-save-hook 'web-beautify-js-buffer t t))))
+  (eval-after-load 'sgml-mode
+    '(add-hook 'html-mode-hook
+               (lambda () (add-hook 'before-save-hook 'web-beautify-html-buffer t t))))
+  (eval-after-load 'css-mode
+    '(add-hook 'css-mode-hook
+               (lambda () (add-hook 'before-save-hook 'web-beautify-css-buffer t t)))))
+
 
 (provide 'init-prog-framework-web)
 
