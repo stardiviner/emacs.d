2645def6c44bc61cf7b9fba498a4b69245c431fe
stardiviner <numbchild@gmail.com>
2020-07-26 17:46:19 +0800

    [mode-line] use align helper function reformat mode-line alignment

--- init/Emacs/init-emacs-mode-line.el
+++ init/Emacs/init-emacs-mode-line.el
@@ -7,6 +7,32 @@
 
 ;;; Code:
 
+;;; [ modify Emacs default mode-line ]
+
+(defun mode-line-align (left right)
+  "Return a string with LEFT and RIGHT at the edges of the
+current window."
+  (format (format "%%s %%%ds" (- (window-total-width) (length left) 2))
+          left right))
+
+(setq-default mode-line-format
+              '(:eval
+                (mode-line-align
+                 (format-mode-line
+                  (list "%e" mode-line-front-space
+                        " " mode-line-client mode-line-remote mode-line-mule-info mode-line-modified
+                        " " mode-line-buffer-identification
+                        " " mode-line-position
+                        " " '(vc-mode vc-mode)
+                        " " mode-line-process))
+                 (format-mode-line
+                  (list " " mode-line-misc-info
+                        " " mode-line-modes
+                        ;; " " mode-name minor-mode-alist
+                        " " mode-line-end-spaces)))))
+
+;;; [ powerline ] -- Rewrite of Powerline.
+
 ;; (require 'init-powerline)
 
 ;;; [ mood-line ] -- a minimal mode-line configuration that aims to replicate some of the features of the doom-modeline.
