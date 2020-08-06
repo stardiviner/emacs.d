fd5b74ea658111e1ce6115075c00be1033f33fbf
stardiviner <numbchild@gmail.com>
2020-08-06 20:13:38 +0800

    [completion] set default completion

--- init/Emacs/init-emacs-completion.el
+++ init/Emacs/init-emacs-completion.el
@@ -13,6 +13,11 @@
 ;;    add a completion command into it with mode locally.
 ;;    (add-hook 'completion-at-point-functions 'completion-function nil t)
 
+(setq-default completion-styles '(basic partial-completion)
+              completion-show-inline-help nil
+              ;; completion-cycle-threshold t
+              completion-ignore-case t)
+
 
 
 (provide 'init-emacs-completion)
