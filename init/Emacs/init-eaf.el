8dfdfddf89e16ee3d26b8618a3fc0a1766605cdd
stardiviner <numbchild@gmail.com>
2020-08-12 13:33:49 +0800

    [EAF] improve EAF config

--- init/Emacs/init-eaf.el
+++ init/Emacs/init-eaf.el
@@ -1,6 +1,6 @@
 ;;; init-eaf.el --- init for emacs-application-framework -*- lexical-binding: t; -*-
 
-;;; Time-stamp: <2020-06-16 11:03:51 stardiviner>
+;;; Time-stamp: <2020-08-12 13:33:38 stardiviner>
 
 ;;; Commentary:
 
@@ -13,20 +13,25 @@
 (use-package eaf
   :load-path "~/Code/Emacs/emacs-application-framework/"
   :diminish eaf-mode
+  ;; :init (setq eaf-enable-debug t)
   :config
   (eaf-setq eaf-camera-save-path "~")
 
   ;; [ Dired ]
   (setq eaf-find-alternate-file-in-dired t)
   (define-key dired-mode-map (kbd "M-RET") 'eaf-open-this-from-dired)
+
+  ;; [ web browser ]
   
   ;; use EAF as default web browser for Emacs.
-  (setq browse-url-browser-function 'eaf-open-browser)
+  ;; (setq browse-url-browser-function 'eaf-open-browser)
   ;; let `eaf-open-browser' support HiDPI screen
   (eaf-setq eaf-browser-default-zoom  "2")
+
   ;; EAF app follow Emacs theme
   (eaf-setq eaf-browser-dark-mode "follow")
   (eaf-setq eaf-pdf-dark-mode "follow")
+  
   ;; set EAF web browser proxy
   (defvar eaf-proxy-enabled nil)
   (defun eaf-toggle-proxy ()
@@ -45,14 +50,15 @@
         (eaf-setq eaf-proxy-port "1086")
         (setq eaf-proxy-enabled t)
         (message "EAF proxy enabled."))))
+
   ;; exclude EAF buffers from `desktop-save-mode'.
   (with-eval-after-load 'desktop
     (add-to-list 'desktop-modes-not-to-save 'eaf-mode))
 
   ;; [ `eaf-org' ] Org Mode integration
-  ;; NOTE set overriding option before loading `eaf-org' to execute if condition.
-  (setq eaf-org-override-pdf-links t)
-  (require 'eaf-org)
+  ;; set overriding option before loading `eaf-org' to execute if condition.
+  (setq eaf-org-override-pdf-links nil)
+  ;; (require 'eaf-org)
 
   ;; eaf-interleave integration
   (add-hook 'eaf-pdf-viewer-hook 'eaf-interleave-app-mode)
