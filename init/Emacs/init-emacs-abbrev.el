1d7c7419f5a537c894916def09e3b24b5c18a507
stardiviner <numbchild@gmail.com>
2020-08-13 18:27:10 +0800

    [abbrev] wrap abbrev-mode config with use-package

--- init/Emacs/init-emacs-abbrev.el
+++ init/Emacs/init-emacs-abbrev.el
@@ -7,32 +7,33 @@
 
 ;;; [ abbrev-mode ]
 
-(require 'abbrev)
+(use-package abbrev
+  :delight abbrev-mode
+  ;; setup my abbrevs file custom path.
+  :custom ((abbrev-file-name (expand-file-name "abbrev_defs" user-emacs-directory))
+           (save-abbrevs 'silently) ; save abbrevs when files are saved, nil: stop asking.
+           )
+  :init
+  ;; reads the abbreviations file on startup quietly.
+  (if (file-exists-p abbrev-file-name)
+      (quietly-read-abbrev-file))
+  :hook ((org-mode . abbrev-mode))
+  :config
+  (define-abbrev-table 'global-abbrev-table
+    '(("mynick" "stardiviner")
+      ("myemail" "numbchild@gmail.com")
+      ("mygmail" "numbchild@gmail.com")
+      ("mygmailformat" "numbchild[@]{gmail}.com")
+      ("myhomepage" "http://stardiviner.github.io")
+      ("mytwitter" "@numbchild")
+      ("myqq" "348284894")))
 
-;;; setup my abbrevs file custom path.
-(setq abbrev-file-name (expand-file-name "abbrev_defs" user-emacs-directory))
-(setq save-abbrevs 'silently) ; save abbrevs when files are saved, nil: stop asking.
-;; reads the abbreviations file on startup quietly.
-(if (file-exists-p abbrev-file-name)
-    (quietly-read-abbrev-file))
-
-(define-abbrev-table 'global-abbrev-table
-  '(("mynick" "stardiviner")
-    ("myemail" "numbchild@gmail.com")
-    ("mygmail" "numbchild@gmail.com")
-    ("mygmailformat" "numbchild[@]{gmail}.com")
-    ("myhomepage" "http://stardiviner.github.io")
-    ("mytwitter" "@numbchild")
-    ("myqq" "348284894")))
-
-;;; define more keybindings [C-x a]
-(define-key abbrev-map (kbd "/") 'exapnd-abbrev)
-(define-key abbrev-map (kbd "e") 'edit-abbrevs)
-(define-key abbrev-map (kbd "M-l") 'list-abbrevs)
-(define-key abbrev-map (kbd "i") 'inverse-add-mode-abbrev)
-(define-key abbrev-map (kbd "I") 'inverse-add-global-abbrev)
-
-;; (add-hook 'org-mode-hook #'abbrev-mode)
+  ;; define more keybindings [C-x a]
+  (define-key abbrev-map (kbd "/") 'exapnd-abbrev)
+  (define-key abbrev-map (kbd "e") 'edit-abbrevs)
+  (define-key abbrev-map (kbd "M-l") 'list-abbrevs)
+  (define-key abbrev-map (kbd "i") 'inverse-add-mode-abbrev)
+  (define-key abbrev-map (kbd "I") 'inverse-add-global-abbrev))
 
 ;;; [ dabbrev ] -- Dynamic Abbrevs [M-/]
 
