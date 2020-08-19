5061a52b11761efb3c862ba69639d4e57772ece7
stardiviner <numbchild@gmail.com>
2020-08-19 20:35:47 +0800

    [linter] flycheck-inline uses `quick-peek' instead of default overlay

--- init/programming/lint/init-prog-lint.el
+++ init/programming/lint/init-prog-lint.el
@@ -59,7 +59,16 @@
 
 (use-package flycheck-inline
   :ensure t
-  :hook (flycheck-mode . flycheck-inline-mode))
+  :hook (flycheck-mode . flycheck-inline-mode)
+  :config
+  ;; use `quick-peek' instead of default overlay.
+  (setq flycheck-inline-display-function (lambda (msg &optional pos err)
+                                           (let* ((ov (quick-peek-overlay-ensure-at pos))
+                                                  (contents (quick-peek-overlay-contents ov)))
+                                             (setf (quick-peek-overlay-contents ov)
+                                                   (concat contents (when contents "\n") msg))
+                                             (quick-peek-update ov)))
+        flycheck-inline-clear-function #'quick-peek-hide))
 
 ;;; [ flycheck-popup-tip ] -- displaying errors from Flycheck using popup.el.
 
