60a7cd5af39e665ef1b82b3e4cc62690edcb1a0b
stardiviner <numbchild@gmail.com>
2020-08-22 09:57:26 +0800

    [screenshot] Emacs 27.1 can use Cairo to take SVG screenshots of itself

--- init/tools/init-tool-screenshot.el
+++ init/tools/init-tool-screenshot.el
@@ -11,6 +11,20 @@
   (define-prefix-command 'screenshot-prefix))
 (define-key tools-prefix (kbd "S") 'screenshot-prefix)
 
+;;; Emacs 27.1 can use Cairo to take SVG screenshots of itself.
+(defun screenshot-take (&optional type)
+  "Save a screenshot of the current frame as an SVG image.
+Saves to a temp file and puts the filename in the kill ring."
+  (interactive (list (completing-read "Output Type: "
+                                      '("svg" "png" "postscript" "pdf"))))
+  (let* ((type (or type "png"))
+         (filename (expand-file-name (format "~/Desktop/%s.%s" (make-temp-name "Emacs_Screenshot_") type)))
+         (data (x-export-frames nil (intern type))))
+    (with-temp-file filename
+      (insert data))
+    (kill-new filename)
+    (message filename)))
+
 (use-package frameshot
   :ensure t
   :commands (frameshot-mode frameshot-take)
