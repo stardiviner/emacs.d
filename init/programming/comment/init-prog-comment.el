f71337fe31542e17363a91cd89a228065b994601
stardiviner <numbchild@gmail.com>
2020-09-30 11:57:02 +0800

    [comment] wrap newcomment config into use-package

--- init/programming/comment/init-prog-comment.el
+++ init/programming/comment/init-prog-comment.el
@@ -10,36 +10,35 @@
 (global-set-key (kbd "M-;") 'prog-comment-prefix)
 (global-set-key (kbd "M-g c") 'prog-comment-prefix)
 
+;;; [ newcomment ] -- (un)comment regions of buffers.
 
-(setq comment-auto-fill-only-comments t
-      comment-multi-line t)
-
-
-;;; prefix: [M-;], `prog-comment-prefix'
-
-(define-key prog-comment-prefix (kbd "M-;") 'comment-dwim)
-;; or with [C-u N]
-(global-set-key (kbd "C-x C-;") 'comment-line)
-(define-key prog-comment-prefix (kbd "r") 'comment-or-uncomment-region)
-(define-key prog-comment-prefix (kbd "l") 'comment-line)
-(define-key prog-comment-prefix (kbd "B") 'comment-box)
-(define-key prog-comment-prefix (kbd "b") 'comment-box-with-fill-column)
-
-(defun comment-box-with-fill-column (b e) ; begin, end
-  "Draw a box comment around the region of B and E.
+(use-package newcomment
+  :custom ((comment-auto-fill-only-comments t)
+           (comment-multi-line t))
+  ;; or with [C-u N]
+  :bind (("C-x C-;" . comment-line)
+         :map prog-comment-prefix
+         ("M-;" . comment-dwim)
+         ("r" . comment-or-uncomment-region)
+         ("l" . comment-line)
+         ("B" . comment-box)
+         ("b" . comment-box-with-fill-column))
+  :init
+  (defun comment-box-with-fill-column (begin end)
+    "Draw a box comment around the region of B and E.
 
 But arrange for the region to extend to at least the fill
 column.  Place the point after the comment box."
-  (interactive "r")
-  (if (region-active-p)
-      (let ((e (copy-marker e t)))
-        (goto-char b)
-        (end-of-line)
-        (insert-char ? (- fill-column (current-column)))
-        (comment-box b e 1)
-        (goto-char e)
-        (set-marker e nil))
-    (user-error "No region selected!")))
+    (interactive "r")
+    (if (region-active-p)
+        (let ((end (copy-marker end t)))
+          (goto-char begin)
+          (end-of-line)
+          (insert-char ? (- fill-column (current-column)))
+          (comment-box begin end 1)
+          (goto-char end)
+          (set-marker end nil))
+      (user-error "No region selected!"))))
 
 ;;; [ banner-comment ] -- turn a comment into a banner.
 
