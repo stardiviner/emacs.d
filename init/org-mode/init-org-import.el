4a7113c7ece782bfb82a76df3c10389a30540020
stardiviner <numbchild@gmail.com>
2020-10-15 08:38:45 +0800

    [Org] new package org-pandoc-import to import non-org file formats

--- init/org-mode/init-org-import.el
+++ init/org-mode/init-org-import.el
@@ -1,6 +1,6 @@
 ;;; init-org-import.el --- Org Mode importer -*- lexical-binding: t; -*-
 
-;;; Time-stamp: <2020-10-15 08:33:16 stardiviner>
+;;; Time-stamp: <2020-10-15 08:35:53 stardiviner>
 
 ;;; Commentary:
 
@@ -8,9 +8,11 @@
 
 ;;; Code:
 
+;;; [ org-pandoc-import ] -- Save yourself from non-org formats, thanks to pandoc.
 
-
-
+(use-package org-pandoc-import
+  :quelpa (org-pandoc-import :fetcher github :repo "tecosaur/org-pandoc-import" :files ("*.el" "filters" "preprocessors"))
+  :commands (org-pandoc-import-transient-mode org-pandoc-import-to-org))
 
 
 
