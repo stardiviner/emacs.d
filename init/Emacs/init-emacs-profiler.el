b5b4b065fb3263d06024f525bbe7b56e44e09624
stardiviner <numbchild@gmail.com>
2020-11-18 11:51:09 +0800

    [profiler] improve Emacs profiler, elp config

--- init/Emacs/init-emacs-profiler.el
+++ init/Emacs/init-emacs-profiler.el
@@ -1,6 +1,6 @@
 ;;; init-emacs-profiler.el --- init profilers for Emacs.
 
-;;; Time-stamp: <2020-07-17 21:56:13 stardiviner>
+;;; Time-stamp: <2020-11-18 11:52:13 stardiviner>
 
 ;;; Commentary:
 
@@ -11,26 +11,26 @@
 ;;; [ profiler ] -- Emacs buolt-in profiler.
 
 (use-package profiler
+  :defer t
   :commands (profiler-start profiler-report profiler-stop)
-  :init
+  :custom 
   ;; make profiler report line length longer
-  (setq profiler-report-cpu-line-format '((70 left)
-                                          (24 right ((19 right) (5 right))))
-        profiler-report-memory-line-format '((70 left)
-                                             (19 right ((14 right profiler-format-number) (5 right))))))
+  ((profiler-report-cpu-line-format '((70 left)
+                                      (24 right ((19 right) (5 right)))))
+   (profiler-report-memory-line-format '((70 left)
+                                         (19 right ((14 right profiler-format-number) (5 right)))))))
 
 ;;; [ elp ] -- Emacs Lisp profiler.
 
 (use-package elp
-  :commands (elp-instrument-package
-             elp-instrument-function
-             elp-instrument-list
-             elp-results)
+  :defer t
+  :commands (elp-instrument-package elp-instrument-function elp-instrument-list elp-results)
   :init (add-to-list 'display-buffer-alist '("\\*ELP Profiling Results\\*" . (display-buffer-below-selected))))
 
 ;;; [ benchmark ] -- support for benchmarking code.
 
 (use-package benchmark
+  :defer t
   :commands (benchmark benchmark-run benchmark-run-compiled benchmark-progn))
 
 ;;; [ trace ] -- tracing facility for Emacs Lisp functions.
