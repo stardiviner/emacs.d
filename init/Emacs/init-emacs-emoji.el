d2a64f8ea556c161fe48aa41d047f1a1f4ccd5c3
stardiviner <numbchild@gmail.com>
2020-12-09 22:25:20 +0800

    [emoji] support displaying unicode emoji

--- init/Emacs/init-emacs-emoji.el
+++ init/Emacs/init-emacs-emoji.el
@@ -6,6 +6,13 @@
 
 ;;; Code:
 ;;; ----------------------------------------------------------------------------
+;;; [ display Unicode Emoji ]
+
+;;; A font that supports emoji is needed. The best results are obtained with
+;;; "Noto Color Emoji" or "Symbola". It might be necessary to instruct Emacs to
+;;; use such font with a line like the following.
+(set-fontset-font t 'symbol
+                  (font-spec :family "Noto Color Emoji") nil 'prepend)
 
 ;;; [ emojify ] display emojis in Emacs.
 
--- init/Emacs/init-emacs-face.el
+++ init/Emacs/init-emacs-face.el
@@ -33,6 +33,14 @@
   (fixed-pitch-serif ((t (:family "DejaVu Serif"))))
   (default ((t (:family "DejaVu Sans Mono" :foundry "PfEd" :height 90)))))
 
+;;; [ display Unicode Emoji ]
+
+;;; A font that supports emoji is needed. The best results are obtained with
+;;; "Noto Color Emoji" or "Symbola". It might be necessary to instruct Emacs to
+;;; use such font with a line like the following.
+(set-fontset-font t 'symbol
+                  (font-spec :family "Noto Color Emoji") nil 'prepend)
+
 
 
 (provide 'init-emacs-face)
