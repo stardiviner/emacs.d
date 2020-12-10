e949f9e7a8525503caa3dfe6399f0132cf12c180
stardiviner <numbchild@gmail.com>
2020-12-10 16:49:04 +0800

    [font] migrate face comments to font init file

--- init/Emacs/init-emacs-face.el
+++ init/Emacs/init-emacs-face.el
@@ -2,24 +2,6 @@
 
 ;;; Commentary:
 
-;; the value is in 1/10pt, so 100 will give you 10pt, etc
-;;
-;; - [C-u C-x =]
-;; - [M-x describe-font]
-;; - [M-x describe-fontset]
-;; - from command: $ fc-list
-;;
-;; Emacs set font functions:
-;;
-;; - `set-face-attribute'
-;; - `set-frame-font'
-;;
-;; - "DejaVu Sans Mono"
-;; - "Hack"
-;; - "Fira Sans"
-;; - "Sarasa Mono SC"
-;; - "Sarasa Nerd"
-
 ;;; Code:
 
 ;;; [ variable-pitch ] -- support for displaying proportional fonts.
