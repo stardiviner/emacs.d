0818eab32eed0dbaed192f2e5fb11d3f73c872e6
stardiviner <numbchild@gmail.com>
2020-12-14 14:29:03 +0800

    [network] set proxy-mode custom options

--- init/Emacs/init-emacs-network.el
+++ init/Emacs/init-emacs-network.el
@@ -28,20 +28,12 @@
 ;;         ;; no proxy for baidu.com, sina.com etc. proxy for all others.
 ;;         ;; ("no_proxy" . "^.*\\(baidu\\|sina)\\.com")
 ;;         ))
-;;
-;; (setq url-using-proxy "http://127.0.0.1:8118")
-
-;;; AUTHORITY
-;; (setq url-http-proxy-basic-auth-storage
-;;       (list (list "proxy.com:8080"
-;; 		  (cons "Input your LDAP UID !"
-;; 			(base64-encode-string "LOGIN:PASSWORD")))))
 
 ;;; [ url-gw (url-gateway) ]
 
-(require 'url-gw)
-(require 'socks)
-
+;; (require 'url-gw)
+;; (require 'socks)
+;;; NOTE: it only works for http: connections.
 ;; (setq url-gateway-method 'socks
 ;;       socks-noproxy '("localhost")
 ;;       socks-server '("Default server" "127.0.0.1" 1086 5))
@@ -50,6 +42,13 @@
 
 (use-package proxy-mode
   :ensure t
+  :custom ((proxy-mode-emacs-socks-proxy '("Default server" "127.0.0.1" 1086 5))
+           (proxy-mode-emacs-http-proxy
+            '(("http"  . "127.0.0.1:8118") ; Privoxy
+              ("https" . "127.0.0.1:8118")
+              ;; NOTE: don't use `localhost', avoid local server like robe no response
+              ;; ("no_proxy" . "127.0.0.1")
+              )))
   :commands (proxy-mode))
 
 ;;; [ use-proxy ] -- Easy way to enable/disable proxies in Emacs and Elisp.
