bd1809659eeff0d500f097d15c39b8d18c6bad6d
stardiviner <numbchild@gmail.com>
2020-12-17 10:22:10 +0800

    [color] enable rainbow-mode

--- init/Emacs/init-emacs-color.el
+++ init/Emacs/init-emacs-color.el
@@ -9,16 +9,16 @@
 
 ;;; [ rainbow-mode ] -- colorize color names in buffers
 
-;; (use-package rainbow-mode
-;;   :ensure t
-;;   :defer t
-;;   :delight rainbow-mode
-;;   :init
-;;   (dolist (hook
-;;            '(emacs-lisp-mode-hook
-;;              css-mode-hook
-;;              html-mode-hook))
-;;     (add-hook hook #'rainbow-mode)))
+(use-package rainbow-mode
+  :ensure t
+  :defer t
+  :delight rainbow-mode
+  :init
+  (dolist (hook
+           '(emacs-lisp-mode-hook
+             css-mode-hook
+             html-mode-hook))
+    (add-hook hook #'rainbow-mode)))
 
 
 (provide 'init-emacs-color)
