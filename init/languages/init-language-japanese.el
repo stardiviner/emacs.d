1bc1e681e09c5f3bc178cb0ccf7bdd36dd1e5d48
stardiviner <numbchild@gmail.com>
2020-12-18 08:34:48 +0800

    [japanese] add new extension ivy-migemo

--- init/languages/init-language-japanese.el
+++ init/languages/init-language-japanese.el
@@ -15,9 +15,29 @@
   :commands (migemo)
   :custom ((migemo-command "cmigemo")
            (migemo-options '("-q" "--emacs")))
-  :bind (:map language-search-prefix
-              ("j" . migemo-isearch-toggle-migemo))
-  ;; :init (migemo-init)
+  :bind (:map language-search-prefix ("j" . migemo-isearch-toggle-migemo))
+  :init (migemo-init))
+
+(use-package ivy-migemo
+  :ensure t
+  :after ivy
+  :init
+  ;; Toggle migemo and fuzzy by command.
+  (define-key ivy-minibuffer-map (kbd "M-f") #'ivy-migemo-toggle-fuzzy)
+  (define-key ivy-minibuffer-map (kbd "M-m") #'ivy-migemo-toggle-migemo)
+  ;; :config
+  ;; ;; If you want to defaultly use migemo on swiper and counsel-find-file:
+  ;; (setq ivy-re-builders-alist '((t . ivy--regex-plus)
+  ;;                               (swiper . ivy-migemo--regex-plus)
+  ;;                               (counsel-find-file . ivy-migemo--regex-plus))
+  ;;       ;;(counsel-other-function . ivy-migemo--regex-plus)
+  ;;       )
+  ;; ;; Or you prefer fuzzy match like ido:
+  ;; (setq ivy-re-builders-alist '((t . ivy--regex-plus)
+  ;;                               (swiper . ivy-migemo--regex-fuzzy)
+  ;;                               (counsel-find-file . ivy-migemo--regex-fuzzy))
+  ;;       ;;(counsel-other-function . ivy-migemo--regex-fuzzy)
+  ;;       )
   )
 
 ;;; [ oniisama ]
