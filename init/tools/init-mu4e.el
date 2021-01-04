749ffe6e4f0abc3f5a64e2ce05e3eeaaf426611d
stardiviner <numbchild@gmail.com>
2021-01-05 02:02:52 +0800

    [mu4e] Fix get face-foreground return nil issue

--- init/tools/init-mu4e.el
+++ init/tools/init-mu4e.el
@@ -56,64 +56,58 @@
         ;; email prefix
         mu4e-headers-seen-mark `("S" . ,(propertize
                                          (all-the-icons-material "mail_outline")
-                                         'face '(:family (all-the-icons-material-family)
-                                                         :foreground (face-background 'default))))
+                                         'face `(:family ,(all-the-icons-material-family)
+                                                         :foreground ,(face-background 'default))))
         mu4e-headers-new-mark `("N" . ,(propertize
                                         (all-the-icons-material "markunread")
-                                        'face '(:family (all-the-icons-material-family)
-                                                        :foreground (face-background 'default))))
+                                        'face `(:family ,(all-the-icons-material-family)
+                                                        :foreground ,(face-background 'default))))
         mu4e-headers-unread-mark `("u" . ,(propertize
                                            (all-the-icons-material "notifications_none")
-                                           'face '(:family (all-the-icons-material-family)
-                                                           :foreground (face-foreground 'mu4e-unread-face))))
+                                           'face 'mu4e-unread-face))
         mu4e-headers-signed-mark `("s" . ,(propertize
                                            (all-the-icons-material "check")
-                                           'face '(:family (all-the-icons-material-family)
+                                           'face `(:family ,(all-the-icons-material-family)
                                                            :foreground "DarkCyan")))
         mu4e-headers-encrypted-mark `("x" . ,(propertize
                                               (all-the-icons-material "enhanced_encryption")
-                                              'face '(:family (all-the-icons-material-family)
+                                              'face `(:family ,(all-the-icons-material-family)
                                                               :foreground "CornflowerBlue")))
         mu4e-headers-draft-mark `("D" . ,(propertize
-                                          (all-the-icons-material "edit")
-                                          'face '(:family (all-the-icons-material-family)
-                                                          :foreground (face-foreground 'mu4e-draft-face))))
+                                          (all-the-icons-material "drafts")
+                                          'face 'mu4e-draft-face))
         mu4e-headers-attach-mark `("a" . ,(propertize
                                            (all-the-icons-material "attachment")
-                                           'face '(:family (all-the-icons-material-family)
-                                                           :foreground (face-foreground 'mu4e-attach-number-face))))
+                                           'face 'mu4e-attach-number-face))
         mu4e-headers-passed-mark `("P" . ,(propertize ; ❯ (I'm participated in thread)
                                            (all-the-icons-material "center_focus_weak")
-                                           'face '(:family (all-the-icons-material-family)
+                                           'face `(:family ,(all-the-icons-material-family)
                                                            :foreground "yellow")))
         mu4e-headers-flagged-mark `("F" . ,(propertize
                                             (all-the-icons-material "flag")
-                                            'face '(:family (all-the-icons-material-family)
-                                                            :foreground (face-foreground 'mu4e-flagged-face))))
+                                            'face 'mu4e-flagged-face))
         mu4e-headers-replied-mark `("R" . ,(propertize
                                             (all-the-icons-material "reply_all")
-                                            'face '(:family (all-the-icons-material-family)
-                                                            :foreground (face-foreground 'mu4e-replied-face))))
+                                            'face 'mu4e-replied-face))
         mu4e-headers-trashed-mark `("T" . ,(propertize
                                             (all-the-icons-material "delete_forever")
-                                            'face '(:family (all-the-icons-material-family)
-                                                            :foreground (face-foreground 'mu4e-trashed-face))))
+                                            'face 'mu4e-trashed-face))
         ;; thread prefix marks
         mu4e-headers-default-prefix `("|" . ,(propertize
                                               (all-the-icons-material "message")
-                                              'face '(:family (all-the-icons-material-family))))
+                                              'face `(:family ,(all-the-icons-material-family))))
         mu4e-headers-has-child-prefix `("+" . ,(propertize ; "Parent" ╰
                                                 (all-the-icons-material "expand_more")
-                                                'face '(:family (all-the-icons-material-family))))
+                                                'face `(:family ,(all-the-icons-material-family))))
         mu4e-headers-empty-parent-prefix `("-" . ,(propertize ; "Orphan"
                                                    (all-the-icons-material "navigate_before")
-                                                   'face '(:family (all-the-icons-material-family))))
+                                                   'face `(:family ,(all-the-icons-material-family))))
         mu4e-headers-first-child-prefix `("\\" . ,(propertize
                                                    (all-the-icons-material "navigate_next")
-                                                   'face '(:family (all-the-icons-material-family))))
+                                                   'face `(:family ,(all-the-icons-material-family))))
         mu4e-headers-duplicate-prefix `("=" . ,(propertize
                                                 (all-the-icons-material "content_copy")
-                                                'face '(:family (all-the-icons-material-family)
+                                                'face `(:family ,(all-the-icons-material-family)
                                                                 :foreground "siennan"))))
 
   ;; mailing list thread fancy threading characters
